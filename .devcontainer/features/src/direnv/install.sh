17c7efbb30102e0529db59e7e7254a18161cf59a
Jacob Woffenden <jacob.woffenden@digital.justice.gov.uk>
2023-01-23 17:05:36 +0000

    Making changes

--- .devcontainer/features/src/direnv/install.sh
+++ .devcontainer/features/src/direnv/install.sh
@@ -4,10 +4,4 @@ set -e
 
 source /usr/local/bin/devcontainer/shared-library
 
-DEBIAN_FRONTEND=noninteractive
-
-apt update --yes
-
-apt-get install --yes --no-install-recommends direnv
-
-rm -rf /var/lib/apt/lists/*
+apt_install "direnv"
--- .devcontainer/features/test/direnv/test.sh
+++ .devcontainer/features/test/direnv/test.sh
@@ -4,6 +4,6 @@ set -e
 
 source dev-container-features-test-lib
 
-check "version" aws-vault --version
+check "version" direnv --version
 
 reportResults
