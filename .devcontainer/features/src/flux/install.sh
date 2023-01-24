46ea55f642c5694d832b443a58cc7f680c0895fc
Jacob Woffenden <jacob.woffenden@digital.justice.gov.uk>
2023-01-24 11:11:55 +0000

    Add Terraform Update variable

--- .devcontainer/features/src/flux/install.sh
+++ .devcontainer/features/src/flux/install.sh
@@ -10,18 +10,18 @@ GITHUB_REPOSITORY="fluxcd/flux2"
 if [ "${VERSION}" == "latest" ]; then
   get_github_latest_tag ${GITHUB_REPOSITORY}
   VERSION="${GITHUB_LATEST_TAG}"
-  VERSION_NO_VERSION_PREFIX="${GITHUB_LATEST_TAG_STRIP_V}"
+  VERSION_STRIP_V="${GITHUB_LATEST_TAG_STRIP_V}"
 else
   VERSION="${VERSION}"
 fi
 
-curl --location https://github.com/${GITHUB_REPOSITORY}/releases/download/${VERSION}/flux_${VERSION_NO_VERSION_PREFIX}_linux_${ARCHITECTURE}.tar.gz \
-  --output flux_${VERSION_NO_VERSION_PREFIX}_linux_${ARCHITECTURE}.tar.gz
+curl --location https://github.com/${GITHUB_REPOSITORY}/releases/download/${VERSION}/flux_${VERSION_STRIP_V}_linux_${ARCHITECTURE}.tar.gz \
+  --output flux_${VERSION_STRIP_V}_linux_${ARCHITECTURE}.tar.gz
 
-tar -zxvf flux_${VERSION_NO_VERSION_PREFIX}_linux_${ARCHITECTURE}.tar.gz
+tar -zxvf flux_${VERSION_STRIP_V}_linux_${ARCHITECTURE}.tar.gz
 
 mv flux /usr/local/bin/flux
   
 chmod +x /usr/local/bin/flux
 
-rm flux_${VERSION_NO_VERSION_PREFIX}_linux_${ARCHITECTURE}.tar.gz
+rm flux_${VERSION_STRIP_V}_linux_${ARCHITECTURE}.tar.gz
