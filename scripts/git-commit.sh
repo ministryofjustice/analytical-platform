0ded5fd51cc98ee0f96eee152fb9e60243f78686
julialawrence <julia.lawrence@digital.justice.gov.uk>
2023-01-25 14:35:39 +0000

    Adding missing scripts.

---
+++ scripts/git-commit.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+if [ ! -z "$2" ]; then
+  GIT_DIR=$2
+  cd $GIT_DIR
+  GITHUB_REPOSITORY=$(basename `git rev-parse --show-toplevel`)
+  GITHUB_REPOSITORY="ministryofjustice/$GITHUB_REPOSITORY"
+  TOKEN=$TERRAFORM_GITHUB_TOKEN
+else
+  TOKEN=$GITHUB_TOKEN
+fi
+file_to_commit="${1}/dependabot.yml"
+branch="date-$(date +%s)"
+commit_message="Workflow: created files in ${1}"
+content=$( base64 -i $file_to_commit )
+sha=$( git rev-parse $branch:$file_to_commit )
+gh api --method PUT /repos/:owner/:repo/contents/$file_to_commit \
+  --field message="$commit_message" \
+  --field content="$content" \
+  --field encoding="base64" \
+  --field branch="$branch" \
+  --field sha="$sha"
