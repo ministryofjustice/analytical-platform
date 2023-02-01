662f52d2acb01387e4846e9c2f2f8d2051e54d9c
julialawrence <julia.lawrence@digital.justice.gov.uk>
2023-02-01 16:25:12 +0000

    Removing unneeded var.

--- terraform/github/variables.tf
+++ terraform/github/variables.tf
@@ -3,9 +3,3 @@ variable "team_github_token" {
   type        = string
   sensitive   = true
 }
-
-variable "repository_github_token" {
-  description = "GitHub token for Repository Management"
-  type        = string
-  sensitive   = true
-}
