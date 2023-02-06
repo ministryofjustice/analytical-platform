c414e625ccdaf429a221f9f005e34fc761b1d3d4
Jacob Woffenden <jacob.woffenden@digital.justice.gov.uk>
2023-02-06 18:27:44 +0000

    Start of DP Shared Networking Added EditorConfig

---
+++ terraform/aws/data-platform-networking/eu-west-1/shared-networking/provider.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_version = ">= 0.13.0"
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "eu-west-1"
+}
