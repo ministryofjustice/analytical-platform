9cd802610fbbbcd9c73f361041615f50e48cc7cb
Jacob Woffenden <jacob.woffenden@digital.justice.gov.uk>
2023-02-11 04:02:50 +0000

    Renamed docs workflow Updated docs workflow for PRs Added .ruby-version Updated Gemfile Added deploy script

---
+++ scripts/tech-docs/deploy.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# Compile source markdown files into HTML in the `/docs` directory
+bundle exec middleman build --build-dir docs --relative-links --verbose
+
+touch docs/.nojekyll
+
+# Internal link check only within the docs folder
+htmlproofer --log-level debug --allow-missing-href true --disable_external true ./docs
+
+tar --dereference --directory docs -cvf artifact.tar --exclude=.git --exclude=.github .
