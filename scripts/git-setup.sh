2a3ac7c40a7dcfbc5f62ed3ed2cbb14087481dd4
Jacob Woffenden <jacob.woffenden@digital.justice.gov.uk>
2023-02-22 18:45:59 +0000

    Updated Dependabot workflow

--- scripts/git-setup.sh
+++ scripts/git-setup.sh
@@ -1,16 +1,7 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-if [ ! -z "$1" ]; then
-  GIT_DIR=$1
-fi
+set -euo pipefail
 
-name=$(git config --get user.name)
-email=$(git config --get user.email)
+git config --global user.name "moj-data-platform-robot"
 
-if [ -z "$name" ]; then
-  git config --global user.name "mojanalytics"
-fi
-
-if [ -z "$email" ]; then
-  git config --global user.email "mojanalytcs@digital.justice.gov.uk"
-fi
+git config --global user.email "data-platform-tech+github-robot-account@digital.justice.gov.uk"
