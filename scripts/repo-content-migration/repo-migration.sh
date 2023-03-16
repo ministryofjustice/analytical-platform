19bc039bb96e575f0438c414d5d4d26272e75784
Tom Webber <thomas.webber@digital.justice.gov.uk>
2023-03-16 12:36:28 +0000

    update migration script: - towards idempotence: allow fail if repo exists locally - fix order of git log commands to once inside - fix detection of master branch in origin for main/master merge - add final rebase step for condition where current branch behind remote - add usage docstring - enforce mandatory args

--- scripts/repo-content-migration/repo-migration.sh
+++ scripts/repo-content-migration/repo-migration.sh
@@ -1,39 +1,68 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-source_repo=''
-target_repo=''
+# get repo arguments
+read -r -d '' usage <<-EOF
+  $(basename "$0") [-h] [-s SOURCE_REPO] [-t TARGET_REPO]
 
-while getopts 's:t:' flag; do
-  case "${flag}" in
-    s) source_repo="${OPTARG}" ;;
-    t) target_repo="${OPTARG}" ;;
+  Migrate repository content from repository in moj-analytical-services GitHub
+  organisation to new location in ministryofjustice GitHub organisation
+
+      -h  show this help text
+      -s  source repo name in moj-analytical-services org
+      -t  target repo name in ministryofjustice org
+EOF
+
+options=':hs:t:'
+while getopts $options option; do
+  case "$option" in
+    h) echo "$usage"; exit;;
+    s) SOURCE_REPO="${OPTARG}" ;;
+    t) TARGET_REPO="${OPTARG}" ;;
+    :) printf "missing argument for -%s\n" "$OPTARG" >&2; echo "$usage" >&2; exit 1;;
+   \?) printf "illegal option: -%s\n" "$OPTARG" >&2; echo "$usage" >&2; exit 1;;
   esac
 done
 
-if [[ $(git --no-pager log -i -E --all --grep="Merge branch 'main' of github.com:ministryofjustice/data-platform-app-template") ]]; then
-    echo "template has been merged in, exiting..."
-    exit $?
-else
-    echo "Has not been processed, continuing..."
+# mandatory arguments
+if [ ! "$SOURCE_REPO" ] || [ ! "$TARGET_REPO" ]; then
+  echo "arguments -s and -t must be provided"
+  echo "$usage" >&2; exit 1
 fi
 
-export WORKING_DIR=./
+WORKING_DIR="$PWD"
+echo "SOURCE_REPO = ${SOURCE_REPO}"
+echo "TARGET_REPO = ${TARGET_REPO}"
 
 # setting repo details
-export SOURCE_ORG="moj-analytical-services"
-export TARGET_ORG="ministryofjustice"
+SOURCE_ORG="moj-analytical-services"
+TARGET_ORG="ministryofjustice"
 
-export SOURCE_REPO=$source_repo
-export SOURCE_REPO_URL="git@github.com:${SOURCE_ORG}/${SOURCE_REPO}.git"
-export TARGET_REPO=$target_repo
-export TARGET_REPO_URL="git@github.com:${TARGET_ORG}/${TARGET_REPO}.git"
-export TEMPLATE_REPO=data-platform-app-template
-export TEMPLATE_REPO_URL="git@github.com:${TARGET_ORG}/${TEMPLATE_REPO}.git"
+SOURCE_REPO_URL="git@github.com:${SOURCE_ORG}/${SOURCE_REPO}.git"
+TARGET_REPO_URL="git@github.com:${TARGET_ORG}/${TARGET_REPO}.git"
+TEMPLATE_REPO=data-platform-app-template
+TEMPLATE_REPO_URL="git@github.com:${TARGET_ORG}/${TEMPLATE_REPO}.git"
+
+CLONE_EXIT_CODE=$(git clone $TARGET_REPO_URL)
+
+if [[ $CLONE_EXIT_CODE -ne 0 ]]; then
+  echo "repo already exists locally"
+fi
+
+cd $TARGET_REPO
+MERGE_EXIT_CODE=$(git --no-pager log -i -E --all --grep="Merge branch 'main' of github.com:ministryofjustice/data-platform-app-template")
+
+if [ -z "$MERGE_EXIT_CODE" ]; then
+  echo "Repo hasn't been processed as the app template merge was not found in the git log. Continuing..."
+else
+  echo "Template has been merged in, skipping..."
+  echo $MERGE_EXIT_CODE
+  exit 1
+fi
 
 # Clone the source repository and set the target repository as its new remote origin
-cd $WORKING_DIR
+cd "$WORKING_DIR"
 git clone --bare $SOURCE_REPO_URL
-cd ${WORKING_DIR}/${SOURCE_REPO}.git
+cd "${WORKING_DIR}/${SOURCE_REPO}.git"
 git remote set-url --push origin $TARGET_REPO_URL
 
 # Fetch all branches and tags from the source repository,
@@ -41,14 +70,12 @@ git remote set-url --push origin $TARGET_REPO_URL
 git fetch -p origin
 git push --mirror
 
-# Clone the target repository and merge the main branch with the origin/master branch,
+# Merge the main branch of the target repo with the origin/master branch of the source repo,
 # resolving any conflicts in favor of the source branch
-cd $WORKING_DIR
-git clone $TARGET_REPO_URL
-
-cd ${WORKING_DIR}/${TARGET_REPO}
+# Not necessary if the source repo was using a 'main' branch
+cd "${WORKING_DIR}/${TARGET_REPO}"
 git fetch --all
-if git show-ref --quiet refs/heads/master; then
+if git show-ref --quiet --verify refs/remotes/origin/master; then
   git checkout main
 
   git merge origin/master --strategy-option theirs --allow-unrelated-histories \
@@ -88,8 +115,14 @@ git commit -m "import template .github files (including workflows) as part of ap
 git config pull.rebase false
 git pull template main --strategy-option ours --allow-unrelated-histories --no-edit
 
+# push even if current branch is 'behind' the remote
+git fetch origin main:tmp
+git rebase tmp || git rebase --skip
+git push origin HEAD:main
+git branch -D tmp
+
 # reset config options on repo
 git config --unset pull.rebase
 git remote remove template
 
-git push
+cd "$WORKING_DIR"
