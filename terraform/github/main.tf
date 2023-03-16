5074485d3799a533a698ade172cfa1c30590e596
Tom Webber <thomas.webber@digital.justice.gov.uk>
2023-03-16 16:53:11 +0000

    linting

--- terraform/github/main.tf
+++ terraform/github/main.tf
@@ -58,7 +58,7 @@ module "data-platform-apps" {
   description   = each.value.description
   homepage_url  = "https://github.com/ministryofjustice/data-platform/blob/main/architecture/decision/README.md"
   template_repo = "data-platform-app-template"
-  visibility  = "internal"
+  visibility    = "internal"
   environments  = ["prod", "dev"]
   topics = [
     "data-platform-apps",
