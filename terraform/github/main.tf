c90fb85d7d05ee91d9b99d2ccaa300e3a66530c5
julialawrence <98329494+julialawrence@users.noreply.github.com>
2023-03-17 08:53:33 +0000

    Syntax error

--- terraform/github/main.tf
+++ terraform/github/main.tf
@@ -58,7 +58,7 @@ module "data-platform-apps" {
   description       = each.value.description
   homepage_url      = "https://github.com/ministryofjustice/data-platform/blob/main/architecture/decision/README.md"
   template_repo     = "data-platform-app-template"
-  visibility        = "internal",
+  visibility        = "internal"
   archive_on_delete = false
   environments      = ["prod", "dev"]
   topics            = [
