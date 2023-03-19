6681cf0d9a1ee9f48f4ca771c2c6f351a9eb968c
julialawrence <julia.lawrence@digital.justice.gov.uk>
2023-03-19 15:07:50 +0000

    Adding app role arn as a APP_ROLE_ARN repo secret.

--- terraform/github/data.tf
+++ terraform/github/data.tf
@@ -6,3 +6,7 @@ data "aws_iam_session_context" "whoami" {
   provider = aws.session-info
   arn      = data.aws_caller_identity.current.arn
 }
+
+data "aws_caller_identity" "data" {
+  provider = aws.data
+}
