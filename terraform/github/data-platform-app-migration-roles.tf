48ea95f8ab3c8374ee04c7db33ee37b67ca7c06d
Tom Webber <thomas.webber@digital.justice.gov.uk>
2023-03-22 15:40:50 +0000

    amend local exec script variable reference inside subshell

--- terraform/github/data-platform-app-migration-roles.tf
+++ terraform/github/data-platform-app-migration-roles.tf
@@ -72,9 +72,9 @@ resource "null_resource" "update_iam_role_trust_policy" {
   provisioner "local-exec" {
     command = <<EOF
       TEMP_CREDS=$(aws sts assume-role --role-arn ${data.aws_iam_session_context.data.arn} --role-session-name your-session-name)
-      export AWS_ACCESS_KEY_ID=$(echo $TEMP_CREDS | jq -r '.Credentials.AccessKeyId')
-      export AWS_SECRET_ACCESS_KEY=$(echo $TEMP_CREDS | jq -r '.Credentials.SecretAccessKey')
-      export AWS_SESSION_TOKEN=$(echo $TEMP_CREDS | jq -r '.Credentials.SessionToken')
+      export AWS_ACCESS_KEY_ID=$(echo ${!TEMP_CREDS} | jq -r '.Credentials.AccessKeyId')
+      export AWS_SECRET_ACCESS_KEY=$(echo ${!TEMP_CREDS} | jq -r '.Credentials.SecretAccessKey')
+      export AWS_SESSION_TOKEN=$(echo ${!TEMP_CREDS} | jq -r '.Credentials.SessionToken')
       aws iam update-assume-role-policy --role-name ${each.value.name} --policy-document '${data.aws_iam_policy_document.updated_trust[each.key].json}'
     EOF
   }
