221aab02ff61e015491b54783c38b465e223760e
Tom Webber <thomas.webber@digital.justice.gov.uk>
2023-03-22 08:55:33 +0000

    ap_migration_apps back into json

--- terraform/github/locals.tf
+++ terraform/github/locals.tf
@@ -21,7 +21,7 @@ locals {
       description = "Core Repository for Data Platform Data Products"
   }]
 
-  ap_migration_apps  = yamldecode(file("./ap_migration_apps.yml"))
+  ap_migration_apps  = jsondecode(file("./ap_migration_apps.json"))
   migration_apps_map = { for app in local.ap_migration_apps : app.name => app } # so can be used in for_each
 
   cloud_platform_eks_oidc_provider_arn = "arn:aws:iam::593291632749:oidc-provider/oidc.eks.eu-west-2.amazonaws.com/id/DF366E49809688A3B16EEC29707D8C09"
