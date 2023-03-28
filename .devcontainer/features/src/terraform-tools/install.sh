bde327080e6c21b280f7b150ec81f984a564c8dd
Jacob Woffenden <jacob.woffenden@digital.justice.gov.uk>
2023-03-28 16:57:25 +0100

    Switching to hcl2json (#247)

--- .devcontainer/features/src/terraform-tools/install.sh
+++ .devcontainer/features/src/terraform-tools/install.sh
@@ -8,6 +8,6 @@ if [[ "${INSTALLTERRAFORMDOCS}" == "true" ]]; then
   bash $( dirname $0 )/install-terraform-docs.sh
 fi
 
-if [[ "${INSTALLHCLQ}" == "true" ]]; then
-  bash $( dirname $0 )/install-hclq.sh
+if [[ "${INSTALLHCL2JSON}" == "true" ]]; then
+  bash $( dirname $0 )/install-hcl2json.sh
 fi
