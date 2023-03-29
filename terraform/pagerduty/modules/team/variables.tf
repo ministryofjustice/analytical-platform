2c8d0650d1a7a4fd3dd3f7cf5654ce8fcacef3cd
Jacob Woffenden <jacob.woffenden@digital.justice.gov.uk>
2023-03-29 13:54:13 +0000

    Add default to team managers and responders

--- terraform/pagerduty/modules/team/variables.tf
+++ terraform/pagerduty/modules/team/variables.tf
@@ -6,9 +6,11 @@ variable "name" {
 variable "managers" {
   type        = set(string)
   description = "List of user IDs to be added as managers to the team"
+  default     = []
 }
 
 variable "responders" {
   type        = set(string)
   description = "List of user IDs to be added as responders to the team"
+  default     = []
 }
