e16d309690885479993deb23d4bfc932a1093754
Tom Webber <thomas.webber@digital.justice.gov.uk>
2023-03-30 10:00:50 +0100

    l***ing

--- terraform/github/modules/repository/main.tf
+++ terraform/github/modules/repository/main.tf
@@ -40,11 +40,11 @@ resource "github_repository" "default" {
 }
 
 resource "github_branch_protection" "default" {
-  count = var.type == "app" ? 0 : 1 # Temp fix for app-migration setup
-  #checkov:skip=CKV_GIT_6:"Following discussions with other teams we will not be enforcing signed commits currently"
+  count          = var.type == "app" ? 0 : 1 # Temp fix for app-migration setup
   repository_id  = github_repository.default.id
   pattern        = "main"
   enforce_admins = true
+  #checkov:skip=CKV_GIT_6:"Following discussions with other teams we will not be enforcing signed commits currently"
   #tfsec:ignore:github-branch_protections-require_signed_commits
   require_signed_commits = var.require_signed_commits
 
