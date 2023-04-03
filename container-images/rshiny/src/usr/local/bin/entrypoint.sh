bce20051113e32e3c18435d122ee333e29a48e0e
Jacob Woffenden <jacob.woffenden@digital.justice.gov.uk>
2023-04-03 15:20:52 +0000

    Updated entrypoint

--- container-images/rshiny/src/usr/local/bin/entrypoint.sh
+++ container-images/rshiny/src/usr/local/bin/entrypoint.sh
@@ -1,14 +1,10 @@
 #!/usr/bin/env bash
 
-# Make sure the directory for individual app logs exists
-mkdir -p /var/log/shiny-server
-chown shiny.shiny /var/log/shiny-server
-
-if [ "$APPLICATION_LOGS_TO_STDOUT" != "false" ];
+if [[ "${APPLICATION_LOGS_TO_STDOUT}" != "false" ]];
 then
-    # push the "real" application logs to stdout with xtail in detached mode
-    exec xtail /var/log/shiny-server/ &
+ # push the "real" application logs to stdout with xtail in detached mode
+ exec xtail /var/log/shiny-server/ &
 fi
 
 # start shiny server
-exec shiny-server 2>&1
+exec /usr/bin/shiny-server 2>&1
