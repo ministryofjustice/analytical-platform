feb28f8492199f75ba410d4c55d4da5e5739ccd3
julialawrence <98329494+julialawrence@users.noreply.github.com>
2023-04-13 18:26:02 +0100

    Removing region as not used either.

--- terraform/data-engineering/main.tf
+++ terraform/data-engineering/main.tf
@@ -4,7 +4,6 @@ data "aws_availability_zones" "available" {
 
 locals {
   name   = "data-engineering-app-vpc"
-  region = "eu-west-1"
 
   vpc_cidr = "10.0.0.0/16"
   azs      = slice(data.aws_availability_zones.available.names, 0, 1)
