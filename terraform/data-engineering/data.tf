b34368675256c9fd9544b7ce81763c895a260f4b
julialawrence <98329494+julialawrence@users.noreply.github.com>
2023-04-13 18:22:05 +0100

    Caller identities for data-eng account aren't needed.

--- terraform/data-engineering/data.tf
+++ terraform/data-engineering/data.tf
@@ -6,12 +6,3 @@ data "aws_iam_session_context" "whoami" {
   provider = aws.session-info
   arn      = data.aws_caller_identity.current.arn
 }
-
-data "aws_caller_identity" "data" {
-  provider = aws.data-engineering
-}
-
-data "aws_iam_session_context" "data" {
-  provider = aws.data-engineering
-  arn      = data.aws_caller_identity.data.arn
-}
