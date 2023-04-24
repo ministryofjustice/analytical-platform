0128857f96f9793e31e99ba350053750179a5273
julialawrence <98329494+julialawrence@users.noreply.github.com>
2023-04-24 15:09:19 +0100

    Adding ability to define multiple github teams as admins for app migration repos. (#364)
    * Converting team from string to list of string to account for multiple teams being admins of the same repo.
    
    * Updating integrations/github version.

--- terraform/github/locals.tf
+++ terraform/github/locals.tf
@@ -30,7 +30,9 @@ locals {
 
   # migration_apps_teams = [for app in local.ap_migration_apps : app.team]
 
-  migration_apps_teams_map = { for app in local.ap_migration_apps : app.team => app.name... }
+  migration_apps_teams_map = {
+    for team in toset(flatten([for app in local.ap_migration_apps : app.team])) : team => [for app in local.ap_migration_apps : app.name if contains(app.team, team)]
+  }
 
   cloud_platform_eks_oidc_provider_arn = "arn:aws:iam::593291632749:oidc-provider/oidc.eks.eu-west-2.amazonaws.com/id/DF366E49809688A3B16EEC29707D8C09"
   cloud_platform_eks_oidc_provider_id  = "oidc.eks.eu-west-2.amazonaws.com/id/DF366E49809688A3B16EEC29707D8C09"
