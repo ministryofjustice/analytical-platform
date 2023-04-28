e639326d42565eca3f92c539f79643df0d4a46f7
Jacob Woffenden <jacob.woffenden@digital.justice.gov.uk>
2023-04-28 21:46:15 +0100

    ✨ PagerDuty on-call notification rewrite (#381)

---
+++ scripts/pagerduty/slack-on-call/README.md
@@ -0,0 +1,46 @@
+# PagerDuty Slack On-Call
+
+This script:
+
+1. obtains the on-call user for the given schedule
+
+1. gets the on-call user's Slack handle
+
+1. posts a message to the given Slack channel
+
+It is designed to be executed every morning by `.github/workflows/platform-pagerduty-on-call.yml`
+
+## Adding a Team
+
+1. Add `pagerduty-schedule-id` and `slack-channel` to `.github/workflows/platform-pagerduty-on-call.yml`
+
+1. Invite `@PagerDuty On-Call` to `slack-channel`
+
+## Testing Locally
+<!-- markdownlint-disable MD013 -->
+```bash
+# Drop into AWS Vault
+aws-vault exec analytical-platform-management-production
+
+# Change into script directory
+cd scripts/pagerduty/slack-on-call
+
+# Run Python container
+docker run -it --rm \
+  --entrypoint /bin/sh \
+  --volume $( pwd ):/app \
+  --workdir /app \
+  --env PAGERDUTY_SCHEDULE_ID="REPLACE_ME" \
+  --env PAGERDUTY_TOKEN="$( aws secretsmanager get-secret-value --secret-id pagerduty-token --query SecretString --output text )" \
+  --env SLACK_CHANNEL="REPLACE_ME" \
+  --env SLACK_TOKEN="$( aws secretsmanager get-secret-value --secret-id slack-pagerduty-on-call-token --query SecretString --output text )" \
+  public.ecr.aws/docker/library/python:3.9
+
+# Install requirements
+pip install --requirement requirements.txt
+
+# Run script
+
+python main.py
+```
+<!-- markdownlint-enable MD013 -->
---
+++ scripts/pagerduty/slack-on-call/main.py
@@ -0,0 +1,61 @@
+import os
+
+from pdpyras import APISession
+from slack_sdk import WebClient
+
+pagerduty_scedule_id = os.environ["PAGERDUTY_SCHEDULE_ID"]
+pagerduty_token = os.environ["PAGERDUTY_TOKEN"]
+
+slack_token = os.environ["SLACK_TOKEN"]
+slack_channel = os.environ["SLACK_CHANNEL"]
+
+pagerduty_client = APISession(pagerduty_token)
+slack_client = WebClient(token=slack_token)
+
+
+def get_on_call_schedule_name():
+    response = pagerduty_client.get("/schedules/" + pagerduty_scedule_id)
+
+    if response.ok:
+        schedule_name = response.json()["schedule"]["name"]
+
+    return schedule_name
+
+
+def get_on_call_user():
+    response = pagerduty_client.get(
+        "/schedules/" + pagerduty_scedule_id + "/users?time_zone=Europe/London"
+    )
+    user_name = None
+    user_email = None
+
+    if response.ok:
+        user_name = response.json()["users"][0]["name"]
+        user_email = response.json()["users"][0]["email"]
+
+    return user_name, user_email
+
+
+def get_slack_user_id():
+    response = slack_client.users_lookupByEmail(email=get_on_call_user()[1])
+    user_id = None
+
+    if response["ok"]:
+        user_id = response["user"]["id"]
+
+    return user_id
+
+
+def main():
+    if get_slack_user_id() is None:
+        message = f"The on-call engineer for {get_on_call_schedule_name()} is {get_on_call_user()[0]} (I can't match their email to a Slack user, sorry!)"  # noqa: E501
+    else:
+        message = f"The on-call engineer for {get_on_call_schedule_name()} is <@{get_slack_user_id()}>"
+
+    slack_client.chat_postMessage(
+        channel=slack_channel,
+        text=message,
+    )
+
+
+main()
