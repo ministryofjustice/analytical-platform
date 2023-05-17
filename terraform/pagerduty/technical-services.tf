20be0e4b224a8358749e1c0c34b54d2b0bc4e014
Jacob Woffenden <jacob.woffenden@digital.justice.gov.uk>
2023-05-17 10:09:28 +0100

    ✨ Adding new PagerDuty services (#435)

--- terraform/pagerduty/technical-services.tf
+++ terraform/pagerduty/technical-services.tf
@@ -1,5 +1,6 @@
 locals {
   technical_services = [
+    /* Old Services - To be decommissioned */
     {
       name              = "Analytical Platform High Priority"
       description       = "High priority alerts for the Analytical Platform"
@@ -171,6 +172,166 @@ locals {
       enable_health_dashboard_integration = true
       enable_security_hub_integration     = true
       enable_email_integration            = true
+    },
+    /* New Services - Does not include Data Platform right now as it doesn't exist */
+    {
+      name              = "Analytical Platform Security"
+      description       = "Security alerts for the Analytical Platform"
+      escalation_policy = module.escalation_policies["Data Platform"].id
+      auto_pause_notifications_parameters = [
+        {
+          enabled = true
+          timeout = 600
+        }
+      ]
+      support_hours = [
+        {
+          type         = "fixed_time_per_day"
+          start_time   = "09:00:00"
+          end_time     = "17:00:00"
+          time_zone    = "Europe/London"
+          days_of_week = [1, 2, 3, 4, 5]
+        }
+      ]
+      incident_urgency_rules = [
+        {
+          type = "use_support_hours"
+          during_support_hours = [
+            {
+              type    = "constant"
+              urgency = "high"
+            }
+          ]
+          outside_support_hours = [
+            {
+              type    = "constant"
+              urgency = "low"
+            }
+          ]
+        }
+      ]
+      enable_cloudwatch_integration   = true
+      enable_cloudtrail_integration   = true
+      enable_guardduty_integration    = true
+      enable_security_hub_integration = true
+      enable_email_integration        = true
+    },
+    {
+      name              = "Analytical Platform Networking"
+      description       = "Networking alerts for the Analytical Platform"
+      escalation_policy = module.escalation_policies["Data Platform"].id
+      auto_pause_notifications_parameters = [
+        {
+          enabled = true
+          timeout = 600
+        }
+      ]
+      support_hours = [
+        {
+          type         = "fixed_time_per_day"
+          start_time   = "09:00:00"
+          end_time     = "17:00:00"
+          time_zone    = "Europe/London"
+          days_of_week = [1, 2, 3, 4, 5]
+        }
+      ]
+      incident_urgency_rules = [
+        {
+          type = "use_support_hours"
+          during_support_hours = [
+            {
+              type    = "constant"
+              urgency = "high"
+            }
+          ]
+          outside_support_hours = [
+            {
+              type    = "constant"
+              urgency = "low"
+            }
+          ]
+        }
+      ]
+      enable_cloudwatch_integration = true
+      enable_email_integration      = true
+    },
+    {
+      name              = "Analytical Platform Compute"
+      description       = "Compute alerts for the Analytical Platform"
+      escalation_policy = module.escalation_policies["Data Platform"].id
+      auto_pause_notifications_parameters = [
+        {
+          enabled = true
+          timeout = 600
+        }
+      ]
+      support_hours = [
+        {
+          type         = "fixed_time_per_day"
+          start_time   = "09:00:00"
+          end_time     = "17:00:00"
+          time_zone    = "Europe/London"
+          days_of_week = [1, 2, 3, 4, 5]
+        }
+      ]
+      incident_urgency_rules = [
+        {
+          type = "use_support_hours"
+          during_support_hours = [
+            {
+              type    = "constant"
+              urgency = "high"
+            }
+          ]
+          outside_support_hours = [
+            {
+              type    = "constant"
+              urgency = "low"
+            }
+          ]
+        }
+      ]
+      enable_cloudwatch_integration = true
+      enable_email_integration      = true
+    },
+    {
+      name              = "Analytical Platform Storage"
+      description       = "Compute alerts for the Analytical Platform"
+      escalation_policy = module.escalation_policies["Data Platform"].id
+      auto_pause_notifications_parameters = [
+        {
+          enabled = true
+          timeout = 600
+        }
+      ]
+      support_hours = [
+        {
+          type         = "fixed_time_per_day"
+          start_time   = "09:00:00"
+          end_time     = "17:00:00"
+          time_zone    = "Europe/London"
+          days_of_week = [1, 2, 3, 4, 5]
+        }
+      ]
+      incident_urgency_rules = [
+        {
+          type = "use_support_hours"
+          during_support_hours = [
+            {
+              type    = "constant"
+              urgency = "high"
+            }
+          ]
+          outside_support_hours = [
+            {
+              type    = "constant"
+              urgency = "low"
+            }
+          ]
+        }
+      ]
+      enable_cloudwatch_integration = true
+      enable_email_integration      = true
     }
   ]
 }
