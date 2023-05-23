f0d5fcd7b91846396c8afa61b920546e76592294
Jacob Woffenden <jacob.woffenden@digital.justice.gov.uk>
2023-05-23 16:10:50 +0000

    Update role_arn

--- terraform/aws/analytical-platform-data-production/airflow/terraform.tf
+++ terraform/aws/analytical-platform-data-production/airflow/terraform.tf
@@ -23,7 +23,7 @@ provider "aws" {
 provider "aws" {
   region = "eu-west-1"
   assume_role {
-    role_arn = can(regex("AWSReservedSSO_AdministratorAccess", data.aws_iam_session_context.session.issuer_arn)) ? "arn:aws:iam::${var.account_ids["analytical-platform-data-production"]}:role/restricted-admin" : "arn:aws:iam::${var.account_ids["analytical-platform-data-production"]}:role/GlobalGitHubActionAdmin"
+    role_arn = "arn:aws:iam::${var.account_ids["analytical-platform-data-production"]}:role/GlobalGitHubActionAdmin"
   }
   default_tags {
     tags = var.tags
@@ -34,7 +34,7 @@ provider "aws" {
   alias  = "analytical-platform-management-production"
   region = "eu-west-1"
   assume_role {
-    role_arn = can(regex("AWSReservedSSO_AdministratorAccess", data.aws_iam_session_context.session.issuer_arn)) ? "arn:aws:iam::${var.account_ids["analytical-platform-management-production"]}:role/restricted-admin" : "arn:aws:iam::${var.account_ids["analytical-platform-management-production"]}:role/GlobalGitHubActionAdmin"
+    role_arn = can(regex("AdministratorAccess", data.aws_iam_session_context.session.issuer_arn)) ? null : "arn:aws:iam::${var.account_ids["analytical-platform-management-production"]}:role/GlobalGitHubActionAdmin"
   }
   default_tags {
     tags = var.tags
