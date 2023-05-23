8c14fc6b888f81c21128769c1825bbd934e15d5d
Jacob Woffenden <jacob.woffenden@digital.justice.gov.uk>
2023-05-23 16:12:36 +0000

    Removed a new line

--- terraform/aws/analytical-platform-data-production/airflow/variables.tf
+++ terraform/aws/analytical-platform-data-production/airflow/variables.tf
@@ -12,12 +12,6 @@ variable "target_account" {
   description = "Name of the account to deploy to"
 }
 
-variable "assume_role" {
-  type        = string
-  description = "Name of the role to assume in each account"
-  default     = "GlobalGitHubActionAdmin"
-}
-
 variable "tags" {
   type        = map(string)
   description = "Map of tags to apply to resources"
