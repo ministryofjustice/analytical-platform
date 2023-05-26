49742a1c17c240d4a64fb41c472c5d958dfe39e1
julialawrence <98329494+julialawrence@users.noreply.github.com>
2023-05-26 08:40:38 +0100

    Adding Data Engineering OIDC Provider Back In (#493)
    * Adding Data Engineering OIDC Provider Back In
    
    * Fixing policy attachment and tag prefixes.
    
    * Fixig policy ref.
    
    * Adding analytical-platform-iam back as a trusted repo.

--- terraform/oidc/oidc-providers.tf
+++ terraform/oidc/oidc-providers.tf
@@ -1,11 +1,11 @@
 module "github-oidc-provider" {
 
   source                 = "github.com/ministryofjustice/modernisation-platform-github-oidc-provider?ref=v2.1.0"
-  github_repositories    = ["ministryofjustice/analytical-platform-iam:*"]
+  github_repositories    = ["ministryofjustice/data-platform:*", "ministryofjustice/analytical-platform-iam:*"]
   additional_permissions = data.aws_iam_policy_document.github_actions_iam_permissions.json
   role_name              = "github-actions-iam"
   tags_common            = local.tags
-  tags_prefix            = "analytical-platform-iam"
+  tags_prefix            = "data-platform"
   providers = {
     aws = aws.management
   }
@@ -60,7 +60,7 @@ module "github-oidc-provider-data-ecr" {
   additional_permissions = data.aws_iam_policy_document.github_actions_iam_permissions_data_ecr.json
   role_name              = "github-actions-ecr-oidc"
   tags_common            = local.tags
-  tags_prefix            = "analytical-platform-iam"
+  tags_prefix            = "data-platform"
   providers = {
     aws = aws.data
   }
@@ -95,11 +95,11 @@ data "aws_iam_policy_document" "github_actions_iam_permissions_data_ecr" {
 module "github-oidc-provider-sandbox" {
 
   source                 = "github.com/ministryofjustice/modernisation-platform-github-oidc-provider?ref=v2.1.0"
-  github_repositories    = ["ministryofjustice/analytical-platform-iam:*"]
+  github_repositories    = ["ministryofjustice/data-platform:*", "ministryofjustice/analytical-platform-iam:*"]
   additional_permissions = data.aws_iam_policy_document.github_actions_iam_permissions_sandbox.json
   role_name              = "github-actions-iam"
   tags_common            = local.tags
-  tags_prefix            = "analytical-platform-iam"
+  tags_prefix            = "data-platform"
   providers = {
     aws = aws.sandbox
   }
@@ -147,3 +147,57 @@ data "aws_iam_policy_document" "github_actions_iam_permissions_sandbox" {
   }
 }
 
+module "github-oidc-provider-data-engineering" {
+
+  source                 = "github.com/ministryofjustice/modernisation-platform-github-oidc-provider?ref=v2.1.0"
+  github_repositories    = ["ministryofjustice/data-platform:*", "ministryofjustice/analytical-platform-iam:*"]
+  additional_permissions = data.aws_iam_policy_document.github_actions_iam_permissions_data_engineering.json
+  role_name              = "github-actions-iam"
+  tags_common            = local.tags
+  tags_prefix            = "data-platform"
+  providers = {
+    aws = aws.data_engineering
+  }
+
+}
+
+data "aws_iam_policy_document" "github_actions_iam_permissions_data_engineering" {
+  statement {
+    sid       = "AllowOIDCToAssumeRoles"
+    effect    = "Allow"
+    resources = [format("arn:aws:iam::%s:role/github-actions-infrastructure", local.accounts["data_engineering"])]
+    actions   = ["sts:AssumeRole"]
+  }
+  statement {
+    sid       = "AllowOIDCToDecryptKMS"
+    effect    = "Allow"
+    resources = ["*"]
+    actions   = ["kms:Decrypt"]
+  }
+
+  statement {
+    sid       = "AllowOIDCReadState"
+    effect    = "Allow"
+    resources = ["arn:aws:s3:::global-tf-state-aqsvzyd5u9/*", "arn:aws:s3:::global-tf-state-aqsvzyd5u9/"]
+    actions = ["s3:Get*",
+    "s3:List*"]
+  }
+
+  statement {
+    sid       = "AllowOIDCWriteState"
+    effect    = "Allow"
+    resources = ["arn:aws:s3:::global-tf-state-aqsvzyd5u9/iam/*"]
+    actions = ["s3:PutObject",
+    "s3:PutObjectAcl"]
+  }
+  statement {
+    sid       = "AllowDynamoDBStateLocking"
+    effect    = "Allow"
+    resources = ["arn:aws:dynamodb:eu-west-2:042130406152:table/global-tf-state-aqsvzyd5u9-locks"]
+    actions = [
+      "dynamodb:GetItem",
+      "dynamodb:PutItem",
+      "dynamodb:DeleteItem"
+    ]
+  }
+}
