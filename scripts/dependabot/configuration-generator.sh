92af2bace2408d1107aaa4bd05cde78cdd7a690b
Jacob Woffenden <jacob.woffenden@digital.justice.gov.uk>
2023-06-05 15:07:41 +0100

    🔀 Added Pip to generator (#546)
    Added statement so it doesn't commit if running locally

--- scripts/dependabot/configuration-generator.sh
+++ scripts/dependabot/configuration-generator.sh
@@ -1,23 +1,25 @@
 #!/usr/bin/env bash
 
-set -euo pipefail
-
 declare -xr DEPENDABOT_CONFIGURATION_FILE=".github/dependabot.yml"
 
 dockerFolders=$( find . -type f -name '*Dockerfile*' | sed 's#/[^/]*$##' | sed 's/.\///'| sort | uniq )
+pythonFolders=$( find . -type f -name '*requirements*.txt' | sed 's#/[^/]*$##' | sed 's/.\///'| sort | uniq )
 terraformFolders=$( find . -type f -name '*.tf' | sed 's#/[^/]*$##' | sed 's/.\///'| sort | uniq )
 
-echo "Docker Folders:"
+echo "=== Docker Folders ==="
 echo "${dockerFolders}"
 
-echo "Terraform Folders:"
+echo "=== Python Folders ==="
+echo "${pythonFolders}"
+
+echo "=== Terraform Folders ==="
 echo "${terraformFolders}"
 
 echo "Generating ${DEPENDABOT_CONFIGURATION_FILE}"
 cat > ${DEPENDABOT_CONFIGURATION_FILE} << EOL
 ---
 # This file is auto-generated here, do not manually amend.
-# https://github.com/ministryofjustice/data-platform/blob/main/scripts/dependabot/generate-configuration.sh
+# https://github.com/ministryofjustice/data-platform/blob/main/scripts/dependabot/configuration-generator.sh
 
 version: 2
 
@@ -37,6 +39,15 @@ echo "    schedule:" >> ${DEPENDABOT_CONFIGURATION_FILE}
 echo "      interval: \"daily\"" >> ${DEPENDABOT_CONFIGURATION_FILE}
 done
 
+for folder in ${pythonFolders}
+do
+echo "Generating entry for ${folder}"
+echo "  - package-ecosystem: \"pip\"" >> ${DEPENDABOT_CONFIGURATION_FILE}
+echo "    directory: \"/${folder}\"" >> ${DEPENDABOT_CONFIGURATION_FILE}
+echo "    schedule:" >> ${DEPENDABOT_CONFIGURATION_FILE}
+echo "      interval: \"daily\"" >> ${DEPENDABOT_CONFIGURATION_FILE}
+done
+
 for folder in ${terraformFolders}
 do
 echo "Generating entry for ${folder}"
@@ -46,14 +57,19 @@ echo "    schedule:" >> ${DEPENDABOT_CONFIGURATION_FILE}
 echo "      interval: \"daily\"" >> ${DEPENDABOT_CONFIGURATION_FILE}
 done
 
-if git diff --exit-code ${DEPENDABOT_CONFIGURATION_FILE} > /dev/null 2>&1; then
-  echo "No difference in files, exiting."
-  exit 0
+if [[ "${GITHUB_ACTIONS}" == "true" ]]; then
+  if git diff --exit-code ${DEPENDABOT_CONFIGURATION_FILE} > /dev/null 2>&1; then
+    echo "No difference in files, exiting."
+    exit 0
+  else
+    gh api --method PUT /repos/${GITHUB_REPOSITORY}/contents/${DEPENDABOT_CONFIGURATION_FILE} \
+      --field branch="${GITHUB_HEAD_REF}" \
+      --field message="Committing updated Dependabot configuration" \
+      --field encoding="base64" \
+      --field content="$( base64 -w 0 ${DEPENDABOT_CONFIGURATION_FILE} )" \
+      --field sha="$( gh api --method GET /repos/${GITHUB_REPOSITORY}/contents/${DEPENDABOT_CONFIGURATION_FILE} --field ref="main" | jq -r '.sha' )"
+  fi
 else
-  gh api --method PUT /repos/${GITHUB_REPOSITORY}/contents/${DEPENDABOT_CONFIGURATION_FILE} \
-    --field branch="${GITHUB_HEAD_REF}" \
-    --field message="Committing updated Dependabot configuration" \
-    --field encoding="base64" \
-    --field content="$( base64 -w 0 ${DEPENDABOT_CONFIGURATION_FILE} )" \
-    --field sha="$( gh api --method GET /repos/${GITHUB_REPOSITORY}/contents/${DEPENDABOT_CONFIGURATION_FILE} --field ref="main" | jq -r '.sha' )"
+  echo "Not running in GitHub Actions, exiting."
+  exit 0
 fi
