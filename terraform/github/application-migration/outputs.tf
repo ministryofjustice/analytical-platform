845ab71354ef887daa8172b06c8af10e30796152
julialawrence <julia.lawrence@digital.justice.gov.uk>
2023-06-21 18:25:20 +0100

    Fixing the creation of team-repo resources

--- terraform/github/application-migration/outputs.tf
+++ terraform/github/application-migration/outputs.tf
@@ -1,6 +1,4 @@
 output "team_list" {
-  value = {
-    for repo, details in data.github_repository_teams.migration_apps_repo_owners :
-    repo => [for team in details.teams : team.name]
-  }
+  value = transpose(local.team_repo_map)
 }
+
