affdc2185e5ac838043bb8f6e5ecb15ecea1799b
Jacob Woffenden <jacob.woffenden@digital.justice.gov.uk>
2023-06-22 22:46:27 +0000

    Add cluster autoscaler

--- terraform/aws/analytical-platform-development/open-metadata/helm-charts.tf
+++ terraform/aws/analytical-platform-development/open-metadata/helm-charts.tf
@@ -1,3 +1,21 @@
+resource "helm_release" "cluster_autoscaler" {
+  name       = "cluster-autoscaler"
+  repository = "https://kubernetes.github.io/autoscaler"
+  chart      = "cluster-autoscaler"
+  version    = "9.29.1"
+  namespace  = "kube-system"
+  values = [
+    templatefile(
+      "${path.module}/src/helm/cluster-autoscaler/values.yml.tftpl",
+      {
+        aws_region   = data.aws_region.current.name
+        cluster_name = module.eks.cluster_name
+        eks_role_arn = module.cluster_autoscaler_iam_role.iam_role_arn
+      }
+    )
+  ]
+}
+
 resource "helm_release" "aws_efs_csi_driver" {
   name       = "aws-efs-csi-driver"
   repository = "https://kubernetes-sigs.github.io/aws-efs-csi-driver"
--- terraform/aws/analytical-platform-development/open-metadata/iam-roles.tf
+++ terraform/aws/analytical-platform-development/open-metadata/iam-roles.tf
@@ -1,3 +1,19 @@
+module "cluster_autoscaler_iam_role" {
+  source  = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
+  version = "5.20.0"
+
+  role_name_prefix                 = "cluster-autoscaler"
+  attach_cluster_autoscaler_policy = true
+  cluster_autoscaler_cluster_names = [module.eks.cluster_name]
+
+  oidc_providers = {
+    main = {
+      provider_arn               = module.eks.oidc_provider_arn
+      namespace_service_accounts = ["kube-system:cluster-autoscaler"]
+    }
+  }
+}
+
 module "ebs_csi_driver_iam_role" {
   source  = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
   version = "5.20.0"
