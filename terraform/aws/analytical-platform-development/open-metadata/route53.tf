5f0c4a75959a3b80254bdf5d9f32cab96ed491ac
Jacob Woffenden <jacob.woffenden@digital.justice.gov.uk>
2023-06-22 16:30:05 +0000

    Add ACM for @julialawrence 🤝

--- terraform/aws/analytical-platform-development/open-metadata/route53.tf
+++ terraform/aws/analytical-platform-development/open-metadata/route53.tf
@@ -23,3 +23,25 @@ resource "aws_acm_certificate_validation" "open_metadata" {
   certificate_arn         = aws_acm_certificate.open_metadata.arn
   validation_record_fqdns = [for record in aws_route53_record.open_metadata : record.fqdn]
 }
+
+resource "aws_route53_record" "datahub" {
+  for_each = {
+    for dvo in aws_acm_certificate.datahub.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
+  }
+
+  allow_overwrite = true
+  name            = each.value.name
+  records         = [each.value.record]
+  ttl             = 60
+  type            = each.value.type
+  zone_id         = aws_route53_zone.data_platform_moj_woffenden_dev.zone_id
+}
+
+resource "aws_acm_certificate_validation" "datahub" {
+  certificate_arn         = aws_acm_certificate.datahub.arn
+  validation_record_fqdns = [for record in aws_route53_record.datahub : record.fqdn]
+}
