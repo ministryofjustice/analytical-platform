f6b075c7ede69e99a6b73bed0aecd331e41f7a8f
Jacob Woffenden <jacob.woffenden@digital.justice.gov.uk>
2023-07-05 14:32:46 +0100

    Add KMS statement

--- terraform/aws/analytical-platform-data-production/rds-s3-exports/iam-policies.tf
+++ terraform/aws/analytical-platform-data-production/rds-s3-exports/iam-policies.tf
@@ -14,6 +14,20 @@ data "aws_iam_policy_document" "export_policy" {
       "arn:aws:s3:::alpha-vcms-data/*"
     ]
   }
+  statement {
+    sid    = "AllowKMS"
+    effect = "Allow"
+    actions = [
+      "kms:Encrypt",
+      "kms:Decrypt",
+      "kms:ReEncrypt*",
+      "kms:GenerateDataKey*",
+      "kms:CreateGrant",
+      "kms:DescribeKey",
+      "kms:RetireGrant"
+    ]
+    resources = [resource.aws_kms_key.rds_s3_export.arn]
+  }
 }
 
 resource "aws_iam_policy" "rds_s3_export" {
