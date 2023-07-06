01e35738799962c7867f72841386000d85d6b659
lalithanagarur <lalitha.nagarur@digital.justice.gov.uk>
2023-07-06 14:41:41 +0100

    changed crawler task name

--- terraform/aws/analytical-platform-data-production/s3-glue-crawler/glue-crawler.tf
+++ terraform/aws/analytical-platform-data-production/s3-glue-crawler/glue-crawler.tf
@@ -1,4 +1,4 @@
-resource "aws_glue_crawler" "example" {
+resource "aws_glue_crawler" "vmcs_data_crawler" {
   database_name = aws_glue_catalog_database.alpha_vcms_data_glue_catalog.name
   name          = "vcms-data-crawler"
   role          = aws_iam_role.glue_vcms_crawler_role.arn
