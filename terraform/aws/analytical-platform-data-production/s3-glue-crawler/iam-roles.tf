ec03064ad840f5c277d2d12b71ed366c1c459bb3
lalithanagarur <lalitha.nagarur@digital.justice.gov.uk>
2023-07-07 11:21:34 +0100

    terraform fmt update

--- terraform/aws/analytical-platform-data-production/s3-glue-crawler/iam-roles.tf
+++ terraform/aws/analytical-platform-data-production/s3-glue-crawler/iam-roles.tf
@@ -11,8 +11,8 @@ data "aws_iam_policy_document" "glue_assume_policy" {
 }
 
 resource "aws_iam_role" "alpha_vcms_data_crawler" {
-  name                = "alpha-vcms-data-crawler"
-  assume_role_policy  = data.aws_iam_policy_document.glue_assume_policy.json
+  name               = "alpha-vcms-data-crawler"
+  assume_role_policy = data.aws_iam_policy_document.glue_assume_policy.json
   managed_policy_arns = [
     aws_iam_policy.alpha_vcms_crawler.arn,
     "arn:aws:iam::aws:policy/AWSGlueServiceRole"
