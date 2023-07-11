da6942edebbfe7432be2c414e44bcd1bafc3f038
Jacob Woffenden <jacob.woffenden@digital.justice.gov.uk>
2023-07-11 15:10:57 +0100

    🔀 Bumps RDS module version (#822)
    Use new RDS secret ARN output
    Bump Terraform version

--- terraform/aws/analytical-platform-development/open-metadata/data.tf
+++ terraform/aws/analytical-platform-development/open-metadata/data.tf
@@ -21,13 +21,13 @@ data "aws_secretsmanager_secret_version" "open_metadata_tenant_id" {
 }
 
 data "aws_secretsmanager_secret_version" "openmetadata_airflow_rds_credentials" {
-  secret_id = "rds!db-5cf2a9f4-656e-43dc-bf22-2b78bc990bb2" /* TODO: This isn't outputted by the RDS module, should probably manage password ourselves */
+  secret_id = module.airflow_rds.db_instance_master_user_secret_arn
 
   depends_on = [module.airflow_rds]
 }
 
 data "aws_secretsmanager_secret_version" "openmetadata_rds_credentials" {
-  secret_id = "rds!db-75d4a9aa-d7c9-4992-bf44-93fc658ee7a7" /* TODO: This isn't outputted by the RDS module, should probably manage password ourselves */
+  secret_id = module.rds.db_instance_master_user_secret_arn
 
   depends_on = [module.rds]
 }
