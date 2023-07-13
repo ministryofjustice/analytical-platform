cd8b51405d48a2920b902ffc0e5bcdcb90c286c4
Jacob Woffenden <jacob.woffenden@digital.justice.gov.uk>
2023-07-13 09:27:56 +0000

    Remove Bogdan Update docs

--- terraform/pagerduty/teams.tf
+++ terraform/pagerduty/teams.tf
@@ -11,7 +11,6 @@ locals {
         module.users["thomas.webber@digital.justice.gov.uk"].id,
         module.users["brian.ellwood@digital.justice.gov.uk"].id,
         module.users["louise.bowler@digital.justice.gov.uk"].id,
-        module.users["bogdan.mania@digital.justice.gov.uk"].id,
         module.users["john.hackett@digital.justice.gov.uk"].id,
         module.users["michael.collins@digital.justice.gov.uk"].id,
         module.users["yikang.mao@justice.gov.uk"].id,
