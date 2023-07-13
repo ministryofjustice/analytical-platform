35376e9d15d1555763c07ccd9b9d183e1ffd961c
Jacob Woffenden <jacob.woffenden@digital.justice.gov.uk>
2023-07-13 09:57:37 +0000

    update syntax

--- terraform/aws/analytical-platform-data-production/airflow/data.tf
+++ terraform/aws/analytical-platform-data-production/airflow/data.tf
@@ -20,5 +20,5 @@ data "aws_eks_cluster" "analytical_platform_development" {
 }
 
 data "tls_certificate" "analytical_platform_development_eks_oidc_issuer" {
-  url = data.aws_eks_cluster.analytical_platform_development.identity.0.oidc.0.issuer
+  url = data.aws_eks_cluster.analytical_platform_development.identity[0].oidc[0].issuer
 }
