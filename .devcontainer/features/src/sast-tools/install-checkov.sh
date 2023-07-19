71b3fdbbe8931b8c7d6ae22901abce7380e24413
Jacob Woffenden <jacob.woffenden@digital.justice.gov.uk>
2023-07-19 16:06:08 +0100

    add version function

--- .devcontainer/features/src/sast-tools/install-checkov.sh
+++ .devcontainer/features/src/sast-tools/install-checkov.sh
@@ -4,4 +4,15 @@ set -e
 
 source /usr/local/bin/devcontainer-utils
 
-pip_install "checkov==${CHECKOVVERSION}"
+VERSION=${CHECKOVVERSION:-"latest"}
+GITHUB_REPOSITORY="bridgecrewio/checkov"
+
+if [ "${VERSION}" == "latest" ]; then
+  get_github_latest_tag ${GITHUB_REPOSITORY}
+  VERSION="${GITHUB_LATEST_TAG}"
+  VERSION_STRIP_V="${GITHUB_LATEST_TAG_STRIP_V}"
+else
+  VERSION="${VERSION}"
+fi
+
+pip_install "checkov==${VERSION}"
