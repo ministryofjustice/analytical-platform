49b7f4ba224429936ff3acef0c0734e10298788a
Jacob Woffenden <jacob.woffenden@digital.justice.gov.uk>
2023-07-20 22:33:40 +0000

    Add kubernetes template Add upload instructions

---
+++ terraform/aws/analytical-platform-development/firebreak-coder/src/coder/templates/README.md
@@ -0,0 +1,23 @@
+# Templates
+
+## Packaging
+
+```bash
+tar -cvf ${TEMPLATE_NAME}.tar -C ${TEMPLATE_NAME} .
+```
+
+## Uploading
+
+### Using the UI
+
+1. Go to https://coder.data-platform.moj.woffenden.dev/templates/new
+
+1. Upload packaged template
+
+  1. Set a name e.g. `kubernetes`
+
+  1. Set an icon e.g. `/icon/k8s.png`
+
+### Using the CLI
+
+_TBC_
---
+++ terraform/aws/analytical-platform-development/firebreak-coder/src/coder/templates/kubernetes/data.tf
@@ -0,0 +1,65 @@
+data "coder_workspace" "user" {}
+
+data "coder_parameter" "cpu" {
+  name         = "cpu"
+  display_name = "CPU"
+  description  = "The number of CPU cores"
+  default      = "2"
+  icon         = "/icon/memory.svg"
+  mutable      = true
+  option {
+    name  = "2 Cores"
+    value = "2"
+  }
+  option {
+    name  = "4 Cores"
+    value = "4"
+  }
+  option {
+    name  = "6 Cores"
+    value = "6"
+  }
+  option {
+    name  = "8 Cores"
+    value = "8"
+  }
+}
+
+data "coder_parameter" "memory" {
+  name         = "memory"
+  display_name = "Memory"
+  description  = "The amount of memory in GB"
+  default      = "2"
+  icon         = "/icon/memory.svg"
+  mutable      = true
+  option {
+    name  = "2 GB"
+    value = "2"
+  }
+  option {
+    name  = "4 GB"
+    value = "4"
+  }
+  option {
+    name  = "6 GB"
+    value = "6"
+  }
+  option {
+    name  = "8 GB"
+    value = "8"
+  }
+}
+
+data "coder_parameter" "home_disk_size" {
+  name         = "home_disk_size"
+  display_name = "Home disk size"
+  description  = "The size of the home disk in GB"
+  default      = "10"
+  type         = "number"
+  icon         = "/emojis/1f4be.png"
+  mutable      = false
+  validation {
+    min = 1
+    max = 99999
+  }
+}
