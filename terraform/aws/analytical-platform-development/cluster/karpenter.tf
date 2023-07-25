18c02e27c12b1aa985d3deeb94353462149b2599
Emterry <emma.terry@digital.justice.gov.uk>
2023-07-25 16:24:43 +0100

    version

--- terraform/aws/analytical-platform-development/cluster/karpenter.tf
+++ terraform/aws/analytical-platform-development/cluster/karpenter.tf
@@ -1,13 +1,12 @@
 module "karpenter" {
-  source  = "terraform-aws-modules/eks/aws//modules/karpenter?ref=19.15.3"
+  #checkov:skip=CKV_TF_1:Module is from Terraform registry
+  
+  source  = "terraform-aws-modules/eks/aws//modules/karpenter"
+  version = "19.5.3"
 
   cluster_name = local.eks_cluster_name
 
   irsa_oidc_provider_arn          = module.eks.oidc_provider_arn
   irsa_namespace_service_accounts = ["karpenter:karpenter"]
-
-  tags = {
-    Environment = "dev"
-    Terraform   = "true"
-  }
 }
+
