6ec2b68804a52ce71309258e036ead6923ab1d5c
lalithanagarur <lalitha.nagarur@digital.justice.gov.uk>
2023-07-25 16:51:10 +0100

    adding checkov skip comment

--- terraform/aws/analytical-platform-data-production/s3-glue-crawler/glue-crawlers.tf
+++ terraform/aws/analytical-platform-data-production/s3-glue-crawler/glue-crawlers.tf
@@ -1,4 +1,7 @@
 resource "aws_glue_crawler" "alpha_vmcs_data" {
+  # checkov:skip=CKV_AWS_195:security configuration disabled
+  # as crawler not writing encrypted data, only crawling
+  # for schema and metadata
   name          = "alpha-vcms-data"
   database_name = aws_glue_catalog_database.alpha_vcms_data.name
   role          = aws_iam_role.alpha_vcms_data_crawler.arn
