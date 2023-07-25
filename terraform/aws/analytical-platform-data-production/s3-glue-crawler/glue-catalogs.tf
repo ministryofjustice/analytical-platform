a12a277bc034dcd78a228a40b80b07afb974e7f3
lalithanagarur <lalitha.nagarur@digital.justice.gov.uk>
2023-07-25 10:44:14 +0100

    update glue crawler and glue database

--- terraform/aws/analytical-platform-data-production/s3-glue-crawler/glue-catalogs.tf
+++ terraform/aws/analytical-platform-data-production/s3-glue-crawler/glue-catalogs.tf
@@ -1,3 +1,7 @@
 resource "aws_glue_catalog_database" "alpha_vcms_data" {
   name = "alpha-vcms-data"
 }
+
+resource "aws_glue_catalog_database" "alpha_vcms_data_underscore_name" {
+  name = "alpha_vcms_data"
+} 
--- terraform/aws/analytical-platform-data-production/s3-glue-crawler/glue-crawlers.tf
+++ terraform/aws/analytical-platform-data-production/s3-glue-crawler/glue-crawlers.tf
@@ -1,7 +1,16 @@
 resource "aws_glue_crawler" "alpha_vmcs_data" {
-  name          = "alpha-vcms-data"
+  name          = "alpha_vcms_data"
   database_name = aws_glue_catalog_database.alpha_vcms_data.name
   role          = aws_iam_role.alpha_vcms_data_crawler.arn
+
+  configuration = jsonencode(
+    {
+      CrawlerOutput = {
+        Partitions = { AddOrUpdateBehavior = "InheritFromTable" }
+      }
+      Version = 1
+    }
+  )
 
   s3_target {
     path = "s3://alpha-vcms-data/vcms-data-3/vcms/"
