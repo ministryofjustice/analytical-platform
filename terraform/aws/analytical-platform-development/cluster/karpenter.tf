c4e66eab2109f20606e817a4eb606715af987154
Emterry <emma.terry@digital.justice.gov.uk>
2023-07-25 13:58:23 +0100

    create roles

--- terraform/aws/analytical-platform-development/cluster/karpenter.tf
+++ terraform/aws/analytical-platform-development/cluster/karpenter.tf
@@ -1,7 +1,7 @@
 module "karpenter" {
   source = "terraform-aws-modules/eks/aws//modules/karpenter"
 
-  cluster_name = module.eks.cluster_name
+  cluster_name = local.eks_cluster_name
 
   irsa_oidc_provider_arn          = module.eks.oidc_provider_arn
   irsa_namespace_service_accounts = ["karpenter:karpenter"]
