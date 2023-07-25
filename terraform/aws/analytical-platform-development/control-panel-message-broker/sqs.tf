b9b9c15cf77ab32a125dc476e6ebb613e5e1d2d0
Gary H <26419401+Gary-H9@users.noreply.github.com>
2023-07-25 16:08:17 +0100

    :wrench: Remove wait periods

--- terraform/aws/analytical-platform-development/control-panel-message-broker/sqs.tf
+++ terraform/aws/analytical-platform-development/control-panel-message-broker/sqs.tf
@@ -1,29 +1,23 @@
 resource "aws_sqs_queue" "s3_queue" {
   name                      = "s3_queue"
-  delay_seconds             = 90
   max_message_size          = 2048
   message_retention_seconds = 86400
-  receive_wait_time_seconds = 10
 
   tags = var.tags
 }
 
 resource "aws_sqs_queue" "iam_queue" {
   name                      = "iam_queue"
-  delay_seconds             = 90
   max_message_size          = 2048
   message_retention_seconds = 86400
-  receive_wait_time_seconds = 10
 
   tags = var.tags
 }
 
 resource "aws_sqs_queue" "auth_queue" {
   name                      = "auth_queue"
-  delay_seconds             = 90
   max_message_size          = 2048
   message_retention_seconds = 86400
-  receive_wait_time_seconds = 10
 
   tags = var.tags
 }
