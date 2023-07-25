1ac5d76522e133f77fe2ef990d2294a774f39eaa
Gary H <26419401+Gary-H9@users.noreply.github.com>
2023-07-25 14:11:20 +0100

    :wrench: Add three SQS queues and output URLs

--- terraform/aws/analytical-platform-development/control-panel-message-broker/sqs.tf
+++ terraform/aws/analytical-platform-development/control-panel-message-broker/sqs.tf
@@ -1,9 +1,29 @@
-module "sqs" {
-  source  = "terraform-aws-modules/sqs/aws"
+resource "aws_sqs_queue" "s3_queue" {
+  name                      = "s3_queue"
+  delay_seconds             = 90
+  max_message_size          = 2048
+  message_retention_seconds = 86400
+  receive_wait_time_seconds = 10
 
-  name = "control-panel-sqs"
+  tags = var.tags
+}
 
-  tags = {
-    Environment = "dev"
-  }
+resource "aws_sqs_queue" "iam_queue" {
+  name                      = "iam_queue"
+  delay_seconds             = 90
+  max_message_size          = 2048
+  message_retention_seconds = 86400
+  receive_wait_time_seconds = 10
+
+  tags = var.tags
+}
+
+resource "aws_sqs_queue" "auth_queue" {
+  name                      = "auth_queue"
+  delay_seconds             = 90
+  max_message_size          = 2048
+  message_retention_seconds = 86400
+  receive_wait_time_seconds = 10
+
+  tags = var.tags
 }
