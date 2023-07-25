dcd2f59ba9ce7f0105552091f8438ab828f64060
Gary H <26419401+Gary-H9@users.noreply.github.com>
2023-07-25 16:23:38 +0100

    :wrench: Add IAM policies for queues

---
+++ terraform/aws/analytical-platform-development/control-panel-message-broker/iam-policies.tf
@@ -0,0 +1,59 @@
+resource "aws_sqs_queue_policy" "s3_queue_iam_policy" {
+  queue_url = "${aws_sqs_queue.s3_queue.id}"
+
+  policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Id": "sqspolicy",
+  "Statement": [
+    {
+      "Sid": "First",
+      "Effect": "Allow",
+      "Principal": "*",
+      "Action": "sqs:SendMessage",
+      "Resource": "${aws_sqs_queue.s3_queue.arn}"
+    }
+  ]
+}
+POLICY
+}
+
+resource "aws_sqs_queue_policy" "iam_queue_iam_policy" {
+  queue_url = "${aws_sqs_queue.iam_queue.id}"
+
+  policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Id": "sqspolicy",
+  "Statement": [
+    {
+      "Sid": "First",
+      "Effect": "Allow",
+      "Principal": "*",
+      "Action": "sqs:SendMessage",
+      "Resource": "${aws_sqs_queue.iam_queue.arn}"
+    }
+  ]
+}
+POLICY
+}
+
+resource "aws_sqs_queue_policy" "auth_queue_iam_policy" {
+  queue_url = "${aws_sqs_queue.auth_queue.id}"
+
+  policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Id": "sqspolicy",
+  "Statement": [
+    {
+      "Sid": "First",
+      "Effect": "Allow",
+      "Principal": "*",
+      "Action": "sqs:SendMessage",
+      "Resource": "${aws_sqs_queue.auth_queue.arn}"
+    }
+  ]
+}
+POLICY
+}
