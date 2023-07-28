683d921062dc402d7662042ff3e1d64cb88bc420
lalithanagarur <lalitha.nagarur@digital.justice.gov.uk>
2023-07-28 13:36:23 +0100

    destroying unwanted resources

--- terraform/aws/analytical-platform-data-production/s3-glue-crawler/glue-catalogs.tf
+++ terraform/aws/analytical-platform-data-production/s3-glue-crawler/glue-catalogs.tf
@@ -1,7 +1,3 @@
-resource "aws_glue_catalog_database" "alpha_vcms_data" {
-  name = "alpha-vcms-data"
-}
-
 resource "aws_glue_catalog_database" "alpha_vcms_data_2" {
   name = "alpha_vcms_data"
 }
--- terraform/aws/analytical-platform-data-production/s3-glue-crawler/glue-crawlers.tf
+++ terraform/aws/analytical-platform-data-production/s3-glue-crawler/glue-crawlers.tf
@@ -1,16 +1,3 @@
-resource "aws_glue_crawler" "alpha_vmcs_data" {
-  # checkov:skip=CKV_AWS_195:security configuration disabled
-  # as crawler not writing encrypted data, only crawling
-  # for schema and metadata
-  name          = "alpha-vcms-data"
-  database_name = aws_glue_catalog_database.alpha_vcms_data.name
-  role          = aws_iam_role.alpha_vcms_data_crawler.arn
-
-  s3_target {
-    path = "s3://alpha-vcms-data/vcms-data-3/vcms/"
-  }
-}
-
 resource "aws_glue_crawler" "alpha_vmcs_data_2" {
   # checkov:skip=CKV_AWS_195:security configuration disabled
   # as crawler not writing encrypted data, only crawling
