43aa9ebeebbd6a56825b1223ffeec0948e1dbb98
Emterry <123941245+Emterry@users.noreply.github.com>
2023-08-02 16:56:54 +0100

    create own irsa (#1052)
    * create own irsa
    
    * linter
    
    * Empty commit
    
    * linter

--- terraform/aws/analytical-platform-development/cluster/karpenter.tf
+++ terraform/aws/analytical-platform-development/cluster/karpenter.tf
@@ -5,8 +5,8 @@ module "karpenter" {
 
   cluster_name = local.eks_cluster_name
 
-  irsa_oidc_provider_arn          = module.eks.oidc_provider_arn
-  irsa_namespace_service_accounts = ["karpenter:karpenter"]
+  create_irsa     = false
+  create_iam_role = true
 }
 
 resource "aws_security_group" "karpenter" {
@@ -34,4 +34,97 @@ resource "aws_security_group_rule" "eks_node_karpenter" {
   protocol                 = "tcp"
   source_security_group_id = module.eks.worker_security_group_id
   security_group_id        = aws_security_group.karpenter.id
+}
+
+resource "aws_iam_service_linked_role" "spot" {
+  aws_service_name = "spot.amazonaws.com"
+}
+
+data "aws_iam_policy_document" "karpenter_controller" {
+  statement {
+    actions = [
+      "ec2:CreateLaunchTemplate",
+      "ec2:CreateFleet",
+      "ec2:CreateTags",
+      "ec2:DescribeLaunchTemplates",
+      "ec2:DescribeImages",
+      "ec2:DescribeInstances",
+      "ec2:DescribeSecurityGroups",
+      "ec2:DescribeSubnets",
+      "ec2:DescribeInstanceTypes",
+      "ec2:DescribeInstanceTypeOfferings",
+      "ec2:DescribeAvailabilityZones",
+      "ec2:DescribeSpotPriceHistory",
+      "pricing:GetProducts",
+      "ec2:TerminateInstances",
+      "ec2:DeleteLaunchTemplate",
+    ]
+
+    resources = ["*"]
+  }
+
+  statement {
+    actions = ["ec2:RunInstances"]
+    resources = [
+      "arn:aws:ec2:*:${data.aws_caller_identity.current.account_id}:launch-template/*",
+    ]
+  }
+
+  statement {
+    actions = ["ec2:RunInstances"]
+    resources = [
+      "arn:aws:ec2:*::image/*",
+      "arn:aws:ec2:*::snapshot/*",
+      "arn:aws:ec2:*:${data.aws_caller_identity.current.account_id}:instance/*",
+      "arn:aws:ec2:*:${data.aws_caller_identity.current.account_id}:spot-instances-request/*",
+      "arn:aws:ec2:*:${data.aws_caller_identity.current.account_id}:security-group/*",
+      "arn:aws:ec2:*:${data.aws_caller_identity.current.account_id}:volume/*",
+      "arn:aws:ec2:*:${data.aws_caller_identity.current.account_id}:network-interface/*",
+      "arn:aws:ec2:*:${data.aws_caller_identity.current.account_id}:subnet/*",
+    ]
+  }
+
+  statement {
+    actions   = ["ssm:GetParameter"]
+    resources = ["arn:aws:ssm:*:*:parameter/aws/service/*"]
+  }
+
+  statement {
+    actions   = ["eks:DescribeCluster"]
+    resources = [module.eks.cluster_arn]
+  }
+
+  statement {
+    actions   = ["iam:PassRole"]
+    resources = [module.karpenter.role_arn]
+  }
+
+  statement {
+    actions = [
+      "sqs:DeleteMessage",
+      "sqs:GetQueueUrl",
+      "sqs:GetQueueAttributes",
+      "sqs:ReceiveMessage",
+    ]
+    resources = [module.karpenter.queue_arn]
+  }
+}
+
+resource "aws_iam_policy" "karpenter_irsa" {
+  name        = "karpenter_controller"
+  path        = "/"
+  description = "Karpenter controller policy"
+
+  policy = data.aws_iam_policy_document.karpenter_controller.json
+}
+
+module "iam_karpenter_controller_irsa" {
+  source                         = "terraform-aws-modules/iam/aws//modules/iam-assumable-role-with-oidc"
+  version                        = "5.28.0"
+  create_role                    = true
+  role_name_prefix               = "karpenter_controller"
+  provider_url                   = module.eks.cluster_oidc_issuer_url
+  role_policy_arns               = [aws_iam_policy.karpenter_irsa.arn]
+  oidc_fully_qualified_subjects  = ["system:serviceaccount:karpenter:karpenter"]
+  oidc_fully_qualified_audiences = ["sts.amazonaws.com"]
 }
