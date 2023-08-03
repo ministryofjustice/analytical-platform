d9ac79a0c38a3c615457f987fe5038a8e1270c97
Gary H <26419401+Gary-H9@users.noreply.github.com>
2023-08-03 16:58:07 +0100

    :pencil2: Terraform formatting

--- terraform/aws/analytical-platform-development/control-panel-message-broker/iam-policies.tf
+++ terraform/aws/analytical-platform-development/control-panel-message-broker/iam-policies.tf
@@ -1,5 +1,5 @@
 resource "aws_sqs_queue_policy" "s3_queue_iam_policy" {
-  queue_url = "${aws_sqs_queue.s3_queue.id}"
+  queue_url = aws_sqs_queue.s3_queue.id
 
   policy = <<POLICY
 {
@@ -27,7 +27,7 @@ POLICY
 }
 
 resource "aws_sqs_queue_policy" "iam_queue_iam_policy" {
-  queue_url = "${aws_sqs_queue.iam_queue.id}"
+  queue_url = aws_sqs_queue.iam_queue.id
 
   policy = <<POLICY
 {
@@ -55,7 +55,7 @@ POLICY
 }
 
 resource "aws_sqs_queue_policy" "auth_queue_iam_policy" {
-  queue_url = "${aws_sqs_queue.auth_queue.id}"
+  queue_url = aws_sqs_queue.auth_queue.id
 
   policy = <<POLICY
 {
--- terraform/aws/analytical-platform-development/control-panel-message-broker/locals.tf
+++ terraform/aws/analytical-platform-development/control-panel-message-broker/locals.tf
@@ -1,3 +1,3 @@
 locals {
-    control_panel_api_arn = "arn:aws:iam::${var.account_ids["analytical-platform-development"]}:role/dev_control_panel_api20230420142935268800000001"
+  control_panel_api_arn = "arn:aws:iam::${var.account_ids["analytical-platform-development"]}:role/dev_control_panel_api20230420142935268800000001"
 }
