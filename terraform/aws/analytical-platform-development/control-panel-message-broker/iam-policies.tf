8fbdf4e7c664cbc3511d72564d21dbdfdee2e025
Gary H <26419401+Gary-H9@users.noreply.github.com>
2023-08-03 11:45:44 +0100

    Revert ":wrench: Update IAM policies permissions"
    This reverts commit 2cfc3da8dfa60573eeea5c470346953533377bc3.

--- terraform/aws/analytical-platform-development/control-panel-message-broker/iam-policies.tf
+++ terraform/aws/analytical-platform-development/control-panel-message-broker/iam-policies.tf
@@ -7,16 +7,11 @@ resource "aws_sqs_queue_policy" "s3_queue_iam_policy" {
   "Id": "sqspolicy",
   "Statement": [
     {
-      "Sid": "CanManageSqsMessages",
+      "Sid": "First",
       "Effect": "Allow",
       "Principal": "*",
-      "Action" : [
-          "sqs:GetQueueUrl",
-          "sqs:DeleteMessage",
-          "sqs:ReceiveMessage",
-          "sqs:SendMessage"
-        ],
-      "Resource": ""arn:aws:sqs::${var.account_ids["analytical-platform-development"]}:*""
+      "Action": "sqs:SendMessage",
+      "Resource": "${aws_sqs_queue.s3_queue.arn}"
     }
   ]
 }
@@ -32,16 +27,11 @@ resource "aws_sqs_queue_policy" "iam_queue_iam_policy" {
   "Id": "sqspolicy",
   "Statement": [
     {
-      "Sid": "CanManageSqsMessages",
+      "Sid": "First",
       "Effect": "Allow",
       "Principal": "*",
-      "Action" : [
-          "sqs:GetQueueUrl",
-          "sqs:DeleteMessage",
-          "sqs:ReceiveMessage",
-          "sqs:SendMessage"
-        ],
-      "Resource": ""arn:aws:sqs::${var.account_ids["analytical-platform-development"]}:*""
+      "Action": "sqs:SendMessage",
+      "Resource": "${aws_sqs_queue.iam_queue.arn}"
     }
   ]
 }
@@ -57,16 +47,11 @@ resource "aws_sqs_queue_policy" "auth_queue_iam_policy" {
   "Id": "sqspolicy",
   "Statement": [
     {
-      "Sid": "CanManageSqsMessages",
+      "Sid": "First",
       "Effect": "Allow",
       "Principal": "*",
-      "Action" : [
-          "sqs:GetQueueUrl",
-          "sqs:DeleteMessage",
-          "sqs:ReceiveMessage",
-          "sqs:SendMessage"
-        ],
-      "Resource": ""arn:aws:sqs::${var.account_ids["analytical-platform-development"]}:*""
+      "Action": "sqs:SendMessage",
+      "Resource": "${aws_sqs_queue.auth_queue.arn}"
     }
   ]
 }
