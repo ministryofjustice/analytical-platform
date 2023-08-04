b2064c031bd4a129b2eef1d717aa4aed4d35a82f
Gary H <26419401+Gary-H9@users.noreply.github.com>
2023-08-04 14:43:15 +0100

    :recycle: Refactor policy into aws_iam_policy_document

--- terraform/aws/analytical-platform-development/control-panel-message-broker/iam-policies.tf
+++ terraform/aws/analytical-platform-development/control-panel-message-broker/iam-policies.tf
@@ -1,83 +1,14 @@
 resource "aws_sqs_queue_policy" "s3_queue_iam_policy" {
   queue_url = aws_sqs_queue.s3_queue.id
-
-  policy = <<POLICY
-{
-  "Version": "2012-10-17",
-  "Id": "sqspolicy",
-  "Statement": [
-    {
-      "Sid": "InboundManagementSqsMessages",
-      "Effect": "Allow",
-      "Principal": {
-         "AWS": "${local.control_panel_api_arn}"
-      },
-      "Action" : [
-          "sqs:GetQueueAttributes",
-          "sqs:GetQueueUrl",
-          "sqs:DeleteMessage",
-          "sqs:ReceiveMessage",
-          "sqs:SendMessage"
-        ],
-      "Resource": "${aws_sqs_queue.s3_queue.arn}"
-    }
-  ]
-}
-POLICY
+  policy    = data.aws_iam_policy_document.sqs_s3_policy_document
 }
 
 resource "aws_sqs_queue_policy" "iam_queue_iam_policy" {
   queue_url = aws_sqs_queue.iam_queue.id
-
-  policy = <<POLICY
-{
-  "Version": "2012-10-17",
-  "Id": "sqspolicy",
-  "Statement": [
-    {
-      "Sid": "InboundManagementSqsMessages",
-      "Effect": "Allow",
-      "Principal": {
-         "AWS": "${local.control_panel_api_arn}"
-      },
-      "Action" : [
-          "sqs:GetQueueAttributes",
-          "sqs:GetQueueUrl",
-          "sqs:DeleteMessage",
-          "sqs:ReceiveMessage",
-          "sqs:SendMessage"
-        ],
-      "Resource": "${aws_sqs_queue.iam_queue.arn}"
-    }
-  ]
-}
-POLICY
+  policy    = data.aws_iam_policy_document.sqs_iam_policy_document
 }
 
 resource "aws_sqs_queue_policy" "auth_queue_iam_policy" {
   queue_url = aws_sqs_queue.auth_queue.id
-
-  policy = <<POLICY
-{
-  "Version": "2012-10-17",
-  "Id": "sqspolicy",
-  "Statement": [
-    {
-      "Sid": "InboundManagementSqsMessages",
-      "Effect": "Allow",
-      "Principal": {
-         "AWS": "${local.control_panel_api_arn}"
-      },
-      "Action" : [
-          "sqs:GetQueueAttributes",
-          "sqs:GetQueueUrl",
-          "sqs:DeleteMessage",
-          "sqs:ReceiveMessage",
-          "sqs:SendMessage"
-        ],
-      "Resource": "${aws_sqs_queue.auth_queue.arn}"
-    }
-  ]
-}
-POLICY
+  policy    = data.aws_iam_policy_document.sqs_auth_policy_document
 }
