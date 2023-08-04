050cd59122f50253377b67af49298342c03c29ab
Gary H <26419401+Gary-H9@users.noreply.github.com>
2023-08-04 11:47:36 +0100

    :pencil2: Terraform formatting

--- terraform/aws/analytical-platform-development/control-panel-message-broker/sqs.tf
+++ terraform/aws/analytical-platform-development/control-panel-message-broker/sqs.tf
@@ -2,7 +2,7 @@ resource "aws_sqs_queue" "s3_queue" {
   name                      = "s3_queue"
   max_message_size          = 2048
   message_retention_seconds = 86400
-  sqs_managed_sse_enabled = true
+  sqs_managed_sse_enabled   = true
 
   tags = var.tags
 }
@@ -11,7 +11,7 @@ resource "aws_sqs_queue" "iam_queue" {
   name                      = "iam_queue"
   max_message_size          = 2048
   message_retention_seconds = 86400
-  sqs_managed_sse_enabled = true
+  sqs_managed_sse_enabled   = true
 
   tags = var.tags
 }
@@ -20,7 +20,7 @@ resource "aws_sqs_queue" "auth_queue" {
   name                      = "auth_queue"
   max_message_size          = 2048
   message_retention_seconds = 86400
-  sqs_managed_sse_enabled = true
+  sqs_managed_sse_enabled   = true
 
   tags = var.tags
 }
