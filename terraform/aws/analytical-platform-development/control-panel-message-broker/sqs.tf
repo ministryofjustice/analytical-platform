cf4c89e4c5e5418444176634a486929a89bbe4ab
Gary H <26419401+Gary-H9@users.noreply.github.com>
2023-08-04 11:44:25 +0100

    :test_tube: Testing explicitly adding SQS for linting

--- terraform/aws/analytical-platform-development/control-panel-message-broker/sqs.tf
+++ terraform/aws/analytical-platform-development/control-panel-message-broker/sqs.tf
@@ -2,6 +2,7 @@ resource "aws_sqs_queue" "s3_queue" {
   name                      = "s3_queue"
   max_message_size          = 2048
   message_retention_seconds = 86400
+  sqs_managed_sse_enabled = true
 
   tags = var.tags
 }
@@ -10,6 +11,7 @@ resource "aws_sqs_queue" "iam_queue" {
   name                      = "iam_queue"
   max_message_size          = 2048
   message_retention_seconds = 86400
+  sqs_managed_sse_enabled = true
 
   tags = var.tags
 }
@@ -18,6 +20,7 @@ resource "aws_sqs_queue" "auth_queue" {
   name                      = "auth_queue"
   max_message_size          = 2048
   message_retention_seconds = 86400
+  sqs_managed_sse_enabled = true
 
   tags = var.tags
 }
