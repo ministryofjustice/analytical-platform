3ba0130d15707933c68b3deb3b172c2b483f9f0d
Gary H <26419401+Gary-H9@users.noreply.github.com>
2023-08-04 12:23:41 +0100

    :wrench: Configure terrascan to skip SQS violation

--- terraform/aws/analytical-platform-development/control-panel-message-broker/sqs.tf
+++ terraform/aws/analytical-platform-development/control-panel-message-broker/sqs.tf
@@ -1,4 +1,6 @@
 resource "aws_sqs_queue" "s3_queue" {
+  #ts:skip=AWS.SQS.NetworkSecurity.High.0570
+
   name                      = "s3_queue"
   max_message_size          = 2048
   message_retention_seconds = 86400
@@ -8,6 +10,8 @@ resource "aws_sqs_queue" "s3_queue" {
 }
 
 resource "aws_sqs_queue" "iam_queue" {
+  #ts:skip=AWS.SQS.NetworkSecurity.High.0570
+
   name                      = "iam_queue"
   max_message_size          = 2048
   message_retention_seconds = 86400
@@ -17,6 +21,8 @@ resource "aws_sqs_queue" "iam_queue" {
 }
 
 resource "aws_sqs_queue" "auth_queue" {
+  #ts:skip=AWS.SQS.NetworkSecurity.High.0570
+
   name                      = "auth_queue"
   max_message_size          = 2048
   message_retention_seconds = 86400
