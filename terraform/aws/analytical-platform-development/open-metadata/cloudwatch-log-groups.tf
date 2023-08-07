2a81107c2f50ce0638dafefcca4a87d1d8b1579b
Jacob Woffenden <jacob.woffenden@digital.justice.gov.uk>
2023-08-07 16:09:12 +0000

    Skip AWS.ACLG.LM.MEDIUM.0068

--- terraform/aws/analytical-platform-development/open-metadata/cloudwatch-log-groups.tf
+++ terraform/aws/analytical-platform-development/open-metadata/cloudwatch-log-groups.tf
@@ -1,3 +1,4 @@
 resource "aws_cloudwatch_log_group" "auth0" {
+  #ts:skip=AWS.ACLG.LM.MEDIUM.0068 this is nonproduction code, will be addresses when productionised
   name = "/aws/events/auth0/alpha-analytics-moj"
 }
