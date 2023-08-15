cd3c7c785d8b9b2c47eeb0920409acceb5dba43b
Murdo <109604278+murdo-moj@users.noreply.github.com>
2023-08-15 15:17:52 +0100

    Dp 865 (#1162)
    * pd-865 check data product exists
    
    * dp-865 reinstate md5 hash check
    
    * Update containers/daap-presigned-url/CHANGELOG.md
    
    Co-authored-by: Jacob Woffenden <jacob.woffenden@digital.justice.gov.uk>
    
    * Fixed trailing whitespace
    
    * Fixed line length
    
    * Added a get to list objects json
    
    ---------
    
    Co-authored-by: Jacob Woffenden <jacob.woffenden@digital.justice.gov.uk>

--- containers/daap-presigned-url/CHANGELOG.md
+++ containers/daap-presigned-url/CHANGELOG.md
@@ -1,4 +1,5 @@
 <!-- markdownlint-disable MD003 -->
+
 # Changelog
 
 All notable changes to this project will be documented in this file.
@@ -7,6 +8,15 @@ The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
 ## [Unreleased]
+
+## [1.1.0]
+
+### Changed
+
+- Upon requesting an upload URL, check to see if the database has a
+  corresponding data product.
+- Add 400, 404 responses
+- Reinstate md5 hash check
 
 ## [1.0.2]
 
--- containers/daap-presigned-url/src/var/task/presigned_url.py
+++ containers/daap-presigned-url/src/var/task/presigned_url.py
@@ -16,8 +16,22 @@ def handler(event, context):
     database = event["queryStringParameters"]["database"]
     table = event["queryStringParameters"]["table"]
     amz_date = datetime.utcnow().strftime("%Y%m%dT%H%M%SZ")
-    # md5 = str(event["queryStringParameters"]["contentMD5"])
+    md5 = str(event["queryStringParameters"]["contentMD5"])
     uuid_string = str(uuid.uuid4())
+
+    if type(database) != str or type(table) != str:
+        return {
+            "statusCode": 400,
+            "headers": {"Content-Type": "application/json"},
+            "body": json.dumps(
+                {
+                    "error": {
+                        "message": "Database or table is not convertible to string type."
+                    }
+                }
+            ),
+        }
+
     file_name = os.path.join(
         "raw_data",
         database,
@@ -29,32 +43,51 @@ def handler(event, context):
         "x-amz-server-side-encryption": "AES256",
         "x-amz-acl": "bucket-owner-full-control",
         "x-amz-date": amz_date,
-        # "Content-MD5": md5,
-        # "Content-Type": "binary/octet-stream",
+        "Content-MD5": md5,
+        "Content-Type": "binary/octet-stream",
     }
     # File upload is capped at 5GB per single upload so content-length-range is 5GB
     conditions = [
         {"x-amz-server-side-encryption": "AES256"},
         {"x-amz-acl": "bucket-owner-full-control"},
         {"x-amz-date": amz_date},
-        # {"Content-MD5": md5},
-        # ["starts-with", "$Content-MD5", ""],
-        # ["starts-with", "$Content-Type", ""],
+        {"Content-MD5": md5},
+        ["starts-with", "$Content-MD5", ""],
+        ["starts-with", "$Content-Type", ""],
         ["starts-with", "$key", file_name],
         ["content-length-range", 0, 5000000000],
     ]
 
     root_logger.info(f"s3 key: {file_name}")
 
-    URL = s3.generate_presigned_post(
-        Bucket=bucket_name,
-        Key=file_name,
-        Fields=fields,
-        Conditions=conditions,
-        ExpiresIn=200,
-    )
-    return {
-        "statusCode": 200,
-        "headers": {"Content-Type": "application/json"},
-        "body": json.dumps({"URL": URL}),
-    }
+    # Check the data product has been registered, ie has associated code or metadata in s3
+    data_product_registration = s3.list_objects_v2(
+        Bucket=bucket_name, Prefix=f"code/{database}"
+    ).get("Contents", [])
+
+    if not any(data_product_registration):
+        return {
+            "statusCode": 404,
+            "headers": {"Content-Type": "application/json"},
+            "body": json.dumps(
+                {
+                    "error": {
+                        "message": "Data product registration relating to database not found."
+                    }
+                }
+            ),
+        }
+
+    if any(data_product_registration) and type(database) == str and type(table) == str:
+        URL = s3.generate_presigned_post(
+            Bucket=bucket_name,
+            Key=file_name,
+            Fields=fields,
+            Conditions=conditions,
+            ExpiresIn=200,
+        )
+        return {
+            "statusCode": 200,
+            "headers": {"Content-Type": "application/json"},
+            "body": json.dumps({"URL": URL}),
+        }
