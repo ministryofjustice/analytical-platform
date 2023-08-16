96973b167c8a52d5e6f4be4efc61937d9347d6e8
Mitch Dawson <Mitchell.Dawson@justice.gov.uk>
2023-08-16 16:22:08 +0100

    :wrench: Updated locals.tf to fix control panel api arn variable

--- terraform/aws/analytical-platform-data-production/control-panel-message-broker/locals.tf
+++ terraform/aws/analytical-platform-data-production/control-panel-message-broker/locals.tf
@@ -1,3 +1,3 @@
 locals {
-  control_panel_api_arn = "arn:aws:iam::${var.account_ids["analytical-platform-production"]}:role/prod_control_panel_api20210906102154527600000001"
+  control_panel_api_arn = "arn:aws:iam::${var.account_ids["analytical-platform-data-production"]}:role/prod_control_panel_api20210906102154527600000001"
 }
