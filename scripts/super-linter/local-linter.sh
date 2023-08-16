8fb4f66453d2ce71af1319a46fbc54a1c3ab11bd
Jacob Woffenden <jacob.woffenden@digital.justice.gov.uk>
2023-08-16 10:54:48 +0000

    Update env Update script Update gitignore

--- scripts/super-linter/local-linter.sh
+++ scripts/super-linter/local-linter.sh
@@ -2,8 +2,10 @@
 
 docker run --rm \
   --env RUN_LOCAL="true" \
+  --env CREATE_LOG_FILE="true" \
+  --env LOG_FILE="super-linter.log" \
   --env USE_FIND_ALGORITHM="true" \
   --env-file ".github/super-linter.env" \
   --volume "${PWD}":/tmp/lint \
   --workdir /tmp/lint \
-  ghcr.io/github/super-linter:v5.0.0
+  ghcr.io/super-linter/super-linter:slim-v5
