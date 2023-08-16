e6d21cdc210b14bdf0a0ab5d6c4d3c707b781787
Mitch Dawson <Mitchell.Dawson@justice.gov.uk>
2023-08-16 16:39:37 +0100

    :wrench: Update resource naming coventions

--- terraform/aws/analytical-platform-data-production/control-panel-message-broker/iam-policies.tf
+++ terraform/aws/analytical-platform-data-production/control-panel-message-broker/iam-policies.tf
@@ -1,14 +1,14 @@
-resource "aws_sqs_queue_policy" "s3_queue_iam_policy" {
-  queue_url = aws_sqs_queue.s3_queue.id
-  policy    = data.aws_iam_policy_document.sqs_s3_policy_document.json
+resource "aws_sqs_queue_policy" "s3" {
+  queue_url = aws_sqs_queue.s3.id
+  policy    = data.aws_iam_policy_document.sqs_s3.json
 }
 
-resource "aws_sqs_queue_policy" "iam_queue_iam_policy" {
-  queue_url = aws_sqs_queue.iam_queue.id
-  policy    = data.aws_iam_policy_document.sqs_iam_policy_document.json
+resource "aws_sqs_queue_policy" "iam" {
+  queue_url = aws_sqs_queue.iam.id
+  policy    = data.aws_iam_policy_document.sqs_iam.json
 }
 
-resource "aws_sqs_queue_policy" "auth_queue_iam_policy" {
-  queue_url = aws_sqs_queue.auth_queue.id
-  policy    = data.aws_iam_policy_document.sqs_auth_policy_document.json
+resource "aws_sqs_queue_policy" "auth" {
+  queue_url = aws_sqs_queue.auth.id
+  policy    = data.aws_iam_policy_document.sqs_auth.json
 }
--- terraform/aws/analytical-platform-data-production/control-panel-message-broker/sqs.tf
+++ terraform/aws/analytical-platform-data-production/control-panel-message-broker/sqs.tf
@@ -1,14 +1,14 @@
-resource "aws_sqs_queue" "s3_queue" {
+resource "aws_sqs_queue" "s3" {
   #ts:skip=AWS.SQS.NetworkSecurity.High.0570
   name = "s3_queue"
 }
 
-resource "aws_sqs_queue" "iam_queue" {
+resource "aws_sqs_queue" "iam" {
   #ts:skip=AWS.SQS.NetworkSecurity.High.0570
   name = "iam_queue"
 }
 
-resource "aws_sqs_queue" "auth_queue" {
+resource "aws_sqs_queue" "auth" {
   #ts:skip=AWS.SQS.NetworkSecurity.High.0570
   name = "auth_queue"
 }
