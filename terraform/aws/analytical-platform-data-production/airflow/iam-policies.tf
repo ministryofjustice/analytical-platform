23bde025c53200c8bcc831a34c2e5e66b8a006b0
julialawrence <julia.lawrence@digital.justice.gov.uk>
2023-08-18 16:36:51 +0100

    Adding airflow-execution-role-policy

--- terraform/aws/analytical-platform-data-production/airflow/iam-policies.tf
+++ terraform/aws/analytical-platform-data-production/airflow/iam-policies.tf
@@ -15,3 +15,30 @@ module "airflow_analytical_platform_development_iam_policy" {
 
   policy = data.aws_iam_policy_document.airflow_analytical_platform_development.json
 }
+
+data "aws_iam_policy_document" "airflow_dev_execution_role_policy" {
+  statement {
+    sid       = ""
+    effect    = "Allow"
+    actions   = ["airflow:PublishMetrics"]
+    resources = ["arn:aws:airflow:eu-west-1:593291632749:environment/dev"]
+  }
+
+  statement {
+    sid       = ""
+    effect    = "Deny"
+    actions   = ["s3:ListAllMyBuckets"]
+    resources = ["*"]
+  }
+  statement {
+    sid     = ""
+    effect  = "Allow"
+    actions = ["s3:List*", "s3:GetObject*", "s3:GetBucket*"]
+    resources = [
+      "arn:aws:s3:::mojap-airflow-dev/*",
+      "arn:aws:s3:::mojap-airflow-dev"
+    ]
+  }
+
+
+}
