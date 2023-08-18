38776dba66497821c444630cccd7dda5c4e3bdab
julialawrence <julia.lawrence@digital.justice.gov.uk>
2023-08-18 10:44:00 +0100

    Creating an inline role with amended trust policy to get around character limitations. This will need a permnanet fix with a change in the oidc module.

--- terraform/aws/analytical-platform/oidc/oidc-roles.tf
+++ terraform/aws/analytical-platform/oidc/oidc-roles.tf
@@ -146,3 +146,62 @@ module "analytical_platform_management_production_github_oidc_role" {
 
   tags = var.tags
 }
+
+
+resource "aws_iam_role" "github_actions" {
+  name               = "github-actions-ecr-oidc"
+  assume_role_policy = data.aws_iam_policy_document.github_oidc_assume_role.json
+}
+
+data "aws_iam_policy_document" "github_oidc_assume_role" {
+  version = "2012-10-17"
+
+  statement {
+    effect  = "Allow"
+    actions = ["sts:AssumeRoleWithWebIdentity"]
+
+    principals {
+      type = "Federated"
+      identifiers = [
+        "arn:aws:iam::${data.aws_caller_identity.current.account_id}:oidc-provider/token.actions.githubusercontent.com"
+      ]
+    }
+
+    condition {
+      test     = "StringEquals"
+      variable = "token.actions.githubusercontent.com:aud"
+      values   = ["sts.amazonaws.com"]
+    }
+
+    condition {
+      test     = "StringLike"
+      variable = "token.actions.githubusercontent.com:sub"
+      values   = formatlist("*:*/%s:*", concat([for app in local.migration_apps_map : app.name], local.additional_repos_for_ecr))
+    }
+
+    condition {
+      test     = "ForAllValues:StringLike"
+      variable = "token.actions.githubusercontent.com:sub"
+      values   = ["repo:ministryofjustice/*"]
+    }
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "read_only" {
+  role       = aws_iam_role.github_actions.name
+  policy_arn = "arn:aws:iam::aws:policy/ReadOnlyAccess"
+}
+
+# Add actions missing from arn:aws:iam::aws:policy/ReadOnlyAccess
+resource "aws_iam_policy" "extra_permissions" {
+  name        = var.role_name
+  path        = "/"
+  description = "A policy for extra permissions for GitHub Actions"
+
+  policy = data.aws_iam_policy_document.analytical_platform_data_production_github_oidc_provider.json
+}
+
+resource "aws_iam_role_policy_attachment" "extra_permissions" {
+  role       = aws_iam_role.github_actions.name
+  policy_arn = aws_iam_policy.extra_permissions.arn
+}
