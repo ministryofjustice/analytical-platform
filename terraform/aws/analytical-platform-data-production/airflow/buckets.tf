ae239fcc51f43b61f552c86b05abc841157cc306
Jacob Hamblin-Pyke <jacob.hamblin-pyke@digital.justice.gov.uk>
2023-08-22 16:13:40 +0100

    Add and import final existing bucket resources

--- terraform/aws/analytical-platform-data-production/airflow/buckets.tf
+++ terraform/aws/analytical-platform-data-production/airflow/buckets.tf
@@ -22,3 +22,39 @@ resource "aws_s3_object" "requirements" {
   # etag = "${md5(file("path/to/file"))}"
   etag = filemd5("./files/requirements.txt")
 }
+
+resource "aws_s3_object" "kubeconfig" {
+  bucket = "mojap-airflow-dev"
+  key    = "dags/.kube/config"
+  source = "./files/config"
+
+  # The filemd5() function is available in Terraform 0.11.12 and later
+  # For Terraform 0.11.11 and earlier, use the md5() function and the file() function:
+  # etag = "${md5(file("path/to/file"))}"
+  etag = filemd5("./files/config")
+}
+
+resource "aws_s3_bucket_policy" "airflow_dev_bucket_policy" {
+  bucket = aws_s3_bucket.mojap_airflow_dev.id
+  policy = data.aws_iam_policy_document.airflow_bucket_policy.json
+}
+
+data "aws_iam_policy_document" "airflow_bucket_policy" {
+  statement {
+    principals {
+      type        = "AWS"
+      identifiers = ["arn:aws:iam::593291632749:role/data-ga-s3-sync"]
+    }
+
+    actions = [
+      "s3:ListBucket",
+      "s3:ListBucketMultipartUploads",
+      "s3:ListBucketVersions"
+    ]
+
+    resources = [
+      aws_s3_bucket.mojap_airflow_dev.arn,
+      "${aws_s3_bucket.mojap_airflow_dev.arn}/*",
+    ]
+  }
+}
