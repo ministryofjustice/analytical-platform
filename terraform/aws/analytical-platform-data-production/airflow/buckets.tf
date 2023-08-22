df9122a5eff758ab29459b9809f2ec077804947a
Jacob Hamblin-Pyke <jacob.hamblin-pyke@digital.justice.gov.uk>
2023-08-22 14:26:31 +0100

    Adding bucket id

--- terraform/aws/analytical-platform-data-production/airflow/buckets.tf
+++ terraform/aws/analytical-platform-data-production/airflow/buckets.tf
@@ -4,7 +4,7 @@ resource "aws_s3_bucket" "mojap_airflow_dev" {
 }
 
 resource "aws_s3_bucket_public_access_block" "mojap_airflow_dev_access_block" {
-  bucket = aws_s3_bucket.mojap_airflow_dev
+  bucket = aws_s3_bucket.mojap_airflow_dev.id
 
   block_public_acls       = true
   block_public_policy     = true
