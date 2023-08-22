169177b1e0255f513953c262dd584238d30d5aab
tam <32667518+tamsinforbes@users.noreply.github.com>
2023-08-22 15:45:45 +0100

    DMT-1238/airflow-network-components-1 (#1254)
    * add main vpc
    
    * add vpn gateway
    
    * add airflow internet gateway

---
+++ terraform/aws/analytical-platform-data-production/airflow/network.tf
@@ -0,0 +1,23 @@
+resource "aws_vpc" "airflow_dev" {
+  cidr_block = "10.200.0.0/16"
+
+  tags = {
+    Name = "airflow-dev"
+  }
+}
+
+resource "aws_vpn_gateway" "airflow_dev" {
+  vpc_id = aws_vpc.airflow_dev.id
+
+  tags = {
+    Name = "airflow-dev"
+  }
+}
+
+resource "aws_internet_gateway" "airflow_dev" {
+  vpc_id = aws_vpc.airflow_dev.id
+
+  tags = {
+    Name = "airflow-dev"
+  }
+}
