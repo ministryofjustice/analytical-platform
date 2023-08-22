13c5ade8047e1ebea87755aee905c769dc79bd5e
tamsinforbes <32667518+tamsinforbes@users.noreply.github.com>
2023-08-22 15:25:30 +0100

    add vpn gateway

--- terraform/aws/analytical-platform-data-production/airflow/network.tf
+++ terraform/aws/analytical-platform-data-production/airflow/network.tf
@@ -5,3 +5,11 @@ resource "aws_vpc" "airflow_dev" {
       Name = "airflow-dev"
     }
 }
+
+resource "aws_vpn_gateway" "airflow_dev" {
+    vpc_id = aws_vpc.airflow_dev.id
+
+    tags = {
+        Name = "airflow-dev"
+    }
+}
