7542d5951f74feec5d7999c73e07214296702fe8
tamsinforbes <32667518+tamsinforbes@users.noreply.github.com>
2023-08-22 15:30:54 +0100

    add airflow internet gateway

--- terraform/aws/analytical-platform-data-production/airflow/network.tf
+++ terraform/aws/analytical-platform-data-production/airflow/network.tf
@@ -1,15 +1,23 @@
 resource "aws_vpc" "airflow_dev" {
-    cidr_block = "10.200.0.0/16"
+  cidr_block = "10.200.0.0/16"
 
-    tags = {
-      Name = "airflow-dev"
-    }
+  tags = {
+    Name = "airflow-dev"
+  }
 }
 
 resource "aws_vpn_gateway" "airflow_dev" {
-    vpc_id = aws_vpc.airflow_dev.id
+  vpc_id = aws_vpc.airflow_dev.id
 
-    tags = {
-        Name = "airflow-dev"
-    }
+  tags = {
+    Name = "airflow-dev"
+  }
+}
+
+resource "aws_internet_gateway" "airflow_dev" {
+  vpc_id = aws_vpc.airflow_dev.id
+
+  tags = {
+    Name = "airflow-dev"
+  }
 }
