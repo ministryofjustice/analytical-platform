335b9ee76db3efaa88fdbe2273d7b42c25ff59f0
Jacob Hamblin-Pyke <jacob.hamblin-pyke@digital.justice.gov.uk>
2023-08-22 14:37:56 +0100

    Adding requirements file

--- terraform/aws/analytical-platform-data-production/airflow/buckets.tf
+++ terraform/aws/analytical-platform-data-production/airflow/buckets.tf
@@ -11,3 +11,14 @@ resource "aws_s3_bucket_public_access_block" "mojap_airflow_dev_access_block" {
   ignore_public_acls      = true
   restrict_public_buckets = true
 }
+
+resource "aws_s3_object" "object" {
+  bucket = "mojap-airflow-dev"
+  key    = "requirements.txt"
+  source = "./files/requirements.txt"
+
+  # The filemd5() function is available in Terraform 0.11.12 and later
+  # For Terraform 0.11.11 and earlier, use the md5() function and the file() function:
+  # etag = "${md5(file("path/to/file"))}"
+  etag = filemd5("./files/requirements.txt")
+}
