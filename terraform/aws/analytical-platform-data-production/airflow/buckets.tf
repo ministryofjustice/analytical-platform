705fca53987f62c9443a468f0fa166bab2a8afa8
Jacob Hamblin-Pyke <jacob.hamblin-pyke@digital.justice.gov.uk>
2023-08-22 14:20:16 +0100

    Adding public access block

--- terraform/aws/analytical-platform-data-production/airflow/buckets.tf
+++ terraform/aws/analytical-platform-data-production/airflow/buckets.tf
@@ -2,3 +2,12 @@ resource "aws_s3_bucket" "mojap_airflow_dev" {
   bucket = "mojap-airflow-dev"
 
 }
+
+resource "aws_s3_bucket_public_access_block" "mojap_airflow_dev_access_block" {
+  bucket = aws_s3_bucket.mojap_airflow_dev
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
