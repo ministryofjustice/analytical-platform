fb3beb5242bac7169b66cfb0b23e1385df362da7
tamsinforbes <32667518+tamsinforbes@users.noreply.github.com>
2023-08-23 15:27:59 +0100

    add eips

--- terraform/aws/analytical-platform-data-production/airflow/network.tf
+++ terraform/aws/analytical-platform-data-production/airflow/network.tf
@@ -22,6 +22,16 @@ resource "aws_internet_gateway" "airflow_dev" {
   }
 }
 
+resource "aws_eip" "airflow_dev_eip" {
+  domain                    = "vpc"
+  count                     = length(var.eip_private_ips)
+  associate_with_private_ip = element(var.eip_private_ips, count.index)
+  depends_on                = [aws_internet_gateway.airflow_dev]
+  tags = {
+    Name = "airflow-dev-${element(var.azs, count.index)}"
+  }
+}
+
 resource "aws_subnet" "public_subnet" {
   vpc_id            = aws_vpc.airflow_dev.id
   count             = length(var.public_subnet_cidrs)
