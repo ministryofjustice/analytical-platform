7ec184a2d4fafaff50bba016f8f9042e8a3e32e7
Murdo Moyse <murdo.moyse1@justice.gov.uk>
2023-08-25 15:21:35 +0100

    Deleted curated parquet files on data product recreate

--- containers/daap-recreate-data-product/src/var/task/recreate_data_product.py
+++ containers/daap-recreate-data-product/src/var/task/recreate_data_product.py
@@ -1,30 +1,67 @@
-import json
-import logging
 import os
+
+import boto3
 
 
 def handler(event, context):
-    logging.info(f"event: {event}")
+    data_product_to_recreate = event.get("data_product", "")
+    raw_data_bucket = os.environ.get("RAW_DATA_BUCKET", "")
+    curated_data_bucket = os.environ.get("CURATED_DATA_BUCKET", "")
+    athena_load_lambda = os.environ.get("ATHENA_LOAD_LAMBDA", "")
 
-    authorizationToken = json.dumps(event["authorizationToken"])
-    characters_to_remove = '"[]"'
-    for character in characters_to_remove:
-        authorizationToken = authorizationToken.replace(character, "")
+    # Check data product has associated data
+    s3 = boto3.client("s3")
+    s3_response = s3.list_objects_v2(
+        Bucket=raw_data_bucket, Prefix=f"raw_data/{data_product_to_recreate}"
+    )
+    data_product_registration = s3_response.get("Contents", [])
+    print(data_product_registration)
+    if not any(data_product_registration):
+        raise ValueError(f"No data product found for {data_product_to_recreate}")
 
-    auth = "Allow" if authorizationToken == os.environ["authorizationToken"] else "Deny"
+    # Drop existing athena tables for that data product
+    glue = boto3.client("glue")
+    glue_response = glue.get_tables(DatabaseName=data_product_to_recreate)
+    data_product_tables = glue_response.get("TableList", [])
+    if not any(data_product_tables):
+        print(f"No tables found for data product {data_product_to_recreate}")
+    for table in data_product_tables:
+        glue.delete_table(DatabaseName=data_product_to_recreate, Name=table["Name"])
 
-    authResponse = {
-        "principalId": "abc123",
-        "policyDocument": {
-            "Version": "2012-10-17",
-            "Statement": [
-                {
-                    "Action": "execute-api:Invoke",
-                    "Resource": [os.environ["api_resource_arn"]],
-                    "Effect": auth,
-                }
-            ],
-        },
-    }
+    # Remove curated data files for that data product
+    paginator = s3.get_paginator("list_objects_v2")
+    pages = paginator.paginate(
+        Bucket=curated_data_bucket,
+        Prefix=f"curated_data/database_name={data_product_to_recreate}/",
+    )
 
-    return authResponse
+    delete_us = dict(Objects=[])
+    for item in pages.search("Contents"):
+        delete_us["Objects"].append(dict(Key=item["Key"]))
+
+        # delete once aws limit reached
+        if len(delete_us["Objects"]) >= 1000:
+            s3.delete_objects(Bucket=curated_data_bucket, Delete=delete_us)
+            delete_us = dict(Objects=[])
+            print(
+                f"deleted 1000 data files from curated_data/database_name={data_product_to_recreate}/"
+            )
+
+    # delete remaining
+    if len(delete_us["Objects"]):
+        s3.delete_objects(Bucket=curated_data_bucket, Delete=delete_us)
+        print(
+            f"deleted all data files from curated_data/database_name={data_product_to_recreate}/"
+        )
+
+    # Feed all data files through the load process again. Curated files are recreated.
+    aws_lambda = boto3.client("lambda")
+    # If there are over 1000 files, the lambda will get jammed up
+    for file in data_product_registration:
+        key = file["Key"]
+        payload = f'{{"detail":{{"bucket":{{"name":"{raw_data_bucket}"}}, "object":{{"key":"{key}"}}}}}}'
+        aws_lambda.invoke(
+            FunctionName=athena_load_lambda, InvocationType="Event", Payload=payload
+        )
+
+    print(f"data product {data_product_to_recreate} recreated")
