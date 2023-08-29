35b9f1deeaed373655408f71008986d4612f3b93
tamsinforbes <32667518+tamsinforbes@users.noreply.github.com>
2023-08-29 14:54:51 +0100

    fix formatting

--- terraform/aws/analytical-platform-data-production/airflow/network.tf
+++ terraform/aws/analytical-platform-data-production/airflow/network.tf
@@ -68,78 +68,8 @@ resource "aws_route_table" "airflow_dev_public" {
   }
 }
 
-
 resource "aws_route_table_association" "airflow_dev_public_route_table_assoc" {
-  count      = length(var.azs)
-  subnet_id = aws_subnet.public_subnet[count.index].id # er, refer to each of aws_subnet.public_subnet[0].id etc 
+  count          = length(var.azs)
+  subnet_id      = aws_subnet.public_subnet[count.index].id
   route_table_id = aws_route_table.airflow_dev_public.id
 }
-
-
-
-
-# local routes are added by default
-# 
-
-
-    # ├─ aws:ec2/vpc:Vpc                                               airflow-dev
-    # │  │  ID: vpc-0a6cb83c3c614dcba
-    # │  ├─ aws:ec2/vpnGateway:VpnGateway                              airflow-dev
-    # │  │     ID: vgw-02f10f1bacf2dd3fa
-
-    # │  ├─ aws:ec2/routeTable:RouteTable                              airflow-dev-public
-    # │  │  │  ID: rtb-0595299e5f01affbc
-    # │  │  ├─ aws:ec2/route:Route                                     airflow-dev-public
-    # │  │  │     ID: r-rtb-0595299e5f01affbc1080289494
-    # │  │  ├─ aws:ec2/routeTableAssociation:RouteTableAssociation     airflow-dev-public-eu-west-1a
-    # │  │  │     ID: rtbassoc-09ff6362679886379
-    # │  │  ├─ aws:ec2/routeTableAssociation:RouteTableAssociation     airflow-dev-public-eu-west-1b
-    # │  │  │     ID: rtbassoc-0fe50d22a9f4e62d6
-    # │  │  └─ aws:ec2/routeTableAssociation:RouteTableAssociation     airflow-dev-public-eu-west-1c
-    # │  │        ID: rtbassoc-0ddc9ee4346646e72
-
-    # │  ├─ aws:ec2/subnet:Subnet                                      airflow-dev-public-eu-west-1b
-    # │  │  │  ID: subnet-0d63aad6294936483
-    # │  │  └─ aws:ec2/natGateway:NatGateway                           airflow-dev-eu-west-1b
-    # │  │        ID: nat-073cf7877cabf937e
-    # │  ├─ aws:ec2/subnet:Subnet                                      airflow-dev-private-eu-west-1a
-    # │  │  │  ID: subnet-02c12ac78aa071d71
-    # │  │  └─ aws:ec2/routeTable:RouteTable                           airflow-dev-private-eu-west-1a
-    # │  │     │  ID: rtb-0253f5d2ff173c5fb
-    # │  │     ├─ aws:ec2/routeTableAssociation:RouteTableAssociation  airflow-dev-private-eu-west-1a
-    # │  │     │     ID: rtbassoc-0ef022e00cd23cafe
-    # │  │     ├─ aws:ec2/route:Route                                  airflow-dev-private-eu-west-1a-moj-noms-live
-    # │  │     │     ID: r-rtb-0253f5d2ff173c5fb3017512065
-    # │  │     ├─ aws:ec2/route:Route                                  airflow-dev-private-eu-west-1a-moj-modernisation-platform
-    # │  │     │     ID: r-rtb-0253f5d2ff173c5fb2015336497
-    # │  │     └─ aws:ec2/route:Route                                  airflow-dev-private-eu-west-1a
-    # │  │           ID: r-rtb-0253f5d2ff173c5fb1080289494
-    # │  ├─ aws:ec2/subnet:Subnet                                      airflow-dev-public-eu-west-1c
-    # │  │  │  ID: subnet-02a13fd4fd56ee6c5
-    # │  │  └─ aws:ec2/natGateway:NatGateway                           airflow-dev-eu-west-1c
-    # │  │        ID: nat-084c200fc4a3d033d
-    # │  ├─ aws:ec2/subnet:Subnet                                      airflow-dev-private-eu-west-1b
-    # │  │  │  ID: subnet-0984267608686a5ce
-    # │  │  └─ aws:ec2/routeTable:RouteTable                           airflow-dev-private-eu-west-1b
-    # │  │     │  ID: rtb-0c26df12e1ebf0f26
-    # │  │     ├─ aws:ec2/routeTableAssociation:RouteTableAssociation  airflow-dev-private-eu-west-1b
-    # │  │     │     ID: rtbassoc-01dfd6f923442daa3
-    # │  │     ├─ aws:ec2/route:Route                                  airflow-dev-private-eu-west-1b-moj-noms-live
-    # │  │     │     ID: r-rtb-0c26df12e1ebf0f263017512065
-    # │  │     ├─ aws:ec2/route:Route                                  airflow-dev-private-eu-west-1b-moj-modernisation-platform
-    # │  │     │     ID: r-rtb-0c26df12e1ebf0f262015336497
-    # │  │     └─ aws:ec2/route:Route                                  airflow-dev-private-eu-west-1b
-    # │  │           ID: r-rtb-0c26df12e1ebf0f261080289494
-    # │  ├─ aws:ec2/subnet:Subnet                                      airflow-dev-private-eu-west-1c
-    # │  │  │  ID: subnet-0f0f0e17d45e9791f
-    # │  │  └─ aws:ec2/routeTable:RouteTable                           airflow-dev-private-eu-west-1c
-    # │  │     │  ID: rtb-01ea0e0adfe7b734c
-    # │  │     ├─ aws:ec2/routeTableAssociation:RouteTableAssociation  airflow-dev-private-eu-west-1c
-    # │  │     │     ID: rtbassoc-05a22f993ab6f9f96
-    # │  │     ├─ aws:ec2/route:Route                                  airflow-dev-private-eu-west-1c-moj-noms-live
-    # │  │     │     ID: r-rtb-01ea0e0adfe7b734c3017512065
-    # │  │     ├─ aws:ec2/route:Route                                  airflow-dev-private-eu-west-1c-moj-modernisation-platform
-    # │  │     │     ID: r-rtb-01ea0e0adfe7b734c2015336497
-    # │  │     └─ aws:ec2/route:Route                                  airflow-dev-private-eu-west-1c
-    # │  │           ID: r-rtb-01ea0e0adfe7b734c1080289494
-
