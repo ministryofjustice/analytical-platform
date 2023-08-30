552a91dd9fc17abce494d9915bd51c950f6fdcb9
Murdo Moyse <murdo.moyse1@justice.gov.uk>
2023-08-30 14:00:56 +0100

    Added custom logger

--- containers/daap-recreate-data-product/src/var/task/recreate_data_product.py
+++ containers/daap-recreate-data-product/src/var/task/recreate_data_product.py
@@ -1,54 +1,76 @@
 import os
+import time
 
 import boto3
+from data_platform_logging import DataPlatformLogger
+
+s3 = boto3.client("s3")
+logger = DataPlatformLogger(
+    extra={
+        "image_version": os.getenv("VERSION", "unknown"),
+        "base_image_version": os.getenv("BASE_VERSION", "unknown"),
+    }
+)
+s3_security_opts = {
+    "ACL": "bucket-owner-full-control",
+    "ServerSideEncryption": "AES256",
+}
+raw_data_bucket = os.environ.get("RAW_DATA_BUCKET", "")
+curated_data_bucket = os.environ.get("CURATED_DATA_BUCKET", "")
+log_bucket = os.environ.get("RAW_DATA_BUCKET", "")
+athena_load_lambda = os.environ.get("ATHENA_LOAD_LAMBDA", "")
 
 
 def handler(event, context):
     data_product_to_recreate = event.get("data_product", "")
-    raw_data_bucket = os.environ.get("RAW_DATA_BUCKET", "")
-    curated_data_bucket = os.environ.get("CURATED_DATA_BUCKET", "")
-    athena_load_lambda = os.environ.get("ATHENA_LOAD_LAMBDA", "")
+    raw_prefix = f"raw_data/{data_product_to_recreate}"
+    logger.info(f"Data product to recreate: {data_product_to_recreate}")
+    logger.info(f"Raw prefix: {raw_prefix}")
+    logger.write_log_dict_to_s3_json(bucket=log_bucket, **s3_security_opts)
 
     # Check data product has associated data
-    s3 = boto3.client("s3")
-    s3_response = s3.list_objects_v2(
-        Bucket=raw_data_bucket, Prefix=f"raw_data/{data_product_to_recreate}"
+    data_product_contents = get_data_product_contents(
+        bucket=raw_data_bucket,
+        data_product_prefix=raw_prefix,
     )
-    data_product_registration = s3_response.get("Contents", [])
-    if not any(data_product_registration):
-        raise ValueError(f"No data product found for {data_product_to_recreate}")
-    print(data_product_registration)
+    print(data_product_contents)
 
     # Drop existing athena tables for that data product
     glue = boto3.client("glue")
     glue_response = glue.get_tables(DatabaseName=data_product_to_recreate)
     data_product_tables = glue_response.get("TableList", [])
     if not any(data_product_tables):
-        print(f"No tables found for data product {data_product_to_recreate}")
+        logger.info(f"No tables found for data product {data_product_to_recreate}")
     for table in data_product_tables:
         glue.delete_table(DatabaseName=data_product_to_recreate, Name=table["Name"])
-
+        logger.info(f"Deleted glue table {data_product_to_recreate}.{table}")
     # Remove curated data files for that data product
     s3_recursive_delete(
         bucket=curated_data_bucket,
         prefix=f"curated_data/database_name={data_product_to_recreate}/",
-        s3_client=s3,
     )
 
     # Feed all data files through the load process again. Curated files are recreated.
-    # If there are over 1000 files, the lambda will get jammed up
     aws_lambda = boto3.client("lambda")
-    for file in data_product_registration:
+    count = 0
+    for file in data_product_contents:
+        if count == 1000:
+            # Wait 5 mins for other invocations to finish
+            time.sleep(360)
+            count = 0
+        count += 1
+
         key = file["Key"]
         payload = f'{{"detail":{{"bucket":{{"name":"{raw_data_bucket}"}}, "object":{{"key":"{key}"}}}}}}'
         aws_lambda.invoke(
             FunctionName=athena_load_lambda, InvocationType="Event", Payload=payload
         )
 
-    print(f"data product {data_product_to_recreate} recreated")
+    logger.info(f"data product {data_product_to_recreate} recreated")
+    logger.write_log_dict_to_s3_json(bucket=log_bucket, **s3_security_opts)
 
 
-def s3_recursive_delete(bucket, prefix, s3_client) -> None:
+def s3_recursive_delete(bucket, prefix, s3_client=s3) -> None:
     """Delete all files from a prefix in s3"""
     paginator = s3_client.get_paginator("list_objects_v2")
     pages = paginator.paginate(Bucket=bucket, Prefix=prefix)
@@ -61,9 +83,22 @@ def s3_recursive_delete(bucket, prefix, s3_client) -> None:
         if len(delete_us["Objects"]) >= 1000:
             s3_client.delete_objects(Bucket=bucket, Delete=delete_us)
             delete_us = dict(Objects=[])
-            print(f"deleted 1000 data files from {prefix}")
+            logger.info(f"deleted 1000 data files from {prefix}")
 
     # delete remaining
     if len(delete_us["Objects"]):
         s3_client.delete_objects(Bucket=bucket, Delete=delete_us)
-        print(f"deleted all data files from {prefix}")
+        logger.info(f"deleted {len(delete_us)} data files from {prefix}")
+
+
+def get_data_product_contents(
+    bucket, data_product_prefix, s3_client=s3, log_bucket=log_bucket
+) -> list[dict]:
+    s3_response = s3_client.list_objects_v2(Bucket=bucket, Prefix=data_product_prefix)
+    data_product_contents = s3_response.get("Contents", [])
+    if not any(data_product_contents):
+        error_text = f"No data product found for {data_product_prefix}"
+        logger.error(error_text)
+        logger.write_log_dict_to_s3_json(bucket=log_bucket, **s3_security_opts)
+        raise ValueError(error_text)
+    return data_product_contents
--- containers/daap-resync-unprocessed-files/src/var/task/resync_unprocessed_files.py
+++ containers/daap-resync-unprocessed-files/src/var/task/resync_unprocessed_files.py
@@ -1,23 +1,39 @@
 import os
 
 import boto3
+from data_platform_logging import DataPlatformLogger
+
+s3 = boto3.client("s3")
+logger = DataPlatformLogger(
+    extra={
+        "image_version": os.getenv("VERSION", "unknown"),
+        "base_image_version": os.getenv("BASE_VERSION", "unknown"),
+    }
+)
+s3_security_opts = {
+    "ACL": "bucket-owner-full-control",
+    "ServerSideEncryption": "AES256",
+}
+raw_data_bucket = os.environ.get("RAW_DATA_BUCKET", "")
+curated_data_bucket = os.environ.get("CURATED_DATA_BUCKET", "")
+log_bucket = os.environ.get("RAW_DATA_BUCKET", "")
+athena_load_lambda = os.environ.get("ATHENA_LOAD_LAMBDA", "")
+s3 = boto3.client("s3")
 
 
 def handler(event, context):
     data_product_to_recreate = event.get("data_product", "")
     raw_prefix = f"raw_data/{data_product_to_recreate}"
     curated_prefix = f"curated_data/database_name={data_product_to_recreate}"
-    raw_data_bucket = os.environ.get("RAW_DATA_BUCKET", "")
-    curated_data_bucket = os.environ.get("CURATED_DATA_BUCKET", "")
-    athena_load_lambda = os.environ.get("ATHENA_LOAD_LAMBDA", "")
+    logger.info(f"Raw prefix: {raw_prefix}")
+    logger.info(f"Curated prefix: {curated_prefix}")
+    logger.write_log_dict_to_s3_json(bucket=log_bucket, **s3_security_opts)
 
     # Check data product has associated data
-    s3 = boto3.client("s3")
-    s3_response = s3.list_objects_v2(Bucket=raw_data_bucket, Prefix=raw_prefix)
-    data_product_registration = s3_response.get("Contents", [])
-    if not any(data_product_registration):
-        raise ValueError(f"No data product found for {data_product_to_recreate}")
-    print(data_product_registration)
+    data_product_contents = get_data_product_contents(
+        bucket=raw_data_bucket,
+        data_product_prefix=raw_prefix,
+    )
 
     # Find extraction timestamps in the raw area, and not in the curated area
     paginator = s3.get_paginator("list_objects_v2")
@@ -51,7 +67,7 @@ def handler(event, context):
         for item in raw_pages.search("Contents")
         if timestamp in item["Key"]
     ]
-    print("raw keys to resync:", raw_keys_to_resync)
+    logger.info(f"raw keys to resync: {raw_keys_to_resync}")
 
     # Feed unprocessed data files through the load process again.
     # If there are over 1000 files, the lambda will get jammed up
@@ -62,7 +78,21 @@ def handler(event, context):
             FunctionName=athena_load_lambda, InvocationType="Event", Payload=payload
         )
 
-    print(
+    logger.info(
         f"data product {data_product_to_recreate} resynced with {len(raw_keys_to_resync)} files"
     )
-    print(str(raw_keys_to_resync))
+    logger.info(str(raw_keys_to_resync))
+    logger.write_log_dict_to_s3_json(bucket=log_bucket, **s3_security_opts)
+
+
+def get_data_product_contents(
+    bucket, data_product_prefix, s3_client=s3, log_bucket=log_bucket
+) -> list[dict]:
+    s3_response = s3_client.list_objects_v2(Bucket=bucket, Prefix=data_product_prefix)
+    data_product_contents = s3_response.get("Contents", [])
+    if not any(data_product_contents):
+        error_text = f"No data product found for {data_product_prefix}"
+        logger.error(error_text)
+        logger.write_log_dict_to_s3_json(bucket=log_bucket, **s3_security_opts)
+        raise ValueError(error_text)
+    return data_product_contents
