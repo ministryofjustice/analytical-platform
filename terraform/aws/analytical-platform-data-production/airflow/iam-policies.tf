5bf9babe5510028732381e2e2a5c248fb0491c2c
murad-ali-MoJ <130611518+murad-ali-MoJ@users.noreply.github.com>
2023-08-31 12:04:27 +0100

    principals to resources
    Co-authored-by: julialawrence <98329494+julialawrence@users.noreply.github.com>

--- terraform/aws/analytical-platform-data-production/airflow/iam-policies.tf
+++ terraform/aws/analytical-platform-data-production/airflow/iam-policies.tf
@@ -199,13 +199,10 @@ data "aws_iam_policy_document" "airflow_dev_node_instance_inline_role_policy" {
   statement {
     sid    = ""
     effect = "Allow"
-    principals {
-      type = "Service"
-      identifiers = [
+      resources = [
         "arn:aws:iam::593291632749:role/airflow-dev-cluster-autoscaler-role",
         "arn:aws:iam::593291632749:role/airflow*"
       ]
-    }
     actions = ["sts:AssumeRole"]
   }
 
