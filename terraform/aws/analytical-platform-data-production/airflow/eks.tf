00102b3a826fcb8a7c2f44e0bbf3325644a009e1
Mitch Dawson <86007219+mitchdawson1982@users.noreply.github.com>
2023-08-31 16:44:16 +0100

    ⦂wrench⦂ Create airflow eks node groups (#1336)
    * :wrench: Create airflow eks node groups
    
    * terraform fmt

--- terraform/aws/analytical-platform-data-production/airflow/eks.tf
+++ terraform/aws/analytical-platform-data-production/airflow/eks.tf
@@ -24,3 +24,48 @@ output "endpoint" {
 output "kubeconfig_certificate_authority_data" {
   value = aws_eks_cluster.airflow_dev_eks_cluster.certificate_authority[0].data
 }
+
+resource "aws_eks_node_group" "dev_node_group_standard" {
+  cluster_name    = aws_eks_cluster.airflow_dev_eks_cluster.name
+  node_group_name = "standard"
+  node_role_arn   = aws_iam_role.airflow_dev_node_instance_role.arn
+  subnet_ids      = aws_subnet.private_subnet[*].id
+
+  scaling_config {
+    desired_size = 1
+    max_size     = 10
+    min_size     = 0
+  }
+
+  update_config {
+    max_unavailable = 1
+  }
+}
+
+resource "aws_eks_node_group" "dev_node_group_high_memory" {
+  cluster_name    = aws_eks_cluster.airflow_dev_eks_cluster.name
+  node_group_name = "high-memory"
+  node_role_arn   = aws_iam_role.airflow_dev_node_instance_role.arn
+  subnet_ids      = aws_subnet.private_subnet[*].id
+
+  scaling_config {
+    desired_size = 0
+    max_size     = 2
+    min_size     = 0
+  }
+
+  update_config {
+    max_unavailable = 1
+  }
+
+  taint {
+    key    = "high-memory"
+    value  = "true"
+    effect = "NO_SCHEDULE"
+  }
+
+  labels = {
+    high-memory = "true"
+  }
+}
+
