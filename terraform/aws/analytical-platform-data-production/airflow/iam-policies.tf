b6114e946093f1a1b8a6f1fd0f18a6cf2708c584
tamsinforbes <32667518+tamsinforbes@users.noreply.github.com>
2023-08-31 15:29:52 +0100

    update from main

--- terraform/aws/analytical-platform-data-production/airflow/iam-policies.tf
+++ terraform/aws/analytical-platform-data-production/airflow/iam-policies.tf
@@ -165,8 +165,6 @@ data "aws_iam_policy_document" "airflow_dev_flow_log_role_policy" {
 
 }
 
-
-
 data "aws_iam_policy_document" "airflow_dev_flow_log_assume_policy" {
   statement {
     sid    = ""
@@ -199,10 +197,10 @@ data "aws_iam_policy_document" "airflow_dev_node_instance_inline_role_policy" {
   statement {
     sid    = ""
     effect = "Allow"
-      resources = [
-        "arn:aws:iam::593291632749:role/airflow-dev-cluster-autoscaler-role",
-        "arn:aws:iam::593291632749:role/airflow*"
-      ]
+    resources = [
+      "arn:aws:iam::593291632749:role/airflow-dev-cluster-autoscaler-role",
+      "arn:aws:iam::593291632749:role/airflow*"
+    ]
     actions = ["sts:AssumeRole"]
   }
 
