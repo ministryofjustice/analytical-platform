561f504dc12ba7589e8354c668cc0ce6c8ba46c4
LavMatt <mattlaverty@gmail.com>
2023-09-04 12:22:02 +0100

    lambda handler for creating first version of data product metadata

---
+++ containers/daap-create-metadata/src/var/task/create_metadata.py
@@ -0,0 +1,64 @@
+import json
+import os
+
+from data_platform_logging import DataPlatformLogger
+from data_product_metadata import DataProductMetadata
+
+
+def handler(event, context):
+
+    logger = DataPlatformLogger(
+        extra={
+            "image_version": os.getenv("VERSION", "unknown"),
+            "base_image_version": os.getenv("BASE_VERSION", "unknown"),
+            "lambda_name": context.function_name,
+        }
+    )
+
+    try:
+        data_product_name = event["metadata"]["name"]
+    except KeyError:
+        logger.error("The name of the data product is missing")
+        return {
+            "statusCode": 400,
+            "headers": {"Content-Type": "application/json"},
+            "body": json.dumps({
+                "message": "The name of the data product is missing, it must be specified in the metadata against the 'name' key"
+            })
+        }
+
+    logger.add_extras({"data_product_name": data_product_name})
+
+    data_product_metadata = DataProductMetadata(data_product_name, logger)
+
+    if not data_product_metadata.metadata_exists:
+        data_product_metadata.validate_metadata(event["metadata"])
+    else:
+        return {
+            "statusCode": 400,
+            "headers": {"Content-Type": "application/json"},
+            "body": json.dumps({
+                "data_product_name": data_product_name,
+                "message": "Your data product already has a version 1 registered metadata"
+            }),
+        }
+
+    if data_product_metadata.valid_metadata:
+        data_product_metadata.write_metadata_json_to_s3()
+        return {
+            "statusCode": 200,
+            "headers": {"Content-Type": "application/json"},
+            "body": json.dumps({
+                "data_product_name": data_product_name,
+                "message": "Data product metadata has been created."
+            }),
+        }
+    else:
+        return {
+            "statusCode": 400,
+            "headers": {"Content-Type": "application/json"},
+            "body": json.dumps({
+                "data_product_name": data_product_name,
+                "message": f"Your metadata failed validation with this error: {data_product_metadata.error_traceback}"
+            }),
+        }
