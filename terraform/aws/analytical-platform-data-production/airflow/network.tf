f64496b9d8f4778ceb0f0d84aede0acf872cf8d4
Mitch Dawson <Mitchell.Dawson@justice.gov.uk>
2023-09-07 16:39:46 +0100

    prod subnet reference changes

--- terraform/aws/analytical-platform-data-production/airflow/network.tf
+++ terraform/aws/analytical-platform-data-production/airflow/network.tf
@@ -288,12 +288,12 @@ resource "aws_route_table" "airflow_prod_private" {
 
 resource "aws_route_table_association" "airflow_prod_private_route_table_assoc" {
   count          = length(var.azs)
-  subnet_id      = aws_subnet.private_subnet[count.index].id
+  subnet_id      = aws_subnet.private_subnet_prod[count.index].id
   route_table_id = aws_route_table.airflow_prod_private[count.index].id
 }
 
 resource "aws_ec2_transit_gateway_vpc_attachment" "airflow_prod_cloud_platform" {
-  subnet_ids         = aws_subnet.private_subnet[*].id
+  subnet_ids         = aws_subnet.private_subnet_prod[*].id
   transit_gateway_id = var.transit_gateway_ids["airflow-cloud-platform"]
   vpc_id             = aws_vpc.airflow_prod.id
   tags = {
@@ -302,7 +302,7 @@ resource "aws_ec2_transit_gateway_vpc_attachment" "airflow_prod_cloud_platform" 
 }
 
 resource "aws_ec2_transit_gateway_vpc_attachment" "airflow_prod_moj" {
-  subnet_ids         = aws_subnet.private_subnet[*].id
+  subnet_ids         = aws_subnet.private_subnet_prod[*].id
   transit_gateway_id = var.transit_gateway_ids["airflow-moj"]
   vpc_id             = aws_vpc.airflow_prod.id
 
