d247f123243a15f4e8c2fcc21b78b219226a71af
Richard Baguley <20583399+bagg3rs@users.noreply.github.com>
2023-09-11 14:49:20 +0100

    Update GH teams
    Add
    - Fabien
    Remove 
    - Amy

--- terraform/github/data-platform/teams.tf
+++ terraform/github/data-platform/teams.tf
@@ -22,11 +22,11 @@ locals {
     "SimonsMOJ",      # Simon Heron
     "mshodge",        # Michael Hodges
     "YvanMOJdigital", # Yvan Smith
-    "amycufflin",
-    "ChikC",        # Chike Chinukwue
-    "EO510",        # Eki Osehenye
-    "Braimah-L",    # Braimah Lat
-    "imkatiewatson" # Katie Watson
+    "ChikC",          # Chike Chinukwue
+    "EO510",          # Eki Osehenye
+    "Braimah-L",      # Braimah Lat
+    "imkatiewatson",  # Katie Watson
+    "f-marry"         # Fabien Marry
   ]
 
   # GitHub usernames for engineers who need full AWS access
