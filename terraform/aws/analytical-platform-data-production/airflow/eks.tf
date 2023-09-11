8030b1592a03195f6b19f55822b70174993afa5e
Gary H <26419401+Gary-H9@users.noreply.github.com>
2023-09-11 12:31:29 +0100

    :wrench: Add prod EKS configuration

--- terraform/aws/analytical-platform-data-production/airflow/eks.tf
+++ terraform/aws/analytical-platform-data-production/airflow/eks.tf
@@ -10,12 +10,11 @@ resource "aws_eks_cluster" "airflow_dev_eks_cluster" {
   version = "1.24"
 
   vpc_config {
-    subnet_ids          = aws_subnet.private_subnet[*].id
+    subnet_ids          = aws_subnet.dev_private_subnet[*].id
     public_access_cidrs = ["0.0.0.0/0"]
     security_group_ids  = ["sg-0bcd3cf5dc6d7b314"]
   }
 }
-
 
 output "endpoint" {
   value = aws_eks_cluster.airflow_dev_eks_cluster.endpoint
@@ -29,7 +28,7 @@ resource "aws_eks_node_group" "dev_node_group_standard" {
   cluster_name    = aws_eks_cluster.airflow_dev_eks_cluster.name
   node_group_name = "standard"
   node_role_arn   = aws_iam_role.airflow_dev_node_instance_role.arn
-  subnet_ids      = aws_subnet.private_subnet[*].id
+  subnet_ids      = aws_subnet.dev_private_subnet[*].id
 
   scaling_config {
     desired_size = 1
@@ -46,11 +45,85 @@ resource "aws_eks_node_group" "dev_node_group_high_memory" {
   cluster_name    = aws_eks_cluster.airflow_dev_eks_cluster.name
   node_group_name = "high-memory"
   node_role_arn   = aws_iam_role.airflow_dev_node_instance_role.arn
-  subnet_ids      = aws_subnet.private_subnet[*].id
+  subnet_ids      = aws_subnet.dev_private_subnet[*].id
 
   scaling_config {
     desired_size = 0
     max_size     = 2
+    min_size     = 0
+  }
+
+  update_config {
+    max_unavailable = 1
+  }
+
+  taint {
+    key    = "high-memory"
+    value  = "true"
+    effect = "NO_SCHEDULE"
+  }
+
+  labels = {
+    high-memory = "true"
+  }
+}
+
+######################################
+########### EKS PRODUCTION ###########
+######################################
+
+resource "aws_eks_cluster" "airflow_prod_eks_cluster" {
+  name     = "airflow-prod"
+  role_arn = var.prod_eks_role_arn
+  enabled_cluster_log_types = ["api",
+    "audit",
+    "authenticator",
+    "controllerManager",
+    "scheduler",
+  ]
+  version = "1.24"
+
+  vpc_config {
+    subnet_ids          = aws_subnet.prod_private_subnet[*].id
+    public_access_cidrs = ["0.0.0.0/0"]
+    security_group_ids  = ["sg-0f73e78564012634a"]
+  }
+}
+
+output "prod_endpoint" {
+  value = aws_eks_cluster.airflow_prod_eks_cluster.endpoint
+}
+
+output "prod_kubeconfig_certificate_authority_data" {
+  value = aws_eks_cluster.airflow_prod_eks_cluster.certificate_authority[0].data
+}
+
+resource "aws_eks_node_group" "prod_node_group_standard" {
+  cluster_name    = aws_eks_cluster.airflow_prod_eks_cluster.name
+  node_group_name = "standard"
+  node_role_arn   = aws_iam_role.airflow_prod_node_instance_role.arn
+  subnet_ids      = aws_subnet.prod_private_subnet[*].id
+
+  scaling_config {
+    desired_size = 1
+    max_size     = 25
+    min_size     = 1
+  }
+
+  update_config {
+    max_unavailable = 1
+  }
+}
+
+resource "aws_eks_node_group" "prod_node_group_high_memory" {
+  cluster_name    = aws_eks_cluster.airflow_prod_eks_cluster.name
+  node_group_name = "high-memory"
+  node_role_arn   = aws_iam_role.airflow_prod_node_instance_role.arn
+  subnet_ids      = aws_subnet.prod_private_subnet[*].id
+
+  scaling_config {
+    desired_size = 1
+    max_size     = 1
     min_size     = 0
   }
 
