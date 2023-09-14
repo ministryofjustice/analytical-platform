c99ff6040536ba05fff5c3f4b95220ddfc534fbe
Tom Webber <thomas.webber@digital.justice.gov.uk>
2023-09-14 17:19:54 +0100

    move providers to terraform.tf

--- terraform/aws/analytical-platform-data-production/airflow/terraform.tf
+++ terraform/aws/analytical-platform-data-production/airflow/terraform.tf
@@ -19,3 +19,57 @@ terraform {
   }
   required_version = "~> 1.5"
 }
+
+provider "aws" {
+  alias = "session"
+}
+
+provider "aws" {
+  region = "eu-west-1"
+  assume_role {
+    role_arn = "arn:aws:iam::${var.account_ids["analytical-platform-data-production"]}:role/GlobalGitHubActionAdmin"
+  }
+  default_tags {
+    tags = var.tags
+  }
+}
+
+provider "aws" {
+  alias  = "analytical-platform-development"
+  region = "eu-west-1"
+  assume_role {
+    role_arn = "arn:aws:iam::${var.account_ids["analytical-platform-development"]}:role/GlobalGitHubActionAdmin"
+  }
+  default_tags {
+    tags = var.tags
+  }
+}
+
+provider "aws" {
+  alias  = "analytical-platform-management-production"
+  region = "eu-west-1"
+  assume_role {
+    role_arn = can(regex("AdministratorAccess", data.aws_iam_session_context.session.issuer_arn)) ? null : "arn:aws:iam::${var.account_ids["analytical-platform-management-production"]}:role/GlobalGitHubActionAdmin"
+  }
+  default_tags {
+    tags = var.tags
+  }
+}
+
+provider "kubernetes" {
+  alias                  = "dev-airflow-cluster"
+  host                   = aws_eks_cluster.airflow_dev_eks_cluster.endpoint
+  cluster_ca_certificate = base64decode(aws_eks_cluster.airflow_dev_eks_cluster.certificate_authority[0].data)
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    args = [
+      "eks",
+      "get-token",
+      "--cluster-name",
+      aws_eks_cluster.airflow_dev_eks_cluster.name,
+      "--role-arn",
+      "arn:aws:iam::${var.account_ids["analytical-platform-data-production"]}:role/restricted-admin"
+    ]
+    command = "aws"
+  }
+}
