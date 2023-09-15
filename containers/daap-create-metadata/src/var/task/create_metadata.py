4ff82a0d53bfad5dfba86ad98beee6892a5633fa
LavMatt <mattlaverty@gmail.com>
2023-09-15 14:48:51 +0100

    updated logger in daap-create-metadata

--- containers/daap-create-metadata/src/var/task/create_metadata.py
+++ containers/daap-create-metadata/src/var/task/create_metadata.py
@@ -10,14 +10,8 @@ def handler(event, context):
         extra={
             "image_version": os.getenv("VERSION", "unknown"),
             "base_image_version": os.getenv("BASE_VERSION", "unknown"),
-            "lambda_name": context.function_name,
         }
     )
-
-    s3_security_opts = {
-        "ACL": "bucket-owner-full-control",
-        "ServerSideEncryption": "AES256",
-    }
 
     logger.info(f"event: {event}")
 
@@ -27,14 +21,12 @@ def handler(event, context):
         data_product_name = request_body["metadata"]["name"]
     except KeyError:
         logger.error("The name of the data product is missing from the metadata.")
-        logger.write_log_dict_to_s3_json(os.environ["BUCKET_NAME"], **s3_security_opts)
         return {
             "statusCode": 400,
             "headers": {"Content-Type": "application/json"},
             "body": json.dumps(
                 {
-                    "message": "Data product name is missing, it must be specified \
-                        in the metadata against the 'name' key"
+                    "message": "Data product name is missing, it must be specified in the metadata against the 'name' key" # noqa E501
                 }
             ),
         }
@@ -46,9 +38,6 @@ def handler(event, context):
     if not data_product_metadata.metadata_exists:
         data_product_metadata.validate(request_body["metadata"])
     else:
-        data_product_metadata.logger.write_log_dict_to_s3_json(
-            os.environ["BUCKET_NAME"], **s3_security_opts
-        )
         return {
             "statusCode": 400,
             "headers": {"Content-Type": "application/json"},
@@ -62,9 +51,6 @@ def handler(event, context):
 
     if data_product_metadata.valid_metadata:
         data_product_metadata.write_json_to_s3()
-        data_product_metadata.logger.write_log_dict_to_s3_json(
-            os.environ["BUCKET_NAME"], **s3_security_opts
-        )
         return {
             "statusCode": 200,
             "headers": {"Content-Type": "application/json"},
@@ -76,17 +62,13 @@ def handler(event, context):
             ),
         }
     else:
-        data_product_metadata.logger.write_log_dict_to_s3_json(
-            os.environ["BUCKET_NAME"], **s3_security_opts
-        )
         return {
             "statusCode": 400,
             "headers": {"Content-Type": "application/json"},
             "body": json.dumps(
                 {
                     "data_product_name": data_product_name,
-                    "message": f"Your metadata failed validation with \
-                        this error: {data_product_metadata.error_traceback}",
+                    "message": f"Your metadata failed validation with this error: {data_product_metadata.error_traceback}", # noqa E501
                 }
             ),
         }
