a38986b26fbf046511364d3af7e70fb2dbe193e9
Jacob Woffenden <jacob.woffenden@digital.justice.gov.uk>
2023-09-19 12:13:07 +0000

    Add users
    Signed-off-by: Jacob Woffenden <jacob.woffenden@digital.justice.gov.uk>

--- terraform/github/data-platform/teams.tf
+++ terraform/github/data-platform/teams.tf
@@ -19,14 +19,17 @@ locals {
   general_members = [ # members of analytics-hq gh team
     "jacobwoffenden",
     "calumabarnett",
-    "SimonsMOJ",      # Simon Heron
-    "mshodge",        # Michael Hodges
-    "YvanMOJdigital", # Yvan Smith
-    "ChikC",          # Chike Chinukwue
-    "EO510",          # Eki Osehenye
-    "Braimah-L",      # Braimah Lat
-    "imkatiewatson",  # Katie Watson
-    "f-marry"         # Fabien Marry
+    "SimonsMOJ",        # Simon Heron
+    "mshodge",          # Michael Hodges
+    "YvanMOJdigital",   # Yvan Smith
+    "ChikC",            # Chike Chinukwue
+    "EO510",            # Eki Osehenye
+    "Braimah-L",        # Braimah Lat
+    "imkatiewatson",    # Katie Watson
+    "f-marry",          # Fabien Marry
+    "alex-vonfeldmann", # Alex von Feldmann
+    "gfowler-moj",      # Greg Fowler
+    "RNTjustice"        # Richard Trist
   ]
 
   # GitHub usernames for engineers who need full AWS access
