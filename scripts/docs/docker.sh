88be8c2f48c2bdb85ecd45eb988d8999a1e83a75
Jacob Woffenden <jacob.woffenden@digital.justice.gov.uk>
2023-09-21 11:47:00 +0100

    Update image to lts/hydrogen Test build workflow
    Signed-off-by: Jacob Woffenden <jacob.woffenden@digital.justice.gov.uk>

--- scripts/docs/docker.sh
+++ scripts/docs/docker.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 
 docker run -it --rm \
+  --name docs-serve \
   --volume "${PWD}"/docs:/docs \
   --volume "${PWD}"/scripts/docs/serve.sh:/usr/local/bin/serve.sh \
   --workdir /docs \
   --publish 8080:8080 \
   --entrypoint /bin/bash \
   --user 1000:1000 \
-  public.ecr.aws/docker/library/node:lts-bookworm \
+  public.ecr.aws/docker/library/node:hydrogen-bookworm \
   /usr/local/bin/serve.sh
