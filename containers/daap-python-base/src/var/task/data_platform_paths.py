ca8b4dbf014193bdcceb4eb460135db0627c2b42
priya.basker <shanmugapriya.basker@digital.justice.gov.uk>
2023-09-22 14:10:00 +0100

    Moved some functions to base image

--- containers/daap-python-base/src/var/task/data_platform_paths.py
+++ containers/daap-python-base/src/var/task/data_platform_paths.py
@@ -108,6 +108,25 @@ def get_account_id() -> str:
     """
     return boto3.client("sts").get_caller_identity()["Account"]
 
+def search_string_for_regex(string: str, regex: str) -> str:
+    """Search a string for a regex pattern and return the first result"""
+    database_name_search = re.search(regex, string)
+    if database_name_search:
+        return database_name_search.groups()[0]
+    else:
+        raise ValueError(f"{regex} not found in {string}")
+
+
+def database_name_regex() -> str:
+    return """database_name=([^\/]*)\/"""  # noqa: W605
+
+
+def table_name_regex() -> str:
+    return """table_name=([^\/]*)\/"""  # noqa: W605
+
+
+def extraction_timestamp_regex() -> str:
+    return """(extraction_timestamp=[^\/]*)\/"""  # noqa: W605
 
 @dataclass
 class DataProductElement:
