73e09e4b40c11774837fd0eef2f8a7c42415f643
Mat Moore <MatMoore@users.noreply.github.com>
2023-09-26 16:05:18 +0100

    Ensure raw athena table is deleted on error
    Since we now use a unique name for this table, we should be careful to
    always delete it, otherwise we will have resources accumulate in AWS.

--- containers/daap-athena-load/src/var/task/athena_load_handler.py
+++ containers/daap-athena-load/src/var/task/athena_load_handler.py
@@ -2,7 +2,7 @@ import os
 
 import boto3
 from create_curated_athena_table import create_curated_athena_table
-from create_raw_athena_table import create_raw_athena_table
+from create_raw_athena_table import temporary_raw_athena_table
 from data_platform_logging import DataPlatformLogger
 from data_platform_paths import QueryTable, RawDataExtraction
 from infer_glue_schema import infer_glue_schema_from_raw_csv
@@ -47,27 +47,24 @@ def handler(
     logger.info(f"{extraction.timestamp=} {data_product_element.curated_data_table=}")
 
     # Create a table of all string-type columns, to load raw data into
-    create_raw_athena_table(
-        metadata_glue=inferred_metadata.metadata_str,
+    with temporary_raw_athena_table(
+        metadata=inferred_metadata,
         logger=logger,
         glue_client=glue_client,
-    )
-
-    # Load the raw string data into the raw tables
-    # Create a curated table with proper datatypes if it doesn't exist
-    # Create a curated parquet file from the raw file
-    # Add a timestamp and insert raw data to the curated table, casting to type
-    create_curated_athena_table(
-        data_product_element=data_product_element,
-        raw_data_table=QueryTable(database=temp_database_name, name=temp_table_name),
-        extraction_timestamp=extraction.timestamp.strftime("%Y%m%dT%H%M%SZ"),
-        metadata=inferred_metadata.metadata,
-        logger=logger,
-        glue_client=glue_client,
-        s3_client=s3_client,
-        athena_client=athena_client,
-    )
-
-    # Delete the raw string tables, which are just used as an intermediary
-    glue_client.delete_table(DatabaseName=temp_database_name, Name=temp_table_name)
-    logger.info(f"removed raw table data_products_raw.{temp_table_name}")
+    ):
+        # Load the raw string data into the raw tables
+        # Create a curated table with proper datatypes if it doesn't exist
+        # Create a curated parquet file from the raw file
+        # Add a timestamp and insert raw data to the curated table, casting to type
+        create_curated_athena_table(
+            data_product_element=data_product_element,
+            raw_data_table=QueryTable(
+                database=temp_database_name, name=temp_table_name
+            ),
+            extraction_timestamp=extraction.timestamp.strftime("%Y%m%dT%H%M%SZ"),
+            metadata=inferred_metadata.metadata,
+            logger=logger,
+            glue_client=glue_client,
+            s3_client=s3_client,
+            athena_client=athena_client,
+        )
