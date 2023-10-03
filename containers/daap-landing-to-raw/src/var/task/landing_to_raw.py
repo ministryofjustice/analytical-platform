f890465e83d6c7e3f305c8ece0951615d043aa3c
Murdo Moyse <murdo.moyse1@justice.gov.uk>
2023-10-03 11:17:19 +0100

    Added landing to raw container

---
+++ containers/daap-landing-to-raw/src/var/task/landing_to_raw.py
@@ -0,0 +1,34 @@
+import os
+
+import boto3
+from data_platform_logging import DataPlatformLogger, s3_security_opts
+from data_platform_paths import get_raw_data_bucket
+
+logger = DataPlatformLogger(
+    extra={
+        "image_version": os.getenv("VERSION", "unknown"),
+        "base_image_version": os.getenv("BASE_VERSION", "unknown"),
+    }
+)
+
+s3 = boto3.client("s3")
+
+
+def handler(event, context):
+    raw_data_bucket = get_raw_data_bucket()
+    bucket_name = event["detail"]["bucket"]["name"]
+    file_key = event["detail"]["object"]["key"]
+    copy_source = {"Bucket": bucket_name, "Key": file_key}
+    destination_key = file_key.replace("landing/", "raw/")
+
+    logger.info(f"Origin bucket: {bucket_name}")
+    logger.info(f"Origin key: {file_key}")
+    logger.info(f"Destination bucket: {raw_data_bucket}")
+    logger.info(f"Destination key: {destination_key}")
+
+    s3.copy(
+        CopySource=copy_source,
+        Bucket=raw_data_bucket,
+        Key=destination_key,
+        ExtraArgs=s3_security_opts,
+    )
