cb35406b340c659abb4cabdda216022aebee4828
Tom Webber <thomas.webber@digital.justice.gov.uk>
2023-10-06 10:31:46 +0100

    add event fixtures to avoid repetition add method parameterise to method_fail test

--- containers/daap-create-metadata/tests/unit/create_metadata_test.py
+++ containers/daap-create-metadata/tests/unit/create_metadata_test.py
@@ -2,51 +2,47 @@ import json
 from unittest.mock import patch
 
 import create_metadata
+import pytest
 
 
-def test_missing_metadata_name_fail(fake_context):
-    response = create_metadata.handler(
-        {"httpMethod": "POST", "body": """{"metadata": {"domain": "MoJ"}}"""},
-        context=fake_context,
-    )
+@pytest.mark.parametrize("body_content", [{"metadata": {"domain": "MoJ"}}])
+def test_missing_metadata_name_fail(fake_event, fake_context):
+    response = create_metadata.handler(event=fake_event, context=fake_context)
+
     assert response["statusCode"] == 400
 
 
-def test_existing_metadata_definition_fail(fake_context):
+def test_existing_metadata_definition_fail(fake_event, fake_context):
     with patch("create_metadata.DataProductMetadata") as mock_metadata:
         mock_metadata.return_value.metadata_exists = True
-        mock_metadata.return_value.valid_metadata = None
-        response = create_metadata.handler(
-            {"httpMethod": "POST", "body": """{"metadata": {"name": "test"}}"""},
-            context=fake_context,
-        )
+
+        response = create_metadata.handler(event=fake_event, context=fake_context)
+
         assert response["statusCode"] == 409
         assert (
             json.loads(response["body"])["error"]["message"]
-            == "Data Product test already has a version 1 registered metadata."
+            == "Data Product test_name already has a version 1 registered metadata."
         )
 
 
-def test_metadata_creation_pass(fake_context):
+def test_metadata_creation_pass(fake_event, fake_context):
     with patch("create_metadata.DataProductMetadata") as mock_metadata:
         mock_metadata.return_value.metadata_exists = False
         mock_metadata.return_value.valid_metadata = True
-        response = create_metadata.handler(
-            {"httpMethod": "POST", "body": """{"metadata": {"name": "test"}}"""},
-            context=fake_context,
-        )
+
+        response = create_metadata.handler(event=fake_event, context=fake_context)
+
         assert response["statusCode"] == 201
 
 
-def test_metadata_validation_fail(fake_context):
+def test_metadata_validation_fail(fake_event, fake_context):
     with patch("create_metadata.DataProductMetadata") as mock_metadata:
         mock_metadata.return_value.metadata_exists = False
         mock_metadata.return_value.valid_metadata = False
         mock_metadata.return_value.error_traceback = "testing"
-        response = create_metadata.handler(
-            {"httpMethod": "POST", "body": """{"metadata": {"name": "test"}}"""},
-            context=fake_context,
-        )
+
+        response = create_metadata.handler(event=fake_event, context=fake_context)
+
         assert response["statusCode"] == 400
         assert (
             json.loads(response["body"])["error"]["message"]
@@ -54,16 +50,16 @@ def test_metadata_validation_fail(fake_context):
         )
 
 
-def test_http_method_fail(fake_context):
+@pytest.mark.parametrize("method", ["GET", "PUT", "DELETE"])
+def test_http_method_fail(fake_event, fake_context, method):
     with patch("create_metadata.DataProductMetadata") as mock_metadata:
         mock_metadata.return_value.metadata_exists = False
         mock_metadata.return_value.valid_metadata = True
-        response = create_metadata.handler(
-            {"httpMethod": "GET", "body": """{"metadata": {"name": "test"}}"""},
-            context=fake_context,
-        )
+
+        response = create_metadata.handler(event=fake_event, context=fake_context)
+
         assert response["statusCode"] == 405
         assert (
             json.loads(response["body"])["error"]["message"]
-            == "Sorry, GET isn't allowed."
+            == f"Sorry, {method} isn't allowed."
         )
