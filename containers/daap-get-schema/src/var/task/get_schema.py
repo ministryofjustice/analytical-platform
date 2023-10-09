e3f4020c9563452c897338273e02ff6804ee5c41
Mat Moore <MatMoore@users.noreply.github.com>
2023-10-09 15:32:56 +0100

    Inline logger and pass data product and table

--- containers/daap-get-schema/src/var/task/get_schema.py
+++ containers/daap-get-schema/src/var/task/get_schema.py
@@ -7,17 +7,19 @@ from data_platform_logging import DataPlatformLogger
 from data_platform_paths import DataProductConfig
 from dataengineeringutils3.s3 import read_json_from_s3
 
-logger = DataPlatformLogger(
-    extra={
-        "image_version": os.getenv("VERSION", "unknown"),
-        "base_image_version": os.getenv("BASE_VERSION", "unknown"),
-    }
-)
-
 
 def handler(event, context):
     data_product_name = event["pathParameters"]["data-product-name"]
     table_name = event["pathParameters"]["table-name"]
+
+    logger = DataPlatformLogger(
+        data_product_name=data_product_name,
+        extra={
+            "image_version": os.getenv("VERSION", "unknown"),
+            "base_image_version": os.getenv("BASE_VERSION", "unknown"),
+            "table_name": table_name,
+        },
+    )
 
     config = DataProductConfig(data_product_name)
     schema_path = config.schema_path(table_name)
@@ -26,9 +28,9 @@ def handler(event, context):
         registered_schema = read_json_from_s3(schema_path.uri)
     except botocore.exceptions.ClientError as e:
         if e.response["Error"]["Code"] == "NoSuchKey":
-            return response_status_404(
-                f"Schema not found for data product '{data_product_name}', table '{table_name}'"
-            )
+            message = f"Schema not found for data product '{data_product_name}', table '{table_name}'"
+            logger.error(f"{message} ({schema_path.uri})")
+            return response_status_404(message)
         else:
             raise
 
