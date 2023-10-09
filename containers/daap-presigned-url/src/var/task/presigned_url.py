6cca46f65a9f026b964c76b21a5e61b1fe6ee376
Mitch Dawson <Mitchell.Dawson@justice.gov.uk>
2023-10-09 14:27:55 +0100

    :bug: Deserialise event body and extract md5 content

--- containers/daap-presigned-url/src/var/task/presigned_url.py
+++ containers/daap-presigned-url/src/var/task/presigned_url.py
@@ -20,7 +20,8 @@ logger = DataPlatformLogger(
 def handler(event, context):
     data_product_name = event["pathParameters"].get("data-product-name")
     table_name = event["pathParameters"].get("table-name")
-    md5 = str(event["body"].get("contentMD5"))
+    body = json.loads(event.get("body"))
+    md5 = str(body.get("contentMD5"))
     amz_date = datetime.utcnow()
     formatted_date = amz_date.strftime("%Y%m%dT%H%M%SZ")
     uuid_value = uuid.uuid4()
