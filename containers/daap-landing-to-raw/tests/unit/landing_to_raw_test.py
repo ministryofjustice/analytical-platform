01f66f8a1e39b91a322a38d098e5d1bbe8370317
Mat <MatMoore@users.noreply.github.com>
2023-10-12 15:34:40 +0100

    DPL-1655 Add schema validation to landing bucket -> raw lambda (#1787)
    * Add type_is_compatable function
    
    This function implements lenient comparisons of glue types.
    
    * Add validate_data_against_schema function
    
    * Add extract_columns_from_schema function
    
    * Run the validation and move to the fail location if invalid
    
    * Split large module
    
    * Bump version
    
    * Handle the case where there is no schema
    
    In this case - do not attempt to validate
    
    * Fix typo
    
    * Add extra case to validation to give more useful messages

--- containers/daap-landing-to-raw/tests/unit/landing_to_raw_test.py
+++ containers/daap-landing-to-raw/tests/unit/landing_to_raw_test.py
@@ -1,22 +1,58 @@
 from unittest.mock import patch
 
-from landing_to_raw import handler
+import pytest
+from botocore.exceptions import ClientError
+from landing_to_raw import extract_columns_from_schema, handler
 
 
-def test_handler(s3_client, fake_context):
-    test_event = {
-        "detail": {
-            "bucket": {"name": "landing"},
-            "object": {
-                "key": "landing/data-product/v1.0/table/load_timestamp=20150210T130000Z/data.csv"
-            },
+class TestHandler:
+    @pytest.fixture
+    def destination_bucket(self):
+        return "test"
+
+    @pytest.fixture
+    def landing_bucket(self):
+        return "landing"
+
+    @pytest.fixture(autouse=True)
+    def setup_buckets(
+        self, s3_client, destination_bucket, landing_bucket, autouse=True
+    ):
+        with patch("landing_to_raw.s3", s3_client):
+            s3_client.create_bucket(Bucket=landing_bucket)
+            s3_client.create_bucket(Bucket=destination_bucket)
+            yield
+
+    @pytest.fixture(autouse=True)
+    def setup_registered_schema(self):
+        result = {
+            "TableInput": {
+                "StorageDescriptor": {
+                    "Columns": [
+                        {"Name": "a", "Type": "integer"},
+                        {"Name": "b", "Type": "integer"},
+                        {"Name": "c", "Type": "integer"},
+                    ]
+                }
+            }
         }
-    }
-    destination_bucket = "test"
 
-    with patch("landing_to_raw.s3", s3_client):
-        s3_client.create_bucket(Bucket=test_event["detail"]["bucket"]["name"])
-        s3_client.create_bucket(Bucket=destination_bucket)
+        with patch("landing_to_raw.read_json_from_s3") as f:
+            f.return_value = result
+            yield
+
+    def test_valid_file(
+        self, s3_client, destination_bucket, landing_bucket, fake_context
+    ):
+        test_event = {
+            "detail": {
+                "bucket": {"name": landing_bucket},
+                "object": {
+                    "key": "landing/data-product/v1.0/table/load_timestamp=20150210T130000Z/data.csv"
+                },
+            }
+        }
+
         s3_client.put_object(
             Key=test_event["detail"]["object"]["key"],
             Bucket=test_event["detail"]["bucket"]["name"],
@@ -28,3 +64,61 @@ def test_handler(s3_client, fake_context):
             Key="raw/data-product/v1.0/table/load_timestamp=20150210T130000Z/data.csv",
             Bucket=destination_bucket,
         )
+
+        with pytest.raises(ClientError):
+            s3_client.get_object(
+                Key="fail/data-product/v1.0/table/load_timestamp=20150210T130000Z/data.csv",
+                Bucket=destination_bucket,
+            )
+
+    def test_invalid_file(
+        self, s3_client, destination_bucket, landing_bucket, fake_context
+    ):
+        test_event = {
+            "detail": {
+                "bucket": {"name": landing_bucket},
+                "object": {
+                    "key": "landing/data-product/v1.0/table/load_timestamp=20150210T130000Z/data.csv"
+                },
+            }
+        }
+
+        s3_client.put_object(
+            Key=test_event["detail"]["object"]["key"],
+            Bucket=test_event["detail"]["bucket"]["name"],
+            Body="d,b,c\n1,2,3\n4,5,6",
+        )
+        handler(test_event, fake_context)
+
+        assert s3_client.get_object(
+            Key="fail/data-product/v1.0/table/load_timestamp=20150210T130000Z/data.csv",
+            Bucket=destination_bucket,
+        )
+
+        with pytest.raises(ClientError):
+            s3_client.get_object(
+                Key="raw/data-product/v1.0/table/load_timestamp=20150210T130000Z/data.csv",
+                Bucket=destination_bucket,
+            )
+
+
+class TestExtractColumnsFromSchema:
+    def test_valid(self):
+        columns = extract_columns_from_schema(
+            {
+                "TableInput": {
+                    "StorageDescriptor": {
+                        "Columns": [
+                            {"Name": "foo", "Type": "string"},
+                            {"Name": "bar", "Type": "int"},
+                        ]
+                    }
+                }
+            }
+        )
+
+        assert columns == {"foo": "string", "bar": "int"}
+
+    def test_invalid(self):
+        with pytest.raises(ValueError):
+            extract_columns_from_schema({})
