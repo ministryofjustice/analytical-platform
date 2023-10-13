b8908afc75c131a382f880a53f95d70b1fe46158
Matt <38562764+LavMatt@users.noreply.github.com>
2023-10-13 08:48:36 +0100

    create_schema lambda handler (#1873)
    * create_schema lambda handler
    
    * get correct paths
    
    * unit tests
    
    * reqs and that
    
    * dockerfile config changelog
    
    * lint
    
    * update test
    
    * no need to double load parent metadata
    
    * lint
    
    * remove stray commented code and duplicate test code

---
+++ containers/daap-create-schema/src/var/task/create_schema.py
@@ -0,0 +1,153 @@
+import json
+import os
+from http import HTTPMethod
+
+import boto3
+from data_platform_api_responses import (
+    response_status_200,
+    response_status_400,
+    response_status_403,
+)
+from data_platform_logging import DataPlatformLogger, s3_security_opts
+from data_platform_paths import DataProductConfig, get_latest_version, get_new_version
+from data_product_metadata import DataProductMetadata, DataProductSchema
+
+s3_client = boto3.client("s3")
+
+
+def s3_copy_folder_to_new_folder(
+    bucket, source_folder, latest_version, new_version, logger
+):
+    paginator = s3_client.get_paginator("list_objects_v2")
+    page_iterator = paginator.paginate(
+        Bucket=bucket,
+        Prefix=source_folder,
+    )
+    keys_to_copy = []
+    try:
+        for page in page_iterator:
+            keys_to_copy += [item["Key"] for item in page["Contents"]]
+    except KeyError as e:
+        logger.error(f"metadata for folder is empty but shouldn't be: {e}")
+    for key in keys_to_copy:
+        copy_source = {"Bucket": bucket, "Key": key}
+        destination_key = key.replace(latest_version, new_version)
+        s3_client.copy(
+            CopySource=copy_source,
+            Bucket=bucket,
+            Key=destination_key,
+            ExtraArgs=s3_security_opts,
+        )
+
+
+def handler(event, context):
+    data_product_name = event["pathParameters"].get("data-product-name")
+    table_name = event["pathParameters"].get("table-name")
+    logger = DataPlatformLogger(
+        extra={
+            "image_version": os.getenv("VERSION", "unknown"),
+            "base_image_version": os.getenv("BASE_VERSION", "unknown"),
+            "data_product_name": data_product_name,
+            "table_name": table_name,
+        }
+    )
+
+    logger.info(f"event: {event}")
+
+    request_body = json.loads(event.get("body"))
+
+    http_method = event.get("httpMethod")
+
+    if not http_method == HTTPMethod.POST:
+        error_message = f"Sorry, {http_method} isn't allowed."
+        logger.error(f"error message: {error_message}, input: {event}")
+        return response_status_400(error_message)
+
+    schema = DataProductSchema(
+        data_product_name=data_product_name,
+        table_name=table_name,
+        logger=logger,
+        input_data=request_body.get("schema"),
+    )
+
+    if schema.exists:
+        error_msg = (
+            f"v1 of this schema for table {table_name} already exists. You can upversion this schema if "
+            "there are changes from v1 using the PUT method of this endpoint. Or if this is a different "
+            "table then please choose a different name for it."
+        )
+        logger.error("create schema called where v1 already exists.")
+        return response_status_403(error_msg)
+
+    if not schema.has_registered_data_product:
+        error_msg = (
+            f"Schema for {table_name} has no registered metadata for the data product it belongs to. "
+            "Please first register the data product metadata using the POST method of the /data-product/register"
+            " endpoint."
+        )
+        logger.error("schema has no associated registered data product metadata.")
+        return response_status_403(error_msg)
+
+    # Code below that handles verisoning of a data product will be moved to a central module eventually.
+
+    # if schema already exist then we need to minor version increment to dataproduct metadata and schema
+    if schema.valid:
+        schema.convert_schema_to_glue_table_input_csv()
+        if not schema.parent_product_has_registered_schema:
+            metadata_dict = schema.parent_data_product_metadata
+            metadata_dict["schemas"] = [table_name]
+
+            # write v1.0 of metadata updated with registered schema, this is the only time we overwrite v1
+            DataProductMetadata(
+                data_product_name=data_product_name,
+                logger=logger,
+                input_data=metadata_dict,
+            ).write_json_to_s3(
+                DataProductConfig(data_product_name).metadata_path("v1.0").key
+            )
+            schema.write_json_to_s3(
+                DataProductConfig(data_product_name).schema_path(table_name, "v1.0").key
+            )
+            msg = f"Schema for {table_name} has been created in the {data_product_name} data product"
+            logger.info("Schema successfully created")
+            return response_status_200(msg)
+        else:
+            # write to next minor version increment
+            latest_version = get_latest_version(data_product_name=data_product_name)
+            new_version = get_new_version(latest_version, "minor")
+            # copy metatdata and schema to new version folder
+            latest_metadata_path = DataProductConfig(data_product_name).metadata_path()
+            folder = os.path.dirname(latest_metadata_path.key) + "/"
+
+            s3_copy_folder_to_new_folder(
+                bucket=latest_metadata_path.bucket,
+                source_folder=folder,
+                latest_version=latest_version,
+                new_version=new_version,
+                logger=logger,
+            )
+
+            schema_key = (
+                DataProductConfig(name=data_product_name)
+                .schema_path(table_name=table_name, version=new_version)
+                .key
+            )
+            metadata_key = (
+                DataProductConfig(name=data_product_name)
+                .metadata_path(version=new_version)
+                .key
+            )
+            metadata_dict = schema.parent_data_product_metadata
+            metadata_dict["schemas"].append(table_name)
+            DataProductMetadata(
+                data_product_name=data_product_name,
+                logger=logger,
+                input_data=metadata_dict,
+            ).write_json_to_s3(write_key=metadata_key)
+            schema.write_json_to_s3(write_key=schema_key)
+            msg = f"Schema for {table_name} has been created in the {data_product_name} data product"
+            logger.info("Schema successfully created")
+            return response_status_200(msg)
+    else:
+        error_msg = f"schema for {table_name} has failed validation with the following error: {schema.error_traceback}"
+        return response_status_400(error_msg)
---
+++ containers/daap-create-schema/tests/unit/create_schema_test.py
@@ -0,0 +1,108 @@
+import json
+import logging
+import os
+import urllib
+from http import HTTPMethod
+from unittest.mock import patch
+
+import pytest
+from create_schema import handler, s3_copy_folder_to_new_folder
+
+
+def load_v1_schema_schema_to_mock_s3(bucket_name, s3_client):
+    with urllib.request.urlopen(
+        "https://raw.githubusercontent.com/ministryofjustice/modernisation-platform-environments/main/terraform/environments/data-platform/data-product-table-schema-json-schema/v1.0.0/moj_data_product_table_spec.json"  # noqa E501
+    ) as url:
+        data = json.load(url)
+    json_data = json.dumps(data)
+    s3_client.put_object(
+        Body=json_data,
+        Bucket=bucket_name,
+        Key="data_product_schema_spec/v1.0.0/moj_data_product_schema_spec.json",
+    )
+
+
+def test_s3_copy_folder_to_new_folder(s3_client):
+    bucket_name = os.environ["BUCKET_NAME"]
+    s3_client.create_bucket(Bucket=bucket_name)
+    for key in ["somefolder/v1.0/somefile1.json", "somefolder/v1.0/somefile2.json"]:
+        s3_client.put_object(
+            Body="b",
+            Bucket=bucket_name,
+            Key=key,
+        )
+
+    with patch("create_schema.s3_client", s3_client):
+        s3_copy_folder_to_new_folder(
+            bucket_name, "somefolder/v1.0/", "v1.0", "v1.1", logging.getLogger()
+        )
+
+        for key in ["somefolder/v1.1/somefile1.json", "somefolder/v1.1/somefile2.json"]:
+            try:
+                s3_client.get_object(Bucket=bucket_name, Key=key)
+                assert True
+            except Exception:
+                assert False
+
+
+@pytest.mark.parametrize("method", [HTTPMethod.GET, HTTPMethod.PUT, HTTPMethod.DELETE])
+def test_http_method_fail(fake_event, fake_context, method):
+    response = handler(event=fake_event, context=fake_context)
+
+    assert (
+        json.loads(response["body"])["error"]["message"]
+        == f"Sorry, {method} isn't allowed."
+    )
+
+
+def test_schema_already_exists(fake_event, fake_context, s3_client):
+    bucket_name = os.environ["BUCKET_NAME"]
+    s3_client.create_bucket(Bucket=bucket_name)
+    load_v1_schema_schema_to_mock_s3(bucket_name, s3_client)
+
+    with patch("create_schema.DataProductSchema") as mock_schema:
+        mock_schema.return_value.exists = True
+        response = handler(event=fake_event, context=fake_context)
+
+    assert json.loads(response["body"])["error"]["message"] == (
+        "v1 of this schema for table test_t already exists. You can upversion this schema if "
+        "there are changes from v1 using the PUT method of this endpoint. Or if this is a different "
+        "table then please choose a different name for it."
+    )
+
+
+def test_schema_does_not_exist_and_is_valid(fake_event, fake_context, s3_client):
+    bucket_name = os.environ["BUCKET_NAME"]
+    s3_client.create_bucket(Bucket=bucket_name)
+    load_v1_schema_schema_to_mock_s3(bucket_name, s3_client)
+
+    with patch("create_schema.DataProductSchema") as mock_schema:
+        mock_schema.return_value.exists = False
+        mock_schema.return_value.valid = True
+        mock_schema.return_value.parent_product_has_registered_schema = False
+        with patch("create_schema.DataProductMetadata") as mock_metadata:
+            mock_metadata.return_value.load.latest_version_saved_data = {
+                "name": "test_p"
+            }
+            response = handler(event=fake_event, context=fake_context)
+
+    assert json.loads(response["body"])["message"] == (
+        "Schema for test_t has been created in the test_p data product"
+    )
+
+
+def test_schema_not_valid(fake_event, fake_context, s3_client):
+    bucket_name = os.environ["BUCKET_NAME"]
+    s3_client.create_bucket(Bucket=bucket_name)
+    load_v1_schema_schema_to_mock_s3(bucket_name, s3_client)
+
+    with patch("create_schema.DataProductSchema") as mock_schema:
+        mock_schema.return_value.exists = False
+        mock_schema.return_value.valid = False
+        mock_schema.return_value.parent_product_has_registered_schema = False
+
+        response = handler(event=fake_event, context=fake_context)
+
+    assert json.loads(response["body"])["error"]["message"][:65] == (
+        "schema for test_t has failed validation with the following error:"
+    )
