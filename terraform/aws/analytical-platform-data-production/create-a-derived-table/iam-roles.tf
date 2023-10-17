125c13fe308d8901cd012577c706d15aeb9d7266
Jacob Hamblin-Pyke <jacob.hamblin-pyke@digital.justice.gov.uk>
2023-10-17 12:55:43 +0000

    Update module version for session_duration

--- terraform/aws/analytical-platform-data-production/create-a-derived-table/iam-roles.tf
+++ terraform/aws/analytical-platform-data-production/create-a-derived-table/iam-roles.tf
@@ -1,9 +1,10 @@
 module "create_a_derived_table_iam_role" {
-  source = "github.com/ministryofjustice/modernisation-platform-github-oidc-role?ref=9d9a2d23cf569348cbdb665c979fcbaed76bb2f4" # v3.1.0
+  source = "github.com/ministryofjustice/modernisation-platform-github-oidc-role?ref=62b8a16c73d8e4422cd81923e46948e8f4b5cf48" # v3.2.0
 
-  role_name           = "create-a-derived-table"
-  github_repositories = ["moj-analytical-services/create-a-derived-table"]
-  policy_arns         = [module.create_a_derived_table_iam_policy.arn]
+  role_name            = "create-a-derived-table"
+  github_repositories  = ["moj-analytical-services/create-a-derived-table"]
+  policy_arns          = [module.create_a_derived_table_iam_policy.arn]
+  max_session_duration = 7200
 
   tags = var.tags
 }
