82909080f13d279840884bf38a77d1581afd259c
Jake H-P <72385707+jhpyke@users.noreply.github.com>
2023-10-17 15:56:42 +0100

    🔀 create-a-derived-table OIDC role (#1941)
    * create-a-derived-table OIDC first draft
    
    * Update module version for session_duration
    
    * dding Checkov skip due to false positive
    
    :wq
    
    B
    B
    B
    B
    B
    B
    B
    B
    B
    B
    B
    B
    B
    B
    B
    B
    B
    B
    B
    B
    B
    B
    B
    B
    B
    B
    B
    A
    A
    A
    A
    A
    A
    A
    A
    A
    A
    A
    A
    A
    A
    A
    B
    B
    B
    B
    B
    B
    B
    B
    B
    B
    B
    B
    B
    B
    B
    B
    B
    B
    B
    B
    B
    B
    
    * Fix dependabot
    
    Signed-off-by: Jacob Woffenden <jacob.woffenden@digital.justice.gov.uk>
    
    * Update path-filter
    
    Signed-off-by: Jacob Woffenden <jacob.woffenden@digital.justice.gov.uk>
    
    ---------
    
    Signed-off-by: Jacob Woffenden <jacob.woffenden@digital.justice.gov.uk>
    Co-authored-by: Jacob Woffenden <jacob.woffenden@digital.justice.gov.uk>

---
+++ terraform/aws/analytical-platform-data-production/create-a-derived-table/iam-roles.tf
@@ -0,0 +1,12 @@
+module "create_a_derived_table_iam_role" {
+  #checkov:skip=CKV_AWS_358:This appears to be a false positive, we are specifying the organisation name
+
+  source = "github.com/ministryofjustice/modernisation-platform-github-oidc-role?ref=62b8a16c73d8e4422cd81923e46948e8f4b5cf48" # v3.2.0
+
+  role_name            = "create-a-derived-table"
+  github_repositories  = ["moj-analytical-services/create-a-derived-table"]
+  policy_arns          = [module.create_a_derived_table_iam_policy.arn]
+  max_session_duration = 7200
+
+  tags = var.tags
+}
