e9c32a2b4e4b24e20b7caf93bd66d52b93db610a
Jacob Woffenden <jacob.woffenden@digital.justice.gov.uk>
2023-10-20 01:33:45 +0000

    Testing bad code
    Signed-off-by: Jacob Woffenden <jacob.woffenden@digital.justice.gov.uk>

---
+++ terraform/github/data-platform-2/test.tf
@@ -0,0 +1,7 @@
+# this is deliberately bad, I'm testing trivy
+# tfsec ignore:AVD-GIT-0001 Ministry of Justice follows https://www.gov.uk/service-manual/technology/making-source-code-open-and-reusable
+resource "github_repository" "bad" {
+  name                 = "bad-repo"
+  visibility           = "public"
+  vulnerability_alerts = true
+}
