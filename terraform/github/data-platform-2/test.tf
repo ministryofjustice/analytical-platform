14974d2129ec399499dc67df530b5ba4c93a8fd8
Jacob Woffenden <jacob.woffenden@digital.justice.gov.uk>
2023-10-20 08:15:21 +0000

    trivy ignore public repo
    Signed-off-by: Jacob Woffenden <jacob.woffenden@digital.justice.gov.uk>

--- terraform/github/data-platform-2/test.tf
+++ terraform/github/data-platform-2/test.tf
@@ -1,5 +1,5 @@
 # this is deliberately bad, I'm testing trivy
-# tfsec ignore:AVD-GIT-0001 Ministry of Justice follows https://www.gov.uk/service-manual/technology/making-source-code-open-and-reusable
+#tfsec:ignore:AVD-GIT-0001 Ministry of Justice follows https://www.gov.uk/service-manual/technology/making-source-code-open-and-reusable
 resource "github_repository" "bad" {
   name                 = "bad-repo"
   visibility           = "public"
