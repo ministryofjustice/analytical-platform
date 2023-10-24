c41a5c4fe5b474f1c5137109f5b9d22f039c642c
Emterry <emma.terry@digital.justice.gov.uk>
2023-10-24 14:10:23 +0100

    linter

--- terraform/aws/analytical-platform/baseline/modules/cost-explorer-anomaly-monitor/main.tf
+++ terraform/aws/analytical-platform/baseline/modules/cost-explorer-anomaly-monitor/main.tf
@@ -32,7 +32,7 @@ resource "aws_kms_key" "ce_anomaly_monitor" {
 }
 
 resource "aws_kms_alias" "ce_anomaly_monitor" {
-  provider      = aws.target
+  provider = aws.target
 
   name          = "alias/cost-explorer-anomaly-monitor"
   target_key_id = aws_kms_key.ce_anomaly_monitor.key_id
