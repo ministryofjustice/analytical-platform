49110bcd8e34b7476bd145b57cc689ff301030c5
Jacob Woffenden <jacob.woffenden@digital.justice.gov.uk>
2023-10-26 13:26:28 +0000

    Removed cleanup Added token obtain function
    Signed-off-by: Jacob Woffenden <jacob.woffenden@digital.justice.gov.uk>

--- containers/actions-runner/src/usr/local/bin/actions-runner-utils/cleanup.sh
+++
@@ -1,8 +0,0 @@
-#/usr/local/bin/env bash
-
-ACTIONS_RUNNER_DIRECTORY="/actions-runner"
-
-echo "Removing runner"
-bash "${ACTIONS_RUNNER_DIRECTORY}/config.sh" \
-  remove \
-  --token "${REPO_TOKEN}"
--- containers/actions-runner/src/usr/local/bin/actions-runner-utils/entrypoint.sh
+++ containers/actions-runner/src/usr/local/bin/actions-runner-utils/entrypoint.sh
@@ -2,22 +2,40 @@
 
 ACTIONS_RUNNER_DIRECTORY="/actions-runner"
 
-echo "Configuring runner"
+echo "Runner parameters:"
+echo "  Repository: ${GITHUB_REPOSITORY}"
+echo "  Runner Name: ${RUNNER_NAME}"
+echo "  Runner Labels: ${RUNNER_LABELS}"
 
-if [[ -z "${RUNNER_NAME_PREFIX}" ]]; then
-  RUNNER_NAME="${RUNNER_NAME}"
+echo "Obtaining registration token"
+getRegistrationToken=$(curl \
+  --silent \
+  --location \
+  --request "POST" \
+  --header "X-GitHub-Api-Version: 2022-11-28" \
+  --header "Authorization: Bearer ${GITHUB_TOKEN}" \
+  https://api.github.com/repos/${GITHUB_REPOSITORY}/actions/runners/registration-token | jq -r '.token'
+)
+export getRegistrationToken
+
+echo "Checking if registration token exists"
+if [[ -z "${getRegistrationToken}" ]]; then
+  echo "Failed to obtain registration token"
+  exit 1
 else
-  RUNNER_NAME="${RUNNER_NAME_PREFIX}-$(hostname)"
+  echo "Registration token obtained successfully"
+  REPO_TOKEN="${getRegistrationToken}"
 fi
 
+echo "Configuring runner"
 bash "${ACTIONS_RUNNER_DIRECTORY}/config.sh" \
   --unattended \
+  --ephemeral \
   --disableupdate \
-  --url "${REPO_URL}" \
+  --url "https://github.com/${GITHUB_REPOSITORY}" \
   --token "${REPO_TOKEN}" \
-  --name "${RUNNER_NAME}" \
+  --name "${RUNNER_NAME}-$(hostname)" \
   --labels "${RUNNER_LABELS}"
 
 echo "Starting runner"
-
 bash "${ACTIONS_RUNNER_DIRECTORY}/run.sh"
