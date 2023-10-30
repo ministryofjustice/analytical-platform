3291045c1d352702193dfb0852dbd5a1788fb98b
Gary H <26419401+Gary-H9@users.noreply.github.com>
2023-10-30 11:33:54 +0000

    :rotating_light: Fix linting

--- terraform/aws/analytical-platform-development/sagemaker/data.tf
+++ terraform/aws/analytical-platform-development/sagemaker/data.tf
@@ -2,8 +2,8 @@ data "aws_caller_identity" "session" {
   provider = aws.session
 }
 
-data "aws_iam_session_context" "session" {
-  provider = aws.session
+# data "aws_iam_session_context" "session" {
+#   provider = aws.session
 
-  arn = data.aws_caller_identity.session.arn
-}
+#   arn = data.aws_caller_identity.session.arn
+# }
