fbdf0ec0de7b45eb4f5caa59a32f467d0e0f7777
Gary H <26419401+Gary-H9@users.noreply.github.com>
2023-10-30 11:41:36 +0000

    :fire: Remove data.tf

--- terraform/aws/analytical-platform-development/sagemaker/data.tf
+++
@@ -1,9 +0,0 @@
-# data "aws_caller_identity" "session" {
-#   provider = aws.session
-# }
-
-# data "aws_iam_session_context" "session" {
-#   provider = aws.session
-
-#   arn = data.aws_caller_identity.session.arn
-# }
