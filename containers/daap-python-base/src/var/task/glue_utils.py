d26d0351cddcd6b9ebc09c64e368e3aae815d5a0
Mitch Dawson <Mitchell.Dawson@justice.gov.uk>
2023-10-31 15:57:48 +0000

    :truck: Move delete glue table funcion to its own base module

---
+++ containers/daap-python-base/src/var/task/glue_utils.py
@@ -0,0 +1,26 @@
+from http import HTTPStatus
+
+import boto3
+from botocore.exceptions import ClientError
+from data_platform_api_responses import format_error_response
+from data_platform_logging import DataPlatformLogger
+
+
+def delete_glue_table(
+    data_product_name: str, table_name: str, logger: DataPlatformLogger
+) -> str | None:
+    """Attempts to locate and delete a glue table for the given data product"""
+    glue_client = boto3.client("glue")
+    try:
+        glue_client.get_table(DatabaseName=data_product_name, Name=table_name)
+    except ClientError as e:
+        if e.response["Error"]["Code"] == "EntityNotFoundException":
+            error_message = f"Could not locate glue table '{table_name}' in database '{data_product_name}'"
+            logger.error(error_message)
+            raise ValueError(e)
+        else:
+            error_message = f"Unexpected ClientError: {e.response['Error']['Code']}"
+            logger.error(error_message)
+            raise ValueError(e)
+    else:
+        glue_client.delete_table(DatabaseName=data_product_name, Name=table_name)
