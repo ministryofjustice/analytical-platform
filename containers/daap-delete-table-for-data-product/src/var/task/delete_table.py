1b228d10be0f421f7d2688a3202ab7e180119539
Mitch Dawson <Mitchell.Dawson@justice.gov.uk>
2023-10-31 16:04:56 +0000

    :fire: Refactor and remove code to VersionCreator class

--- containers/daap-delete-table-for-data-product/src/var/task/delete_table.py
+++ containers/daap-delete-table-for-data-product/src/var/task/delete_table.py
@@ -5,14 +5,9 @@ import boto3
 from botocore.exceptions import ClientError
 from data_platform_api_responses import format_error_response
 from data_platform_logging import DataPlatformLogger, s3_security_opts
-from data_platform_paths import (
-    DataProductElement,
-    get_metadata_bucket,
-    get_new_version,
-    DataProductConfig,
-)
 from data_product_metadata import DataProductMetadata, DataProductSchema
-from versioning import s3_copy_folder_to_new_folder
+from glue_utils import delete_glue_table
+from versioning import VersionCreator, InvalidUpdate
 
 logger = DataPlatformLogger(
     extra={
@@ -22,91 +17,8 @@ logger = DataPlatformLogger(
 )
 
 s3_client = boto3.client("s3")
-s3_resource = boto3.resource("s3")
 glue_client = boto3.client("glue")
 athena_client = boto3.client("athena")
-
-
-def get_all_versions(data_product_name: str) -> list[str]:
-    """Returns all versions of a given data product"""
-    metadata_bucket = get_metadata_bucket()
-    metadata_versions = s3_client.list_objects_v2(
-        Bucket=metadata_bucket, Prefix=data_product_name + "/"
-    )
-    # This is the case in which the data product is new.
-    if not metadata_versions.get("Contents"):
-        return ["v1.0"]
-
-    versions = [
-        version["Key"].split("/")[1]
-        for version in metadata_versions["Contents"]
-        if version["Size"] > 0
-    ]
-    versions.sort(key=lambda x: [int(y.replace("v", "")) for y in x.split(".")])
-
-    return versions
-
-
-def get_latest_version(data_product_name: str) -> str:
-    return get_all_versions(data_product_name=data_product_name)[-1]
-
-
-def generate_all_element_version_prefixes(
-    path_prefix: str, data_product_name: str, table_name: str
-) -> list[str]:
-    """Generates element prefixes for all data product versions"""
-    data_product_versions = get_all_versions(data_product_name)
-    element_prefixes = []
-
-    for version in data_product_versions:
-        element_prefixes.append(
-            f"{path_prefix}/{data_product_name}/{version}/{table_name}/"
-        )
-
-    return element_prefixes
-
-
-def delete_all_element_version_data_files(data_product_name: str, table_name: str):
-    """Deletes raw and curated data for all element versions"""
-    # Proceed to delete the raw data
-    element = DataProductElement.load(
-        element_name=table_name, data_product_name=data_product_name
-    )
-    raw_prefixes = generate_all_element_version_prefixes(
-        "raw", data_product_name, table_name
-    )
-    curated_prefixes = generate_all_element_version_prefixes(
-        "curated", data_product_name, table_name
-    )
-
-    s3_recursive_delete(element.data_product.raw_data_bucket, raw_prefixes)
-    s3_recursive_delete(element.data_product.curated_data_bucket, curated_prefixes)
-
-
-def s3_recursive_delete(bucket_name: str, prefixes: list[str]) -> None:
-    """Delete all files from a prefix in s3"""
-    bucket = s3_resource.Bucket(bucket_name)
-    for prefix in prefixes:
-        bucket.objects.filter(Prefix=prefix).delete()
-
-
-def delete_glue_table(
-    data_product_name: str, table_name: str, event: dict
-) -> str | None:
-    """Attempts to locate and delete a glue table for the given data product"""
-    try:
-        glue_client.get_table(DatabaseName=data_product_name, Name=table_name)
-    except ClientError as e:
-        if e.response["Error"]["Code"] == "EntityNotFoundException":
-            error_message = f"Could not locate glue table '{table_name}' in database '{data_product_name}'"
-            logger.error(error_message)
-        else:
-            error_message = f"Unexpected ClientError: {e.response['Error']['Code']}"
-            logger.error(error_message)
-        return format_error_response(HTTPStatus.BAD_REQUEST, event, error_message)
-    else:
-        glue_client.delete_table(DatabaseName=data_product_name, Name=table_name)
-    return
 
 
 def handler(event, context):
@@ -148,47 +60,25 @@ def handler(event, context):
         logger=logger,
         input_data=None,
     )
-
     # Validate the existence of the table schema for the latest data product version
     if not table_schema.exists:
         error_message = f"Could not locate valid schema for table: {table_name}."
         logger.error(error_message)
         return format_error_response(HTTPStatus.BAD_REQUEST, event, error_message)
 
-    # Attempt deletion of the glue table
-    glue_error = delete_glue_table(
-        data_product_name=data_product_name, table_name=table_name, event=event
-    )
-    if glue_error is not None:
-        return glue_error
+    metadata = data_product_metadata.load().latest_version_saved_data
+    metadata["schemas"].remove(table_name)
 
-    # Delete a given elements raw and curated data for all versions of the data product
-    delete_all_element_version_data_files(
-        data_product_name=data_product_name, table_name=table_name
-    )
-
-    latest_version = get_latest_version(data_product_name)
-    new_version = get_new_version(latest_version, "major")
-    logger.info(f"latest version: {latest_version}, new version: {new_version}")
-
-    # Metadata Path "data-product-name/version/metadata.json"
-    # Schema Path "data-product-name/version/table-name/schema.json"
-    metadata_prefix = f"{data_product_name}/{latest_version}/"
-    metadata_bucket = get_metadata_bucket()
-
-    # Copy metadata and schema files from the current version
-    # to the new version
-    s3_copy_folder_to_new_folder(
-        metadata_bucket, metadata_prefix, latest_version, new_version, logger
-    )
-    # Get the latest version of the schema
-    schema_path = DataProductConfig(name=data_product_name).schema_path(table_name)
-
-    # Delete the schema for the table we wish to remove
-    s3_client.delete_object(Bucket=metadata_bucket, Key=schema_path.key)
-
-    # Investigate how to reload the athena table data ?
-
-    msg = f"Successfully deleted table '{table_name}' and raw & curated data files"
-    logger.info(msg)
-    return format_error_response(HTTPStatus.OK, event, msg)
+    version_creator = VersionCreator(data_product_name=data_product_name, logger=logger)
+    try:
+        result = version_creator.update_metadata_remove_schema(input_data=metadata)
+    except InvalidUpdate as e:
+        error_message = f"Invalid Update : {str(e)}"
+        logger.error(error_message)
+        return format_error_response(HTTPStatus.BAD_REQUEST, event, error_message)
+    except ValueError as e:
+        return format_error_response(HTTPStatus.BAD_REQUEST, event, str(e))
+    else:
+        msg = f"Successfully deleted table '{table_name}' and raw & curated data files"
+        logger.info(msg)
+        return format_error_response(HTTPStatus.OK, event, msg)
