159a0e888e2a17719283864baab37a16f80ea6de
LavMatt <mattlaverty@gmail.com>
2023-11-02 15:07:09 +0000

    add from dict methods to entities

--- python-libraries/data-platform-catalogue/data_platform_catalogue/entities.py
+++ python-libraries/data-platform-catalogue/data_platform_catalogue/entities.py
@@ -20,11 +20,61 @@ class DataProductMetadata:
     dpia_required: bool
     tags: list[str] = field(default_factory=list)
 
+    @staticmethod
+    def from_data_product_metadata_dict(
+        metadata: dict, version, owner_id: str | None = None
+    ):
+        """
+        Expects a dict containing data product metatdata information as per the
+        required fields in the json schema at
+        https://github.com/ministryofjustice/modernisation-platform-environments/tree/main/terraform/environments/data-platform/data-product-metadata-json-schema
+
+        and should be pass version and owner id as in openmetadata.
+
+        Then populates a DataProductMetadata object with the given data.
+        """
+        new_metadata = DataProductMetadata(
+            name=metadata.get("name", "Not provided"),
+            description=metadata.get("description", "Not provided"),
+            version=version,
+            owner=owner_id,
+            email=metadata.get("email", "Not provided"),
+            retention_period_in_days=metadata.get("retentionPeriod", "Not provided"),
+            domain=metadata.get("domain", "Not provided"),
+            dpia_required=metadata.get("dpiaRequired", "Not provided"),
+            tags=metadata.get("tags", {}),
+        )
+
+        return new_metadata
+
 
 @dataclass
 class TableMetadata:
     name: str
     description: str
-    column_types: dict[str, str]
+    column_details: list[tuple]
     retention_period_in_days: int
     tags: list[str] = field(default_factory=list)
+
+    @staticmethod
+    def from_data_product_schema_dict(
+        metadata: dict, table_name, retention_period: int | None = None
+    ):
+        """
+        Expects a dict containing data product table schema information as per the
+        required fields in the json schema at
+        https://github.com/ministryofjustice/modernisation-platform-environments/tree/main/terraform/environments/data-platform/data-product-table-schema-json-schema
+
+        and should be passed table name and optionally a retention period
+
+        Then populates a TableMetadata object with the given data.
+        """
+        new_metadata = TableMetadata(
+            name=table_name,
+            description=metadata.get("tableDescription", "Not provided"),
+            column_details=metadata.get("columns"),
+            retention_period_in_days=retention_period,
+            tags=metadata.get("tags", {}),
+        )
+
+        return new_metadata
