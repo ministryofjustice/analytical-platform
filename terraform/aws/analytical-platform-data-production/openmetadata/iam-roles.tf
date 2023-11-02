6af967b7426e6aa9387c87c04a91c908a7f06c14
Jacob Woffenden <jacob.woffenden@digital.justice.gov.uk>
2023-11-02 19:44:50 +0000

    Add OMD component for AP data production
    Signed-off-by: Jacob Woffenden <jacob.woffenden@digital.justice.gov.uk>

---
+++ terraform/aws/analytical-platform-data-production/openmetadata/iam-roles.tf
@@ -0,0 +1,17 @@
+
+module "openmetadata_iam_role" {
+  source  = "terraform-aws-modules/iam/aws//modules/iam-assumable-role"
+  version = "~> 5.0"
+
+  create_role = true
+
+  role_name         = "openmetadata"
+  role_requires_mfa = false
+
+  trusted_role_arns = ["arn:aws:iam::${var.account_ids["data-platform-apps-and-tools-development"]}:root"]
+
+  custom_role_policy_arns = [
+    "arn:aws:iam::aws:policy/service-role/AWSQuicksightAthenaAccess",
+    module.openmetadata_iam_policy.arn
+  ]
+}
