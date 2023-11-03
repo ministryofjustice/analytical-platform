753132948e23eba5d0240a214ec5a5343ff0eba0
Mat Moore <MatMoore@users.noreply.github.com>
2023-11-03 14:18:55 +0000

    Use the correct name for logging to s3

--- containers/daap-landing-to-raw/src/var/task/landing_to_raw.py
+++ containers/daap-landing-to-raw/src/var/task/landing_to_raw.py
@@ -43,7 +43,7 @@ def handler(event, context):
         extra={
             "image_version": os.getenv("VERSION", "unknown"),
             "base_image_version": os.getenv("BASE_VERSION", "unknown"),
-            "data_product": config.element.data_product.name,
+            "data_product_name": config.element.data_product.name,
             "table": config.element.curated_data_table.name,
         }
     )
