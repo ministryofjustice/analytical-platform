e611e39a99de555deb82834eba3fee510cdb7357
Mitch Dawson <Mitchell.Dawson@justice.gov.uk>
2023-11-08 11:19:56 +0000

    :construction: Inital Commit

---
+++ containers/daap-delete-data-product/src/var/task/delete_data_product.py
@@ -0,0 +1,58 @@
+import os
+from http import HTTPStatus
+
+import boto3
+from botocore.exceptions import ClientError
+from data_platform_api_responses import format_error_response
+from data_platform_logging import DataPlatformLogger
+from data_platform_paths import DataProductElement, get_metadata_bucket
+from data_product_metadata import DataProductMetadata, DataProductSchema
+
+logger = DataPlatformLogger(
+    extra={
+        "image_version": os.getenv("VERSION", "unknown"),
+        "base_image_version": os.getenv("BASE_VERSION", "unknown"),
+    }
+)
+
+s3_client = boto3.client("s3")
+s3_resource = boto3.resource("s3")
+glue_client = boto3.client("glue")
+athena_client = boto3.client("athena")
+
+
+def handler(event, context):
+    """
+    Handles requests that are passed through the Amazon API Gateway data-product/ REST API endpoint.
+    POST requests result in a success code to report that the product has been registered
+    (metadata has been created).
+    GET, PUT, and DELETE requests result in a 405 response.
+
+    :param event: The event dict sent by Amazon API Gateway that contains all of the
+                  request data.
+    :param context: The context in which the function is called.
+    :return: A response that is sent to Amazon API Gateway, to be wrapped into
+             an HTTP response. The 'statusCode' field is the HTTP status code
+             and the 'body' field is the body of the response.
+    """
+
+    data_product_name = event["pathParameters"].get("data-product-name")
+
+    logger.add_extras({"data_product_name": data_product_name})
+    logger.info(f"event: {event}")
+
+    data_product_metadata = DataProductMetadata(
+        data_product_name=data_product_name, logger=logger, input_data=None
+    )
+    if not data_product_metadata.exists:
+        error_message = (
+            f"Could not locate metadata for data product: {data_product_name}."
+        )
+        logger.error(error_message)
+        return format_error_response(HTTPStatus.BAD_REQUEST, event, error_message)
+
+    # Locate all associated schemas
+
+    # Delete all associated schemas
+
+    # Delete all data associated with the data product i.e. prexif = raw/data
