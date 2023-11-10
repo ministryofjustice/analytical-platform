595b6b46baba06dce3724f38f37485744c355342
Gary H <26419401+Gary-H9@users.noreply.github.com>
2023-11-10 14:46:38 +0000

    :rotating_light: Trivy linting

--- terraform/aws/analytical-platform-development/auth0-log-streams/cloudwatch-log-groups.tf
+++ terraform/aws/analytical-platform-development/auth0-log-streams/cloudwatch-log-groups.tf
@@ -1,3 +1,4 @@
+#tfsec:ignore:AVD-AWS-0017
 resource "aws_cloudwatch_log_group" "auth0" {
   #ts:skip=AWS.ACLG.LM.MEDIUM.0068 this is nonproduction code, will be addresses when productionised
   #checkov:skip=CKV_AWS_66 This code is being retired
