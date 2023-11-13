a37dd45e2f1ef0e63e4ec559585ceed3e10d1f51
Jacob Woffenden <jacob.woffenden@digital.justice.gov.uk>
2023-11-13 15:15:50 +0000

    🚑 Ship Auth0 logs to eu-west-2 (#2326)
    Signed-off-by: Jacob Woffenden <jacob.woffenden@digital.justice.gov.uk>

--- terraform/auth0/alpha-analytics-moj/auth0-log-streams.tf
+++ terraform/auth0/alpha-analytics-moj/auth0-log-streams.tf
@@ -5,7 +5,7 @@ resource "auth0_log_stream" "aws_eventbridge_data_platform_apps_and_tools_produc
 
   sink {
     aws_account_id = "096705367497"
-    aws_region     = data.aws_region.current.name
+    aws_region     = "eu-west-2"
   }
 }
 
@@ -16,6 +16,6 @@ resource "auth0_log_stream" "aws_eventbridge_analytical_platform_development" {
 
   sink {
     aws_account_id = "525294151996"
-    aws_region     = data.aws_region.current.name
+    aws_region     = "eu-west-2"
   }
 }
