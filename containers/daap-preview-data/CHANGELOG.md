0e4eca1d9305c73bfbf75be680f6d675174d8192
Mat Moore <MatMoore@users.noreply.github.com>
2023-11-13 11:44:43 +0000

    Update preview-data to base image 6.2.0

--- containers/daap-preview-data/CHANGELOG.md
+++ containers/daap-preview-data/CHANGELOG.md
@@ -9,6 +9,12 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [Unreleased]
 
+## [2.1.0] - 2023-11-13
+
+### Changed
+
+- Updated base image to 6.2.0
+
 ## [2.0.1] - 2023-11-08
 
 ### Changed
--- containers/daap-preview-data/src/var/task/preview_data.py
+++ containers/daap-preview-data/src/var/task/preview_data.py
@@ -106,11 +106,7 @@ def handler(event, context, athena_client=athena_client):
 
     logger = DataPlatformLogger(
         data_product_name=data_product_name,
-        extra={
-            "image_version": os.getenv("VERSION", "unknown"),
-            "base_image_version": os.getenv("BASE_VERSION", "unknown"),
-            "table_name": table_name,
-        },
+        table_name=table_name,
     )
 
     dataproduct_exists = DataProductSchema(
