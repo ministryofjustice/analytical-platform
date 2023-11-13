9362181abe7d2d700d63007ad1e5bb99f8cc2530
Mat Moore <MatMoore@users.noreply.github.com>
2023-11-13 11:21:57 +0000

    Update delete table to base image 6.2.0

--- containers/daap-delete-table-for-data-product/src/var/task/delete_table.py
+++ containers/daap-delete-table-for-data-product/src/var/task/delete_table.py
@@ -7,12 +7,7 @@ from data_platform_logging import DataPlatformLogger
 from data_product_metadata import DataProductMetadata, DataProductSchema
 from versioning import InvalidUpdate, VersionCreator
 
-logger = DataPlatformLogger(
-    extra={
-        "image_version": os.getenv("VERSION", "unknown"),
-        "base_image_version": os.getenv("BASE_VERSION", "unknown"),
-    }
-)
+logger = DataPlatformLogger()
 
 s3_client = boto3.client("s3")
 glue_client = boto3.client("glue")
@@ -37,9 +32,7 @@ def handler(event, context):
     data_product_name = event["pathParameters"].get("data-product-name")
     table_name = event["pathParameters"].get("table-name")
 
-    logger.add_extras(
-        {"data_product_name": data_product_name, "table_name": table_name}
-    )
+    logger.add_data_product(data_product_name=data_product_name, table_name=table_name)
     logger.info(f"event: {event}")
 
     data_product_metadata = DataProductMetadata(
