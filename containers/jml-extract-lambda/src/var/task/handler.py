6796e67355ecce65efc2ce572ca5cb09f59f3449
Jacob Hamblin-Pyke <jacob.hamblin-pyke@digital.justice.gov.uk>
2023-11-17 14:58:29 +0000

    Linting fixes

--- containers/jml-extract-lambda/src/var/task/handler.py
+++ containers/jml-extract-lambda/src/var/task/handler.py
@@ -6,16 +6,17 @@ from notifications_python_client.notifications import NotificationsAPIClient
 from notifications_python_client import prepare_upload
 from urllib.error import HTTPError
 
+
 def handler(
     event,
     context,
 ):
     boto3.setup_default_session(region_name="eu-west-2")
-    secrets_client = boto3.client('secretsmanager')
-    response = client.get_secret_value(
+    secrets_client = boto3.client("secretsmanager")
+    response = secrets_client.get_secret_value(
         SecretId="gov-uk-notify/production/api-key",
     )
-    api_key = response['SecretString']
+    api_key = response["SecretString"]
     notifications_client = NotificationsAPIClient(api_key)
 
     now = dt.now()
@@ -39,29 +40,30 @@ def handler(
     start_datetime = dt(year, previous_month, 1, 0, 0, 0)
 
     dataframe = wr.cloudwatch.read_logs(
-        log_group_names=['/aws/events/auth0/alpha-analytics-moj'],
+        log_group_names=["/aws/events/auth0/alpha-analytics-moj"],
         query=query,
         start_time=start_datetime,
         end_time=end_datetime,
     )
 
-    dataframe['last_login'] = pd.to_datetime(dataframe['last_login'], format="%Y-%m-%d %H:%M:%S.%f").apply(lambda x: str(dt.strftime(x, "%Y/%m/%d")))
+    dataframe["last_login"] = pd.to_datetime(
+        dataframe["last_login"], format="%Y-%m-%d %H:%M:%S.%f"
+    ).apply(lambda x: str(dt.strftime(x, "%Y/%m/%d")))
 
-    dataframe.to_excel('/tmp/test.xlsx', index=False)
+    dataframe.to_excel("/tmp/test.xlsx", index=False)
 
     # client = boto3.client('s3')
     # client.upload_file('/tmp/test.xlsx', 'jml-export-bucket-REPLACE-ME', 'test.xlsx')
-    with open('/tmp/test.xlsx', 'rb') as f:
+    with open("/tmp/test.xlsx", "rb") as f:
         try:
             response = notifications_client.send_email_notification(
-                email_address='CHANGE_ME@example.com',
-                template_id='de618989-db86-4d9a-aa55-4724d5485fa5',
+                email_address="CHANGE_ME@example.com",
+                template_id="de618989-db86-4d9a-aa55-4724d5485fa5",
                 personalisation={
-                    'date': current_date,
-                    'link_to_file': prepare_upload(f),
-                }
+                    "date": current_date,
+                    "link_to_file": prepare_upload(f),
+                },
             )
         except HTTPError as e:
             print(e)
-            raise(e)
-
+            raise e
