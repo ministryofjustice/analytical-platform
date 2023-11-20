d409a0a6107c13223b5d96b7185ef50a83ac48f2
Jacob Woffenden <jacob.woffenden@digital.justice.gov.uk>
2023-11-20 11:49:20 +0000

    go go go
    Signed-off-by: Jacob Woffenden <jacob.woffenden@digital.justice.gov.uk>

--- terraform/aws/analytical-platform-development/open-metadata/terraform.tf
+++ terraform/aws/analytical-platform-development/open-metadata/terraform.tf
@@ -88,8 +88,3 @@ provider "kubernetes" {
     args        = ["../../../../scripts/eks/terraform-authentication.sh", data.aws_caller_identity.current.account_id, module.eks.cluster_name]
   }
 }
-
-provider "grafana" {
-  url  = "https://${module.managed_grafana.workspace_endpoint}"
-  auth = aws_grafana_workspace_api_key.automation_key.key
-}
