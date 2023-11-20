2cd522f73f8d3f8724d9ef3d5fa266b4146733fa
Jacob Woffenden <jacob.woffenden@digital.justice.gov.uk>
2023-11-20 15:42:36 +0000

    Add aws-nuke
    Signed-off-by: Jacob Woffenden <jacob.woffenden@digital.justice.gov.uk>

--- .devcontainer/features/src/aws-tools/install.sh
+++ .devcontainer/features/src/aws-tools/install.sh
@@ -7,3 +7,7 @@ fi
 if [[ "${INSTALLAWSVAULT}" == "true" ]]; then
   bash $( dirname $0 )/install-aws-vault.sh
 fi
+
+if [[ "${INSTALLAWSNUKE}" == "true" ]]; then
+  bash $( dirname $0 )/install-aws-nuke.sh
+fi
--- .devcontainer/features/test/aws-tools/test.sh
+++ .devcontainer/features/test/aws-tools/test.sh
@@ -6,5 +6,6 @@ source dev-container-features-test-lib
 
 check "aws-cli version" aws --version
 check "aws-vault version" aws-vault --version
+check "aws-nuke version" aws-nuke version
 
 reportResults
