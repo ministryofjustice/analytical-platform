28e1d42ca2e57a060b88a8e65d574eb9e086c514
Mitch Dawson <Mitchell.Dawson@justice.gov.uk>
2023-11-21 15:29:18 +0000

    :truck: Change paramater to specify database name

--- containers/daap-python-base/src/var/task/glue_and_athena_utils.py
+++ containers/daap-python-base/src/var/task/glue_and_athena_utils.py
@@ -30,24 +30,22 @@ def create_glue_database(
 
 
 def delete_glue_table(
-    data_product_name: str,
+    database_name: str,
     table_name: str,
     logger: DataPlatformLogger,
 ) -> str | None:
     """Attempts to locate and delete a glue table for the given data product"""
     try:
-        glue_client.get_table(DatabaseName=data_product_name, Name=table_name)
+        glue_client.get_table(DatabaseName=database_name, Name=table_name)
     except ClientError as e:
         if e.response["Error"]["Code"] == "EntityNotFoundException":
-            error_message = f"Could not locate glue table '{table_name}' in database '{data_product_name}'"
+            error_message = f"Could not locate glue table '{table_name}' in database '{database_name}'"
             logger.error(error_message)
             raise ValueError(error_message)
         else:
             raise
     else:
-        result = glue_client.delete_table(
-            DatabaseName=data_product_name, Name=table_name
-        )
+        result = glue_client.delete_table(DatabaseName=database_name, Name=table_name)
         return result
 
 
