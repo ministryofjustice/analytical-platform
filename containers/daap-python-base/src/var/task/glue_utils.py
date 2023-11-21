26c638571b2a82cab141fcf51686b28af5e83d14
Mitch Dawson <Mitchell.Dawson@justice.gov.uk>
2023-11-21 14:58:51 +0000

    :recycle: No longer raises entityNotFoundException back to the client

--- containers/daap-python-base/src/var/task/glue_utils.py
+++ containers/daap-python-base/src/var/task/glue_utils.py
@@ -6,20 +6,17 @@ glue_client = boto3.client("glue")
 
 
 def delete_glue_table(
-    data_product_name: str, table_name: str, logger: DataPlatformLogger
+    database_name: str, table_name: str, logger: DataPlatformLogger
 ) -> str | None:
     """Attempts to locate and delete a glue table for the given data product"""
     try:
-        glue_client.get_table(DatabaseName=data_product_name, Name=table_name)
+        glue_client.get_table(DatabaseName=database_name, Name=table_name)
     except ClientError as e:
         if e.response["Error"]["Code"] == "EntityNotFoundException":
-            error_message = f"Could not locate glue table '{table_name}' in database '{data_product_name}'"
-            logger.error(error_message)
-            raise ValueError(error_message)
+            error_message = f"Could not locate glue table '{table_name}' in database '{database_name}'"
+            return error_message
         else:
             raise
     else:
-        result = glue_client.delete_table(
-            DatabaseName=data_product_name, Name=table_name
-        )
+        result = glue_client.delete_table(DatabaseName=database_name, Name=table_name)
         return result
