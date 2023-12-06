355c4a5ec9e53ef43e05eed206514ed37a19d293
Mat Moore <MatMoore@users.noreply.github.com>
2023-12-06 16:35:58 +0000

    Add powertools

--- containers/daap-api/hello_world/app.py
+++ containers/daap-api/hello_world/app.py
@@ -1,33 +1,27 @@
-import json
+from aws_lambda_powertools import Logger, Metrics
+from aws_lambda_powertools.event_handler import APIGatewayRestResolver
+from aws_lambda_powertools.logging import correlation_paths
+from aws_lambda_powertools.metrics import MetricUnit
+from aws_lambda_powertools.utilities.typing import LambdaContext
+
+app = APIGatewayRestResolver()
+logger = Logger()
+metrics = Metrics(namespace="Powertools")
 
 
-def lambda_handler(event, context):
-    """Sample pure Lambda function
+@app.get("/hello")
+def hello():
+    # adding custom metrics
+    # See: https://awslabs.github.io/aws-lambda-powertools-python/latest/core/metrics/
+    metrics.add_metric(name="HelloWorldInvocations", unit=MetricUnit.Count, value=1)
 
-    Parameters
-    ----------
-    event: dict, required
-        API Gateway Lambda Proxy Input Format
+    logger.info("Hello world API - HTTP 200")
 
-        Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
+    return {"message": "hello world"}
 
-    context: object, required
-        Lambda Context runtime methods and attributes
 
-        Context doc: https://docs.aws.amazon.com/lambda/latest/dg/python-context-object.html
-
-    Returns
-    ------
-    API Gateway Lambda Proxy Output Format: dict
-
-        Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
-    """
-
-    return {
-        "statusCode": 200,
-        "body": json.dumps(
-            {
-                "message": "hello world",
-            }
-        ),
-    }
+# Enrich logging with contextual information from Lambda
+@logger.inject_lambda_context(correlation_id_path=correlation_paths.API_GATEWAY_REST)
+@metrics.log_metrics(capture_cold_start_metric=True)
+def lambda_handler(event: dict, context: LambdaContext) -> dict:
+    return app.resolve(event, context)
