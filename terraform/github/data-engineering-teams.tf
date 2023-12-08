9b516f47171077ad6bf444c9e1cdddbffc06903e
Gary H <26419401+Gary-H9@users.noreply.github.com>
2023-12-08 16:52:26 +0000

    :busts_in_silhouette: Add Parminder

--- terraform/github/data-engineering-teams.tf
+++ terraform/github/data-engineering-teams.tf
@@ -1,44 +1,45 @@
 locals {
   data_engineering_team_members = [
-    "calumabarnett",      # Calum Barnett
-    "PriyaBasker23",      # Priya Basker
-    "SoumayaMauthoorMOJ", # Soumaya Mauthoor
-    "mamonu",             # Theodoros Manassis
-    "mratford",           # Mike Ratford
-    "Danjiv",             # Danjiv Ramkhalawon
-    "K1Br",               # Kimberley Brett
-    "vonbraunbates",      # Francesca von Braun-Bates
-    "AnthonyCody",        # Anthony Cody
-    "gwionap",            # Gwion ApRhobat
-    "tmpks",              # Tapan Perkins
-    "Thomas-Hirsch",      # Thomas Hirsch
-    "LavMatt",            # Matt Laverty
-    "williamorrie",       # William Orr
-    "gustavmoller",       # Gustav Moller
-    "jhpyke",             # Jake Hamblin-Pyke
-    "murad-ali-MoJ",      # Murad Ali
-    "oliver-critchfield", # Oliver Critchfield
-    "davidbridgwood",     # David Bridgwood
-    "mshodge",            # Michael Hodge
-    "Andy-Cook",          # Andy Cook
-    "samnlindsay",        # Sam Lindsay
-    "ThomasHepworth",     # Thomas Hepworth
-    "zslade",             # Zoe Slade
-    "ADBond",             # Andrew Bond
-    "aliceoleary0",       # Alice O'Leary
-    "RossKen",            # Ross Kennedy
-    "afua-moj",           # Afua Kokayi
-    "murdo-moj",          # Murdo Moyse
-    "matmoore",           # Mat Moore
-    "AntFMoJ",            # Anthony Fitzroy
-    "tomholt1",           # Tom Holt
-    "lalithanagarur",     # Lalitha Nagarur
-    "sivabathina2",       # Siva Bathina
-    "hemeshpatel-moj",    # Hemesh Patel
-    "kraihanmoj",         # Khristiania Raihan
-    "andrewc-moj",        # Andrew Craik
-    "pricemg",            # Matthew Price
-    "matt-heery"          # Matt Heery
+    "calumabarnett",        # Calum Barnett
+    "PriyaBasker23",        # Priya Basker
+    "SoumayaMauthoorMOJ",   # Soumaya Mauthoor
+    "mamonu",               # Theodoros Manassis
+    "mratford",             # Mike Ratford
+    "Danjiv",               # Danjiv Ramkhalawon
+    "K1Br",                 # Kimberley Brett
+    "vonbraunbates",        # Francesca von Braun-Bates
+    "AnthonyCody",          # Anthony Cody
+    "gwionap",              # Gwion ApRhobat
+    "tmpks",                # Tapan Perkins
+    "Thomas-Hirsch",        # Thomas Hirsch
+    "LavMatt",              # Matt Laverty
+    "williamorrie",         # William Orr
+    "gustavmoller",         # Gustav Moller
+    "jhpyke",               # Jake Hamblin-Pyke
+    "murad-ali-MoJ",        # Murad Ali
+    "oliver-critchfield",   # Oliver Critchfield
+    "davidbridgwood",       # David Bridgwood
+    "mshodge",              # Michael Hodge
+    "Andy-Cook",            # Andy Cook
+    "samnlindsay",          # Sam Lindsay
+    "ThomasHepworth",       # Thomas Hepworth
+    "zslade",               # Zoe Slade
+    "ADBond",               # Andrew Bond
+    "aliceoleary0",         # Alice O'Leary
+    "RossKen",              # Ross Kennedy
+    "afua-moj",             # Afua Kokayi
+    "murdo-moj",            # Murdo Moyse
+    "matmoore",             # Mat Moore
+    "AntFMoJ",              # Anthony Fitzroy
+    "tomholt1",             # Tom Holt
+    "lalithanagarur",       # Lalitha Nagarur
+    "sivabathina2",         # Siva Bathina
+    "hemeshpatel-moj",      # Hemesh Patel
+    "kraihanmoj",           # Khristiania Raihan
+    "andrewc-moj",          # Andrew Craik
+    "pricemg",              # Matthew Price
+    "matt-heery",           # Matt Heery
+    "parminder-thindal-moj" # Parminder Thindal
   ]
 }
 
