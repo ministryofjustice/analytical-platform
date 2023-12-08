004c0eafb099165c60282a8fd0bdfd869d9564e9
murad-ali-MoJ <murad.ali@justice.gov.uk>
2023-12-08 14:10:52 +0000

    delete the new file correct the old one

--- containers/jml-extract-lambda/src/var/task/handler.py
+++ containers/jml-extract-lambda/src/var/task/handler.py
@@ -8,27 +8,33 @@ from notifications_python_client import prepare_upload
 from notifications_python_client.notifications import NotificationsAPIClient
 
 
-def handler(
-    event,
-    context,
-):
-    boto3.setup_default_session(region_name="eu-west-2")
+def handler(event, context):
+    # Constants
+    AWS_REGION = "eu-west-2"
+    SECRET_ID = "gov-uk-notify/production/api-key"
+    LOG_GROUP_NAMES = ["/aws/events/auth0/alpha-analytics-moj"]
+    EMAIL_ADDRESS = "CHANGE_ME@example.com"
+    TEMPLATE_ID = "de618989-db86-4d9a-aa55-4724d5485fa5"
+
+    boto3.setup_default_session(region_name=AWS_REGION)
     secrets_client = boto3.client("secretsmanager")
-    response = secrets_client.get_secret_value(
-        SecretId="gov-uk-notify/production/api-key",
-    )
+    response = secrets_client.get_secret_value(SecretId=SECRET_ID)
     api_key = response["SecretString"]
     notifications_client = NotificationsAPIClient(api_key)
 
     now = dt.now()
     current_date = dt.strftime(now.date(), "%d/%m/%Y")
 
+    # Query
     query = f"""fields detail.data.user_id as user, detail.data.user_name as email
     | filter detail.data.type = "s"
     | filter detail.data.connection = "github"
     | stats max(@timestamp) as last_login by "{current_date}" as effective_date, user, email
     | sort last_login desc
     """
+    secrets_client = boto3.client("secretsmanager")
+
+    # Date range
     year = dt.now().year
     current_month = dt.now().month
     previous_month = current_month - 1
@@ -40,26 +46,28 @@ def handler(
 
     start_datetime = dt(year, previous_month, 1, 0, 0, 0)
 
+   # Read logs
     dataframe = wr.cloudwatch.read_logs(
-        log_group_names=["/aws/events/auth0/alpha-analytics-moj"],
+        log_group_names=LOG_GROUP_NAMES,
         query=query,
         start_time=start_datetime,
         end_time=end_datetime,
     )
+    
 
+     # Datestamp
+    datestamp = end_datetime.strftime(format="%Y_%m_%d")
     dataframe["last_login"] = pd.to_datetime(
         dataframe["last_login"], format="%Y-%m-%d %H:%M:%S.%f"
     ).apply(lambda x: str(dt.strftime(x, "%Y/%m/%d")))
 
-    dataframe.to_excel("/tmp/test.xlsx", index=False)
-
-    # client = boto3.client('s3')
-    # client.upload_file('/tmp/test.xlsx', 'jml-export-bucket-REPLACE-ME', 'test.xlsx')
-    with open("/tmp/test.xlsx", "rb") as f:
+    # Save to Excel
+    #Send email notification
+    with open(excel_filename, "rb") as f:
         try:
             response = notifications_client.send_email_notification(
-                email_address="CHANGE_ME@example.com",
-                template_id="de618989-db86-4d9a-aa55-4724d5485fa5",
+                email_address=EMAIL_ADDRESS,
+                template_id=TEMPLATE_ID,
                 personalisation={
                     "date": current_date,
                     "link_to_file": prepare_upload(f),
