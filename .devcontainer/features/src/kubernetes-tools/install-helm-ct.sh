3131dabf14e9af36dc9f9b6891b2f9c14d75d836
Jacob Woffenden <jacob.woffenden@digital.justice.gov.uk>
2023-12-14 14:13:53 +0000

    Add Helm chart testing
    Signed-off-by: Jacob Woffenden <jacob.woffenden@digital.justice.gov.uk>

---
+++ .devcontainer/features/src/kubernetes-tools/install-helm-ct.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+set -e
+
+source /usr/local/bin/devcontainer-utils
+
+VERSION=${HELMCTVERSION:-"latest"}
+GITHUB_REPOSITORY="helm/chart-testing"
+
+if [ "${VERSION}" == "latest" ]; then
+  get_github_latest_tag ${GITHUB_REPOSITORY}
+  VERSION="${GITHUB_LATEST_TAG}"
+  VERSION_STRIP_V="${GITHUB_LATEST_TAG_STRIP_V}"
+else
+  VERSION="${VERSION}"
+fi
+
+### Install
+
+curl --location https://github.com/helm/chart-testing/releases/download/${VERSION}/chart-testing_${VERSION_STRIP_V}_linux_${ARCHITECTURE}.tar.gz \
+    --output chart-testing_${VERSION_STRIP_V}_linux_${ARCHITECTURE}.tar.gz
+
+tar -zxvf chart-testing_${VERSION_STRIP_V}_linux_${ARCHITECTURE}.tar.gz
+
+mv ct /usr/local/bin/ct
+
+chmod +x /usr/local/bin/ct
+
+rm --force --recursive chart-testing_${VERSION_STRIP_V}_linux_${ARCHITECTURE}.tar.gz LICENSE README.md /etc/chart_schema.yaml /etc/lintconf.yaml 
