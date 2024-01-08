8e63b5bf88588234958aa803c38b7a3832606419
priya.basker <shanmugapriya.basker@digital.justice.gov.uk>
2024-01-08 14:01:40 +0000

    added change log

--- containers/daap-python-base/tests/unit/curated_data/test_curated_data_query_builder.py
+++ containers/daap-python-base/tests/unit/curated_data/test_curated_data_query_builder.py
@@ -21,11 +21,8 @@ class TestCuratedDataQueryBuilder:
         assert dedent(result) == dedent(
             """
             UNLOAD (
-                SELECT
-                    CAST(NULLIF("foo",'') as VARCHAR) as "foo",
-                    CAST(NULLIF("bar",'') as None) as "bar",
-                    CAST(NULLIF("value",0) as real) as "value"
-                    '20230101T000000Z' as load_timestamp
+                SELECT CAST(NULLIF("foo",'') as VARCHAR) as "foo",CAST(NULLIF("bar",'') as None) as "bar",
+                CAST(NULLIF({col_name},0) as real) as "value",'20230101T000000Z' as load_timestamp
                 FROM data_products_raw.table_raw
             )
             TO 's3://bucket_name/curated_data/database_name=dataproduct/table_name=table_name/'
