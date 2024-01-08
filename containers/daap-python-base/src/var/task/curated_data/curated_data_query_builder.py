f253cadeddf222d5efd47badeb42d99af53b452e
priya.basker <shanmugapriya.basker@digital.justice.gov.uk>
2024-01-08 12:45:40 +0000

    fixed issue with unit test

--- containers/daap-python-base/src/var/task/curated_data/curated_data_query_builder.py
+++ containers/daap-python-base/src/var/task/curated_data/curated_data_query_builder.py
@@ -25,8 +25,9 @@ class CuratedDataQueryBuilder:
                 "string": "VARCHAR",
                 "float": "real"
             }
-            col_type = type_mapping.get(column_type, "default_type")
-            col_no_zero_len_str = f"NULLIF({col_name},'')"
+            col_type = type_mapping.get(column_type, column_type)
+            col_no_zero_len_str = f"NULLIF({col_name},'')" if col_type =='string'\
+                                   else f"NULLIF({col_name},0)"
             select_list.append(
                 f"CAST({col_no_zero_len_str} as {col_type}) as {col_name}"
             )
