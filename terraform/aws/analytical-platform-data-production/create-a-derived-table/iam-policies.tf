10b09032c93627d57c4fe00a0785271233112d96
gwionap <32736223+gwionap@users.noreply.github.com>
2024-01-10 17:13:44 +0000

    Update iam-policies.tf
    Add S3 permissions for OPG LPA dashboard app bucket

--- terraform/aws/analytical-platform-data-production/create-a-derived-table/iam-policies.tf
+++ terraform/aws/analytical-platform-data-production/create-a-derived-table/iam-policies.tf
@@ -18,6 +18,9 @@ data "aws_iam_policy_document" "create_a_derived_table" {
       "arn:aws:s3:::mojap-manage-offences",
       "arn:aws:s3:::mojap-hub-exports/probation_referrals_dump/*",
       "arn:aws:s3:::mojap-hub-exports",
+      "arn:aws:s3:::alpha-app-opg-lpa-dashboard",
+      "arn:aws:s3:::alpha-app-opg-lpa-dashboard/dev/models/domain_name=opg/*",
+      "arn:aws:s3:::alpha-app-opg-lpa-dashboard/prod/models/domain_name=opg/*"
     ]
   }
   statement {
