afa87715a53d112772808e903a8d7584aeac2bbf
Tom Webber <thomas.webber@digital.justice.gov.uk>
2024-01-16 17:10:15 +0000

    putting in pre-requisites for using MockDataHubGraph as per https://github.com/datahub-project/datahub/blob/master/metadata-ingestion/tests/unit/api/entities/dataproducts/test_dataproduct.py#L10C46-L10C62

---
+++ python-libraries/data-platform-catalogue/tests/conftest.py
@@ -0,0 +1,37 @@
+from tests.test_helpers.graph_helpers import MockDataHubGraph
+import difflib
+from pathlib import Path
+from typing import Any, Dict
+import pytest
+from freezegun import freeze_time
+
+from datahub.api.entities.dataproduct.dataproduct import DataProduct
+from datahub.metadata.schema_classes import DomainPropertiesClass
+from tests.test_helpers.graph_helpers import MockDataHubGraph
+from tests.test_helpers.mce_helpers import check_golden_file
+
+FROZEN_TIME = "2023-04-14 07:00:00"
+
+import pytest
+
+@pytest.fixture
+def base_entity_metadata():
+    return {
+        "urn:li:domain:12345": {
+            "domainProperties": DomainPropertiesClass(
+                name="Marketing", description="Marketing Domain"
+            )
+        }
+    }
+
+
+@pytest.fixture
+def base_mock_graph(
+    base_entity_metadata: Dict[str, Dict[str, Any]]
+) -> MockDataHubGraph:
+    return MockDataHubGraph(entity_graph=base_entity_metadata)
+
+
+@pytest.fixture
+def test_resources_dir(pytestconfig: pytest.Config) -> Path:
+    return pytestconfig.rootpath / "tests/unit/api/entities/dataproducts"
--- python-libraries/data-platform-catalogue/tests/test_client.py
+++ python-libraries/data-platform-catalogue/tests/test_client.py
@@ -12,6 +12,107 @@ from data_platform_catalogue.entities import (
     TableMetadata,
 )
 from datahub.configuration.common import OperationalError
+from datahub.testing.compare_metadata_json import
+from datahub.ingestion.graph.client import DataHubGraph
+
+import difflib
+from pathlib import Path
+from typing import Any, Dict
+
+import pytest
+from freezegun import freeze_time
+
+from datahub.api.entities.dataproduct.dataproduct import DataProduct
+from datahub.metadata.schema_classes import DomainPropertiesClass
+from tests.test_helpers.graph_helpers import MockDataHubGraph
+from tests.test_helpers.mce_helpers import check_golden_file
+
+FROZEN_TIME = "2023-04-14 07:00:00"
+
+
+
+
+def check_yaml_golden_file(input_file: str, golden_file: str) -> bool:
+    with open(input_file) as input:
+        input_lines = input.readlines()
+
+    with open(golden_file) as golden:
+        golden_lines = golden.readlines()
+
+    diff_exists = False
+    for line in difflib.unified_diff(
+        input_lines, golden_lines, fromfile=input_file, tofile=golden_file, lineterm=""
+    ):
+        print(line)
+        diff_exists = True
+
+    return diff_exists
+
+
+@freeze_time(FROZEN_TIME)
+@pytest.mark.parametrize(
+    "data_product_filename, upsert,golden_filename",
+    [
+        ("dataproduct.yaml", False, "golden_dataproduct_out.json"),
+        ("dataproduct_upsert.yaml", True, "golden_dataproduct_out_upsert.json"),
+    ],
+    ids=["update", "upsert"],
+)
+def test_dataproduct_from_yaml(
+    pytestconfig: pytest.Config,
+    test_resources_dir: Path,
+    tmp_path: Path,
+    base_mock_graph: MockDataHubGraph,
+    data_product_filename: str,
+    upsert: bool,
+    golden_filename: str,
+) -> None:
+    data_product_file = test_resources_dir / data_product_filename
+    mock_graph = base_mock_graph
+    data_product = DataProduct.from_yaml(data_product_file, mock_graph)
+    assert data_product._resolved_domain_urn == "urn:li:domain:12345"
+    assert data_product.assets is not None
+    assert len(data_product.assets) == 3
+
+    for mcp in data_product.generate_mcp(upsert=upsert):
+        mock_graph.emit(mcp)
+
+    output_file = Path(tmp_path / "test_dataproduct_out.json")
+    mock_graph.sink_to_file(output_file)
+    golden_file = Path(test_resources_dir / golden_filename)
+    check_golden_file(pytestconfig, output_file, golden_file)
+
+
+@freeze_time(FROZEN_TIME)
+def test_dataproduct_from_datahub(
+    pytestconfig: pytest.Config,
+    test_resources_dir: Path,
+    tmp_path: Path,
+    base_mock_graph: MockDataHubGraph,
+) -> None:
+    mock_graph = base_mock_graph
+    golden_file = Path(test_resources_dir / "golden_dataproduct_out.json")
+    mock_graph.import_file(golden_file)
+
+    data_product: DataProduct = DataProduct.from_datahub(
+        mock_graph, id="urn:li:dataProduct:pet_of_the_week"
+    )
+    assert data_product.domain == "urn:li:domain:12345"
+    assert data_product.assets is not None
+    assert len(data_product.assets) == 3
+
+    # validate that output looks exactly the same
+
+    for mcp in data_product.generate_mcp(upsert=False):
+        mock_graph.emit(mcp)
+
+    output_file = Path(tmp_path / "test_dataproduct_to_datahub_out.json")
+    mock_graph.sink_to_file(output_file)
+    golden_file = Path(test_resources_dir / "golden_dataproduct_out.json")
+    check_golden_file(pytestconfig, output_file, golden_file)
+
+
+
 
 
 class TestCatalogueClient:
@@ -116,7 +217,7 @@ class TestCatalogueClient:
     @pytest.fixture
     def table(self):
         return TableMetadata(
-            name="my_table",
+            name="some-table",
             description="bla bla",
             column_details=[
                 {"name": "foo", "type": "string", "description": "a"},
@@ -341,7 +442,7 @@ class TestCatalogueClient:
 
         # this method makes 2 calls to 'graph.emit()', first is to update dataset, second is to update tags
         fqn = datahub_client.upsert_table(metadata=table)
-        fqn_out = "urn:li:dataset:(urn:li:dataPlatform:glue,my_table,PROD)"
+        fqn_out = "urn:li:dataset:(urn:li:dataPlatform:glue,some-table,PROD)"
 
         aspects_count = sum(
             [
@@ -360,7 +461,7 @@ class TestCatalogueClient:
             response_json = requests_mock.last_request.json()
         assert (
             json.loads(response_json["proposal"]["aspect"]["value"])["schemaName"]
-            == "my_table"
+            == "some-table"
         )
         assert response_json["proposal"]["entityUrn"] == fqn_out
         assert fqn == fqn_out
