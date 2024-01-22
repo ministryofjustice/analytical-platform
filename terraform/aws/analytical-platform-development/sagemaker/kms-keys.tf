d19aaa32946694bee26d49a1ee02b734dd448cf8
Anthony Fitzroy <anthony.fitzroy@justice.gov.uk>
2024-01-22 10:16:18 +0000

    fixed error in kms_keys.tf

--- terraform/aws/analytical-platform-development/sagemaker/kms-keys.tf
+++ terraform/aws/analytical-platform-development/sagemaker/kms-keys.tf
@@ -1,8 +1,7 @@
 ##################################################
-# EKS Encryption Key
+# EFS Encryption Key
 ##################################################
 
-resource "aws_kms_key" "eks" {
-  description         = "EKS Secret Encryption Key"
-  enable_key_rotation = true
+resource "aws_kms_key" "sagemaker_cmk" {
+  description = "EFS Secret Encryption Key for Sagemaker"
 }
