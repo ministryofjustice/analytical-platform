3f24eb6ee9f1fe40b6777ff4609e929d9afdc753
Anthony Fitzroy <anthony.fitzroy@justice.gov.uk>
2024-01-31 15:54:50 +0000

    revert changes to data.tf and iam_policies.tf

--- terraform/aws/analytical-platform-development/control-panel-message-broker/data.tf
+++ terraform/aws/analytical-platform-development/control-panel-message-broker/data.tf
@@ -1,7 +1,6 @@
-data "aws_iam_policy_document" "sqs_policy" {
+data "aws_iam_policy_document" "source" {
   statement {
     sid = "InboundManagementSqsMessages"
-
     actions = [
       "sqs:GetQueueAttributes",
       "sqs:GetQueueUrl",
@@ -10,9 +9,29 @@ data "aws_iam_policy_document" "sqs_policy" {
       "sqs:SendMessage"
     ]
 
+    resources = ["*"]
+
     principals {
       type        = "AWS"
       identifiers = [local.control_panel_api_arn]
     }
+  }
+}
+data "aws_iam_policy_document" "sqs_iam" {
+  source_policy_documents = [data.aws_iam_policy_document.source.json]
+  statement {
+    resources = [aws_sqs_queue.iam.arn]
+  }
+}
+data "aws_iam_policy_document" "sqs_s3" {
+  source_policy_documents = [data.aws_iam_policy_document.source.json]
+  statement {
+    resources = [aws_sqs_queue.s3.arn]
+  }
+}
+data "aws_iam_policy_document" "sqs_auth" {
+  source_policy_documents = [data.aws_iam_policy_document.source.json]
+  statement {
+    resources = [aws_sqs_queue.auth.arn]
   }
 }
