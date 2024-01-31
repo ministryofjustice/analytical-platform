5a2200affcfbf9c861e71235d830b81ce8da7975
Murdo Moyse <murdo.moyse1@justice.gov.uk>
2024-01-31 14:19:25 +0000

    Chnaged indentation

--- python-libraries/data-platform-catalogue/tests/test_search_types.py
+++ python-libraries/data-platform-catalogue/tests/test_search_types.py
@@ -2,11 +2,11 @@ from data_platform_catalogue.search_types import SortOption
 
 def test_format_sort_option():
     expected = {
-            "sortCriterion": {
-                "field": "test",
-                "sortOrder": "DESCENDING",
-            }
+        "sortCriterion": {
+            "field": "test",
+            "sortOrder": "DESCENDING",
         }
+    }
     result = SortOption(field="test", ascending=False)
 
     assert result.format() == expected
