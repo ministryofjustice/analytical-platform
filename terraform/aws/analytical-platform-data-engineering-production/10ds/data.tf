f48e3b086282919fd863d11fbdbfd08ec15e65cc
Emterry <123941245+Emterry@users.noreply.github.com>
2024-02-02 11:06:30 +0000

    add policy for kms (#3143)
    * add policy for kms
    
    * refactor
    
    * linter
    
    * add data
    
    * refactor
    
    * amend kms permissions
    
    * linter
    
    * spelling fix
    
    * amend key permissions
    
    * linter
    
    * amend kms permissions
    
    * remove unused value

--- terraform/aws/analytical-platform-data-engineering-production/10ds/data.tf
+++ terraform/aws/analytical-platform-data-engineering-production/10ds/data.tf
@@ -13,4 +13,10 @@ data "aws_iam_session_context" "session" {
   arn = data.aws_caller_identity.session.arn
 }
 
+data "aws_iam_roles" "analytical_platform_data_engineering_sso_administrator_access_roles" {
+  name_regex  = "AWSReservedSSO_AdministratorAccess_.*"
+  path_prefix = "/aws-reserved/sso.amazonaws.com/"
+}
+
 data "aws_availability_zones" "available" {}
+
