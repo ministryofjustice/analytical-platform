540ee7d67af2a55461c48baf78a1aa190b76084b
LavMatt <mattlaverty@gmail.com>
2024-02-16 16:49:11 +0000

    tests dataset properties return as expected

--- python-libraries/data-platform-catalogue/tests/test_integration_with_datahub_server.py
+++ python-libraries/data-platform-catalogue/tests/test_integration_with_datahub_server.py
@@ -4,7 +4,7 @@ Integration test that runs against a DataHub server
 Run with:
 export API_URL='https://catalogue.apps-tools.development.data-platform.service.justice.gov.uk/api'
 export JWT_TOKEN=******
-poetry run pytest tests/test_integration_with_server.py
+poetry run pytest tests/test_integration_with_datahub_server.py
 """
 
 import os
@@ -74,6 +74,21 @@ def test_upsert_test_hierarchy():
     # Ensure data went through
     assert client.graph.get_aspect(table_fqn, DatasetPropertiesClass)
     assert client.graph.get_aspect(table_fqn, SchemaMetadataClass)
+
+    dataset_properties = client.graph.get_aspect(
+        table_fqn, aspect_type=DatasetPropertiesClass
+    )
+    # check properties been loaded to datahub dataset
+    assert dataset_properties.description == table.description
+    assert dataset_properties.name == table.name
+    assert (
+        dataset_properties.customProperties["sourceDatasetName"]
+        == table.source_dataset_name
+    )
+    assert (
+        dataset_properties.customProperties["whereToAccessDataset"]
+        == table.where_to_access_dataset
+    )
 
 
 @runs_on_development_server
