3e4f2eb7c9ae270246196fce0eeed945b7eb0cb5
Jacob Woffenden <jacob.woffenden@digital.justice.gov.uk>
2024-02-26 16:20:47 +0000

    Add Auth0 connection client for Visual Studio Code GitHub
    Signed-off-by: Jacob Woffenden <jacob.woffenden@digital.justice.gov.uk>

--- terraform/auth0/alpha-analytics-moj/clients.tf
+++ terraform/auth0/alpha-analytics-moj/clients.tf
@@ -4,3 +4,8 @@ resource "auth0_client" "visual_studio_code" {
   callbacks           = ["https://*-vscode.tools.analytical-platform.service.justice.gov.uk/callback"]
   allowed_logout_urls = ["https://*-vscode.tools.analytical-platform.service.justice.gov.uk"]
 }
+
+resource "auth0_connection_client" "visual_studio_code_github" {
+  client_id     = auth0_client.visual_studio_code.id
+  connection_id = "con_RWgK9R3ISqKJLcZu"
+}
