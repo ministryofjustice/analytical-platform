1e64cf02aa7292d7c154ad444c560a4f2a3e930a
julialawrence <julia.lawrence@digital.justice.gov.uk>
2024-03-01 14:00:36 +0000

    Adding data-prod roles.

--- terraform/aws/analytical-platform-landing-production/ithc/iam-roles.tf
+++ terraform/aws/analytical-platform-landing-production/ithc/iam-roles.tf
@@ -35,3 +35,22 @@ module "analytical_platform_production_pen_tester" {
   ]
 }
 
+module "analytical_platform_data_production_pen_tester" {
+  source  = "terraform-aws-modules/iam/aws//modules/iam-assumable-role"
+  version = "5.35.0"
+  providers = {
+    aws = aws.analytical-platform-data-production
+  }
+  allow_self_assume_role = true
+  attach_readonly_policy = true
+  trusted_role_arns      = formatlist("arn:aws:iam::${var.account_ids["analytical-platform-landing-production"]}:user/%s", values(nonsensitive(local.ithc_testers)))
+
+  create_role = true
+
+  role_name             = "PenTester"
+  role_requires_mfa     = true
+  force_detach_policies = true
+  custom_role_policy_arns = [
+    "arn:aws:iam::aws:policy/SecurityAudit"
+  ]
+}
