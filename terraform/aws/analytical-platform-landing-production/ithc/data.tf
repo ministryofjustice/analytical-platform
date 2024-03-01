8674d0cae4a7ea87e901337f2c62df0ab4ddb2c8
julialawrence <julia.lawrence@digital.justice.gov.uk>
2024-03-01 14:09:53 +0000

    Removing unused datasources.

--- terraform/aws/analytical-platform-landing-production/ithc/data.tf
+++ terraform/aws/analytical-platform-landing-production/ithc/data.tf
@@ -1,25 +1,6 @@
 ##################################################
 # AWS
 ##################################################
-
-# Calling session
-data "aws_caller_identity" "session" {
-  provider = aws.session
-}
-
-data "aws_iam_session_context" "session" {
-  provider = aws.session
-
-  arn = data.aws_caller_identity.session.arn
-}
-
-data "aws_iam_roles" "analytical_platform_landing_production_sso_administrator_access_roles" {
-  name_regex  = "AWSReservedSSO_AdministratorAccess_.*"
-  path_prefix = "/aws-reserved/sso.amazonaws.com/"
-}
-
-data "aws_availability_zones" "available" {}
-
 data "aws_secretsmanager_secret" "ithc_testers" {
   arn = module.secrets_manager.secret_arn
 }
