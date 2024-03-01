a84a00db062c7e41014abdded6effa6259a1d5ce
julialawrence <julia.lawrence@digital.justice.gov.uk>
2024-03-01 13:41:36 +0000

    Adding AP-Prod roles.

--- terraform/aws/analytical-platform-landing-production/ithc/iam-roles.tf
+++ terraform/aws/analytical-platform-landing-production/ithc/iam-roles.tf
@@ -10,6 +10,28 @@ module "analytical_platform_development_pen_tester" {
 
   create_role = true
 
-  role_name         = "PenTester"
-  role_requires_mfa = true
+  role_name             = "PenTester"
+  role_requires_mfa     = true
+  force_detach_policies = true
 }
+
+module "analytical_platform_production_pen_tester" {
+  source  = "terraform-aws-modules/iam/aws//modules/iam-assumable-role"
+  version = "5.35.0"
+  providers = {
+    aws = aws.analytical-platform-production
+  }
+  allow_self_assume_role = true
+  attach_readonly_policy = true
+  trusted_role_arns      = formatlist("arn:aws:iam::${var.account_ids["analytical-platform-landing-production"]}:user/%s", values(nonsensitive(local.ithc_testers)))
+
+  create_role = true
+
+  role_name             = "PenTester"
+  role_requires_mfa     = true
+  force_detach_policies = true
+  custom_role_policy_arns = [
+    "arn:aws:iam::aws:policy/SecurityAudit"
+  ]
+}
+
