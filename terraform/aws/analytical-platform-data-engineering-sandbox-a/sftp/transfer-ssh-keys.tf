9d44f9db2d3f37db101dc4a7b94ac6fbd7eff92c
Gary H <26419401+Gary-H9@users.noreply.github.com>
2024-03-04 16:30:48 +0000

    ✏️ Add a dummy SSH key

--- terraform/aws/analytical-platform-data-engineering-sandbox-a/sftp/transfer-ssh-keys.tf
+++ terraform/aws/analytical-platform-data-engineering-sandbox-a/sftp/transfer-ssh-keys.tf
@@ -1,5 +1,5 @@
-resource "aws_transfer_ssh_key" "example" {
+resource "aws_transfer_ssh_key" "ssh_key" {
   server_id = aws_transfer_server.this.id
   user_name = aws_transfer_user.user.user_name
-  body      = "... SSH key ..."
+  body      = "ecdsa-sha2-nistp384 AAAAE2VjZHNhLXNoYTItbmlzdHAzODQAAAAIbmlzdHAzODQAAABhBMzzvEd8KCDG8lp9O6/D2tHK8aKZKIQiPWbcoPxr0I9CmDhr+DNVH8MnevXFrMx+aVreuK0lHEHWKInJBrxHXaK8OnsJjZwYjzAQSG4oBZYmHFW8r7xDkbKelOby0gNDzh=="
 }
