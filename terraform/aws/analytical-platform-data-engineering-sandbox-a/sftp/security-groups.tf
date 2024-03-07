92aec2fd27f552f11ac5882999c34516f8bd072d
Jacob Woffenden <jacob.woffenden@digital.justice.gov.uk>
2024-03-07 09:22:44 +0000

    multi tenancy
    Signed-off-by: Jacob Woffenden <jacob.woffenden@digital.justice.gov.uk>

--- terraform/aws/analytical-platform-data-engineering-sandbox-a/sftp/security-groups.tf
+++ terraform/aws/analytical-platform-data-engineering-sandbox-a/sftp/security-groups.tf
@@ -2,15 +2,3 @@ resource "aws_security_group" "transfer_server" {
   name   = "transfer-server"
   vpc_id = module.vpc.vpc_id
 }
-
-resource "aws_security_group_rule" "jacobwoffenden" {
-  type      = "ingress"
-  from_port = 2222
-  to_port   = 2222
-  protocol  = "tcp"
-  cidr_blocks = [
-    "90.246.52.170/32", # Home
-    "82.132.238.3/32"   # Work Mobile
-  ]
-  security_group_id = aws_security_group.transfer_server.id
-}
--- terraform/aws/analytical-platform-data-engineering-sandbox-a/sftp/transfer-user.tf
+++ terraform/aws/analytical-platform-data-engineering-sandbox-a/sftp/transfer-user.tf
@@ -1,87 +1,27 @@
-data "aws_iam_policy_document" "transfer_user_jacobwoffenden" {
-  statement {
-    sid    = "AllowKMS"
-    effect = "Allow"
-    actions = [
-      "kms:ReEncrypt*",
-      "kms:GenerateDataKey*",
-      "kms:Encrypt",
-      "kms:DescribeKey",
-      "kms:Decrypt",
-    ]
-    resources = [module.s3_landing_kms.key_arn]
-  }
-  # TODO: review the permissions
-  statement {
-    sid     = "AllowS3ListBucket"
-    effect  = "Allow"
-    actions = ["s3:ListBucket"]
-    resources = [
-      "arn:aws:s3:::${module.landing_bucket.s3_bucket_id}",
-      "arn:aws:s3:::${module.landing_bucket.s3_bucket_id}/jacobwoffenden/*"
-    ]
-  }
-  # TODO: review the permissions
-  statement {
-    sid       = "AllowS3ObjectActions"
-    effect    = "Allow"
-    actions   = ["s3:*"]
-    resources = ["arn:aws:s3:::${module.landing_bucket.s3_bucket_id}/jacobwoffenden/*"]
+locals {
+  sftp_users = {
+    "jacobwoffenden" = {
+      ssh_key     = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIN+3qaLVtn6Pd+DasWHhIOBoXEEhF9GZAG+DYfJBeySS Ministry of Justice"
+      cidr_blocks = ["90.246.52.170/32", "82.132.238.3/32"]
+    },
+    "garyhenderson" = {
+      ssh_key     = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAID2lrI7AhZ9Sy/JAVDfPPEkCZawuuVJ7MHg6NNAwYImb"
+      cidr_blocks = ["154.47.111.68/32"]
+    }
   }
 }
 
-module "transfer_user_jacobwoffenden_policy" {
-  #checkov:skip=CKV_TF_1:Module registry does not support commit hashes for versions
-  source  = "terraform-aws-modules/iam/aws//modules/iam-policy"
-  version = "~> 5.0"
+module "sftp_users" {
+  for_each = local.sftp_users
 
-  name_prefix = "transfer-user-jacobwoffenden"
+  source = "./modules/transfer-family/user"
 
-  policy = data.aws_iam_policy_document.transfer_user_jacobwoffenden.json
+  name        = each.key
+  ssh_key     = each.value.ssh_key
+  cidr_blocks = each.value.cidr_blocks
+
+  transfer_server                = aws_transfer_server.this.id
+  transfer_server_security_group = aws_security_group.transfer_server.id
+  landing_bucket                 = module.landing_bucket.s3_bucket_id
+  landing_bucket_kms_key         = module.s3_landing_kms.key_arn
 }
-
-module "transfer_user_jacobwoffenden_role" {
-  #checkov:skip=CKV_TF_1:Module registry does not support commit hashes for versions
-
-  source  = "terraform-aws-modules/iam/aws//modules/iam-assumable-role"
-  version = "~> 5.0"
-
-  create_role = true
-
-  role_name         = "transfer-user-jacobwoffenden"
-  role_requires_mfa = false
-
-  trusted_role_services = ["transfer.amazonaws.com"]
-
-  custom_role_policy_arns = [module.transfer_user_jacobwoffenden_policy.arn]
-}
-
-resource "aws_transfer_user" "jacobwoffenden" {
-  server_id = aws_transfer_server.this.id
-  user_name = "jacobwoffenden"
-  role      = module.transfer_user_jacobwoffenden_role.iam_role_arn
-
-  # This doesn't work unless optimised directory is disabled
-  # home_directory_type = "LOGICAL"
-  # home_directory_mappings {
-  #   entry  = "/upload"
-  #   target = "/${module.landing_bucket.s3_bucket_id}/jacobwoffenden/upload"
-  # }
-
-  # home_directory_mappings {
-  #   entry  = "/download"
-  #   target = "/${module.landing_bucket.s3_bucket_id}/jacobwoffenden/download"
-  # }
-
-  # This works
-  home_directory = "/${module.landing_bucket.s3_bucket_id}/jacobwoffenden" # TODO: do we need an SFTP specific landing bucket?
-
-  # TODO: Tagging
-}
-
-resource "aws_transfer_ssh_key" "jacobwoffenden" {
-  server_id = aws_transfer_server.this.id
-  user_name = aws_transfer_user.jacobwoffenden.user_name
-  body      = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIN+3qaLVtn6Pd+DasWHhIOBoXEEhF9GZAG+DYfJBeySS Ministry of Justice"
-}
-
