6cfb532521c8de86dcdaa64e88b1728b7251db4e
julialawrence <98329494+julialawrence@users.noreply.github.com>
2024-03-11 12:11:22 +0000

    🐛 Bugfix: Typos in policy name (#3680)
    * Adding bedrock policy.
    
    * Policy assignment.
    
    * Bug fix, policy name typo.

--- terraform/aws/analytical-platform-development/cluster/tooling-integration-iam-policies.tf
+++ terraform/aws/analytical-platform-development/cluster/tooling-integration-iam-policies.tf
@@ -61,7 +61,7 @@ data "aws_iam_policy_document" "bedrock_integration" {
   }
 }
 resource "aws_iam_policy" "bedrock_integration" {
-  name        = "anaytial-platform-bedrock-integration"
+  name        = "analytical-platform-bedrock-integration"
   description = "Permissions needed to allow access to Bedrock in Frankfurt from tooling."
   policy      = data.aws_iam_policy_document.bedrock_integration.json
 }
