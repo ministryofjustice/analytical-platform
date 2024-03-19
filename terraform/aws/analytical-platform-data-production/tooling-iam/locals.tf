df666f886c1bb4f0480a2e911e099e72fd96ca25
Tom Webber <thomas.webber@digital.justice.gov.uk>
2024-03-19 15:40:22 +0000

    redo map generation / value interpolation *the right way*

--- terraform/aws/analytical-platform-data-production/tooling-iam/locals.tf
+++ terraform/aws/analytical-platform-data-production/tooling-iam/locals.tf
@@ -1,8 +1,6 @@
 locals {
-  datahub_cp_irsa_role_arns = flatten(
-    [
-      for env, role_name in var.datahub_cp_irsa_role_names :
-      { (env) = "arn:aws:iam::${var.account_ids["cloud-platform"]}:role/${role_name}" }
-    ]
-  )
+  datahub_cp_irsa_role_arns = {
+    for env, role_name in var.datahub_cp_irsa_role_names :
+    env => "arn:aws:iam::${var.account_ids["cloud-platform"]}:role/${role_name}"
+  }
 }
