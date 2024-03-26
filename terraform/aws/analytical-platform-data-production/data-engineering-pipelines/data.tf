38fd2a57f37c01774e4cec91b5f0b9cd5623a768
julialawrence <98329494+julialawrence@users.noreply.github.com>
2024-03-26 15:09:26 +0000

    Importing Glue Ireland policy. (#3856)
    * Importing Glue Ireland policy.
    
    * newline added.
    
    * Removing partition var.
    
    * Adding dependabot config.

---
+++ terraform/aws/analytical-platform-data-production/data-engineering-pipelines/data.tf
@@ -0,0 +1,8 @@
+data "aws_caller_identity" "current" {}
+
+data "aws_region" "current" {}
+
+data "aws_iam_role" "glue_policy_role" {
+  for_each = toset(local.unique_role_names)
+  name     = each.value
+}
