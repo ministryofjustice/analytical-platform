2c6c214226c78825f17ee0b60a0fb0902d661795
julialawrence <julia.lawrence@digital.justice.gov.uk>
2024-03-26 15:25:07 +0000

    Bug fix: Adding data engineering SSO role to Ireland glue policy

--- terraform/aws/analytical-platform-data-production/data-engineering-pipelines/data.tf
+++ terraform/aws/analytical-platform-data-production/data-engineering-pipelines/data.tf
@@ -6,3 +6,14 @@ data "aws_iam_role" "glue_policy_role" {
   for_each = toset(local.unique_role_names)
   name     = each.value
 }
+
+
+data "aws_iam_roles" "aws_sso_modernisation_platform_data_eng" {
+  name_regex  = "AWSReservedSSO_modernisation-platform-data-eng_.*"
+  path_prefix = "/aws-reserved/sso.amazonaws.com/"
+}
+
+
+data "aws_iam_role" "aws_sso_modernisation_platform_data_eng" {
+  name = one(data.aws_iam_roles.aws_sso_modernisation_platform_data_eng.names)
+}
