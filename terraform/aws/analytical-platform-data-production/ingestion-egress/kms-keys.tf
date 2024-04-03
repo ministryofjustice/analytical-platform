8b28ae25d542edbb3edd860de3c64f25e5f935db
Jacob Woffenden <jacob.woffenden@digital.justice.gov.uk>
2024-04-03 13:17:01 +0100

    🚚 Ingestion egress component (#3857)
    Signed-off-by: Jacob Woffenden <jacob.woffenden@digital.justice.gov.uk>

---
+++ terraform/aws/analytical-platform-data-production/ingestion-egress/kms-keys.tf
@@ -0,0 +1,25 @@
+module "development_kms" {
+  #checkov:skip=CKV_TF_1:Module registry does not support commit hashes for versions
+
+  source  = "terraform-aws-modules/kms/aws"
+  version = "2.2.1"
+
+  aliases               = ["s3/mojap-data-production-bold-egress-development"]
+  description           = "MoJ AP BOLD Egress - Development"
+  enable_default_policy = true
+
+  deletion_window_in_days = 7
+}
+
+module "production_kms" {
+  #checkov:skip=CKV_TF_1:Module registry does not support commit hashes for versions
+
+  source  = "terraform-aws-modules/kms/aws"
+  version = "2.2.1"
+
+  aliases               = ["s3/mojap-data-production-bold-egress-production"]
+  description           = "MoJ AP BOLD Egress - Production"
+  enable_default_policy = true
+
+  deletion_window_in_days = 7
+}
