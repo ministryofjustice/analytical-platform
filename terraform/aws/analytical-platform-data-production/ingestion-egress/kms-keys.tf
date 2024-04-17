9e3e2746f43b2ce4eff3dadbc53e603671ebd3a3
Jacob Woffenden <jacob.woffenden@digital.justice.gov.uk>
2024-04-17 15:19:30 +0000

    Add eu-west-1 provider Add KMS replica
    Signed-off-by: GitHub <noreply@github.com>

--- terraform/aws/analytical-platform-data-production/ingestion-egress/kms-keys.tf
+++ terraform/aws/analytical-platform-data-production/ingestion-egress/kms-keys.tf
@@ -12,6 +12,25 @@ module "development_kms" {
   deletion_window_in_days = 7
 }
 
+module "development_kms_eu_west_1_replica" {
+  #checkov:skip=CKV_TF_1:Module registry does not support commit hashes for versions
+
+  source  = "terraform-aws-modules/kms/aws"
+  version = "2.2.1"
+
+  providers = {
+    aws = aws.eu-west-1
+  }
+
+  aliases               = ["s3/mojap-data-production-bold-egress-development-replica"]
+  description           = "MoJ AP BOLD Egress - Development - Replica"
+  enable_default_policy = true
+  create_replica        = true
+  primary_key_arn       = module.development_kms.key_arn
+
+  deletion_window_in_days = 7
+}
+
 module "production_kms" {
   #checkov:skip=CKV_TF_1:Module registry does not support commit hashes for versions
 
@@ -22,6 +41,26 @@ module "production_kms" {
   description           = "MoJ AP BOLD Egress - Production"
   enable_default_policy = true
   multi_region          = true
+
+  deletion_window_in_days = 7
+}
+
+
+module "production_kms_eu_west_1_replica" {
+  #checkov:skip=CKV_TF_1:Module registry does not support commit hashes for versions
+
+  source  = "terraform-aws-modules/kms/aws"
+  version = "2.2.1"
+
+  providers = {
+    aws = aws.eu-west-1
+  }
+
+  aliases               = ["s3/mojap-data-production-bold-egress-production-replica"]
+  description           = "MoJ AP BOLD Egress - Production - Replica"
+  enable_default_policy = true
+  create_replica        = true
+  primary_key_arn       = module.production_kms.key_arn
 
   deletion_window_in_days = 7
 }
