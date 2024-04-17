6cb3e0a2778de25419f9cb58111a94ee477090f1
Jacob Woffenden <jacob.woffenden@digital.justice.gov.uk>
2024-04-17 16:50:58 +0100

    🇮🇪 Add `eu-west-1` replica of BOLD KMS (#4108)
    Signed-off-by: Jacob Woffenden <jacob.woffenden@digital.justice.gov.uk>

--- terraform/aws/analytical-platform-data-production/ingestion-egress/terraform.tf
+++ terraform/aws/analytical-platform-data-production/ingestion-egress/terraform.tf
@@ -31,6 +31,17 @@ provider "aws" {
 }
 
 provider "aws" {
+  alias  = "eu-west-1"
+  region = "eu-west-1"
+  assume_role {
+    role_arn = "arn:aws:iam::${var.account_ids["analytical-platform-data-production"]}:role/GlobalGitHubActionAdmin"
+  }
+  default_tags {
+    tags = var.tags
+  }
+}
+
+provider "aws" {
   alias  = "analytical-platform-management-production"
   region = "eu-west-2"
   assume_role {
