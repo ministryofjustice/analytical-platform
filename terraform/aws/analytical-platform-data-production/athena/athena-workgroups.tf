52e7345d79b1b9852b785ab5bc1fa10ccde059f7
Jacob Woffenden <jacob.woffenden@digital.justice.gov.uk>
2024-04-17 17:24:01 +0000

    Add HMCTS
    Signed-off-by: GitHub <noreply@github.com>

--- terraform/aws/analytical-platform-data-production/athena/athena-workgroups.tf
+++ terraform/aws/analytical-platform-data-production/athena/athena-workgroups.tf
@@ -1,21 +1,101 @@
+resource "aws_athena_workgroup" "airflow_dev" {
+  name = "airflow-dev-workgroup"
+
+  configuration {
+    bytes_scanned_cutoff_per_query  = 1099511627776
+    enforce_workgroup_configuration = true
+    engine_version {
+      selected_engine_version = "Athena engine version 3"
+    }
+    result_configuration {
+      output_location = "s3://dbt-query-dump/"
+    }
+  }
+
+  tags = merge(var.tags,
+    {
+      "Name"             = "airflow-dev-workgroup"
+      "application"      = "airflow"
+      "environment-name" = "dev"
+      "is-production"    = "true"
+      "owner"            = "Data Engineering:dataengineering@digital.justice.gov.uk"
+    }
+  )
+}
+
 resource "aws_athena_workgroup" "airflow_prod" {
   name = "airflow-prod-workgroup"
 
   configuration {
-    bytes_scanned_cutoff_per_query  = 1000000000
-    enforce_workgroup_configuration = false
+    bytes_scanned_cutoff_per_query  = 1099511627776
+    enforce_workgroup_configuration = true
     engine_version {
       selected_engine_version = "Athena engine version 3"
+    }
+    result_configuration {
+      output_location = "s3://dbt-query-dump/"
     }
   }
 
   tags = merge(var.tags,
     {
       "Name"             = "airflow-prod-workgroup"
-      "application"      = "Airflow"
+      "application"      = "airflow"
       "environment-name" = "prod"
-      "is-production"    = "True"
+      "is-production"    = "true"
       "owner"            = "Data Engineering:dataengineering@digital.justice.gov.uk"
+    }
+  )
+}
+
+resource "aws_athena_workgroup" "airflow_dev_hmcts" {
+  name = "airflow-dev-workgroup-hmcts"
+
+  configuration {
+    bytes_scanned_cutoff_per_query  = 1099511627776
+    enforce_workgroup_configuration = true
+    engine_version {
+      selected_engine_version = "Athena engine version 3"
+    }
+    result_configuration {
+      output_location = "s3://dbt-query-dump/"
+    }
+  }
+
+  tags = merge(var.tags,
+    {
+      "Name"             = "airflow-dev-workgroup-hmcts"
+      "application"      = "airflow"
+      "environment-name" = "dev"
+      "is-production"    = "true"
+      "owner"            = "Data Engineering:dataengineering@digital.justice.gov.uk"
+      "business-unit"    = "HMCTS"
+    }
+  )
+}
+
+resource "aws_athena_workgroup" "airflow_prod_hmcts" {
+  name = "airflow-prod-workgroup-hmcts"
+
+  configuration {
+    bytes_scanned_cutoff_per_query  = 1099511627776
+    enforce_workgroup_configuration = true
+    engine_version {
+      selected_engine_version = "Athena engine version 3"
+    }
+    result_configuration {
+      output_location = "s3://dbt-query-dump/"
+    }
+  }
+
+  tags = merge(var.tags,
+    {
+      "Name"             = "airflow-prod-workgroup"
+      "application"      = "airflow"
+      "environment-name" = "prod"
+      "is-production"    = "true"
+      "owner"            = "Data Engineering:dataengineering@digital.justice.gov.uk"
+      "business-unit"    = "HMCTS"
     }
   )
 }
