f6026846c28a74624b44198c1491c88e517fd513
BrianEllwood <brian.ellwood@digital.justice.gov.uk>
2024-04-19 10:51:51 +0000

    add glue and athena to policy

--- terraform/aws/analytical-platform-data-production/github-airflow-cjs-dashboard-data/iam-policies.tf
+++ terraform/aws/analytical-platform-data-production/github-airflow-cjs-dashboard-data/iam-policies.tf
@@ -13,7 +13,50 @@ data "aws_iam_policy_document" "github_airflow_cjs_dashboard_data" {
       "arn:aws:s3:::alpha-cjs-scorecard/*"
     ]
   }
+
+  statement {
+    sid    = "AthenaAccess"
+    effect = "Allow"
+    actions = [
+      "athena:List*",
+      "athena:Get*",
+      "athena:StartQueryExecution",
+      "athena:StopQueryExecution"
+    ]
+    resources = [
+      "arn:aws:athena:*:${var.account_ids["analytical-platform-data-production"]}:datacatalog/AwsDataCatalog",
+      "arn:aws:athena:*:${var.account_ids["analytical-platform-data-production"]}:workgroup/primary"
+    ]
+  }
+  statement {
+    sid    = "GlueAccess"
+    effect = "Allow"
+    actions = [
+      "glue:Get*",
+      "glue:DeleteTable",
+      "glue:DeleteSchema",
+      "glue:DeletePartition",
+      "glue:UpdateTable",
+      "glue:UpdateSchema",
+      "glue:UpdatePartition",
+      "glue:UpdateDatabase",
+      "glue:CreateTable",
+      "glue:CreateSchema",
+      "glue:CreatePartition",
+      "glue:CreatePartitionIndex",
+      "glue:BatchCreatePartition"
+    ]
+    resources = [
+      # todo database name for line 52 and 53
+      "arn:aws:glue:*:${var.account_ids["analytical-platform-data-production"]}:schema/*",
+      "arn:aws:glue:*:${var.account_ids["analytical-platform-data-production"]}:database/*",
+      "arn:aws:glue:*:${var.account_ids["analytical-platform-data-production"]}:table/*/cjs_dashboard_temp_table",
+      "arn:aws:glue:*:${var.account_ids["analytical-platform-data-production"]}:catalog"
+    ]
+  }
 }
+
+
 
 module "github_airflow_cjs_dashboard_data_iam_policy" {
   #checkov:skip=CKV_TF_1:Module is from Terraform registry
