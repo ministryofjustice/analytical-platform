dd8fd5b9cde67798c085c4d0b970818877def84d
BrianEllwood <45009603+BrianEllwood@users.noreply.github.com>
2024-04-25 13:03:12 +0100

    Add Glue and Athena access to policy (#4153)
    * add glue and athena to policy
    
    * add database name
    
    * replaced wildcard actions with specific actions
    
    * removed commented out line
    
    * tidied whitespace
    
    * linting pt.1
    
    ---------
    
    Co-authored-by: Anthony Fitzroy <anthony.fitzroy@justice.gov.uk>
    Co-authored-by: Anthony Fitzroy <101649764+AntFMoJ@users.noreply.github.com>

--- terraform/aws/analytical-platform-data-production/github-airflow-cjs-dashboard-data/iam-policies.tf
+++ terraform/aws/analytical-platform-data-production/github-airflow-cjs-dashboard-data/iam-policies.tf
@@ -13,6 +13,64 @@ data "aws_iam_policy_document" "github_airflow_cjs_dashboard_data" {
       "arn:aws:s3:::alpha-cjs-scorecard/*"
     ]
   }
+
+  statement {
+    sid    = "AthenaAccess"
+    effect = "Allow"
+    actions = [
+      "athena:ListNamedQueries",
+      "athena:ListWorkGroups",
+      "athena:GetNamedQuery",
+      "athena:GetQueryExecution",
+      "athena:GetQueryResults",
+      "athena:GetQueryResultsStream",
+      "athena:GetWorkGroup",
+      "athena:GetCatalogs",
+      "athena:GetExecutionEngine",
+      "athena:GetExecutionEngines",
+      "athena:GetNamespace",
+      "athena:GetNamespaces",
+      "athena:GetTable",
+      "athena:GetTables",
+      "athena:StartQueryExecution",
+      "athena:StopQueryExecution"
+    ]
+    resources = [
+      "arn:aws:athena:*:${var.account_ids["analytical-platform-data-production"]}:datacatalog/AwsDataCatalog",
+      "arn:aws:athena:*:${var.account_ids["analytical-platform-data-production"]}:workgroup/primary"
+    ]
+  }
+
+  statement {
+    sid    = "GlueAccess"
+    effect = "Allow"
+    actions = [
+      "glue:GetTable",
+      "glue:GetTables",
+      "glue:GetDatabase",
+      "glue:GetDatabases",
+      "glue:GetPartition",
+      "glue:GetPartitions",
+      "glue:GetPartitionIndexes",
+      "glue:DeleteTable",
+      "glue:DeleteSchema",
+      "glue:DeletePartition",
+      "glue:UpdateTable",
+      "glue:UpdateSchema",
+      "glue:UpdatePartition",
+      "glue:UpdateDatabase",
+      "glue:CreateTable",
+      "glue:CreateSchema",
+      "glue:CreatePartition",
+      "glue:CreatePartitionIndex",
+      "glue:BatchCreatePartition"
+    ]
+    resources = [
+      "arn:aws:glue:*:${var.account_ids["analytical-platform-data-production"]}:database/cjs_dashboard_sandbox",
+      "arn:aws:glue:*:${var.account_ids["analytical-platform-data-production"]}:table/cjs_dashboard_sandbox/cjs_dashboard_temp_table",
+      "arn:aws:glue:*:${var.account_ids["analytical-platform-data-production"]}:catalog"
+    ]
+  }
 }
 
 module "github_airflow_cjs_dashboard_data_iam_policy" {
@@ -25,4 +83,3 @@ module "github_airflow_cjs_dashboard_data_iam_policy" {
 
   policy = data.aws_iam_policy_document.github_airflow_cjs_dashboard_data.json
 }
-
