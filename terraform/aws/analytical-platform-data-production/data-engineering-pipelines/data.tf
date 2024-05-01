dc4b0a96126a9146bf2aafbbba35dccd8f641376
julialawrence <julia.lawrence@digital.justice.gov.uk>
2024-05-01 19:07:18 +0100

    Linting fixes

--- terraform/aws/analytical-platform-data-production/data-engineering-pipelines/data.tf
+++ terraform/aws/analytical-platform-data-production/data-engineering-pipelines/data.tf
@@ -9,12 +9,10 @@ data "aws_iam_role" "glue_policy_role" {
   name     = each.value
 }
 
-
 data "aws_iam_roles" "aws_sso_modernisation_platform_data_eng" {
   name_regex  = "AWSReservedSSO_modernisation-platform-data-eng_.*"
   path_prefix = "/aws-reserved/sso.amazonaws.com/"
 }
-
 
 data "aws_iam_role" "aws_sso_modernisation_platform_data_eng" {
   name = one(data.aws_iam_roles.aws_sso_modernisation_platform_data_eng.names)
