1447a9c4df92b9bc263f7e83f233625c1c22a90c
Jacob Woffenden <jacob.woffenden@digital.justice.gov.uk>
2024-05-02 08:57:00 +0000

    a working cluster
    Signed-off-by: Jacob Woffenden <jacob.woffenden@digital.justice.gov.uk>

---
+++ terraform/aws/analytical-platform-development/compute/data.tf
@@ -0,0 +1,22 @@
+data "aws_caller_identity" "session" {
+  provider = aws.session
+}
+
+data "aws_iam_session_context" "session" {
+  provider = aws.session
+
+  arn = data.aws_caller_identity.session.arn
+}
+
+data "aws_region" "current" {}
+data "aws_caller_identity" "current" {}
+data "aws_availability_zones" "available" {}
+
+data "aws_iam_roles" "eks_sso_access_role" {
+  name_regex  = "AWSReservedSSO_${local.eks_sso_access_role}_.*"
+  path_prefix = "/aws-reserved/sso.amazonaws.com/"
+}
+
+output "eks_sso_access_role" {
+  value = one(data.aws_iam_roles.eks_sso_access_role.names)
+}
