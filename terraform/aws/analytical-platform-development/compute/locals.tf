db8e5e3b6d0ba65d23bc00db531707c639c45a11
Jacob Woffenden <jacob.woffenden@digital.justice.gov.uk>
2024-05-02 09:06:38 +0000

    Fix TFlint
    Signed-off-by: Jacob Woffenden <jacob.woffenden@digital.justice.gov.uk>

--- terraform/aws/analytical-platform-development/compute/locals.tf
+++ terraform/aws/analytical-platform-development/compute/locals.tf
@@ -1,8 +1,6 @@
 locals {
-  vpc_name                                = "development"
-  vpc_flow_logs_cloudwatch_log_group_name = "/aws/vpc/${local.vpc_name}/flow-logs"
+  vpc_name = "development"
 
-  eks_cluster_name              = "development"
-  eks_cloudwatch_log_group_name = "/aws/eks/${local.eks_cluster_name}/cluster"
-  eks_sso_access_role           = "AdministratorAccess"
+  eks_cluster_name    = "development"
+  eks_sso_access_role = "AdministratorAccess"
 }
