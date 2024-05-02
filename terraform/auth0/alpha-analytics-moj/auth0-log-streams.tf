445995ebf3c752e845a873922e4076bcf8c23174
Jason Birchall <31217584+jasonBirchall@users.noreply.github.com>
2024-05-02 16:35:07 +0100

    ✨ Stream Auth0 Logs to Operations Engineering Account (#4260)
    This commit enables streaming to the Operations Engineering AWS account in Mod Platform. By enabling this streaming, we are allowing Operations Engineering to identify users who only use GitHub to auth to various services, avoiding unnecessary account removal due to inactivity.

--- terraform/auth0/alpha-analytics-moj/auth0-log-streams.tf
+++ terraform/auth0/alpha-analytics-moj/auth0-log-streams.tf
@@ -19,3 +19,14 @@ resource "auth0_log_stream" "aws_eventbridge_analytical_platform_development" {
     aws_region     = "eu-west-2"
   }
 }
+
+resource "auth0_log_stream" "aws_eventbridge_operations_engineering_development" {
+  name   = "operations-engineering-development"
+  type   = "eventbridge"
+  status = "active"
+
+  sink {
+    aws_account_id = "211125434264"
+    aws_region     = "eu-west-2"
+  }
+}
