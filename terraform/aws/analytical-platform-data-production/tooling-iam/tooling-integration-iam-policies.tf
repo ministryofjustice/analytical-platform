50a5f1feaa410a85ef0ef5fde0b35a0f5758d4fe
Jacob Woffenden <jacob.woffenden@digital.justice.gov.uk>
2024-05-07 09:55:34 +0100

    🦝

--- terraform/aws/analytical-platform-data-production/tooling-iam/tooling-integration-iam-policies.tf
+++ terraform/aws/analytical-platform-data-production/tooling-iam/tooling-integration-iam-policies.tf
@@ -56,7 +56,7 @@ data "aws_iam_policy_document" "bedrock_integration" {
       variable = "aws:RequestedRegion"
       values   = [
         "eu-central-1",
-        "eu-west-3"
+        "eu-west-3",
         "us-east-1"
       ]
     }
