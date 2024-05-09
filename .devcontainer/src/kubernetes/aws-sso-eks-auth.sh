5d245be7201aee2e2b315b39bef8939aee743b7c
Jacob Woffenden <jacob.woffenden@digital.justice.gov.uk>
2024-05-09 16:27:11 +0000

    Add Kubernetes configuration
    Signed-off-by: Jacob Woffenden <jacob.woffenden@digital.justice.gov.uk>

---
+++ .devcontainer/src/kubernetes/aws-sso-eks-auth.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+if [[ -z "${AWS_SSO}" ]]; then
+  aws-sso exec --profile ${AWS_SSO_PROFILE} -- \
+    aws eks get-token \
+      --region ${AWS_REGION} \
+      --cluster-name ${AWS_EKS_CLUSTER_NAME}
+else
+  aws eks get-token \
+    --region ${AWS_REGION} \
+    --cluster-name ${AWS_EKS_CLUSTER_NAME}
+fi
