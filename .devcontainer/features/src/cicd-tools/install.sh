f7b1ee43b75d30144100702a2b446ba0908c18f4
Jacob Woffenden <jacob.woffenden@digital.justice.gov.uk>
2024-05-09 15:18:40 +0000

    Switch to new dev container pattern
    Signed-off-by: Jacob Woffenden <jacob.woffenden@digital.justice.gov.uk>

--- .devcontainer/features/src/cicd-tools/install.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-if [[ "${INSTALLACT}" == "true" ]]; then
-  bash $( dirname $0 )/install-act.sh
-fi
--- .devcontainer/features/test/aws-tools/install_version.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-set -e
-
-source dev-container-features-test-lib
-
-check "aws-cli version" aws --version | grep "aws-cli/2.9.21"
-
-reportResults
