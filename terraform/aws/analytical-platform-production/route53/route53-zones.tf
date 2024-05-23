b9cdfcf2f00717aa35a3ddd9eedaea48065f82ab
Jacob Woffenden <jacob.woffenden@digital.justice.gov.uk>
2024-05-23 15:16:00 +0100

    🌐 Import `analytical-platform.service.justice.gov.uk` (#4398)
    Signed-off-by: Jacob Woffenden <jacob.woffenden@digital.justice.gov.uk>

---
+++ terraform/aws/analytical-platform-production/route53/route53-zones.tf
@@ -0,0 +1,20 @@
+module "route53_zones" {
+  #checkov:skip=CKV_TF_1:Module registry does not support commit hashes for versions
+  #checkov:skip=CKV_TF_2:Module registry does not support tags for versions
+  #checkov:skip=CKV2_AWS_38:Will address in the future, this is just an import of the zone
+  #checkov:skip=CKV2_AWS_39:Will address in the future, this is just an import of the zone
+
+  source  = "terraform-aws-modules/route53/aws//modules/zones"
+  version = "2.11.1"
+
+  zones = {
+    "analytical-platform.service.justice.gov.uk" = {
+      comment = "Managed by Terraform"
+    }
+  }
+}
+
+import {
+  to = module.route53_zones.aws_route53_zone.this["analytical-platform.service.justice.gov.uk"]
+  id = "Z2TGLAURT6S808"
+}
