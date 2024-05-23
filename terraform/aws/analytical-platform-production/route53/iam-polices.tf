5d896c986dc1ad44339d6bdb25649b37f33f1c40
Jacob Woffenden <jacob.woffenden@digital.justice.gov.uk>
2024-05-23 17:45:43 +0100

    🛂 Create IAM role for Analytical Platform Compute Route53 (#4403)
    * Add AP Compute NS records
    
    Signed-off-by: Jacob Woffenden <jacob.woffenden@digital.justice.gov.uk>
    
    * Create cross account role for external DNS
    
    Signed-off-by: Jacob Woffenden <jacob.woffenden@digital.justice.gov.uk>
    
    * Add checkov skip for AWS Route53 IAM policy
    
    Signed-off-by: Jacob Woffenden <jacob.woffenden@digital.justice.gov.uk>
    
    * Add trivy
    
    Signed-off-by: Jacob Woffenden <jacob.woffenden@digital.justice.gov.uk>
    
    ---------
    
    Signed-off-by: Jacob Woffenden <jacob.woffenden@digital.justice.gov.uk>

---
+++ terraform/aws/analytical-platform-production/route53/iam-polices.tf
@@ -0,0 +1,35 @@
+#trivy:ignore:avd-aws-0057:Wildcard is suggested from listing zones
+data "aws_iam_policy_document" "analytical_platform_compute_route53_access" {
+  #checkov:skip=CKV_AWS_356:Wildcard is suggested from listing zones
+
+  statement {
+    sid    = "AllowRoute53List"
+    effect = "Allow"
+    actions = [
+      "route53:ListHostedZones",
+      "route53:ListResourceRecordSets",
+      "route53:ListTagsForResource"
+    ]
+    resources = ["*"]
+  }
+  statement {
+    sid    = "AllowRoute53ChangeResourceRecordSets"
+    effect = "Allow"
+    actions = [
+      "route53:ChangeResourceRecordSets"
+    ]
+    resources = [module.route53_zones.route53_zone_zone_arn["analytical-platform.service.justice.gov.uk"]]
+  }
+}
+
+module "analytical_platform_compute_route53_access_iam_policy" {
+  #checkov:skip=CKV_TF_1:Module registry does not support commit hashes for versions
+  #checkov:skip=CKV_TF_2:Module registry does not support tags for versions
+
+  source  = "terraform-aws-modules/iam/aws//modules/iam-policy"
+  version = "5.39.1"
+
+  name_prefix = "analytical-platform-compute-route53-access"
+
+  policy = data.aws_iam_policy_document.analytical_platform_compute_route53_access.json
+}
