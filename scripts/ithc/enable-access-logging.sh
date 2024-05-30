af0413025cee365707bb47978242c3131ace6e1b
Gary H <26419401+Gary-H9@users.noreply.github.com>
2024-05-30 16:54:05 +0000

    Rename

--- scripts/ithc/enable-access-logging.sh
+++ scripts/ithc/enable-access-logging.sh
@@ -4,12 +4,12 @@
 log_target_bucket="moj-analytics-s3-logs"
 
 # List of buckets where access logging is disabled
-buckets_to_enable_logging=("")
+buckets_to_enable_logging=("user-guidance.services.alpha.mojanalytics.xyz-cloudfront-logs")
 
 # Enable access logging for each bucket
 for bucket in "${buckets_to_enable_logging[@]}"; do
   echo "🪣 Enabling access logging for bucket: $bucket"
-  
+
   # Set the logging configuration
   aws s3api put-bucket-logging --bucket "$bucket" \
     --bucket-logging-status '{
@@ -18,7 +18,7 @@ for bucket in "${buckets_to_enable_logging[@]}"; do
         "TargetPrefix": "'$bucket/'"
       }
     }'
-  
+
   # Check if the command was successful
   if [ $? -ne 0 ]; then
     echo "❌ Failed to enable logging for bucket: $bucket"
