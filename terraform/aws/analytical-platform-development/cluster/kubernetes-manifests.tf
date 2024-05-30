23885da6ba37145365fec176001faa211a9cb855
BrianEllwood <brian.ellwood@digital.justice.gov.uk>
2024-05-30 07:49:18 +0000

    add slicing manefest

--- terraform/aws/analytical-platform-development/cluster/kubernetes-manifests.tf
+++ terraform/aws/analytical-platform-development/cluster/kubernetes-manifests.tf
@@ -1,3 +1,7 @@
 resource "kubernetes_manifest" "nvidia_device_plugin" {
   manifest = yamldecode(file("src/kubernetes/nvidia-device-plugin.yml"))
 }
+
+# resource "kubernetes_manifest" "nvidia_gpu_slicing" {
+#   manifest = yamldecode(file("src/kubernetes/nvidia-gpu-slicing.yml"))
+# }
