f6059ca4591526fae25a580aa35071cf5c1632bc
Jacob Woffenden <jacob.woffenden@digital.justice.gov.uk>
2024-05-30 14:10:33 +0100

    Update IAM module version Update CDAT role OIDC providers

--- terraform/aws/analytical-platform-data-production/create-a-derived-table/iam-policies.tf
+++ terraform/aws/analytical-platform-data-production/create-a-derived-table/iam-policies.tf
@@ -96,7 +96,7 @@ module "create_a_derived_table_iam_policy" {
   #checkov:skip=CKV_TF_1:Module is from Terraform registry
 
   source  = "terraform-aws-modules/iam/aws//modules/iam-policy"
-  version = "5.39.0"
+  version = "5.39.1"
 
   name_prefix = "create-a-derived-table"
 
