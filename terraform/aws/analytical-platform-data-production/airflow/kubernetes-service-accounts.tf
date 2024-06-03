6ce89494e4498e024210e44a7b611afc3056f25b
Jacob Hamblin-Pyke <jacob.hamblin-pyke@digital.justice.gov.uk>
2024-06-03 11:56:51 +0000

    Create initial structures for IRSA

---
+++ terraform/aws/analytical-platform-data-production/airflow/kubernetes-service-accounts.tf
@@ -0,0 +1,9 @@
+resource "kubernetes_service_account" "airflow" {
+  metadata {
+    namespace = kubernetes_namespace.dev_airflow.metadata[0].name
+    name      = "airflow"
+    annotations = {
+      "eks.amazonaws.com/role-arn" = "arn:aws:iam::593291632749:role/airflow_dev_monitoring"
+    }
+  }
+}
