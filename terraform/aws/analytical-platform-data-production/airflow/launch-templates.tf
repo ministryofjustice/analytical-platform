403491c2cb2e317220653585ca2c3c5903991f4e
Gary H <26419401+Gary-H9@users.noreply.github.com>
2024-06-05 16:36:08 +0000

    :wrench: Update high-memory node group AMI

--- terraform/aws/analytical-platform-data-production/airflow/launch-templates.tf
+++ terraform/aws/analytical-platform-data-production/airflow/launch-templates.tf
@@ -52,7 +52,7 @@ resource "aws_launch_template" "dev_standard" {
 
 resource "aws_launch_template" "dev_high_memory" {
   name          = "dev_high_memory"
-  image_id      = "ami-03857889452e262ff"
+  image_id      = "ami-0e169b410a06c4a29"
   instance_type = "r6i.8xlarge"
 
   disable_api_stop        = false
