4c730582d3d9ae68a8a280672a4734bebdb02c57
Emterry <123941245+Emterry@users.noreply.github.com>
2024-06-06 11:35:06 +0100

    oidc fix (#4495)

--- terraform/aws/analytical-platform-data-production/airflow/iam-openid-connect-providers.tf
+++ terraform/aws/analytical-platform-data-production/airflow/iam-openid-connect-providers.tf
@@ -9,3 +9,8 @@ resource "aws_iam_openid_connect_provider" "airflow_dev" {
   client_id_list  = ["sts.amazonaws.com"]
   thumbprint_list = [data.tls_certificate.airflow_dev_eks_cluster.certificates[0].sha1_fingerprint]
 }
+
+import {
+  to = aws_iam_openid_connect_provider.airflow_dev
+  id = "arn:aws:iam::593291632749:oidc-provider/oidc.eks.eu-west-1.amazonaws.com/id/59429428EBABBB9F911A173D7B8E8179"
+}
