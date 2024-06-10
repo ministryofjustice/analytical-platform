a3db38fa70d8a0111f8faaf46808abd6888e9789
Gary H <26419401+Gary-H9@users.noreply.github.com>
2024-06-10 15:29:16 +0000

    :wrench: Update kube-proxy Add-on

--- terraform/aws/analytical-platform-data-production/airflow/eks.tf
+++ terraform/aws/analytical-platform-data-production/airflow/eks.tf
@@ -289,7 +289,7 @@ resource "kubernetes_namespace" "kyverno_prod" {
 resource "aws_eks_addon" "kube_proxy_dev" {
   cluster_name                = var.dev_eks_cluster_name
   addon_name                  = "kube-proxy"
-  addon_version               = "v1.27.12-eksbuild.5"
+  addon_version               = "v1.28.8-eksbuild.5"
   resolve_conflicts_on_create = "OVERWRITE"
 }
 
