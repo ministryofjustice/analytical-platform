ea31a296b7ec271bcbce162da93f6ff29c2b4a97
Gary H <26419401+Gary-H9@users.noreply.github.com>
2024-06-11 12:33:33 +0000

    :wrench: Upgrade high-memory nodes via launch templates

--- terraform/aws/analytical-platform-data-production/airflow/eks.tf
+++ terraform/aws/analytical-platform-data-production/airflow/eks.tf
@@ -310,14 +310,14 @@ resource "aws_eks_addon" "coredns_dev" {
 resource "aws_eks_addon" "kube_proxy_prod" {
   cluster_name                = var.prod_eks_cluster_name
   addon_name                  = "kube-proxy"
-  addon_version               = "v1.25.14-eksbuild.2"
+  addon_version               = "v1.26.15-eksbuild.2"
   resolve_conflicts_on_create = "OVERWRITE"
 }
 
 resource "aws_eks_addon" "vpc_cni_prod" {
   cluster_name                = var.prod_eks_cluster_name
   addon_name                  = "vpc-cni"
-  addon_version               = "v1.16.0-eksbuild.1"
+  addon_version               = "v1.18.1-eksbuild.3"
   resolve_conflicts_on_create = "OVERWRITE"
 }
 
