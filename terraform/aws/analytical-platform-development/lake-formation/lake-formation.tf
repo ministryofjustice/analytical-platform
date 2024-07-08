1870b8a4f0d5d13215b2c5818039e89ff9bf8cb1
julialawrence <julia.lawrence@digital.justice.gov.uk>
2024-07-08 12:48:30 +0100

    Test terraform for lake-formation module

---
+++ terraform/aws/analytical-platform-development/lake-formation/lake-formation.tf
@@ -0,0 +1,33 @@
+data "aws_caller_identity" "session" {
+  provider = aws.session
+}
+
+data "aws_iam_session_context" "session" {
+  provider = aws.session
+
+  arn = data.aws_caller_identity.session.arn
+}
+
+module "lake_formation" {
+  source = "git::https://github.com/ministryofjustice/terraform-aws-analytical-platform-lakeformation.git?ref=feature/configure-lake-formation-sharing"
+  data_locations = [
+    {
+      data_location = "arn:aws:s3:::lf-antfmoj-test"
+      hybrid_mode   = true
+      register      = true
+      share         = true
+    }
+  ]
+  databases_to_share = [
+    {
+      name        = "antfmoj_test_db"
+      target_db   = "antfmoj_test_db_target"
+      permissions = ["DESCRIBE"]
+    }
+  ]
+  providers = {
+    aws = aws
+    # aws.target_account = aws.analytical-platform-management-production
+    aws.target = aws.analytical-platform-management-production-eu-west-2
+  }
+}
