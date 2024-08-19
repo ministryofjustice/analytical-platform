65140d261fc4d3735b5fc92257742ba3f88f9167
Jacob Woffenden <jacob.woffenden@digital.justice.gov.uk>
2024-08-19 08:54:08 +0000

    Merge existing GHA components Add new role for bold_rr_pilot_families_nlp
    Signed-off-by: Jacob Woffenden <jacob.woffenden@digital.justice.gov.uk>

--- terraform/aws/analytical-platform-data-production/github-actions-roles/mojas-cjs-dashboard-app.tf
+++ terraform/aws/analytical-platform-data-production/github-actions-roles/mojas-cjs-dashboard-app.tf
@@ -23,3 +23,18 @@ module "cjs_dashboard_app_iam_policy" {
 
   policy = data.aws_iam_policy_document.cjs_dashboard_app.json
 }
+
+module "cjs_dashboard_app_iam_role" {
+  #checkov:skip=CKV_TF_1:Module is from Terraform registry
+
+  source  = "terraform-aws-modules/iam/aws//modules/iam-github-oidc-role"
+  version = "5.44.0"
+
+  name = "github-cjs-dashboard-app"
+
+  subjects = ["ministryofjustice/cjs-dashboard:*"]
+
+  policies = {
+    cjs_dashboard_app = module.cjs_dashboard_app_iam_policy.arn
+  }
+}
