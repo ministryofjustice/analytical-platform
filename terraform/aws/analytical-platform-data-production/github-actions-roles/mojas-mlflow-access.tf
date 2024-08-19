0d18941aa6033c3f59bc6d5e9733e5b0395a7c2c
Jacob Woffenden <jacob.woffenden@digital.justice.gov.uk>
2024-08-19 12:05:27 +0000

    Update policy
    Signed-off-by: Jacob Woffenden <jacob.woffenden@digital.justice.gov.uk>

--- terraform/aws/analytical-platform-data-production/github-actions-roles/mojas-mlflow-access.tf
+++ terraform/aws/analytical-platform-data-production/github-actions-roles/mojas-mlflow-access.tf
@@ -11,7 +11,68 @@ data "aws_iam_policy_document" "mlflow_access" {
     ]
     resources = [
       "arn:aws:s3:::alpha-analytical-platform-mlflow-development",
-      "arn:aws:s3:::alpha-analytical-platform-mlflow-development/*"
+      "arn:aws:s3:::alpha-analytical-platform-mlflow-development/*",
+      "arn:aws:s3:::mojap-athena-query-dump",
+      "arn:aws:s3:::mojap-athena-query-dump/*"
+    ]
+  }
+
+  statement {
+    sid    = "AthenaAccess"
+    effect = "Allow"
+    actions = [
+      "athena:ListNamedQueries",
+      "athena:ListWorkGroups",
+      "athena:GetNamedQuery",
+      "athena:GetQueryExecution",
+      "athena:GetQueryResults",
+      "athena:GetQueryResultsStream",
+      "athena:GetWorkGroup",
+      "athena:GetCatalogs",
+      "athena:GetExecutionEngine",
+      "athena:GetExecutionEngines",
+      "athena:GetNamespace",
+      "athena:GetNamespaces",
+      "athena:GetTable",
+      "athena:GetTables",
+      "athena:StartQueryExecution",
+      "athena:StopQueryExecution",
+      "athena:GetTableMetadata"
+    ]
+    resources = [
+      "arn:aws:athena:*:${var.account_ids["analytical-platform-data-production"]}:datacatalog/AwsDataCatalog",
+      "arn:aws:athena:*:${var.account_ids["analytical-platform-data-production"]}:workgroup/primary"
+    ]
+  }
+
+  statement {
+    sid    = "GlueAccess"
+    effect = "Allow"
+    actions = [
+      "glue:GetTable",
+      "glue:GetTables",
+      "glue:GetDatabase",
+      "glue:GetDatabases",
+      "glue:GetPartition",
+      "glue:GetPartitions",
+      "glue:GetPartitionIndexes",
+      "glue:DeleteTable",
+      "glue:DeleteSchema",
+      "glue:DeletePartition",
+      "glue:UpdateTable",
+      "glue:UpdateSchema",
+      "glue:UpdatePartition",
+      "glue:UpdateDatabase",
+      "glue:CreateTable",
+      "glue:CreateSchema",
+      "glue:CreatePartition",
+      "glue:CreatePartitionIndex",
+      "glue:BatchCreatePartition"
+    ]
+    resources = [
+      "arn:aws:glue:*:${var.account_ids["analytical-platform-data-production"]}:database/mlflow_access",
+      "arn:aws:glue:*:${var.account_ids["analytical-platform-data-production"]}:table/mlflow_access/users",
+      "arn:aws:glue:*:${var.account_ids["analytical-platform-data-production"]}:catalog"
     ]
   }
 }
