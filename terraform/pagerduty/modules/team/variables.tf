c210fc1aac6bf948f779a07d37dd3027e1ec5ce7
julialawrence <julia.lawrence@digital.justice.gov.uk>
2024-09-17 17:53:00 +0100

    Comment explaining my thinking

--- terraform/pagerduty/modules/team/variables.tf
+++ terraform/pagerduty/modules/team/variables.tf
@@ -20,3 +20,7 @@ variable "responders" {
   description = "List of user IDs to be added as responders to the team"
   default     = {}
 }
+
+
+# I think there are better ways to get here than explicitly defining the managers and responders as maps of objects rather than strings except I think in map of strings, the ids were still somehow were being used as a key which is why it was complaining.
+# So...I wasn't entirely off-track and in similar situations, I think making sure that there's no way that an unknown value is gonna be a key in a for_each will resolve it. I did it via explicitly defining the key of the object to be something else.
