a7e168eb3be9780dfe92f397f615a460fb40bbd6
jamesstottmoj <james.stott@digital.justice.gov.uk>
2024-10-03 11:05:40 +0100

    Revert change to description

--- terraform/aws/analytical-platform-development/cluster/tooling-integration-iam-policies.tf
+++ terraform/aws/analytical-platform-development/cluster/tooling-integration-iam-policies.tf
@@ -118,7 +118,7 @@ data "aws_iam_policy_document" "textract_integration" {
 
 resource "aws_iam_policy" "bedrock_integration" {
   name        = "analytical-platform-bedrock-integration"
-  description = "Permissions needed to allow access to Bedrock from tooling."
+  description = "Permissions needed to allow access to Bedrock in Frankfurt from tooling."
   policy      = data.aws_iam_policy_document.bedrock_integration.json
 }
 
