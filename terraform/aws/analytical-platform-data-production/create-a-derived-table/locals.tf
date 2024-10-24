25c9a5298b8c1c22942ec5a2bfc8245b9247794e
Tom Webber <thomas.webber@digital.justice.gov.uk>
2024-10-24 14:22:43 +0100

    feat: add kms key id

--- terraform/aws/analytical-platform-data-production/create-a-derived-table/locals.tf
+++ terraform/aws/analytical-platform-data-production/create-a-derived-table/locals.tf
@@ -1,3 +1,4 @@
 locals {
-  mojap_apc_prod_cadet_replication_bucket = "mojap-compute-production-derived-tables-replication"
+  mojap_apc_prod_cadet_replication_bucket     = "mojap-compute-production-derived-tables-replication"
+  mojap_apc_prod_cadet_replication_kms_key_id = "1223ea30-ba03-487b-81dc-509126ac8a2b"
 }
