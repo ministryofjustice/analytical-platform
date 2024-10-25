c742640c2f16e6a0392552739102c130a80eeb32
Tom Webber <thomas.webber@digital.justice.gov.uk>
2024-10-25 11:58:44 +0100

    fix: revert use of aws_kms_alias for apc-prod kms key id

--- terraform/aws/analytical-platform-data-production/create-a-derived-table/data.tf
+++ terraform/aws/analytical-platform-data-production/create-a-derived-table/data.tf
@@ -2,10 +2,6 @@ data "aws_caller_identity" "current" {}
 
 data "aws_canonical_user_id" "current" {}
 
-data "aws_kms_alias" "mojap_apc_prod_cadet_replication_bucket" {
-  name = "alias/s3/mojap-derived-tables-replication"
-}
-
 data "aws_kms_alias" "s3_source" {
   name = "alias/aws/s3"
 }
--- terraform/aws/analytical-platform-data-production/create-a-derived-table/locals.tf
+++ terraform/aws/analytical-platform-data-production/create-a-derived-table/locals.tf
@@ -1,5 +1,6 @@
 locals {
-  mojap_apc_prod_cadet_replication_bucket = "mojap-compute-production-derived-tables-replication"
-  destination_region                      = "eu-west-2"
-  default_region                          = "eu-west-1"
+  mojap_apc_prod_cadet_replication_bucket     = "mojap-compute-production-derived-tables-replication"
+  mojap_apc_prod_cadet_replication_kms_key_id = "1223ea30-ba03-487b-81dc-509126ac8a2b" #gitleaks:allow
+  destination_region                          = "eu-west-2"
+  default_region                              = "eu-west-1"
 }
