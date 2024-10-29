b0d4dffd498db818239895f30640cfac5844d479
Tom Webber <thomas.webber@digital.justice.gov.uk>
2024-10-29 09:58:33 +0000

    fix: update kms key in ap-compute-prod to reflect new key in correct region

--- terraform/aws/analytical-platform-data-production/create-a-derived-table/locals.tf
+++ terraform/aws/analytical-platform-data-production/create-a-derived-table/locals.tf
@@ -1,6 +1,6 @@
 locals {
   mojap_apc_prod_cadet_replication_bucket     = "mojap-compute-production-derived-tables-replication"
-  mojap_apc_prod_cadet_replication_kms_key_id = "1223ea30-ba03-487b-81dc-509126ac8a2b" #gitleaks:allow
+  mojap_apc_prod_cadet_replication_kms_key_id = "c85993f5-6182-4a01-a318-25c193cb9f65" #gitleaks:allow
   destination_region                          = "eu-west-1"
   default_region                              = "eu-west-1"
 }
