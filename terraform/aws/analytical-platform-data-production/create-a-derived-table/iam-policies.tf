f239eca664051dcb02ec637f5cb558312cb2454c
Tom Webber <thomas.webber@digital.justice.gov.uk>
2024-10-29 15:17:36 +0000

    feat: add `s3:InitiateReplication` permission for replication iam policy
    see https://docs.aws.amazon.com/AmazonS3/latest/userguide/batch-ops-iam-role-policies.html#batch-ops-batch-replication-policy for example policy for this batch replication

--- terraform/aws/analytical-platform-data-production/create-a-derived-table/iam-policies.tf
+++ terraform/aws/analytical-platform-data-production/create-a-derived-table/iam-policies.tf
@@ -15,9 +15,10 @@ data "aws_iam_policy_document" "mojap_cadet_production_replication" {
     sid    = "SourceBucketManifestPermissions"
     effect = "Allow"
     actions = [
-      "s3:PutObject",
       "s3:GetObject",
-      "s3:GetObjectVersion"
+      "s3:GetObjectVersion",
+      "s3:InitiateReplication",
+      "s3:PutObject"
     ]
     resources = ["${module.mojap_cadet_production.s3_bucket_arn}/batch-manifest/*"]
   }
