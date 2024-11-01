7c0d30502f0c03ef6ba8b4a6d72c1869f76c6d08
Jacob Woffenden <jacob.woffenden@digital.justice.gov.uk>
2024-11-01 09:08:58 +0000

    add non-spot sa

--- terraform/aws/analytical-platform-data-production/github-actions-roles/mojas-create-a-derived-table.tf
+++ terraform/aws/analytical-platform-data-production/github-actions-roles/mojas-create-a-derived-table.tf
@@ -121,7 +121,10 @@ module "create_a_derived_table_iam_role" {
   oidc_providers = {
     analytical-platform-compute-production = {
       provider_arn               = "arn:aws:iam::593291632749:oidc-provider/oidc.eks.eu-west-2.amazonaws.com/id/801920EDEF91E3CAB03E04C03A2DE2BB"
-      namespace_service_accounts = ["actions-runners:actions-runner-mojas-create-a-derived-table"]
+      namespace_service_accounts = [
+        "actions-runners:actions-runner-mojas-create-a-derived-table",
+        "actions-runners:actions-runner-mojas-create-a-derived-table-non-spot"
+      ]
     }
     cloud-platform = {
       provider_arn               = "arn:aws:iam::593291632749:oidc-provider/oidc.eks.eu-west-2.amazonaws.com/id/DF366E49809688A3B16EEC29707D8C09"
