6d2c09e24bccbe1316cc514d1225b79e5dfb143d
Murdo <109604278+murdo-moj@users.noreply.github.com>
2024-11-05 14:14:02 +0000

    Added glue role for find moj data (#5971)
    * Added glue role for find moj data

---
+++ terraform/aws/analytical-platform-data-production/github-actions-roles/find-moj-data-glue-ingestion.tf
@@ -0,0 +1,41 @@
+data "aws_iam_policy_document" "find_moj_data_glue_ingestion" {
+  statement {
+    sid    = "GlueMetadataAccess"
+    effect = "Allow"
+    actions = [
+      "glue:GetDatabases",
+      "glue:GetTables"
+    ]
+    resources = [
+      "arn:aws:glue:*:${var.account_ids["analytical-platform-data-production"]}:catalog",
+      "arn:aws:glue:*:${var.account_ids["analytical-platform-data-production"]}:database/*",
+      "arn:aws:glue:*:${var.account_ids["analytical-platform-data-production"]}:table/*"
+    ]
+  }
+}
+
+module "find_moj_data_glue_access_iam_policy" {
+  #checkov:skip=CKV_TF_1:Module is from Terraform registry
+
+  source  = "terraform-aws-modules/iam/aws//modules/iam-policy"
+  version = "5.47.1"
+
+  name_prefix = "github-find-moj-data-glue-access"
+
+  policy = data.aws_iam_policy_document.find_moj_data_glue_ingestion.json
+}
+
+module "find_moj_data_glue_access_iam_role" {
+  #checkov:skip=CKV_TF_1:Module is from Terraform registry
+
+  source  = "terraform-aws-modules/iam/aws//modules/iam-github-oidc-role"
+  version = "5.47.1"
+
+  name = "github-find-moj-data-glue-access"
+
+  subjects = ["ministryofjustice/data-catalogue:*"]
+
+  policies = {
+    github_find_moj_data_glue_access = module.find_moj_data_glue_access_iam_policy.arn
+  }
+}
