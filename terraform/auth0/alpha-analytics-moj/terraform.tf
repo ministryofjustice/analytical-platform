96b736b56355e64fe097e6c96182baee665759e4
Jacob Woffenden <jacob.woffenden@justice.gov.uk>
2024-12-02 10:12:52 +0000

    Migrate logs to DP
    Signed-off-by: Jacob Woffenden <jacob.woffenden@justice.gov.uk>

--- terraform/auth0/alpha-analytics-moj/terraform.tf
+++ terraform/auth0/alpha-analytics-moj/terraform.tf
@@ -10,7 +10,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "5.75.1"
+      version = "5.78.0"
     }
     auth0 = {
       source  = "auth0/auth0"
---
+++ terraform/aws/analytical-platform-data-production/auth0-log-streams/main.tf
@@ -0,0 +1,16 @@
+locals {
+  auth0_log_streams = {
+    "alpha-analytics-moj" = {
+      event_source_name = "aws.partner/auth0.com/alpha-analytics-moj-8b47abb4-3107-4f42-9a8f-f13126a80493/auth0.logs"
+    }
+  }
+}
+
+module "auth0_log_streams" {
+  source = "./modules/auth0-log-streams"
+
+  for_each = local.auth0_log_streams
+
+  name              = each.key
+  event_source_name = each.value.event_source_name
+}
