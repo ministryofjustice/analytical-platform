51ca4c1375bfc5e3551ba83e5e2f1b4e245aa008
Tom Webber <thomas.webber@digital.justice.gov.uk>
2024-12-03 15:12:24 +0000

    fix: noncurrent_version_expiration.days must be at least `1`
    chore: linting

--- terraform/aws/analytical-platform-data-production/create-a-derived-table/buckets.tf
+++ terraform/aws/analytical-platform-data-production/create-a-derived-table/buckets.tf
@@ -1,4 +1,5 @@
 #trivy:ignore:avd-aws-0132:Replicating existing bucket that does not encrypt data with a customer managed key
+#trivy:ignore:avd-aws-0090:Bucket versioning is not preferred for this bucket for now as data is produced on-demand (and versioning is expensive)
 module "mojap_cadet_production" {
   #checkov:skip=CKV_TF_1:Module registry does not support commit hashes for versions
 
@@ -43,7 +44,7 @@ module "mojap_cadet_production" {
       id      = "remove-object-versions"
 
       noncurrent_version_expiration = {
-        days = 0
+        days = 1
       }
     },
     {
