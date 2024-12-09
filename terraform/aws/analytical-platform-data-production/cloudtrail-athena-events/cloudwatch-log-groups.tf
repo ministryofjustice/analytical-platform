798b7ad56cb91fb77336894247d4bf4453234dd9
Jacob Woffenden <jacob.woffenden@justice.gov.uk>
2024-12-09 09:05:34 +0000

    Add retention_in_days

--- terraform/aws/analytical-platform-data-production/cloudtrail-athena-events/cloudwatch-log-groups.tf
+++ terraform/aws/analytical-platform-data-production/cloudtrail-athena-events/cloudwatch-log-groups.tf
@@ -4,5 +4,6 @@ module "cloudtrail_athena_events_log_group" {
   source  = "terraform-aws-modules/cloudwatch/aws//modules/log-group"
   version = "5.6.1"
 
-  name = "cloudtrail-athena-events"
+  name              = "cloudtrail-athena-events"
+  retention_in_days = 400
 }
