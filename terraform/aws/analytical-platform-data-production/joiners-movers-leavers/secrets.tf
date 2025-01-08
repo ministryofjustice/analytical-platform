0e6b6f7b9350f4dec9522cae2fe056550c040509
Gary H <26419401+Gary-H9@users.noreply.github.com>
2025-01-08 14:43:07 +0000

    Refactor

--- terraform/aws/analytical-platform-data-production/joiners-movers-leavers/secrets.tf
+++ terraform/aws/analytical-platform-data-production/joiners-movers-leavers/secrets.tf
@@ -1,11 +1,11 @@
 resource "aws_secretsmanager_secret" "govuk_notify_api_key" {
-  # checkov:skip=CKV2_AWS_57:Reason for ignoring CKV2_AWS_57
-  # checkov:skip=CKV2_AWS_149:Reason for ignoring CKV2_AWS_149
+  # checkov:skip=CKV2_AWS_57:These won't be rotated in the traditional manner
+  # checkov:skip=CKV_AWS_149:No KMS key needed as per above, these won't be rotated
   name = "gov-uk-notify/production/api-key"
 }
 
 resource "aws_secretsmanager_secret" "jml_email" {
-  # checkov:skip=CKV2_AWS_57:Reason for ignoring CKV2_AWS_57
-  # checkov:skip=CKV2_AWS_149:Reason for ignoring CKV2_AWS_149
+  # checkov:skip=CKV2_AWS_57:These won't be rotated in the traditional manner
+  # checkov:skip=CKV_AWS_149:No KMS key needed as per above, these won't be rotated
   name = "jml/email"
 }
