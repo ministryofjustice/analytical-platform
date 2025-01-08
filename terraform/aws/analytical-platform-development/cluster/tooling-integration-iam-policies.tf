290629c1eae1ac5ada966fbf6db6ee6ebc7203f9
Emterry <123941245+Emterry@users.noreply.github.com>
2025-01-08 12:38:07 +0000

    update bedrock permissions for access to AWS Bedrock batch inference (#6522)
    update bedrock permissions

--- terraform/aws/analytical-platform-development/cluster/tooling-integration-iam-policies.tf
+++ terraform/aws/analytical-platform-development/cluster/tooling-integration-iam-policies.tf
@@ -50,7 +50,11 @@ data "aws_iam_policy_document" "bedrock_integration" {
       "bedrock:CreateFoundationModelAgreement",
       "bedrock:DeleteFoundationModelAgreement",
       "bedrock:ListFoundationModelAgreementOffers",
-      "bedrock:GetUseCaseForModelAccess"
+      "bedrock:GetUseCaseForModelAccess",
+      "bedrock:CreateModelInvocationJob",
+      "bedrock:GetModelInvocationJob",
+      "bedrock:ListModelInvocationJobs",
+      "bedrock:StopModelInvocationJob"
     ]
 
     resources = ["*"]
