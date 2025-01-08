336c053aeb8ad0bd51ba9426b5d5c5f9755d0285
Gary H <26419401+Gary-H9@users.noreply.github.com>
2025-01-08 13:53:48 +0000

    Add checkov skips

--- terraform/aws/analytical-platform-data-production/joiners-movers-leavers/secrets.tf
+++ terraform/aws/analytical-platform-data-production/joiners-movers-leavers/secrets.tf
@@ -1,7 +1,11 @@
 resource "aws_secretsmanager_secret" "govuk_notify_api_key" {
+  # checkov:skip=CKV2_AWS_57:Reason for ignoring CKV2_AWS_57
+  # checkov:skip=CKV2_AWS_149:Reason for ignoring CKV2_AWS_149
   name = "gov-uk-notify/production/api-key"
 }
 
 resource "aws_secretsmanager_secret" "jml_email" {
+  # checkov:skip=CKV2_AWS_57:Reason for ignoring CKV2_AWS_57
+  # checkov:skip=CKV2_AWS_149:Reason for ignoring CKV2_AWS_149
   name = "jml/email"
 }
