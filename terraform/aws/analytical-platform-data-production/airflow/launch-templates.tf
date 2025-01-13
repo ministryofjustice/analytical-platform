1d5355d86a9f74f95420442fa14a0130ff147df9
BrianEllwood <brian.ellwood@digital.justice.gov.uk>
2025-01-13 12:13:37 +0000

    a couple more to destroy

--- terraform/aws/analytical-platform-data-production/airflow/launch-templates.tf
+++ terraform/aws/analytical-platform-data-production/airflow/launch-templates.tf
@@ -50,57 +50,6 @@ resource "aws_launch_template" "dev_standard" {
   }
 }
 
-resource "aws_launch_template" "dev_high_memory" {
-  name          = "dev_high_memory"
-  image_id      = "ami-064e6ccd9f6d74534"
-  instance_type = "r6i.8xlarge"
-
-  disable_api_stop        = false
-  disable_api_termination = false
-
-  security_group_names = []
-
-  user_data = "TUlNRS1WZXJzaW9uOiAxLjAKQ29udGVudC1UeXBlOiBtdWx0aXBhcnQvbWl4ZWQ7IGJvdW5kYXJ5PSIvLyIKCi0tLy8KQ29udGVudC1UeXBlOiB0ZXh0L3gtc2hlbGxzY3JpcHQ7IGNoYXJzZXQ9InVzLWFzY2lpIgojIS9iaW4vYmFzaApzZXQgLWV4CkI2NF9DTFVTVEVSX0NBPUxTMHRMUzFDUlVkSlRpQkRSVkpVU1VaSlEwRlVSUzB0TFMwdENrMUpTVU0xZWtORFFXTXJaMEYzU1VKQlowbENRVVJCVGtKbmEzRm9hMmxIT1hjd1FrRlJjMFpCUkVGV1RWSk5kMFZSV1VSV1VWRkVSWGR3Y21SWFNtd0tZMjAxYkdSSFZucE5RalJZUkZSSmVVMUVUWGxOYWtVMFRrUlJlRTR4YjFoRVZFMTVUVVJOZUU5VVJUUk9SRkY0VGpGdmQwWlVSVlJOUWtWSFFURlZSUXBCZUUxTFlUTldhVnBZU25WYVdGSnNZM3BEUTBGVFNYZEVVVmxLUzI5YVNXaDJZMDVCVVVWQ1FsRkJSR2RuUlZCQlJFTkRRVkZ2UTJkblJVSkJUWGhQQ25sMmFFMXdaalJaU2pNMWFHWmFPSE5sVlhNd016UjJja0pWYTI1d2VHZEROR0pIVm5VNWRUTXdlamRYU3lzeWIyazVWRlYyV2pWb1N6RjRWaXRDY1VvS1FuZFNVRmx3Vmtnd1VISlFVakJZWlVKcFMzWnJUVE51TjBwbFNHOHJXWG94WlhsMU9XSlhlR2htYkhocWIyNVhiMUZNVGxCR05FVmtVV2N5UjNkSU1RcHdiRXd5Tm13eVowczBWelZZY0VSUlpWTkpWMVZzVVZGbWJXRTJOVzR6ZDBkbGJHdHRkVXB5U3pKcU1rSkVaRUZDSzJwTVJHWnpWMm94U0dseVRsQkJDa2RsT0dwUmFtOXpSV0Y1YlVWV1FUUTRkM1U0VUV4UU5VWkNlbXBuUjJWTmQyeGFMek5XV0VSNVlWTmFNM3BHZURoSWRGaEJOVXRITkhNM1JrNU9NRWtLYVdGUGJURk5jMWcwVFdOeFMxQkZRVTFYYkRSMVJVTjJhVGw1UTJkWGJVdEVSVTVCVms5SFRGVnZUR2MwYzI4eVpWUm9MM013ZVVKSlYwc3ZkRzB5VlFwbmJUVkxhbTVNYkZCclltUkVSMjVWTTBORlEwRjNSVUZCWVU1RFRVVkJkMFJuV1VSV1VqQlFRVkZJTDBKQlVVUkJaMHRyVFVFNFIwRXhWV1JGZDBWQ0NpOTNVVVpOUVUxQ1FXWTRkMGhSV1VSV1VqQlBRa0paUlVaSlNrMVZiVU5VUzJwdGIwaHVNeTkwZGsxT01tdHNaVVV2UzI1TlFUQkhRMU54UjFOSllqTUtSRkZGUWtOM1ZVRkJORWxDUVZGQ2VreEZRMDlOZVd4WmFtZzBlakJ1T1dwR09UaEVVR2hvYW5CcGVqSlBUVkZ4Y0cxYVFuZE5WMjlVVEZWRFdtUkdTQXBLV1dwRFFuQjZSM1JPUTFSeFpETm5VSGxRUTFsS2RtdDZiemh4UjBoWWNWVkJUakpJUm14NFNUZ3pSSEE1V0ZoTVNFeFFWaXRxWVdSUVNXaG1hWGhJQ25GVFFrdElUekJOU3k4eVRWcHJPVkY2YzJORlNuTXpkazFrU21KeFpIZ3ZXV2QzTUhKWGRFNUxTRzFvV0V4dllrcHFLMUJvVHpaUlkyWlhSeXNyVFdrS1RtRTJNMjh6WmtRelJFdEtPRVp5Um10VmNGbEZRazAwVm0xbE5FeHJTVGhCVml0Rk5YQlZVR3huTUdsQ1pIZDNUSGhWTTBGMkx6UkdjbU5tZDFVNWNRcE1abWx3YlVseVMyb3daR2QwYjFvdmJXMTBXV0p5THpZNVVFdHJiU3QzVldkelNqQlBOMmc1Ym05R2RURTROMVpOWjJVM2NrazFhRVoxUTNOV1MzTllDaloxV0VwRVQwdHFWREV3Vm5wSmNtSnNibFpxSzA1WGVWcFJNM2x2TkRrd1YwODROQW90TFMwdExVVk9SQ0JEUlZKVVNVWkpRMEZVUlMwdExTMHRDZz09CkFQSV9TRVJWRVJfVVJMPWh0dHBzOi8vNTk0Mjk0MjhFQkFCQkI5RjkxMUExNzNEN0I4RTgxNzkuZ3I3LmV1LXdlc3QtMS5la3MuYW1hem9uYXdzLmNvbQpLOFNfQ0xVU1RFUl9ETlNfSVA9MTcyLjIwLjAuMTAKL2V0Yy9la3MvYm9vdHN0cmFwLnNoIGFpcmZsb3ctZGV2IC0ta3ViZWxldC1leHRyYS1hcmdzICctLW5vZGUtbGFiZWxzPWVrcy5hbWF6b25hd3MuY29tL25vZGVncm91cC1pbWFnZT1hbWktMDYwZmE1MjZkZTA1MjFjMDcsZWtzLmFtYXpvbmF3cy5jb20vY2FwYWNpdHlUeXBlPU9OX0RFTUFORCxoaWdoLW1lbW9yeT10cnVlLGVrcy5hbWF6b25hd3MuY29tL25vZGVncm91cD1oaWdoLW1lbW9yeSAtLXJlZ2lzdGVyLXdpdGgtdGFpbnRzPWhpZ2gtbWVtb3J5PXRydWU6Tm9TY2hlZHVsZSAtLW1heC1wb2RzPTIzNCcgLS1iNjQtY2x1c3Rlci1jYSAkQjY0X0NMVVNURVJfQ0EgLS1hcGlzZXJ2ZXItZW5kcG9pbnQgJEFQSV9TRVJWRVJfVVJMIC0tZG5zLWNsdXN0ZXItaXAgJEs4U19DTFVTVEVSX0ROU19JUCAtLXVzZS1tYXgtcG9kcyBmYWxzZQoKLS0vLy0t"
-
-  block_device_mappings {
-    device_name = "/dev/xvda"
-    ebs {
-      delete_on_termination = "true"
-      iops                  = 3000
-      throughput            = 250
-      volume_size           = 200
-      volume_type           = "gp3"
-    }
-  }
-
-  metadata_options {
-    http_put_response_hop_limit = 2
-    http_tokens                 = "optional"
-  }
-
-  network_interfaces {
-    device_index       = 0
-    ipv4_address_count = 0
-    ipv4_addresses     = []
-    ipv4_prefix_count  = 0
-    ipv4_prefixes      = []
-    ipv6_address_count = 0
-    ipv6_addresses     = []
-    ipv6_prefix_count  = 0
-    ipv6_prefixes      = []
-    network_card_index = 0
-    security_groups = [
-      "sg-089008308707d83b7"
-    ]
-  }
-
-  tags = {
-    "eks:cluster-name"                                          = "airflow-dev"
-    "eks:nodegroup-name"                                        = "high-memory"
-    "k8s.io/cluster-autoscaler/node-template/label/high-memory" = "true"
-    "k8s.io/cluster-autoscaler/node-template/taint/high-memory" = "true:NoSchedule"
-  }
-}
 
 resource "aws_launch_template" "prod_standard" {
   name          = "prod_standard"
