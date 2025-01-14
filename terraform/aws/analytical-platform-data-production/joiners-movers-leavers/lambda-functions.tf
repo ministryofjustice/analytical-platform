089d011eb75c5ff0fda9ab230d74e856a9159459
Gary <26419401+Gary-H9@users.noreply.github.com>
2025-01-14 17:45:32 +0000

    :wrench: Rename λ: 'data-platform' to 'analytical-platform' (#6558)
    * :wrench: Rename: 'data-platform' to 'analytical-platform'
    
    * :pencil: casing
    
    * Update names

--- terraform/aws/analytical-platform-data-production/joiners-movers-leavers/lambda-functions.tf
+++ terraform/aws/analytical-platform-data-production/joiners-movers-leavers/lambda-functions.tf
@@ -1,5 +1,5 @@
 #tfsec:ignore:avd-aws-0066:no need for tracing
-module "jml_extract_lambda" {
+module "jml_report_lambda" {
   #checkov:skip=CKV_TF_1:Module is from Terraform registry
   source  = "terraform-aws-modules/lambda/aws"
   version = "7.20.0"
@@ -7,12 +7,12 @@ module "jml_extract_lambda" {
   publish        = true
   create_package = false
 
-  function_name = "data_platform_jml_extract"
+  function_name = "analytical-platform-jml-report"
   description   = "Generates a JML report and sends it to JMLv4"
   package_type  = "Image"
   memory_size   = 512
   timeout       = 120
-  image_uri     = "374269020027.dkr.ecr.eu-west-2.amazonaws.com/data-platform-jml-extract-lambda-ecr-repo:1.0.3"
+  image_uri     = "509399598587.dkr.ecr.eu-west-2.amazonaws.com/analytical-platform-jml-report:1.4.4"
 
   environment_variables = {
     SECRET_ID       = data.aws_secretsmanager_secret_version.govuk_notify_api_key.id
