25bf541383193957bd42ca6a7de06f5a40814540
Emterry <emma.terry@digital.justice.gov.uk>
2025-01-16 11:11:12 +0000

    add s3 batch inference-po licy attachment

--- terraform/aws/analytical-platform-development/cluster/tooling-integration-iam-policies.tf
+++ terraform/aws/analytical-platform-development/cluster/tooling-integration-iam-policies.tf
@@ -201,3 +201,7 @@ resource "aws_iam_policy" "bedrock_batch_inference_s3_access" {
   policy      = data.aws_iam_policy_document.bedrock_batch_inference_s3_access.json
 }
 
+resource "aws_iam_role_policy_attachment" "bedrock_batch_inference_s3_access" {
+  role       = aws_iam_role.bedrock_batch_inference.name
+  policy_arn = aws_iam_policy.bedrock_batch_inference_s3_access.arn
+}
