a8c6d9b9c0a54afe83ff0067cca3a6a4c1c20937
julialawrence <julia.lawrence@digital.justice.gov.uk>
2025-01-26 15:52:32 +0000

    Removing associations and interfaces

--- terraform/aws/analytical-platform-data-production/hmcts-sdp-direct-connect/dx-gateway-associations.tf
+++ terraform/aws/analytical-platform-data-production/hmcts-sdp-direct-connect/dx-gateway-associations.tf
@@ -1,11 +1,11 @@
-resource "aws_dx_gateway_association" "hmcts_gateway_airflow_dev" {
-  dx_gateway_id         = aws_dx_gateway.hmcts_gateway.id
-  associated_gateway_id = data.aws_vpn_gateway.airflow_dev.id
-  allowed_prefixes      = [data.aws_vpc.airflow_dev.cidr_block]
-}
+# resource "aws_dx_gateway_association" "hmcts_gateway_airflow_dev" {
+#   dx_gateway_id         = aws_dx_gateway.hmcts_gateway.id
+#   associated_gateway_id = data.aws_vpn_gateway.airflow_dev.id
+#   allowed_prefixes      = [data.aws_vpc.airflow_dev.cidr_block]
+# }
 
-resource "aws_dx_gateway_association" "hmcts_gateway_airflow_prod" {
-  dx_gateway_id         = aws_dx_gateway.hmcts_gateway.id
-  associated_gateway_id = data.aws_vpn_gateway.airflow_prod.id
-  allowed_prefixes      = [data.aws_vpc.airflow_prod.cidr_block]
-}
+# resource "aws_dx_gateway_association" "hmcts_gateway_airflow_prod" {
+#   dx_gateway_id         = aws_dx_gateway.hmcts_gateway.id
+#   associated_gateway_id = data.aws_vpn_gateway.airflow_prod.id
+#   allowed_prefixes      = [data.aws_vpc.airflow_prod.cidr_block]
+# }
