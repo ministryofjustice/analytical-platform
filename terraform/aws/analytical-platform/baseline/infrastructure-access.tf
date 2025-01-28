2953bf288e725263affaa5b5eb150143d37740ae
Jacob Woffenden <jacob.woffenden@justice.gov.uk>
2025-01-28 10:42:42 +0000

    specify provider
    Signed-off-by: Jacob Woffenden <jacob.woffenden@justice.gov.uk>

--- terraform/aws/analytical-platform/baseline/infrastructure-access.tf
+++ terraform/aws/analytical-platform/baseline/infrastructure-access.tf
@@ -4,4 +4,8 @@
 
 module "data_production_infrastructure_access" {
   source = "./modules/infrastructure-access"
+
+  providers = {
+    aws = aws.analytical-platform-data-production-eu-west-2
+  }
 }
