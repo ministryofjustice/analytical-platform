98c07567abcd9c1cd16e49bddd68e77f94becd83
zerj9 <96551236+zerj9@users.noreply.github.com>
2025-01-31 16:02:16 +0000

    fix tflint for example tf file

--- terraform/aws/modules/data-engineering/dms/examples/example-readme/main.tf
+++ terraform/aws/modules/data-engineering/dms/examples/example-readme/main.tf
@@ -1,3 +1,14 @@
+terraform {
+  required_version = ">= 1.0.0, < 2.0.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+
 data "aws_availability_zones" "available" {}
 
 locals {
