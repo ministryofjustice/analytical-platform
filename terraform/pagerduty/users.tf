7fa7cba06d974b45dd27593247ffc94013cafda8
Jacob Woffenden <jacob@woffenden.io>
2025-02-04 16:07:08 +0000

    Remove Emma

--- terraform/pagerduty/users.tf
+++ terraform/pagerduty/users.tf
@@ -11,11 +11,6 @@ locals {
       role  = "responder"
     },
     {
-      name  = "Emma Terry"
-      email = "emma.terry@digital.justice.gov.uk"
-      role  = "responder"
-    },
-    {
       name  = "Gary Henderson"
       email = "gary.henderson@digital.justice.gov.uk"
       role  = "responder"
