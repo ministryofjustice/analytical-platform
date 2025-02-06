ddb072353458837dfc22bf2995ca31c0cb1956fb
BrianEllwood <brian.ellwood@digital.justice.gov.uk>
2025-02-06 13:26:26 +0000

    all bar vpc

--- terraform/aws/analytical-platform-management-production/cluster/data.tf
+++ terraform/aws/analytical-platform-management-production/cluster/data.tf
@@ -26,10 +26,10 @@ data "aws_iam_roles" "aws_sso_administrator_access" {
   path_prefix = "/aws-reserved/sso.amazonaws.com/"
 }
 
-data "aws_eks_cluster" "cluster" {
-  name = module.eks.cluster_id
-}
+# data "aws_eks_cluster" "cluster" {
+#   name = module.eks.cluster_id
+# }
 
-data "aws_eks_cluster_auth" "cluster" {
-  name = module.eks.cluster_id
-}
+# data "aws_eks_cluster_auth" "cluster" {
+#   name = module.eks.cluster_id
+# }
