c5f4d45ed10419e7b4f379e5fe078a40fe45eda6
Gary H <26419401+Gary-H9@users.noreply.github.com>
2025-02-06 17:58:44 +0000

    :wrench: Add new Policy/Role/KMS

--- terraform/aws/analytical-platform-data-production/ingestion-egress/iam-policies.tf
+++ terraform/aws/analytical-platform-data-production/ingestion-egress/iam-policies.tf
@@ -127,3 +127,69 @@ module "production_replication_iam_policy" {
 
   policy = data.aws_iam_policy_document.production_replication.json
 }
+
+
+data "aws_iam_policy_document" "production_cica_dms_replication" {
+  statement {
+    sid    = "DestinationBucketPermissions"
+    effect = "Allow"
+    actions = [
+      "s3:ReplicateObject",
+      "s3:ObjectOwnerOverrideToBucketOwner",
+      "s3:GetObjectVersionTagging",
+      "s3:ReplicateTags",
+      "s3:ReplicateDelete"
+    ]
+    resources = ["arn:aws:s3:::mojap-ingestion-production-cica-dms-egress/*"]
+  }
+  statement {
+    sid    = "SourceBucketPermissions"
+    effect = "Allow"
+    actions = [
+      "s3:GetReplicationConfiguration",
+      "s3:ListBucket"
+    ]
+    resources = [module.cica_dms_egress_s3.s3_bucket_arn]
+  }
+  statement {
+    sid    = "SourceBucketObjectPermissions"
+    effect = "Allow"
+    actions = [
+      "s3:GetObjectVersionForReplication",
+      "s3:GetObjectVersionAcl",
+      "s3:GetObjectVersionTagging",
+      "s3:ObjectOwnerOverrideToBucketOwner"
+    ]
+    resources = ["${module.cica_dms_egress_s3.s3_bucket_arn}/*"]
+  }
+  statement {
+    sid    = "SourceBucketKMSKey"
+    effect = "Allow"
+    actions = [
+      "kms:Decrypt",
+      "kms:GenerateDataKey"
+    ]
+    resources = [module.production_cica_dms_kms.key_arn]
+  }
+  statement {
+    sid    = "DestinationBucketKMSKey"
+    effect = "Allow"
+    actions = [
+      "kms:Encrypt",
+      "kms:GenerateDataKey"
+    ]
+    resources = ["arn:aws:kms:eu-west-2:471112983409:key/159671dd-57fa-497c-93b9-2aa9aa8b0fd1"] # TODO: Alter this
+  }
+}
+
+module "production_replication_cica_dms_iam_policy" {
+  #checkov:skip=CKV_TF_1:Module is from Terraform registry
+  #checkov:skip=CKV_TF_2:Module registry does not support tags for versions
+
+  source  = "terraform-aws-modules/iam/aws//modules/iam-policy"
+  version = "5.52.2"
+
+  name_prefix = "mojap-data-production-cica-dms-egress-production"
+
+  policy = data.aws_iam_policy_document.production_cica_dms_replication.json
+}
