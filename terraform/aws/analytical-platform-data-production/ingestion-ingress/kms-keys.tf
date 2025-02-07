7f7685f7aefe16c440714531b8589afdfd5b4164
Tom Webber <thomas.webber@digital.justice.gov.uk>
2025-02-07 18:00:45 +0000

    feat: add buckets for opg datasync replication
    - add opg datasync buckets (dev + prod) with replication destination trust permissions for APingestion replication role
    - add kms keys with trust permissions for APingestion replication role
    - add locals with APIngestion details

--- terraform/aws/analytical-platform-data-production/ingestion-ingress/kms-keys.tf
+++ terraform/aws/analytical-platform-data-production/ingestion-ingress/kms-keys.tf
@@ -27,3 +27,67 @@ module "production_cica_dms_kms" {
   ]
   deletion_window_in_days = 7
 }
+
+module "datasync_opg_development_kms" {
+  #checkov:skip=CKV_TF_1:Module registry does not support commit hashes for versions
+  #checkov:skip=CKV_TF_2:Module registry does not support tags for versions
+
+  source  = "terraform-aws-modules/kms/aws"
+  version = "3.1.1"
+
+  aliases               = ["s3/mojap-data-development-datasync-opg"]
+  description           = "MoJ AP OPG DataSync - Development"
+  enable_default_policy = true
+  multi_region          = true
+
+  deletion_window_in_days = 7
+
+  policy = jsonencode({
+    Statement = [
+      {
+        Sid    = "AllowS3ReplicationSourceRoleToUseTheKey",
+        Effect = "Allow",
+        Principal = {
+          AWS = local.environment_configurations.development.datasync_opg_replication_iam_role_arn
+        },
+        Action = [
+          "kms:GenerateDataKey",
+          "kms:Encrypt"
+        ],
+        Resource = "*"
+      }
+    ]
+  })
+}
+
+module "datasync_opg_production_kms" {
+  #checkov:skip=CKV_TF_1:Module registry does not support commit hashes for versions
+  #checkov:skip=CKV_TF_2:Module registry does not support tags for versions
+
+  source  = "terraform-aws-modules/kms/aws"
+  version = "3.1.1"
+
+  aliases               = ["s3/mojap-data-production-datasync-opg"]
+  description           = "MoJ AP OPG DataSync - Production"
+  enable_default_policy = true
+  multi_region          = true
+
+  deletion_window_in_days = 7
+
+  policy = jsonencode({
+    Statement = [
+      {
+        Sid    = "AllowS3ReplicationSourceRoleToUseTheKey",
+        Effect = "Allow",
+        Principal = {
+          AWS = local.environment_configurations.production.datasync_opg_replication_iam_role_arn
+        },
+        Action = [
+          "kms:GenerateDataKey",
+          "kms:Encrypt"
+        ],
+        Resource = "*"
+      }
+    ]
+  })
+}
--- terraform/aws/analytical-platform-data-production/ingestion-ingress/s3.tf
+++ terraform/aws/analytical-platform-data-production/ingestion-ingress/s3.tf
@@ -56,3 +56,142 @@ module "cica_dms_ingress_s3" {
     }
   }
 }
+
+module "datasync_opg_development" {
+  #checkov:skip=CKV_TF_1:Module registry does not support commit hashes for versions
+  #checkov:skip=CKV_TF_2:Module registry does not support tags for versions
+  #checkov:skip=CKV_AWS_18:Access logging not enabled currently
+  #checkov:skip=CKV_AWS_21:Versioning is enabled, but not detected by Checkov
+  #checkov:skip=CKV_AWS_145:Bucket is encrypted with CMK KMS, but not detected by Checkov
+  #checkov:skip=CKV_AWS_300:Lifecycle configuration not enabled currently
+  #checkov:skip=CKV_AWS_144:Cross-region replication is not required currently
+  #checkov:skip=CKV2_AWS_6:Public access block is enabled, but not detected by Checkov
+  #checkov:skip=CKV2_AWS_61:Lifecycle configuration not enabled currently
+  #checkov:skip=CKV2_AWS_62:Bucket notifications not required currently
+  #checkov:skip=CKV2_AWS_67:Regular CMK key rotation is not required currently
+
+  source  = "terraform-aws-modules/s3-bucket/aws"
+  version = "4.5.0"
+
+  bucket        = "mojap-data-production-datasync-opg-development"
+  force_destroy = true
+
+  versioning = {
+    enabled = true
+  }
+
+  server_side_encryption_configuration = {
+    rule = {
+      apply_server_side_encryption_by_default = {
+        kms_master_key_id = module.datasync_opg_development_kms.key_arn
+        sse_algorithm     = "aws:kms"
+      }
+    }
+  }
+}
+
+data "aws_iam_policy_document" "datasync_opg_development" {
+
+  statement {
+    sid    = "Permissions on objects"
+    effect = "Allow"
+    actions = [
+      "s3:ReplicateDelete",
+      "s3:ReplicateObject",
+      "s3:ObjectOwnerOverrideToBucketOwner"
+    ]
+    resources = ["${module.datasync_opg_development.s3_bucket_arn}/*"]
+    principals {
+      type = "AWS"
+      identifiers = [
+        local.environment_configurations.development.datasync_opg_replication_iam_role_arn
+      ]
+    }
+  }
+
+  statement {
+    sid    = "Permissions on bucket"
+    effect = "Allow"
+    actions = [
+      "s3:List*",
+      "s3:GetBucketVersioning",
+      "s3:PutBucketVersioning"
+    ]
+    resources = [module.datasync_opg_development.s3_bucket_arn]
+    principals {
+      type = "AWS"
+      identifiers = [
+        local.environment_configurations.development.datasync_opg_replication_iam_role_arn
+      ]
+    }
+  }
+}
+
+module "datasync_opg_production" {
+  #checkov:skip=CKV_TF_1:Module registry does not support commit hashes for versions
+  #checkov:skip=CKV_TF_2:Module registry does not support tags for versions
+  #checkov:skip=CKV_AWS_18:Access logging not enabled currently
+  #checkov:skip=CKV_AWS_21:Versioning is enabled, but not detected by Checkov
+  #checkov:skip=CKV_AWS_145:Bucket is encrypted with CMK KMS, but not detected by Checkov
+  #checkov:skip=CKV_AWS_300:Lifecycle configuration not enabled currently
+  #checkov:skip=CKV_AWS_144:Cross-region replication is not required currently
+  #checkov:skip=CKV2_AWS_6:Public access block is enabled, but not detected by Checkov
+  #checkov:skip=CKV2_AWS_61:Lifecycle configuration not enabled currently
+  #checkov:skip=CKV2_AWS_62:Bucket notifications not required currently
+  #checkov:skip=CKV2_AWS_67:Regular CMK key rotation is not required currently
+
+  source  = "terraform-aws-modules/s3-bucket/aws"
+  version = "4.5.0"
+
+  bucket        = "mojap-data-production-datasync-opg"
+  force_destroy = true
+
+  versioning = {
+    enabled = true
+  }
+
+  server_side_encryption_configuration = {
+    rule = {
+      apply_server_side_encryption_by_default = {
+        kms_master_key_id = module.datasync_opg_production_kms.key_arn
+        sse_algorithm     = "aws:kms"
+      }
+    }
+  }
+}
+
+data "aws_iam_policy_document" "datasync_opg_production" {
+
+  statement {
+    sid    = "Permissions on objects"
+    effect = "Allow"
+    actions = [
+      "s3:ReplicateDelete",
+      "s3:ReplicateObject"
+    ]
+    resources = ["${module.datasync_opg_production.s3_bucket_arn}/*"]
+    principals {
+      type = "AWS"
+      identifiers = [
+        local.environment_configurations.production.datasync_opg_replication_iam_role_arn
+      ]
+    }
+  }
+
+  statement {
+    sid    = "Permissions on bucket"
+    effect = "Allow"
+    actions = [
+      "s3:List*",
+      "s3:GetBucketVersioning",
+      "s3:PutBucketVersioning"
+    ]
+    resources = [module.datasync_opg_production.s3_bucket_arn]
+    principals {
+      type = "AWS"
+      identifiers = [
+        local.environment_configurations.production.datasync_opg_replication_iam_role_arn
+      ]
+    }
+  }
+}
