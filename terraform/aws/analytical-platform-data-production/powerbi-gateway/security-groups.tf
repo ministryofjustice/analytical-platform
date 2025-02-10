5393f55a329d5fec5c38e4a6ceaf86da6f85c874
Tom Webber <thomas.webber@digital.justice.gov.uk>
2025-02-10 15:48:46 +0000

    fix: allow all egress traffic, remove rdp ingress traffic

--- terraform/aws/analytical-platform-data-production/powerbi-gateway/security-groups.tf
+++ terraform/aws/analytical-platform-data-production/powerbi-gateway/security-groups.tf
@@ -28,8 +28,8 @@ module "test_powerbi_security_group" {
   vpc_id      = module.vpc.vpc_id
 
   ingress_cidr_blocks = ["10.0.0.0/16"]
-  ingress_rules       = ["https-443-tcp", "all-icmp", "rdp-tcp"]
-  egress_rules        = []
+  ingress_rules       = ["https-443-tcp", "all-icmp"]
+  egress_rules        = ["all-all"]
 
   tags = {
     Name = local.name
