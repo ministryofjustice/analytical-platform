ce87b774217058ba670014dbf067aa284bc5caa2
BrianEllwood <brian.ellwood@digital.justice.gov.uk>
2025-02-10 13:22:14 +0000

    fix tflint issues

--- terraform/aws/analytical-platform-management-production/cluster/data.tf
+++ terraform/aws/analytical-platform-management-production/cluster/data.tf
@@ -2,29 +2,29 @@
 # AWS
 ##################################################
 
-data "aws_caller_identity" "session" {
-  provider = aws.session
-}
+# data "aws_caller_identity" "session" {
+#   provider = aws.session
+# }
 
-data "aws_iam_session_context" "session" {
-  provider = aws.session
+# data "aws_iam_session_context" "session" {
+#   provider = aws.session
 
-  arn = data.aws_caller_identity.session.arn
-}
+#   arn = data.aws_caller_identity.session.arn
+# }
 
-data "aws_caller_identity" "current" {}
+# data "aws_caller_identity" "current" {}
 
 data "aws_availability_zones" "available" {}
 
-data "aws_route53_zone" "main" {
-  name         = var.route53_zone
-  private_zone = false
-}
+# data "aws_route53_zone" "main" {
+#   name         = var.route53_zone
+#   private_zone = false
+# }
 
-data "aws_iam_roles" "aws_sso_administrator_access" {
-  name_regex  = "AWSReservedSSO_${var.aws_sso_role_prefix}_.*"
-  path_prefix = "/aws-reserved/sso.amazonaws.com/"
-}
+# data "aws_iam_roles" "aws_sso_administrator_access" {
+#   name_regex  = "AWSReservedSSO_${var.aws_sso_role_prefix}_.*"
+#   path_prefix = "/aws-reserved/sso.amazonaws.com/"
+# }
 
 # data "aws_eks_cluster" "cluster" {
 #   name = module.eks.cluster_id
