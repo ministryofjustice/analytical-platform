76a44a7cd44d65cfa7d4f4a1899b416a0ea3fa16
BrianEllwood <brian.ellwood@digital.justice.gov.uk>
2025-02-10 10:58:00 +0000

    plan following cluster delete

--- terraform/aws/analytical-platform-management-production/cluster/kms-keys.tf
+++ terraform/aws/analytical-platform-management-production/cluster/kms-keys.tf
@@ -2,7 +2,7 @@
 # EKS Encryption Key
 ##################################################
 
-resource "aws_kms_key" "eks" {
-  description         = "EKS Secret Encryption Key"
-  enable_key_rotation = true
-}
+# resource "aws_kms_key" "eks" {
+#   description         = "EKS Secret Encryption Key"
+#   enable_key_rotation = true
+# }
