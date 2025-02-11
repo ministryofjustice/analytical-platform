13b798fa0879bf3a7857e9393303c93facd50f3a
Gary H <26419401+Gary-H9@users.noreply.github.com>
2025-02-11 12:38:40 +0000

    🔑 Update description + add key_statements

--- terraform/aws/analytical-platform-data-production/ingestion-ingress/kms-keys.tf
+++ terraform/aws/analytical-platform-data-production/ingestion-ingress/kms-keys.tf
@@ -6,9 +6,24 @@ module "production_cica_dms_kms" {
   version = "3.1.1"
 
   aliases               = ["s3/mojap-data-production-cica-dms-ingress-production"]
-  description           = "MoJ AP CICA DMS Ibgress - Production"
+  description           = "MoJ AP CICA DMS Ingress - Production"
   enable_default_policy = true
-  multi_region          = true
-
+  key_statements = [
+    {
+      sid = "AllowAnalyticalPlatformIngestionProduction"
+      actions = [
+        "kms:Encrypt",
+        "kms:GenerateDataKey"
+      ]
+      resources = ["*"]
+      effect    = "Allow"
+      principals = [
+        {
+          type        = "AWS"
+          identifiers = ["arn:aws:iam::471112983409:role/cica-dms-ingress-production-replication"]
+        }
+      ]
+    }
+  ]
   deletion_window_in_days = 7
 }
