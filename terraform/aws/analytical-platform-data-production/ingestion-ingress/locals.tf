94dfb6f4303d768479d3ed220c752247570b4b9e
Tom Webber <thomas.webber@digital.justice.gov.uk>
2025-02-12 11:02:10 +0000

    feat: remove development replication

--- terraform/aws/analytical-platform-data-production/ingestion-ingress/locals.tf
+++ terraform/aws/analytical-platform-data-production/ingestion-ingress/locals.tf
@@ -5,10 +5,5 @@ locals {
       datasync_opg_source_bucket_arn        = "arn:aws:s3:::mojap-ingestion-production-datasync-opg"
       datasync_opg_replication_kms_key_arn  = "arn:aws:kms:eu-west-2:471112983409:key/db1f2938-3fc6-45a7-8744-095a72c78f9b" #gitleaks:allow
     }
-    development = {
-      datasync_opg_replication_iam_role_arn = "arn:aws:iam::730335344807:role/datasync-replication"
-      datasync_opg_source_bucket_arn        = "arn:aws:s3:::mojap-ingestion-development-datasync-opg"
-      datasync_opg_replication_kms_key_arn  = "arn:aws:kms:eu-west-2:730335344807:key/5e1160c6-cc08-4946-b68a-f1694a9935d3" #gitleaks:allow
-    }
   }
 }
