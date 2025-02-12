1cba7ab04368229587bc5b605aab8753d51c0a81
Tom Webber <thomas.webber@digital.justice.gov.uk>
2025-02-12 10:47:54 +0000

    style: rename kms file

--- terraform/aws/analytical-platform-data-production/ingestion-ingress/kms-keys.tf
+++ terraform/aws/analytical-platform-data-production/ingestion-ingress/kms-keys.tf
@@ -1,3 +1,33 @@
+module "production_cica_dms_kms" {
+  #checkov:skip=CKV_TF_1:Module registry does not support commit hashes for versions
+  #checkov:skip=CKV_TF_2:Module registry does not support tags for versions
+
+  source  = "terraform-aws-modules/kms/aws"
+  version = "3.1.1"
+
+  aliases               = ["s3/mojap-data-production-cica-dms-ingress-production"]
+  description           = "MoJ AP CICA DMS Ingress - Production"
+  enable_default_policy = true
+  key_statements = [
+    {
+      sid = "AllowAnalyticalPlatformIngestionProduction"
+      actions = [
+        "kms:Encrypt",
+        "kms:GenerateDataKey"
+      ]
+      resources = ["*"]
+      effect    = "Allow"
+      principals = [
+        {
+          type        = "AWS"
+          identifiers = ["arn:aws:iam::471112983409:role/cica-dms-ingress-production-replication"]
+        }
+      ]
+    }
+  ]
+  deletion_window_in_days = 7
+}
+
 module "datasync_opg_development_kms" {
   #checkov:skip=CKV_TF_1:Module registry does not support commit hashes for versions
   #checkov:skip=CKV_TF_2:Module registry does not support tags for versions
