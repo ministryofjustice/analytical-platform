77cbd06214ef9a8674c2b2ba6b6aecf29f231e60
julialawrence <julia.lawrence@digital.justice.gov.uk>
2025-02-14 12:11:46 +0000

    Setting ignore AMI to false for today, true thereafter

--- terraform/aws/analytical-platform-data-production/powerbi-gateway/ec2-instances.tf
+++ terraform/aws/analytical-platform-data-production/powerbi-gateway/ec2-instances.tf
@@ -153,7 +153,7 @@ module "test_powerbi_gateway_ec2" {
   monitoring                  = true
   create_iam_instance_profile = true
   iam_role_description        = "IAM role for PowerBI Gateway Instance"
-  ignore_ami_changes          = true
+  ignore_ami_changes          = timestamp() > "2025-15-02T00:00:00Z"
   enable_volume_tags          = false
   associate_public_ip_address = false
   iam_role_name               = local.powerbi_gateway_role
