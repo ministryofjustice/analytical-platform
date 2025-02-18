b7246c890305116580b027077537c433c60ca644
jnayak-moj <jyotiranjan.nayak@justice.gov.uk>
2025-02-18 09:28:24 +0000

    6640 athena spark workgroup fixed (#6923)
    * dbt spark athena workgroup added
    
    * new line aaded
    
    * athena execution role added
    
    * new line added
    
    * fixed config error
    
    * fixing the tf issue
    
    * fixed tf issue
    
    * athena iam role fixed
    
    * module name fixed
    
    * removed iam policy and fixed the role name
    
    * tf linting

---
+++ terraform/aws/analytical-platform-data-production/athena/iam-roles.tf
@@ -0,0 +1,24 @@
+resource "aws_iam_role" "athena_spark_execution_role" {
+  name = "AthenaSparkExecutionRole"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Principal = {
+        Service = "athena.amazonaws.com"
+      }
+      Action = "sts:AssumeRole"
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "athena_spark_s3_access" {
+  role       = aws_iam_role.athena_spark_execution_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonAthenaFullAccess"
+}
+
+resource "aws_iam_role_policy_attachment" "athena_spark_glue_access" {
+  role       = aws_iam_role.athena_spark_execution_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSGlueServiceRole"
+}
