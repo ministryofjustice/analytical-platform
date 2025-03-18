2a44d7ee4970264fefe92c8a57d4b38dd0583715
Jacob Woffenden <jacob.woffenden@justice.gov.uk>
2025-03-18 14:40:52 +0000

    Add permissions on bucket and key
    Signed-off-by: GitHub <noreply@github.com>

--- terraform/aws/analytical-platform-data-production/sagemaker-service/s3-buckets.tf
+++ terraform/aws/analytical-platform-data-production/sagemaker-service/s3-buckets.tf
@@ -1,3 +1,20 @@
+data "aws_iam_policy_document" "sagemaker_ai_probation_search_models_s3" {
+  statement {
+    sid     = "AllowSageMakerAIExecutionRoles"
+    effect  = "Allow"
+    actions = ["s3:GetObject"]
+    principals {
+      type = "AWS"
+      identifiers = [
+        "arn:aws:iam::381491960855:role/hmpps-probation-search-dev-sagemaker-exec-role",
+        "arn:aws:iam::992382429243:role/hmpps-probation-search-preprod-sagemaker-exec-role",
+        "arn:aws:iam::992382429243:role/hmpps-probation-search-prod-sagemaker-exec-role"
+      ]
+    }
+    resources = ["arn:aws:s3:::mojap-data-production-sagemaker-ai-probation-search-models/*"]
+  }
+}
+
 #tfsec:ignore:AVD-AWS-0089:Bucket logging not enabled currently
 module "sagemaker_ai_probation_search_models_s3" {
   #checkov:skip=CKV_TF_1:Module registry does not support commit hashes for versions
@@ -9,6 +26,9 @@ module "sagemaker_ai_probation_search_models_s3" {
   bucket = "mojap-data-production-sagemaker-ai-probation-search-models"
 
   force_destroy = true
+
+  attach_policy = true
+  policy        = data.aws_iam_policy_document.sagemaker_ai_probation_search_models_s3.json
 
   versioning = {
     enabled = true
