d9f110a07cf3b9a1d59726dcb0989d000497ba12
Jacob Woffenden <jacob.woffenden@justice.gov.uk>
2025-03-18 18:02:18 +0000

    🛂 SageMaker Service permissions (#7264)
    * Add permissions on bucket and key
    
    Signed-off-by: GitHub <noreply@github.com>
    
    * commit the lockfile
    
    Signed-off-by: GitHub <noreply@github.com>
    
    * Add ListBucket
    
    Signed-off-by: GitHub <noreply@github.com>
    
    ---------
    
    Signed-off-by: GitHub <noreply@github.com>

--- terraform/aws/analytical-platform-data-production/sagemaker-service/kms-keys.tf
+++ terraform/aws/analytical-platform-data-production/sagemaker-service/kms-keys.tf
@@ -9,5 +9,26 @@ module "sagemaker_ai_probation_search_models_kms" {
   description           = "SageMaker AI Probation Search Models KMS Key"
   enable_default_policy = true
 
+  key_statements = [
+    {
+      sid = "AllowSageMakerAIExecutionRoles"
+      actions = [
+        "kms:Decrypt",
+        "kms:GenerateDataKey"
+      ]
+      principals = [
+        {
+          type = "AWS"
+          identifiers = [
+            "arn:aws:iam::381491960855:role/hmpps-probation-search-dev-sagemaker-exec-role",
+            "arn:aws:iam::992382429243:role/hmpps-probation-search-preprod-sagemaker-exec-role",
+            "arn:aws:iam::992382429243:role/hmpps-probation-search-prod-sagemaker-exec-role"
+          ]
+        }
+      ]
+      resources = ["*"]
+    }
+  ]
+
   deletion_window_in_days = 7
 }
