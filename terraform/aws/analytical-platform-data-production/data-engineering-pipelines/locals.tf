296906834fba071b08a2d3c440bd87380c8ca820
Gary <26419401+Gary-H9@users.noreply.github.com>
2025-04-02 12:00:19 +0100

    :wrench: Allow Directory in Ingestion (#7445)

--- terraform/aws/analytical-platform-data-production/data-engineering-pipelines/locals.tf
+++ terraform/aws/analytical-platform-data-production/data-engineering-pipelines/locals.tf
@@ -709,7 +709,8 @@ locals {
                 "arn:aws:s3:::mojap-land",
                 "arn:aws:s3:::mojap-land/bold/essex-police/*",
                 "arn:aws:s3:::mojap-land/sscl/sscl_data_dump/*",
-                "arn:aws:s3:::mojap-land/cps/*"
+                "arn:aws:s3:::mojap-land/cps/*",
+                "arn:aws:s3:::mojap-land/property/planetfm/backupfiles/*"
               ]
             },
             {
