0297ddcacf8837b85f51ac867d9835710f985742
tamsinforbes <32667518+tamsinforbes@users.noreply.github.com>
2025-04-15 16:44:24 +0000

    ignore trivy 0104 from above

--- terraform/aws/analytical-platform-development/powerbi-test/security-groups.tf
+++ terraform/aws/analytical-platform-development/powerbi-test/security-groups.tf
@@ -1,7 +1,6 @@
+#trivy:ignore:avd-aws-0104:We intend unrestricted egress
 module "security_group" {
   #checkov:skip=CKV_TF_1:Module registry does not support commit hashes for versions
-  #trivy:ignore:avd-aws-0104:We intend unrestricted egress
-  #tfsec:ignore:avd-aws-0104:We intend unrestricted egress
 
   source  = "terraform-aws-modules/security-group/aws"
   version = "5.3.0"
@@ -19,10 +18,9 @@ module "security_group" {
   }
 }
 
+#trivy:ignore:avd-aws-0104:We intend unrestricted egress
 module "powerbi_test_security_group" {
   #checkov:skip=CKV_TF_1:Module registry does not support commit hashes for versions
-  #trivy:ignore:avd-aws-0104:We intend unrestricted egress
-  #tfsec:ignore:avd-aws-0104:We intend unrestricted egress
 
   source  = "terraform-aws-modules/security-group/aws"
   version = "5.3.0"
