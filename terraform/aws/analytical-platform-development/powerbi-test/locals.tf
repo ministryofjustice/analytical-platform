4e0071f7a1eca0ba61f709e5161519e8b4e6e875
Gary <26419401+Gary-H9@users.noreply.github.com>
2025-04-16 16:52:50 +0100

    🔑  PowerBI EC2: Replace Public Key (#7586)
    🔑 Replace Key

--- terraform/aws/analytical-platform-development/powerbi-test/locals.tf
+++ terraform/aws/analytical-platform-development/powerbi-test/locals.tf
@@ -13,6 +13,6 @@ locals {
     virtualization_type = "hvm"
     owner_account       = "801119661308" # amazon
     instance_type       = "t3a.xlarge"   # 4vCPU, 16G RAM
-    ssh_pub_key         = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDe4WCVutEI/uMSwGOZOMAen9X2e/nDMXmroGFSNpOx9ynXzojOX6kmqqUTsIzsrmsy+1iXQaGoj2FUbeK08GhUcB0Vjqr3iVZehkrapzRbuWK0DEaHtqeE9Ic0x4hJaYYK5y/1+7Mm2ZFG5WB6eWnk9U7J/TQHB0QkMnxYV7gp9uS3LtMeiejul7uqqOuzodtSFMW1byRqRZMmebnUXT2JGki+61w/17IxRcG7duOb0JmRthM+rqEQnYT3++SMCs5WQD46JrBOmsQdSNggwQeKkkh5z3zjkQpqXdYuGQOdT6t2bBoqaxhx5ZGo2Yjqk1+eQNAnVGT+PatlJxam5Vu7O0zDm1WWNA5XYTUZ4PFVF2V3h0UmR0MBtiQN2uDC/x1aMaqD9GENYLp8OauQ3viCmBUfJqq3fg5T8U+IkhUTD/Fd1OSHNW+dlfV0rZK6XSuW/VvRYi1VWhOuVK3HGrf4j9TfcRFAAN9ZWjvjtqcim2ipAaHUWN+hWJLV2FsUkU6M9iWSf7Nb9Ed289/HMbDdfoC3CrogU0IoVW4fK5HyzIiUGDtAH/LvTjYuU9GKHbnd7KAyAXfhLWMm4kjFWtzMc7/xszKuoAeLQYJnJ6iN+C6x1jchb2GwTsNbiLGxRlFRkUTQkQUX20Apy66QcQ2maImd4l00Fh8rIDBwupzjsQ== vscode@c1ac7c3d2c32"
+    ssh_pub_key         = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDUblimrIplKnxcf0uX1rRTEa7I83C1iBQsrohjT8CFWD+DmYVXPceQqe2YK06m3f3zMeAYaWBupUd6BoqgiZBLl+hUF/qm/ppeRjcjwvQLy4VyqIbTLiSPRgiqT84noOvphIQOkteYebyRpwv3evRzCWWImBDHSkwno42dqU3VcbI7FjQh+p5Lx0ZgUGiEMidwb18BGV+aO2AtYoLFRxpEpuSh1cm5wyugXvc/T9XX3TKmpPf1dQYvi4LSbmhpfeCYrlMlE80upVkl176GsCNM1gsY9DjcJSdSZGTD+OhfGqCT1Jgs9c/lq7Vx6HqXOxIcP5z5BASRWGPK8O6OsJDoW+EC95Lh+cq2zUYaZXCwbgfAylE9oUEwdgooe6ADrBn9zZLa7HxIlj+AjN4zKzhFczvlcbawyZVp5iiKuncJcrDmHkre4CqO5WSf1OR+LRPWwdZqVBjoKo1+CwsMplm8imOts5/m0TSEIRZMVdN8TSbsnflIqZ5+UNH9lt9xwo0= gary.henderson@MJ004516"
   }
 }
