8ad00815c51607165f0c05061e6ab96cbe72b8b3
Michael Collins <15347726+michaeljcollinsuk@users.noreply.github.com>
2025-04-23 15:24:30 +0100

    Add Dashboard Service Auth0 clients (#7627)
    For dev and prod. Also setup email connections

--- terraform/auth0/alpha-analytics-moj/clients.tf
+++ terraform/auth0/alpha-analytics-moj/clients.tf
@@ -21,3 +21,25 @@ resource "auth0_connection_client" "visual_studio_code_github" {
   client_id     = auth0_client.visual_studio_code.id
   connection_id = "con_RWgK9R3ISqKJLcZu"
 }
+
+resource "auth0_client" "dashboard_service" {
+  name     = "Dashboard Service"
+  app_type = "regular_web"
+  callbacks = [
+    "https://dashboards.analytical-platform.service.justice.gov.uk/callback/",
+  ]
+  allowed_logout_urls = [
+    "https://dashboards.analytical-platform.service.justice.gov.uk/",
+  ]
+  oidc_conformant   = true
+  sso               = true
+  cross_origin_auth = true
+  jwt_configuration {
+    alg = "HS256"
+  }
+}
+
+resource "auth0_connection_client" "dashboard_service_email" {
+  client_id     = auth0_client.dashboard_service.id
+  connection_id = "con_6zNyH9PCyBMhbCaD"
+}
