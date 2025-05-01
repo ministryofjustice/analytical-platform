ad450fd90743c4b14972fe1d37e7b5a584603283
matt heery <matt.heery@justice.gov.uk>
2025-05-01 14:21:17 +0100

    correctly signed commit

--- terraform/aws/analytical-platform-data-production/lakeformation-external-data/electronic-monitoring-data-test/locals.tf
+++ terraform/aws/analytical-platform-data-production/lakeformation-external-data/electronic-monitoring-data-test/locals.tf
@@ -1,30 +1,8 @@
 locals {
-  data_locations = [
+  source_databases = [
     {
-      data_location = "arn:aws:s3:::emds-cadt-test"
-      hybrid_access = false
-      register      = true
-      share         = true
-
+      source_name = "derived_test_dbt"
     }
   ]
-
-  databases = [
-    {
-      name        = "staged_fms_test_dbt"
-      permissions = ["DESCRIBE"]
-
-    }
-  ]
-  tables = [
-    {
-      source_table         = "account"
-      source_database      = "staged_fms_test_dbt"
-      data_filter_name     = "filter-account-acfd15b3547e6c190937dabba14245cdf39af4256bc72fffdb64f9c91e0e1144"
-      permissions          = ["SELECT"]
-      destination_database = { database_name = "staged_fms_test_dbt" }
-    }
-  ]
-
   account_ids = jsondecode(data.aws_secretsmanager_secret_version.account_ids_version.secret_string)
 }
