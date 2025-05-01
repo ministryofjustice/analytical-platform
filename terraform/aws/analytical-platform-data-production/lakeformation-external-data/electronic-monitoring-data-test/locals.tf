08933d92b67e047d705c4ea4ab5b3b80dd2dc27f
matt heery <matt.heery@justice.gov.uk>
2025-05-01 13:56:38 +0100

    lint

--- terraform/aws/analytical-platform-data-production/lakeformation-external-data/electronic-monitoring-data-test/locals.tf
+++ terraform/aws/analytical-platform-data-production/lakeformation-external-data/electronic-monitoring-data-test/locals.tf
@@ -1,7 +1,7 @@
 locals {
   source_databases = [
     {
-      source_name        = "derived_test_dbt"
+      source_name = "derived_test_dbt"
     }
   ]
   account_ids = jsondecode(data.aws_secretsmanager_secret_version.account_ids_version.secret_string)
