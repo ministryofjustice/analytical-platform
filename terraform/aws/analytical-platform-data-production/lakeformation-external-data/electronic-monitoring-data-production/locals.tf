42f6126227816b5f243e632a05e219602dc511e0
matt-heery <116661071+matt-heery@users.noreply.github.com>
2025-05-23 14:50:13 +0100

    EM: Add GPS resource link for DS use case (#7808)
    feat: add gps data for data science

--- terraform/aws/analytical-platform-data-production/lakeformation-external-data/electronic-monitoring-data-production/locals.tf
+++ terraform/aws/analytical-platform-data-production/lakeformation-external-data/electronic-monitoring-data-production/locals.tf
@@ -2,6 +2,9 @@ locals {
   source_databases = [
     {
       source_name = "derived"
+    },
+    {
+      source_name = "g4s_gps"
     }
   ]
   account_ids = jsondecode(data.aws_secretsmanager_secret_version.account_ids_version.secret_string)
