a2241d055972146036ebee177a3d629f15f72304
Anthony Cody <anthony.cody@digital.justice.gov.uk>
2025-06-03 11:58:45 +0100

    DPR development LF external share (#7871)
    * Create terraform.tf
    
    * Create data.tf
    
    * Create locals.tf
    
    * Create main.tf
    
    * Create terraform.tfvars
    
    * Create variables.tf
    
    * Update terraform/aws/analytical-platform-data-production/lakeformation-external-data/digital-prisons-reporting-development/terraform.tf
    
    Co-authored-by: Andrew <72750362+andrewc-moj@users.noreply.github.com>
    
    * Update terraform/aws/analytical-platform-data-production/lakeformation-external-data/digital-prisons-reporting-development/locals.tf
    
    ---------
    
    Co-authored-by: Andrew <72750362+andrewc-moj@users.noreply.github.com>

---
+++ terraform/aws/analytical-platform-data-production/lakeformation-external-data/digital-prisons-reporting-development/locals.tf
@@ -0,0 +1,22 @@
+locals {
+  data_locations = [
+    {
+      data_location = "arn:aws:s3:::dpr-structured-historical-development"
+      hybrid_access = true
+      register      = true
+      share         = true
+
+    }
+  ]
+
+  databases = [
+    {
+      name                         = "dpr-ap-integration"
+      share_all_tables             = true
+      share_all_tables_permissions = ["DESCRIBE", "SELECT"]
+
+    }
+  ]
+
+  account_ids = jsondecode(data.aws_secretsmanager_secret_version.account_ids_version.secret_string)
+}
