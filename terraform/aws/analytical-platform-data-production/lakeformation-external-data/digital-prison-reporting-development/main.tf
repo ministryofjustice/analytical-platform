c3988240ea380f99628ff550e9955f723228abfd
Andrew Craik <72750362+andrewc-moj@users.noreply.github.com>
2025-06-10 14:55:02 +0100

    dmet-prisons-351: create resource link - one table only

--- terraform/aws/analytical-platform-data-production/lakeformation-external-data/digital-prison-reporting-development/main.tf
+++ terraform/aws/analytical-platform-data-production/lakeformation-external-data/digital-prison-reporting-development/main.tf
@@ -1,4 +1,6 @@
 # Module: lake_formation_analytical_platform_data_prod
+# This created a share from the DPR dev account to the AP prod account 
+# Shares were created for "data_locations" and "databases" in locals.tf
 module "lake_formation_analytical_platform_data_prod" {
   source = "github.com/ministryofjustice/terraform-aws-analytical-platform-lakeformation?ref=6fab8677e457c2e276fa1feec8ee83bbccc1220a"
 
@@ -10,4 +12,13 @@ module "lake_formation_analytical_platform_data_prod" {
 
   data_locations     = local.data_locations
   databases_to_share = local.databases
+}
+
+
+resource "aws_lakeformation_resource_link" "dpr_ap_integration_test_link" {
+  provider        = aws.eu_west_1  
+  name            = "test_table_1"            
+  database_name   = "dpr_ap_integration_test_resource_link"
+  target_table    = "test_table_1"                 
+  target_database = "dpr-ap-integration-test"  
 }
