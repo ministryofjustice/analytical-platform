9947daeb45d6e5a7f462f5cc07bb2529e28852b1
Siva Bathina <siva.bathina@justice.gov.uk>
2025-06-10 15:57:22 +0100

    fixed formating error

--- terraform/aws/analytical-platform-data-engineering-production/dms-dev/secrets.tf
+++ terraform/aws/analytical-platform-data-engineering-production/dms-dev/secrets.tf
@@ -1,6 +1,6 @@
 resource "aws_secretsmanager_secret" "slack_webhook" {
   # checkov:skip=CKV2_AWS_57: Skipping because automatic rotation not needed.
-  name = "oasys-dev-slack-webhook"
+  name       = "oasys-dev-slack-webhook"
   kms_key_id = module.dms_dev_kms.key_arn
 }
 
