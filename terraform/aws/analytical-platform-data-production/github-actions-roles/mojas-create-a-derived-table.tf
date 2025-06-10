1f4126e0e222a31a23f0c92faf0103926d159999
gwionap <32736223+gwionap@users.noreply.github.com>
2025-06-10 11:58:13 +0100

    Update mojas-create-a-derived-table.tf
    Adding mojap-hmt-performance

--- terraform/aws/analytical-platform-data-production/github-actions-roles/mojas-create-a-derived-table.tf
+++ terraform/aws/analytical-platform-data-production/github-actions-roles/mojas-create-a-derived-table.tf
@@ -22,7 +22,9 @@ data "aws_iam_policy_document" "create_a_derived_table" {
       "arn:aws:s3:::alpha-app-opg-lpa-dashboard/dev/models/domain_name=opg/*",
       "arn:aws:s3:::alpha-app-opg-lpa-dashboard/prod/models/domain_name=opg/*",
       "arn:aws:s3:::alpha-bold-data-shares",
-      "arn:aws:s3:::alpha-bold-data-shares/reducing-reoffending/*"
+      "arn:aws:s3:::alpha-bold-data-shares/reducing-reoffending/*",
+      "arn:aws:s3:::mojap-hmt-performance",
+      "arn:aws:s3:::mojap-hmt-performance/*"
     ]
   }
   statement {
