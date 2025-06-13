46c4cc6f3aa94f04909c2c0d51fc4ad1cb0c601c
matt-heery <116661071+matt-heery@users.noreply.github.com>
2025-06-13 12:44:04 +0100

    Data lake access role (#7943)
    * feat: add role for data eng datalake access
    
    * fix: dont just copy and paste
    
    * chore: remove whitespace
    
    * Add common account ID
    Move to assumable role for this special role
    
    ---------
    
    Co-authored-by: Jacob Woffenden <jacob@woffenden.io>

---
+++ terraform/aws/analytical-platform-data-production/github-actions-roles/data-engineering-datalake-access.tf
@@ -0,0 +1,91 @@
+data "aws_iam_policy_document" "data_engineering_datalake_access" {
+  statement {
+    sid    = "GlueAccess"
+    effect = "Allow"
+    actions = [
+      "glue:GetDatabase",
+      "glue:GetDatabases",
+      "glue:CreateDatabase",
+      "glue:UpdateDatabase",
+      "glue:DeleteDatabase",
+      "glue:GetTable",
+      "glue:GetTables",
+      "glue:CreateTable",
+      "glue:UpdateTable",
+      "glue:DeleteTable",
+      "glue:GetPartition",
+      "glue:GetPartitions",
+      "glue:CreatePartition",
+      "glue:UpdatePartition",
+      "glue:DeletePartition",
+      "glue:BatchCreatePartition",
+      "glue:BatchDeletePartition",
+      "glue:BatchUpdatePartition",
+    ]
+    resources = [
+      "arn:aws:glue:*:${var.account_ids["analytical-platform-data-production"]}:catalog",
+      "arn:aws:glue:*:${var.account_ids["analytical-platform-data-production"]}:database/*",
+      "arn:aws:glue:*:${var.account_ids["analytical-platform-data-production"]}:table/*"
+    ]
+  }
+  statement {
+    sid       = "LakeFormationAdminPermissions"
+    effect    = "Allow"
+    actions   = ["lakeformation:*"]
+    resources = ["*"]
+  }
+  statement {
+    sid    = "IAMAccess"
+    effect = "Allow"
+    actions = [
+      "iam:CreatePolicy",
+      "iam:CreatePolicyVersion",
+      "iam:DeletePolicy",
+      "iam:DeletePolicyVersion",
+      "iam:GetPolicy",
+      "iam:GetPolicyVersion",
+      "iam:ListPolicyVersions",
+      "iam:SetDefaultPolicyVersion",
+      "iam:AttachRolePolicy",
+      "iam:DetachRolePolicy",
+      "iam:ListAttachedRolePolicies",
+      "iam:ListRolePolicies",
+      "iam:PutRolePolicy",
+      "iam:GetRolePolicy",
+      "iam:DeleteRolePolicy",
+      "iam:GetRole",
+      "iam:UpdateRole",
+      "iam:UpdateAssumeRolePolicy"
+    ]
+    resources = ["arn:aws:iam::*:role/alpha*"]
+  }
+}
+
+module "data_engineering_datalake_access_iam_policy" {
+  #checkov:skip=CKV_TF_1:Module registry does not support commit hashes for versions
+  #checkov:skip=CKV_TF_2:Module registry does not support tags for versions
+
+  source  = "terraform-aws-modules/iam/aws//modules/iam-policy"
+  version = "5.58.0"
+
+  name_prefix = "data-engineering-datalake-access"
+
+  policy = data.aws_iam_policy_document.data_engineering_datalake_access.json
+}
+
+module "data_engineering_datalake_access_iam_role" {
+  #checkov:skip=CKV_TF_1:Module registry does not support commit hashes for versions
+  #checkov:skip=CKV_TF_2:Module registry does not support tags for versions
+
+  source  = "terraform-aws-modules/iam/aws//modules/iam-assumable-role"
+  version = "5.58.0"
+
+  create_role = true
+
+  role_name         = "data-engineering-datalake-access"
+  role_requires_mfa = false
+
+  trusted_role_arns = ["arn:aws:iam::${var.account_ids["analytical-platform-commons-production"]}:role/data-engineering-datalake-access-github-actions"]
+
+  custom_role_policy_arns = [module.data_engineering_datalake_access_iam_policy.arn]
+}
