7e43f751c4a66e7196b866a54371fb54674acbd7
Andrew Craik <72750362+andrewc-moj@users.noreply.github.com>
2025-06-19 17:07:36 +0100

    dmet-prisons-367-remove code not working

--- terraform/aws/analytical-platform-data-production/lakeformation-external-data/digital-prison-reporting-development/lake_formation_describe.tf
+++ terraform/aws/analytical-platform-data-production/lakeformation-external-data/digital-prison-reporting-development/lake_formation_describe.tf
@@ -7,16 +7,15 @@ resource "aws_lakeformation_permissions" "grant_describe_on_resource_link" {
     name = "dpr_ap_integration_test_tag_dev_dbt_resource_link" # The resource link DB name in APDP
   }
 }
-
-resource "aws_lakeformation_permissions" "grant_describe_on_table_link" {
-  provider = aws.session
-  for_each = toset(["dev_model_1_notag", "dev_model_2_tag"])
-
-  principal   = "arn:aws:iam::593291632749:role/alpha_user_andrewc-moj"
-  permissions = ["DESCRIBE"]
-
-  table {
-    database_name = "dpr_ap_integration_test_tag_dev_dbt_resource_link"
-    name          = each.key
-  }
-}
+# resource "aws_lakeformation_permissions" "grant_describe_on_table_link" {
+#   provider = aws.session
+#   for_each = toset(["dev_model_1_notag", "dev_model_2_tag"])
+#
+#   principal   = "arn:aws:iam::593291632749:role/alpha_user_andrewc-moj"
+#   permissions = ["DESCRIBE"]
+#
+#   table {
+#     database_name = "dpr_ap_integration_test_tag_dev_dbt_resource_link"
+#     name          = each.key
+#   }
+# }
