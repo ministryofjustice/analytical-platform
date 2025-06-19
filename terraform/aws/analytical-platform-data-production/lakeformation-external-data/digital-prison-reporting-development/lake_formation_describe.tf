a979e8763baba4a2035e7d52f05f4729788db380
Andrew <72750362+andrewc-moj@users.noreply.github.com>
2025-06-19 17:14:07 +0100

    dmet-prisons-367-remove code not working (#7984)
    * dmet-prisons-367-remove code not working
    
    * dmet-prisons-367-and-the-rest

--- terraform/aws/analytical-platform-data-production/lakeformation-external-data/digital-prison-reporting-development/lake_formation_describe.tf
+++ terraform/aws/analytical-platform-data-production/lakeformation-external-data/digital-prison-reporting-development/lake_formation_describe.tf
@@ -1,22 +1,21 @@
-resource "aws_lakeformation_permissions" "grant_describe_on_resource_link" {
-  provider    = aws.session
-  principal   = "arn:aws:iam::593291632749:role/alpha_user_andrewc-moj" # APDP role
-  permissions = ["DESCRIBE"]
-
-  database {
-    name = "dpr_ap_integration_test_tag_dev_dbt_resource_link" # The resource link DB name in APDP
-  }
-}
-
-resource "aws_lakeformation_permissions" "grant_describe_on_table_link" {
-  provider = aws.session
-  for_each = toset(["dev_model_1_notag", "dev_model_2_tag"])
-
-  principal   = "arn:aws:iam::593291632749:role/alpha_user_andrewc-moj"
-  permissions = ["DESCRIBE"]
-
-  table {
-    database_name = "dpr_ap_integration_test_tag_dev_dbt_resource_link"
-    name          = each.key
-  }
-}
+# resource "aws_lakeformation_permissions" "grant_describe_on_resource_link" {
+#   provider    = aws.session
+#   principal   = "arn:aws:iam::593291632749:role/alpha_user_andrewc-moj" # APDP role
+#   permissions = ["DESCRIBE"]
+#
+#   database {
+#     name = "dpr_ap_integration_test_tag_dev_dbt_resource_link" # The resource link DB name in APDP
+#   }
+# }
+# resource "aws_lakeformation_permissions" "grant_describe_on_table_link" {
+#   provider = aws.session
+#   for_each = toset(["dev_model_1_notag", "dev_model_2_tag"])
+#
+#   principal   = "arn:aws:iam::593291632749:role/alpha_user_andrewc-moj"
+#   permissions = ["DESCRIBE"]
+#
+#   table {
+#     database_name = "dpr_ap_integration_test_tag_dev_dbt_resource_link"
+#     name          = each.key
+#   }
+# }
