8c2383446245aedd29d2d19eda15016465b82800
Andrew Craik <72750362+andrewc-moj@users.noreply.github.com>
2025-06-19 17:08:33 +0100

    dmet-prisons-367-and-the-rest

--- terraform/aws/analytical-platform-data-production/lakeformation-external-data/digital-prison-reporting-development/lake_formation_describe.tf
+++ terraform/aws/analytical-platform-data-production/lakeformation-external-data/digital-prison-reporting-development/lake_formation_describe.tf
@@ -1,12 +1,12 @@
-resource "aws_lakeformation_permissions" "grant_describe_on_resource_link" {
-  provider    = aws.session
-  principal   = "arn:aws:iam::593291632749:role/alpha_user_andrewc-moj" # APDP role
-  permissions = ["DESCRIBE"]
-
-  database {
-    name = "dpr_ap_integration_test_tag_dev_dbt_resource_link" # The resource link DB name in APDP
-  }
-}
+# resource "aws_lakeformation_permissions" "grant_describe_on_resource_link" {
+#   provider    = aws.session
+#   principal   = "arn:aws:iam::593291632749:role/alpha_user_andrewc-moj" # APDP role
+#   permissions = ["DESCRIBE"]
+#
+#   database {
+#     name = "dpr_ap_integration_test_tag_dev_dbt_resource_link" # The resource link DB name in APDP
+#   }
+# }
 # resource "aws_lakeformation_permissions" "grant_describe_on_table_link" {
 #   provider = aws.session
 #   for_each = toset(["dev_model_1_notag", "dev_model_2_tag"])
