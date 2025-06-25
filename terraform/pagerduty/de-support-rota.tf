7052511c43d3d32c521ae5e06376dde9fc9dcf8f
Guy Wheeler <guy.wheeler@justice.gov.uk>
2025-06-25 10:11:18 +0100

    add new users and set rota to refresh at start of cycle

--- terraform/pagerduty/de-support-rota.tf
+++ terraform/pagerduty/de-support-rota.tf
@@ -7,7 +7,7 @@ locals {
         {
           name                         = "DE Daily Support Rota HEO_SEO"
           start                        = "2023-03-27T09:00:00Z"
-          rotation_virtual_start       = "2024-09-20T09:00:00+01:00"
+          rotation_virtual_start       = "2025-07-04T09:00:00+01:00"
           rotation_turn_length_seconds = 86400
           users = [
             module.users_de_seo["guy.wheeler@justice.gov.uk"].id,
@@ -17,9 +17,7 @@ locals {
             module.users_de_seo["anthony.cody@digital.justice.gov.uk"].id,
             module.users_de_seo["thomas.hirsch@justice.gov.uk"].id,
             module.users_de_seo["william.orr@digital.justice.gov.uk"].id,
-            module.users_de_seo["Mohammed.Ahad1@justice.gov.uk"].id,
-            module.users_de_seo["Theodoros.MANASSIS@justice.gov.uk"].id,
-            module.users_de_seo["Khristiania.Raihan@justice.gov.uk"].id,
+            module.users_de_seo["khristiania.raihan@justice.gov.uk"].id,
           ]
           restrictions = [
             {
@@ -63,17 +61,18 @@ locals {
         {
           name                         = "DE Daily Support Rota G7"
           start                        = "2023-03-27T09:00:00Z"
-          rotation_virtual_start       = "2024-09-20T09:00:00+01:00"
+          rotation_virtual_start       = "2025-07-04T09:00:00+01:00"
           rotation_turn_length_seconds = 86400
           users = [
             module.users_de_g7["matt.heery@justice.gov.uk"].id,
-            module.users_de_g7["lalitha.nagarur@digital.justice.gov.uk"].id,
+            module.users_de_g7["lalitha.nagarur3@justice.gov.uk"].id,
             module.users_de_g7["matthew.price2@justice.gov.uk"].id,
             module.users_de_g7["andrew.craik@justice.gov.uk"].id,
             module.users_de_g7["supratik.chowdhury@digital.justice.gov.uk"].id,
             module.users_de_g7["tapan.perkins@digital.justice.gov.uk"].id,
             module.users_de_g7["thomas.hepworth@justice.gov.uk"].id,
-            module.users_de_g7["Philip.Sinfield@justice.gov.uk"].id,
+            module.users_de_g7["philip.sinfield@justice.gov.uk"].id,
+            module.users_de_g7["laurence.droy@justice.gov.uk"].id,
           ]
           restrictions = [
             {
@@ -172,18 +171,8 @@ locals {
       role  = "responder"
     },
     {
-      name  = "Mohammed Ahad"
-      email = "Mohammed.Ahad1@justice.gov.uk"
-      role  = "responder"
-    },
-    {
-      name  = "Theodoros Manassis"
-      email = "Theodoros.MANASSIS@justice.gov.uk"
-      role  = "responder"
-    },
-    {
       name  = "Khristiania Raihan"
-      email = "Khristiania.Raihan@justice.gov.uk"
+      email = "khristiania.raihan@justice.gov.uk"
       role  = "responder"
     },
 
@@ -197,7 +186,7 @@ locals {
     },
     {
       name  = "Lalitha Nagarur"
-      email = "lalitha.nagarur@digital.justice.gov.uk"
+      email = "lalitha.nagarur3@justice.gov.uk"
       role  = "responder"
     },
     {
@@ -227,7 +216,12 @@ locals {
     },
     {
       name  = "Philip Sinfield"
-      email = "Philip.Sinfield@justice.gov.uk"
+      email = "philip.sinfield@justice.gov.uk"
+      role  = "responder"
+    },
+    {
+      name  = "Laurence Droy"
+      email = "laurence.droy@justice.gov.uk"
       role  = "responder"
     },
 
@@ -281,8 +275,8 @@ import {
 }
 
 import {
-  to = module.users_de_g7["lalitha.nagarur@digital.justice.gov.uk"].pagerduty_user.this
-  id = "PIT5XAZ"
+  to = module.users_de_g7["lalitha.nagarur3@justice.gov.uk"].pagerduty_user.this
+  id = "PKIIO6K"
 }
 
 import {
@@ -336,6 +330,16 @@ import {
 }
 
 import {
-  to = module.users_de_g7["thomas.hepworth@justice.gov.uk"].pagerduty_user.this
-  id = "PINRNK0"
+  to = module.users_de_g7["laurence.droy@justice.gov.uk"].pagerduty_user.this
+  id = "P2SZG76"
+}
+
+import {
+  to = module.users_de_g7["philip.sinfield@justice.gov.uk"].pagerduty_user.this
+  id = "P94ZLYO"
+}
+
+import {
+  to = module.users_de_seo["khristiania.raihan@justice.gov.uk"].pagerduty_user.this
+  id = "P0O354I"
 }
