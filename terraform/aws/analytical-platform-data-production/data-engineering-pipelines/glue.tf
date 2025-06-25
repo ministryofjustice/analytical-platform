f857196f32aa1c9b58d1018567294855c09c88ff
lalithanagarur <123457715+lalithanagarur@users.noreply.github.com>
2025-06-25 08:44:24 +0100

    updated glue policy (#7994)
    * updated glue policy
    
    * spelling correction
    
    ---------
    
    Co-authored-by: Jacob Woffenden <jacob.woffenden@justice.gov.uk>

--- terraform/aws/analytical-platform-data-production/data-engineering-pipelines/glue.tf
+++ terraform/aws/analytical-platform-data-production/data-engineering-pipelines/glue.tf
@@ -45,7 +45,11 @@ data "aws_iam_policy_document" "glue_ireland" {
             data.aws_caller_identity.current.account_id,
             data.aws_iam_role.aws_sso_modernisation_platform_data_eng.unique_id,
             "${data.aws_iam_role.aws_sso_modernisation_platform_data_eng.unique_id}:*" // data engineering role protection bypass
-          ]
+          ],
+          [for user_id in statement.value.de_role_names_to_exempt : [
+            data.aws_iam_role.de_glue_policy_role[user_id].unique_id,
+            "${data.aws_iam_role.de_glue_policy_role[user_id].unique_id}:*"
+          ]]
         ])
       }
     }
@@ -58,27 +62,11 @@ data "aws_iam_policy_document" "glue_ireland" {
       sid    = "${statement.value.name}DE"
       effect = "Allow"
       actions = [
-        "glue:UpdateTable",
-        "glue:UpdatePartition",
-        "glue:UpdateDatabase",
-        "glue:DeleteTableVersion",
-        "glue:DeleteTable",
-        "glue:DeletePartition",
-        "glue:DeleteDatabase",
-        "glue:CreateTable",
-        "glue:CreatePartition",
-        "glue:CreateDatabase",
-        "glue:BatchDeleteTableVersion",
-        "glue:BatchDeleteTable",
-        "glue:BatchDeletePartition",
-        "glue:BatchCreatePartition",
         "glue:GetDatabase",
         "glue:GetTable"
       ]
       resources = flatten([
         for pattern in statement.value.database_string_pattern : [
-          "arn:aws:glue:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:database/${pattern}",
-          "arn:aws:glue:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:table/${pattern}/*",
           "arn:aws:glue:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:catalog"
         ]
       ])
@@ -90,9 +78,9 @@ data "aws_iam_policy_document" "glue_ireland" {
         test     = "StringLike"
         variable = "aws:userId"
         values = flatten(
-          [for user_id in statement.value.data_engineering_role_names_to_allow : [
-            data.aws_iam_role.data_engineering_glue_policy_role[user_id].unique_id,
-            "${data.aws_iam_role.data_engineering_glue_policy_role[user_id].unique_id}:*"
+          [for user_id in statement.value.de_role_names_to_allow : [
+            data.aws_iam_role.de_glue_policy_role_allow[user_id].unique_id,
+            "${data.aws_iam_role.de_glue_policy_role_allow[user_id].unique_id}:*"
           ]]
         )
       }
