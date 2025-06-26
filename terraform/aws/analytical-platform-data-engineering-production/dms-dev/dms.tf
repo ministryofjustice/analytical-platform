5ada9c5b46a571456dc76f874b2caff9b5e58584
lalithanagarur <lalitha.nagarur3@justice.gov.uk>
2025-06-26 14:46:03 +0100

    added glue catalog role arn

--- terraform/aws/analytical-platform-data-engineering-production/dms-dev/dms.tf
+++ terraform/aws/analytical-platform-data-engineering-production/dms-dev/dms.tf
@@ -49,4 +49,5 @@ module "dev_dms_oasys" {
   )
 
   glue_catalog_arn = "arn:aws:glue:eu-west-1:${var.account_ids["analytical-platform-data-production"]}:catalog"
+  glue_catalog_role_arn = "arn:aws:iam::${var.account_ids["analytical-platform-data-production"]}:role/data-engineering-probation-glue"
 }
