ad2b5a1d29ecd838ec471680aaa883c74ea0b6d0
julialawrence <julia.lawrence@digital.justice.gov.uk>
2025-06-28 23:02:10 +0100

    Added LAA ingress information, updated directory name to be more generic and match state taxonomy

--- terraform/aws/analytical-platform-data-production/ingestion-ingress/datasync/s3.tf
+++ terraform/aws/analytical-platform-data-production/ingestion-ingress/datasync/s3.tf
@@ -61,3 +61,62 @@ module "datasync_opg_ingress_s3" {
     }
   }
 }
+
+data "aws_iam_policy_document" "datasync_laa_ingress_bucket_policy" {
+  statement {
+    sid    = "DataSyncLAAReplicationPermissions"
+    effect = "Allow"
+    principals {
+      type        = "AWS"
+      identifiers = ["arn:aws:iam::${var.account_ids["analytical-platform-ingestion-production"]}:role/laa-data-analysis-production-replication"]
+    }
+    actions = [
+      "s3:ReplicateObject",
+      "s3:ObjectOwnerOverrideToBucketOwner",
+      "s3:GetObjectVersionTagging",
+      "s3:ReplicateTags",
+      "s3:ReplicateDelete"
+    ]
+    resources = ["arn:aws:s3:::mojap-data-production-datasync-laa-ingress-production/*"]
+  }
+}
+
+#tfsec:ignore:AVD-AWS-0088:Bucket is encrypted with CMK KMS, but not detected by Trivy
+#tfsec:ignore:AVD-AWS-0089:Bucket logging not enabled currently
+#tfsec:ignore:AVD-AWS-0132:Bucket is encrypted with CMK KMS, but not detected by Trivy
+module "datasync_laa_ingress_s3" {
+  #checkov:skip=CKV_TF_1:Module registry does not support commit hashes for versions
+  #checkov:skip=CKV_TF_2:Module registry does not support tags for versions
+  #checkov:skip=CKV_AWS_18:Access logging not enabled currently
+  #checkov:skip=CKV_AWS_21:Versioning is enabled, but not detected by Checkov
+  #checkov:skip=CKV_AWS_145:Bucket is encrypted with CMK KMS, but not detected by Checkov
+  #checkov:skip=CKV_AWS_300:Lifecycle configuration not enabled currently
+  #checkov:skip=CKV_AWS_144:Cross-region replication is not required currently
+  #checkov:skip=CKV2_AWS_6:Public access block is enabled, but not detected by Checkov
+  #checkov:skip=CKV2_AWS_61:Lifecycle configuration not enabled currently
+  #checkov:skip=CKV2_AWS_62:Bucket notifications not required currently
+  #checkov:skip=CKV2_AWS_67:Regular CMK key rotation is not required currently
+
+  source  = "terraform-aws-modules/s3-bucket/aws"
+  version = "4.11.0"
+
+  bucket = "mojap-data-production-datasync-laa-ingress-production"
+
+  force_destroy = true
+
+  versioning = {
+    enabled = true
+  }
+
+  attach_policy = true
+  policy        = data.aws_iam_policy_document.datasync_laa_ingress_bucket_policy.json
+
+  server_side_encryption_configuration = {
+    rule = {
+      apply_server_side_encryption_by_default = {
+        kms_master_key_id = module.datasync_laa_kms.key_arn
+        sse_algorithm     = "aws:kms"
+      }
+    }
+  }
+}
