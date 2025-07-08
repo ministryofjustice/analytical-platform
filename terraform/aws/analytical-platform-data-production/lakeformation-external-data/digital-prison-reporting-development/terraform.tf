b3bb42dc5ff1c055c82a6b0d592f86a62478070d
Andrew Craik <72750362+andrewc-moj@users.noreply.github.com>
2025-07-08 15:52:03 +0100

    Revert "dmet-prisons: remove dev share - now moved to 'data-engineering-datalake-access'"
    This reverts commit ed526a0bcda3aa94ff55de345b8cff5dccc69267.

---
+++ terraform/aws/analytical-platform-data-production/lakeformation-external-data/digital-prison-reporting-development/terraform.tf
@@ -0,0 +1,57 @@
+terraform {
+  backend "s3" {
+    acl            = "private"
+    bucket         = "global-tf-state-aqsvzyd5u9"
+    encrypt        = true
+    key            = "aws/analytical-platform-data-production/lakeformation-external-data/digital-prison-reporting-development/terraform.tfstate"
+    region         = "eu-west-2"
+    dynamodb_table = "global-tf-state-aqsvzyd5u9-locks"
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.94.1"
+    }
+  }
+
+  required_version = "~> 1.5"
+}
+
+provider "aws" {
+  alias  = "session"
+  region = "eu-west-2"
+}
+
+provider "aws" {
+  alias  = "digital_prison_reporting_dev_eu_west_2"
+  region = "eu-west-2"
+  assume_role {
+    role_arn = "arn:aws:iam::${local.account_ids["digital-prison-reporting-development"]}:role/analytical-platform-data-production-share-role"
+  }
+  default_tags {
+    tags = var.tags
+  }
+}
+
+provider "aws" {
+  region = "eu-west-1"
+  assume_role {
+    role_arn = "arn:aws:iam::${local.account_ids["analytical-platform-data-production"]}:role/GlobalGitHubActionAdmin"
+  }
+  default_tags {
+    tags = var.tags
+  }
+}
+
+
+provider "aws" {
+  alias  = "analytical_platform_management_production"
+  region = "eu-west-1"
+  assume_role {
+    role_arn = can(regex("AdministratorAccess", data.aws_iam_session_context.session.issuer_arn)) ? null : "arn:aws:iam::${local.account_ids["analytical-platform-management-production"]}:role/GlobalGitHubActionAdmin"
+  }
+  default_tags {
+    tags = var.tags
+  }
+}
---
+++ terraform/aws/analytical-platform-data-production/lakeformation-external-data/digital-prison-reporting-development/terraform.tfvars
@@ -0,0 +1,14 @@
+##################################################
+# General
+##################################################
+
+tags = {
+  business-unit          = "Platforms"
+  application            = "lakeformation-external-data/digital-prison-reporting-development"
+  component              = "lakeformation-external-data/digital-prison-reporting-development"
+  environment            = "production"
+  is-production          = "true"
+  owner                  = "data-platform:data-platform-tech@digital.justice.gov.uk"
+  infrastructure-support = "data-platform:data-platform-tech@digital.justice.gov.uk"
+  source-code            = "github.com/ministryofjustice/data-platform/terraform/aws/analytical-platform-data-production/lakeformation-external-data/digital-prison-reporting-development"
+}
