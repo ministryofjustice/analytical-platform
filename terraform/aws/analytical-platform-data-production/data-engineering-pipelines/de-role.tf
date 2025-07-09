2f637643ec2e1866ae4b726a582b25e514cb3e91
sivabathina2 <123939271+sivabathina2@users.noreply.github.com>
2025-07-09 16:49:58 +0100

    Update de-role.tf

--- terraform/aws/analytical-platform-data-production/data-engineering-pipelines/de-role.tf
+++ terraform/aws/analytical-platform-data-production/data-engineering-pipelines/de-role.tf
@@ -14,7 +14,8 @@ module "data_engineering_probation_glue_access_iam_role" {
     "arn:aws:iam::189157455002:role/oasys-dev-metadata-generator",
     "arn:aws:iam::189157455002:role/oasys-preprod-metadata-generator",
     "arn:aws:iam::189157455002:role/oasys-prod-metadata-generator",
-    "arn:aws:iam::189157455002:role/delius-preprod-metadata-generator"
+    "arn:aws:iam::189157455002:role/delius-preprod-metadata-generator",
+    "arn:aws:iam::189157455002:role/oasys-prod-metadata-generator"
   ]
 
   inline_policy_statements = [
