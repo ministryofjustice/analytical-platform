5002c6564ae8d9e4ecacc09ea9fb93b4068c4a9b
Andrew Craik <72750362+andrewc-moj@users.noreply.github.com>
2025-07-15 12:24:23 +0100

    dmet-prisons: update key

--- terraform/aws/analytical-platform-data-production/lakeformation-external-data/digital-prisons-reporting-production/terraform.tf
+++ terraform/aws/analytical-platform-data-production/lakeformation-external-data/digital-prisons-reporting-production/terraform.tf
@@ -27,7 +27,7 @@ provider "aws" {
   alias  = "digital_prisons_reporting_prod_eu_west_2"
   region = "eu-west-2"
   assume_role {
-    role_arn = "arn:aws:iam::${local.account_ids["digital-prisons-reporting-production"]}:role/analytical-platform-data-production-share-role"
+    role_arn = "arn:aws:iam::${local.account_ids["digital-prison-reporting-production"]}:role/analytical-platform-data-production-share-role"
   }
   default_tags {
     tags = var.tags
