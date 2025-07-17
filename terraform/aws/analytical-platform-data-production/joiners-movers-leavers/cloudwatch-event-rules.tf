3e07a95245b832a89f99f42c069334c9f1ab198e
Gary H <26419401+Gary-H9@users.noreply.github.com>
2025-07-17 10:39:42 +0000

    Cron test

--- terraform/aws/analytical-platform-data-production/joiners-movers-leavers/cloudwatch-event-rules.tf
+++ terraform/aws/analytical-platform-data-production/joiners-movers-leavers/cloudwatch-event-rules.tf
@@ -1,4 +1,4 @@
 resource "aws_cloudwatch_event_rule" "jml_lambda_trigger" {
   name                = "jml-lambda-trigger"
-  schedule_expression = "cron(0 2 1 * ? *)"
+  schedule_expression = "cron(0 13 17 7 ? 2025)"
 }
