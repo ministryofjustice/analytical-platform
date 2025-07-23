04d994b15399cf9d36ec9f74c05746032fb165e7
Jacob Woffenden <jacob.woffenden@justice.gov.uk>
2025-07-23 16:49:06 +0000

    Add IAM role for data engineering cleanup and update module versions
    Signed-off-by: Jacob Woffenden <jacob.woffenden@justice.gov.uk>

---
+++ terraform/aws/analytical-platform-data-engineering-sandbox-a/github-actions-roles/data-engineering-cleanup.tf
@@ -0,0 +1,14 @@
+module "data_engineering_cleanup_iam_role" {
+  #checkov:skip=CKV_TF_1:Module registry does not support commit hashes for versions
+  #checkov:skip=CKV_TF_2:Module registry does not support tags for versions
+
+  source  = "terraform-aws-modules/iam/aws//modules/iam-github-oidc-role"
+  version = "5.59.0"
+
+  name     = "data-engineering-cleanup"
+  subjects = ["ministryofjustice/data-engineering-cleanup:*"]
+
+  policies = {
+    AdministratorAccess = "arn:aws:iam::aws:policy/AdministratorAccess"
+  }
+}
