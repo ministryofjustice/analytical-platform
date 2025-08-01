f76eab7d042a861e9bc02d0d304c1da95fe2400e
Jacob Woffenden <jacob.woffenden@justice.gov.uk>
2025-08-01 13:07:54 +0000

    Add Checkov skip comment for Glue crawler security configuration
    Signed-off-by: Jacob Woffenden <jacob.woffenden@justice.gov.uk>

--- terraform/aws/analytical-platform-data-production/coat-integration/glue-crawlers.tf
+++ terraform/aws/analytical-platform-data-production/coat-integration/glue-crawlers.tf
@@ -1,4 +1,6 @@
 resource "aws_glue_crawler" "coat" {
+  #checkov:skip=CKV_AWS_195:Glue Security Configuration is not required for this crawler
+
   name          = "${local.bucket_name}-glue-crawler"
   database_name = aws_glue_catalog_database.coat.name
   role          = module.glue_crawler_iam_role.iam_role_name
