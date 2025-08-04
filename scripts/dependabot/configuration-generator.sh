d89acb256e9261165f517e1961a91653bf7f5f1b
Gary <26419401+Gary-H9@users.noreply.github.com>
2025-08-04 17:35:44 +0100

    test

--- scripts/dependabot/configuration-generator.sh
+++ scripts/dependabot/configuration-generator.sh
@@ -38,11 +38,11 @@ for package_ecosystem in pip terraform; do
   esac
 
   printf "  - package-ecosystem: \"%s\"\n" "${package_ecosystem}" >>"${DEPENDABOT_CONFIGURATION_FILE}"
-  printf "    open-pull-requests-limit: 15\n" >>"${DEPENDABOT_CONFIGURATION_FILE}"
   printf "    schedule:\n" >>"${DEPENDABOT_CONFIGURATION_FILE}"
   printf "      interval: \"daily\"\n" >>"${DEPENDABOT_CONFIGURATION_FILE}"
   printf "      time: \"09:00\"\n" >>"${DEPENDABOT_CONFIGURATION_FILE}"
   printf "      timezone: \"Europe/London\"\n" >>"${DEPENDABOT_CONFIGURATION_FILE}"
+  printf "    open-pull-requests-limit: 15\n" >>"${DEPENDABOT_CONFIGURATION_FILE}"
   printf "    commit-message:\n" >>"${DEPENDABOT_CONFIGURATION_FILE}"
   printf "      prefix: \":dependabot: %s\"\n" "${package_ecosystem}" >>"${DEPENDABOT_CONFIGURATION_FILE}"
   printf "      include: \"scope\"\n" >>"${DEPENDABOT_CONFIGURATION_FILE}"
