aba19279a9b8b738bfaa38bf10f86824f5379279
sivabathina2 <123939271+sivabathina2@users.noreply.github.com>
2025-08-07 09:20:27 +0100

    Update dms.tf

--- terraform/aws/analytical-platform-data-engineering-production/dms-dev/dms.tf
+++ terraform/aws/analytical-platform-data-engineering-production/dms-dev/dms.tf
@@ -2,7 +2,7 @@ module "dev_dms_oasys" {
   # checkov:skip=CKV_TF_1: Skipping because currently want to reference a branch whilst making changes to the dms module. Will update once dms module is stable.
   # checkov:skip=CKV_TF_2: Skipping as waiting for dms module to be stable before making a release.
 
-  source      = "github.com/ministryofjustice/terraform-dms-module?ref=bfaadb04af766dffabdf157f8c1628e80169452d"
+  source      = "github.com/ministryofjustice/terraform-dms-module?ref=0d0898f21dcaae9bdecdbc9cb2d98fdcce8aaad5"
   vpc_id      = module.vpc.vpc_id
   environment = var.tags.environment-name
 
