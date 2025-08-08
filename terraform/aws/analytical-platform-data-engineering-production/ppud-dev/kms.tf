5ce1e89aa765bf4d2d9dd6b0ef3af50af77c3629
lalithanagarur <123457715+lalithanagarur@users.noreply.github.com>
2025-08-08 10:29:16 +0100

    Ppud dev add secret (#8293)
    * adding secret
    
    * linting update
    
    * trivy comments

---
+++ terraform/aws/analytical-platform-data-engineering-production/ppud-dev/kms.tf
@@ -0,0 +1,34 @@
+module "rds_export_kms_dev" {
+
+  # Commit hash for v3.1.0
+  source = "git::https://github.com/terraform-aws-modules/terraform-aws-kms?ref=fe1beca2118c0cb528526e022a53381535bb93cd"
+
+  aliases               = ["rds/rds-ppud-dev"]
+  description           = "Used in the HMPPS probation domain to encode secrets and exported snapshots for RDS export"
+  enable_default_policy = true
+
+  key_statements = [
+    {
+      sid    = "AllowLambdaServiceAccess"
+      effect = "Allow"
+      actions = [
+        "kms:Encrypt*",
+        "kms:Decrypt*",
+        "kms:GenerateDataKey*",
+        "kms:Describe*"
+      ]
+      resources = ["*"]
+
+      principals = [
+        {
+          type        = "Service"
+          identifiers = ["lambda.amazonaws.com"]
+        }
+      ]
+    }
+  ]
+
+  deletion_window_in_days = 7
+
+  tags = var.tags
+}
