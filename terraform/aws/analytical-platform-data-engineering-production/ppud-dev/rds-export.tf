a27d8a9c4604b2201a33e875ec1259bc93d4213d
lalithanagarur <lalitha.nagarur3@justice.gov.uk>
2025-08-13 17:15:27 +0100

    updates made to module

--- terraform/aws/analytical-platform-data-engineering-production/ppud-dev/rds-export.tf
+++ terraform/aws/analytical-platform-data-engineering-production/ppud-dev/rds-export.tf
@@ -2,36 +2,11 @@ locals {
   name = "ppud-dev"
 }
 
-data "aws_vpc" "selected" {
-  id     = "vpc-0b9d7482fd1f0c601"
-  region = "eu-west-1"
-}
-
-# Get all subnets with the tag "network" = "Private" in the selected VPC
-data "aws_subnets" "private_subnets" {
-  filter {
-    name   = "tag:network"
-    values = ["Private"]
-  }
-
-  filter {
-    name   = "vpc-id"
-    values = [data.aws_vpc.selected.id]
-  }
-}
-
-resource "aws_db_subnet_group" "export" {
-  name       = local.name
-  subnet_ids = data.aws_subnets.private_subnets.ids
-
-  tags = var.tags
-}
-
 # Security group for the RDS instance
 resource "aws_security_group" "db" {
   name        = local.name
   description = "Security group for RDS instance ${local.name}"
-  vpc_id      = data.aws_vpc.selected.id
+  vpc_id      = module.vpc.vpc_id
 
   tags = var.tags
 }
@@ -43,7 +18,7 @@ resource "aws_security_group_rule" "db_ingress" {
   to_port           = 1433
   protocol          = "tcp"
   security_group_id = aws_security_group.db.id
-  cidr_blocks       = [data.aws_vpc.selected.cidr_block]
+  cidr_blocks       = [module.vpc.vpc_cidr_block]
 }
 
 module "rds_export" {
@@ -54,8 +29,8 @@ module "rds_export" {
   }
 
   name                  = local.name
-  vpc_id                = data.aws_vpc.selected.id
-  database_subnet_ids   = data.aws_subnets.private_subnets.ids
+  vpc_id                = module.vpc.vpc_id
+  database_subnet_ids   = module.vpc.database_subnets
   kms_key_arn           = module.rds_export_kms_dev.key_arn
   master_user_secret_id = aws_secretsmanager_secret.rds_export_dev.arn
 
