e99582ee1779f5bf24b85ad898e781d3edc03a41
Jacob Woffenden <jacob.woffenden@justice.gov.uk>
2025-08-14 16:32:27 +0000

    🔧 Update Terraform provider versions and IAM policies for Route53
    Signed-off-by: Jacob Woffenden <jacob.woffenden@justice.gov.uk>

--- terraform/aws/analytical-platform-production/route53/iam-roles.tf
+++ terraform/aws/analytical-platform-production/route53/iam-roles.tf
@@ -2,19 +2,30 @@ module "analytical_platform_compute_route53_access_iam_role" {
   #checkov:skip=CKV_TF_1:Module registry does not support commit hashes for versions
   #checkov:skip=CKV_TF_2:Module registry does not support tags for versions
 
-  source  = "terraform-aws-modules/iam/aws//modules/iam-assumable-role"
-  version = "5.60.0"
+  source  = "terraform-aws-modules/iam/aws//modules/iam-role"
+  version = "6.0.0"
 
-  create_role = true
+  name            = "analytical-platform-compute-route53-access"
+  use_name_prefix = false
 
-  role_name         = "analytical-platform-compute-route53-access"
-  role_requires_mfa = false
+  trust_policy_permissions = {
+    trusted_role_arns = {
+      actions = [
+        "sts:AssumeRole",
+        "sts:TagSession"
+      ]
+      principals = [{
+        type = "AWS"
+        identifiers = [
+          "arn:aws:iam::${var.account_ids["analytical-platform-compute-development"]}:root",
+          "arn:aws:iam::${var.account_ids["analytical-platform-compute-test"]}:root",
+          "arn:aws:iam::${var.account_ids["analytical-platform-compute-production"]}:root"
+        ]
+      }]
+    }
+  }
 
-  trusted_role_arns = [
-    "arn:aws:iam::${var.account_ids["analytical-platform-compute-development"]}:root",
-    "arn:aws:iam::${var.account_ids["analytical-platform-compute-test"]}:root",
-    "arn:aws:iam::${var.account_ids["analytical-platform-compute-production"]}:root",
-  ]
-
-  custom_role_policy_arns = [module.analytical_platform_compute_route53_access_iam_policy.arn]
+  policies = {
+    analytical_platform_compute_route53_access = module.analytical_platform_compute_route53_access_iam_policy.arn
+  }
 }
