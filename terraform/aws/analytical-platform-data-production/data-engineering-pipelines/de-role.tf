ee2a78faecbee18d927f89d757d1dbeb7c66af73
Jacob Woffenden <jacob.woffenden@justice.gov.uk>
2025-08-14 12:50:28 +0000

    Update S3 bucket module version and refactor IAM role configuration
    Signed-off-by: Jacob Woffenden <jacob.woffenden@justice.gov.uk>

--- terraform/aws/analytical-platform-data-production/data-engineering-pipelines/de-role.tf
+++ terraform/aws/analytical-platform-data-production/data-engineering-pipelines/de-role.tf
@@ -2,24 +2,43 @@ module "data_engineering_probation_glue_access_iam_role" {
   #checkov:skip=CKV_TF_1:Module registry does not support commit hashes for versions
   #checkov:skip=CKV_TF_2:Module registry does not support tags for versions
 
-  source  = "terraform-aws-modules/iam/aws//modules/iam-assumable-role"
-  version = "5.60.0"
+  source  = "terraform-aws-modules/iam/aws//modules/iam-role"
+  version = "6.0.0"
 
-  create_role = true
 
-  role_name         = "data-engineering-probation-glue"
-  role_requires_mfa = false
+  name            = "data-engineering-probation-glue"
+  use_name_prefix = false
 
-  trusted_role_arns = [
-    "arn:aws:iam::189157455002:role/oasys-dev-metadata-generator",
-    "arn:aws:iam::189157455002:role/oasys-preprod-metadata-generator",
-    "arn:aws:iam::189157455002:role/oasys-prod-metadata-generator",
-    "arn:aws:iam::189157455002:role/delius-preprod-metadata-generator",
-    "arn:aws:iam::189157455002:role/delius-prod-metadata-generator"
-  ]
+  trust_policy_permissions = {
+    trusted_role_arns = {
+      actions = [
+        "sts:AssumeRole",
+        "sts:TagSession"
+      ]
+      principals = [{
+        type = "AWS"
+        identifiers = [
+          "arn:aws:iam::189157455002:role/oasys-dev-metadata-generator",
+          "arn:aws:iam::189157455002:role/oasys-preprod-metadata-generator",
+          "arn:aws:iam::189157455002:role/oasys-prod-metadata-generator",
+          "arn:aws:iam::189157455002:role/delius-preprod-metadata-generator",
+          "arn:aws:iam::189157455002:role/delius-prod-metadata-generator"
+        ]
+      }]
+    }
+  }
 
-  inline_policy_statements = [
-    {
+  # trusted_role_arns = [
+  #   "arn:aws:iam::189157455002:role/oasys-dev-metadata-generator",
+  #   "arn:aws:iam::189157455002:role/oasys-preprod-metadata-generator",
+  #   "arn:aws:iam::189157455002:role/oasys-prod-metadata-generator",
+  #   "arn:aws:iam::189157455002:role/delius-preprod-metadata-generator",
+  #   "arn:aws:iam::189157455002:role/delius-prod-metadata-generator"
+  # ]
+
+  create_inline_policy = true
+  inline_policy_permissions = {
+    glue_access = {
       sid    = "GlueAccess"
       effect = "Allow"
       actions = [
@@ -49,6 +68,39 @@ module "data_engineering_probation_glue_access_iam_role" {
         "arn:aws:glue:eu-west-1:593291632749:catalog"
       ]
     }
-  ]
+  }
+
+  # inline_policy_statements = [
+  #   {
+  #     sid    = "GlueAccess"
+  #     effect = "Allow"
+  #     actions = [
+  #       "glue:GetTable",
+  #       "glue:GetDatabase",
+  #       "glue:GetCatalog",
+  #       "glue:UpdateTable",
+  #       "glue:UpdatePartition",
+  #       "glue:UpdateDatabase",
+  #       "glue:DeleteTableVersion",
+  #       "glue:DeleteTable",
+  #       "glue:DeletePartition",
+  #       "glue:DeleteDatabase",
+  #       "glue:CreateTable",
+  #       "glue:CreatePartition",
+  #       "glue:CreateDatabase",
+  #       "glue:BatchDeleteTableVersion",
+  #       "glue:BatchDeleteTable",
+  #       "glue:BatchDeletePartition",
+  #       "glue:BatchCreatePartition"
+  #     ]
+  #     resources = [
+  #       "arn:aws:glue:eu-west-1:593291632749:table/oasys*/*",
+  #       "arn:aws:glue:eu-west-1:593291632749:database/oasys*",
+  #       "arn:aws:glue:eu-west-1:593291632749:table/delius*/*",
+  #       "arn:aws:glue:eu-west-1:593291632749:database/delius*",
+  #       "arn:aws:glue:eu-west-1:593291632749:catalog"
+  #     ]
+  #   }
+  # ]
 
 }
