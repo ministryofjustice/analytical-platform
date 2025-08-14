be4e2c59305015df7d22e424179649d29d865877
Gary H <26419401+Gary-H9@users.noreply.github.com>
2025-08-14 17:14:03 +0000

    Undo changes

--- terraform/aws/analytical-platform-data-production/ingestion-ingress/dms/iam-roles.tf
+++ terraform/aws/analytical-platform-data-production/ingestion-ingress/dms/iam-roles.tf
@@ -4,7 +4,7 @@ module "dms_ingress_iam_role" {
 
   for_each = local.analytical_platform_ingestion_environments
 
-  source  = "terraform-aws-modules/iam/aws//modules/iam-role"
+  source  = "terraform-aws-modules/iam/aws//modules/iam-assumable-role"
   version = "5.60.0"
 
   create_role = true
