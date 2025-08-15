07a01f300d743ce092b50c13f57eb4e720dc1a40
Jacob Woffenden <jacob.woffenden@justice.gov.uk>
2025-08-15 12:17:49 +0000

    🔧 Update AWS provider and IAM module versions to 6.9.0 and 6.1.0 respectively
    Signed-off-by: Jacob Woffenden <jacob.woffenden@justice.gov.uk>

--- terraform/aws/analytical-platform-production/route53/iam-roles.tf
+++ terraform/aws/analytical-platform-production/route53/iam-roles.tf
@@ -3,7 +3,7 @@ module "analytical_platform_compute_route53_access_iam_role" {
   #checkov:skip=CKV_TF_2:Module registry does not support tags for versions
 
   source  = "terraform-aws-modules/iam/aws//modules/iam-role"
-  version = "6.0.0"
+  version = "6.1.0"
 
   name            = "analytical-platform-compute-route53-access"
   use_name_prefix = false
