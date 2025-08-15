cd2b31a8ba1b4fa6020be0d7c7da4b26ff716b4e
Jacob Woffenden <jacob.woffenden@justice.gov.uk>
2025-08-15 13:51:54 +0100

    Update iam-roles.tf

--- terraform/aws/analytical-platform-production/route53/iam-roles.tf
+++ terraform/aws/analytical-platform-production/route53/iam-roles.tf
@@ -9,7 +9,7 @@ module "analytical_platform_compute_route53_access_iam_role" {
   use_name_prefix = false
 
   trust_policy_permissions = {
-    trusted_role_arns = {
+    TrustRoleAndServiceToAssume = {
       actions = [
         "sts:AssumeRole",
         "sts:TagSession"
