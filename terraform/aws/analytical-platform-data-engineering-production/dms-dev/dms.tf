b5bf0ddff5a783fdab77f9d8e6f151b891bd56e1
lalithanagarur <lalitha.nagarur3@justice.gov.uk>
2025-08-20 16:51:08 +0100

    imported relevant resources

--- terraform/aws/analytical-platform-data-engineering-production/dms-dev/dms.tf
+++ terraform/aws/analytical-platform-data-engineering-production/dms-dev/dms.tf
@@ -1,7 +1,4 @@
 module "dev_dms_oasys" {
-  # checkov:skip=CKV_TF_1: Skipping because currently want to reference a branch whilst making changes to the dms module. Will update once dms module is stable.
-  # checkov:skip=CKV_TF_2: Skipping as waiting for dms module to be stable before making a release.
-
   source      = "github.com/ministryofjustice/terraform-dms-module?ref=a8f5d7d6f4984d3d7cf62d1410d49512a31f3556"
   vpc_id      = module.vpc.vpc_id
   environment = var.tags.environment-name
