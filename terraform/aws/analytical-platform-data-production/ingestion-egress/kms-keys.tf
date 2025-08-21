b8f616e3ce05b1e3c9f81574d9d3f967a2ba1210
Jacob Woffenden <jacob.woffenden@justice.gov.uk>
2025-08-21 16:17:52 +0100

    🚚 SSCT Egress (#8543)
    Signed-off-by: Jacob Woffenden <jacob.woffenden@justice.gov.uk>

--- terraform/aws/analytical-platform-data-production/ingestion-egress/kms-keys.tf
+++ terraform/aws/analytical-platform-data-production/ingestion-egress/kms-keys.tf
@@ -67,3 +67,38 @@ module "production_kms_eu_west_1_replica" {
 
   deletion_window_in_days = 7
 }
+
+module "shared_services_client_team_gov_29148_egress_kms" {
+  #checkov:skip=CKV_TF_1:Module registry does not support commit hashes for versions
+  #checkov:skip=CKV_TF_2:Module registry does not support tags for versions
+
+  source  = "terraform-aws-modules/kms/aws"
+  version = "4.0.0"
+
+  aliases               = ["s3/mojap-data-production-ssct-gov-29148-egress"]
+  description           = "Shared Services Client Team GOV-29148 Egress"
+  enable_default_policy = true
+  multi_region          = true
+
+  deletion_window_in_days = 7
+}
+
+module "shared_services_client_team_gov_29148_egress_kms_eu_west_1_replica" {
+  #checkov:skip=CKV_TF_1:Module registry does not support commit hashes for versions
+  #checkov:skip=CKV_TF_2:Module registry does not support tags for versions
+
+  source  = "terraform-aws-modules/kms/aws"
+  version = "4.0.0"
+
+  providers = {
+    aws = aws.eu-west-1
+  }
+
+  aliases               = ["s3/mojap-data-production-ssct-gov-29148-egress-replica"]
+  description           = "Shared Services Client Team GOV-29148 Egress - Replica"
+  enable_default_policy = true
+  create_replica        = true
+  primary_key_arn       = module.shared_services_client_team_gov_29148_egress_kms.key_arn
+
+  deletion_window_in_days = 7
+}
