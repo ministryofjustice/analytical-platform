432a5cd294774703ccb99e57aa107c8e63dba6f1
Jacob Woffenden <jacob.woffenden@justice.gov.uk>
2025-08-21 12:21:09 +0000

    feat: add shared services client team GOV-29148 egress IAM roles, policies, KMS keys, and S3 bucket configuration
    Signed-off-by: Jacob Woffenden <jacob.woffenden@justice.gov.uk>

--- terraform/aws/analytical-platform-data-production/ingestion-egress/iam-roles.tf
+++ terraform/aws/analytical-platform-data-production/ingestion-egress/iam-roles.tf
@@ -61,3 +61,35 @@ module "production_replication_iam_role" {
     custom = module.production_replication_iam_policy.arn
   }
 }
+
+module "shared_services_client_team_gov_29148_egress_iam_role" {
+  #checkov:skip=CKV_TF_1:Module is from Terraform registry
+  #checkov:skip=CKV_TF_2:Module registry does not support tags for versions
+
+  source  = "terraform-aws-modules/iam/aws//modules/iam-role"
+  version = "6.1.2"
+
+  name            = "mojap-data-production-ssct-gov-29148-egress"
+  use_name_prefix = false
+
+  trust_policy_permissions = {
+    TrustS3AndIngestRoles = {
+      actions = [
+        "sts:AssumeRole",
+        "sts:TagSession"
+      ]
+      principals = concat(
+        [
+          {
+            type        = "Service"
+            identifiers = ["s3.amazonaws.com"]
+          }
+        ]
+      )
+    }
+  }
+
+  policies = {
+    custom = module.shared_services_client_team_gov_29148_egress_iam_policy.arn
+  }
+}
