69ba566a4918e23aa06bc207a4b74cc40c17e2da
lalithanagarur <lalitha.nagarur3@justice.gov.uk>
2025-08-27 11:32:03 +0100

    remove old files

--- terraform/aws/analytical-platform-data-engineering-sandbox-a/db-export/secrets.tf
+++
@@ -1,6 +0,0 @@
-resource "aws_secretsmanager_secret" "rds_export_sandbox" {
-  # checkov:skip=CKV2_AWS_57: Skipping because automatic rotation not needed.
-  name       = "rds_export_sandbox"
-  kms_key_id = module.rds_export_kms.key_arn
-  tags = local.tags
-}
--- terraform/aws/analytical-platform-data-engineering-sandbox-a/ppud-sandbox/terraform.tf
+++ terraform/aws/analytical-platform-data-engineering-sandbox-a/ppud-sandbox/terraform.tf
@@ -13,5 +13,6 @@ terraform {
     key     = "rds-export-serj-test/terraform.tfstate"
     #use_lockfile = true
     region = "eu-west-1"
+    # profile = "sso-de-sandbox"
   }
 }
