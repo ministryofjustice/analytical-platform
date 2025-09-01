685f877264f948f0e6cf4de56e9c423cff7c0b42
Gary H <26419401+Gary-H9@users.noreply.github.com>
2025-09-01 08:37:47 +0000

    Remove data block

--- terraform/aws/analytical-platform-data-engineering-sandbox-a/coat-datasync-testing/data.tf
+++ terraform/aws/analytical-platform-data-engineering-sandbox-a/coat-datasync-testing/data.tf
@@ -7,9 +7,3 @@ data "aws_iam_session_context" "session" {
 
   arn = data.aws_caller_identity.session.arn
 }
-
-data "aws_secretsmanager_secret_version" "analytical_platform_compute_cluster_data" {
-  provider = aws.analytical-platform-management-production
-
-  secret_id = "analytical-platform-compute/cluster-data"
-}
