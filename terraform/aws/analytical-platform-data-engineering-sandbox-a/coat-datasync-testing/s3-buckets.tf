fa1a9f997b4e329f249b5a4e0f4b21ffc8b3ca9d
Gary H <26419401+Gary-H9@users.noreply.github.com>
2025-09-01 13:12:21 +0000

    Update bucket policy

--- terraform/aws/analytical-platform-data-engineering-sandbox-a/coat-datasync-testing/s3-buckets.tf
+++ terraform/aws/analytical-platform-data-engineering-sandbox-a/coat-datasync-testing/s3-buckets.tf
@@ -1,3 +1,33 @@
+data "aws_iam_policy_document" "coat_bucket_policy" {
+  statement {
+    sid    = "DataSyncCreateS3LocationAndTaskAccess"
+    effect = "Allow"
+
+    principals {
+      type        = "AWS"
+      identifiers = ["arn:aws:iam::684969100054:role/coat-datasync-iam-role"] #TODO: Change this in the root account implementation
+    }
+
+    actions = [
+      "s3:GetBucketLocation",
+      "s3:ListBucket",
+      "s3:ListBucketMultipartUploads",
+      "s3:AbortMultipartUpload",
+      "s3:DeleteObject",
+      "s3:GetObject",
+      "s3:ListMultipartUploadParts",
+      "s3:PutObject",
+      "s3:GetObjectTagging",
+      "s3:PutObjectTagging",
+    ]
+
+    resources = [
+      "arn:aws:s3:::coat-testing-bucket",  #TODO: Update call
+      "arn:aws:s3:::coat-testing-bucket/*" #TODO: Update call
+    ]
+  }
+}
+
 #trivy:ignore:AVD-AWS-0089:Bucket logging not enabled currently
 module "coat_s3" {
   #checkov:skip=CKV_TF_1:Module registry does not support commit hashes for versions
@@ -14,8 +44,8 @@ module "coat_s3" {
     enabled = true
   }
 
-  # attach_policy = true
-  # policy        = data.aws_iam_policy_document.coat_bucket_policy.json
+  attach_policy = true
+  policy        = data.aws_iam_policy_document.coat_bucket_policy.json
 
   server_side_encryption_configuration = {
     bucket_key_enabled = true
