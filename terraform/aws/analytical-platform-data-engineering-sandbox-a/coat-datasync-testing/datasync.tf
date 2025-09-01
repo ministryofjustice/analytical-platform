05a324777039ec983a8d6d3be226267e60650252
Gary H <26419401+Gary-H9@users.noreply.github.com>
2025-09-01 14:48:03 +0000

    Add region to DataSync destination location

--- terraform/aws/analytical-platform-data-engineering-sandbox-a/coat-datasync-testing/datasync.tf
+++ terraform/aws/analytical-platform-data-engineering-sandbox-a/coat-datasync-testing/datasync.tf
@@ -6,17 +6,17 @@ resource "aws_datasync_location_s3" "root_account" {
   subdirectory  = "/"
 
   s3_config {
-    bucket_access_role_arn = "arn:aws:iam::684969100054:role/coat-datasync-iam-role"
+    bucket_access_role_arn = "arn:aws:iam::684969100054:role/coat-datasync-iam-role" #TODO: Update call
   }
 }
 
 # DESTINATION - APDP
 resource "aws_datasync_location_s3" "apdp_account" {
-  provider      = aws.analytical-platform-data-production-eu-west-1
+  region        = "eu-west-1"
   s3_bucket_arn = "arn:aws:s3:::mojap-data-production-coat-cur-reports-v2-hourly" #TODO: Update how this is derived in the root account
   subdirectory  = "/"
 
   s3_config {
-    bucket_access_role_arn = "arn:aws:iam::684969100054:role/coat-datasync-iam-role"
+    bucket_access_role_arn = "arn:aws:iam::684969100054:role/coat-datasync-iam-role" #TODO: Update call
   }
 }
