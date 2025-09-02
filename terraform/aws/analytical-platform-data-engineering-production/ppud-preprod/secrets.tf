24a9de0a1599d0947b89964636a4f7f454ad125b
lalithanagarur <123457715+lalithanagarur@users.noreply.github.com>
2025-09-02 15:01:18 +0100

    Ppud preprod updates (#8717)
    * updated preprod
    
    * added rds export module

---
+++ terraform/aws/analytical-platform-data-engineering-production/ppud-preprod/secrets.tf
@@ -0,0 +1,16 @@
+module "rds_export_secret" {
+  #checkov:skip=CKV_TF_1:Module registry does not support commit hashes for versions
+  #checkov:skip=CKV_TF_2:Module registry does not support tags for versions
+
+  source  = "terraform-aws-modules/secrets-manager/aws"
+  version = "1.3.1"
+
+  name       = "rds_export_ppud_preprod"
+  kms_key_id = module.rds_export_kms_preprod.key_arn
+
+  ignore_secret_changes  = true
+  create_random_password = true
+  random_password_length = 13
+
+  tags = var.tags
+}
