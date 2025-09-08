b91855428597c66c41f8a628e46fad95cfcb78da
Siva Bathina <siva.bathina@justice.gov.uk>
2025-09-08 15:56:54 +0100

    new reference dms module commit and changed to 4:30pm

--- terraform/aws/analytical-platform-data-engineering-production/dms-dev/scheduler.tf
+++ terraform/aws/analytical-platform-data-engineering-production/dms-dev/scheduler.tf
@@ -1,13 +1,5 @@
 # Execution role for EventBridge Scheduler
 
-
-data "aws_region" "current" {}
-data "aws_caller_identity" "current" {}
-
-locals {
-  dms_task_id  = "oasys-dev-cdc"
-  dms_task_arn = "arn:aws:dms:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:task:${local.dms_task_id}"
-}
 resource "aws_iam_role" "scheduler_dms_role" {
   name = "eventbridge-scheduler-dms-dev"
 
@@ -35,16 +27,16 @@ resource "aws_iam_role_policy" "scheduler_dms_policy" {
         "dms:StartReplicationTask",
         "dms:StopReplicationTask"
       ],
-      Resource = local.dms_task_arn
+      Resource = module.dev_dms_oasys.dms_cdc_task_arn
     }]
   })
 }
 
 # Stop DMS task — Monday 14:00 UK time (later need to change to weekend for preprod)
-resource "aws_scheduler_schedule" "dms_stop_mon_13" {
-  name                         = "dms-stop-mon-2pm-uk-dev"
-  description                  = "Stop DMS replication task every mon 14:00 UK"
-  schedule_expression          = "cron(0 14 ? * 2 *)"
+resource "aws_scheduler_schedule" "dms_stop_mon_1630" {
+  name                         = "dms-stop-mon-4:30pm-uk-dev"
+  description                  = "Stop DMS replication task every mon 16:30 UK"
+  schedule_expression          = "cron(30 16 ? * 2 *)"
   schedule_expression_timezone = "Europe/London"
   state                        = "ENABLED"
 
@@ -53,16 +45,16 @@ resource "aws_scheduler_schedule" "dms_stop_mon_13" {
   target {
     arn      = "arn:aws:scheduler:::aws-sdk:dms:stopReplicationTask"
     role_arn = aws_iam_role.scheduler_dms_role.arn
-    input    = jsonencode({ ReplicationTaskArn = local.dms_task_arn })
+    input    = jsonencode({ ReplicationTaskArn = module.dev_dms_oasys.dms_cdc_task_arn })
   }
 
 }
 
 # Start DMS task — Thursdays 13:30 UK time (later need to change to weekend for preprod)
-resource "aws_scheduler_schedule" "dms_start_mon_1430" {
-  name                         = "dms-start-mon-2-30pm-uk-dev"
-  description                  = "Start DMS replication task every mon 14:30 UK"
-  schedule_expression          = "cron(30 14 ? * 2 *)"
+resource "aws_scheduler_schedule" "dms_start_mon_1700" {
+  name                         = "dms-start-mon-5pm-uk-dev"
+  description                  = "Start DMS replication task every mon 17:00 UK"
+  schedule_expression          = "cron(0 17 ? * 2 *)"
   schedule_expression_timezone = "Europe/London"
   state                        = "ENABLED"
 
@@ -72,7 +64,7 @@ resource "aws_scheduler_schedule" "dms_start_mon_1430" {
     arn      = "arn:aws:scheduler:::aws-sdk:dms:startReplicationTask"
     role_arn = aws_iam_role.scheduler_dms_role.arn
     input = jsonencode({
-      ReplicationTaskArn       = local.dms_task_arn
+      ReplicationTaskArn       = module.dev_dms_oasys.dms_cdc_task_arn,
       StartReplicationTaskType = "resume-processing"
     })
   }
