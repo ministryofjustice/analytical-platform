4fbeb4d12cd0370889b069f1127ec3161aec47f5
sivabathina2 <123939271+sivabathina2@users.noreply.github.com>
2025-09-09 10:45:20 +0100

    Update scheduler.tf

--- terraform/aws/analytical-platform-data-engineering-production/dms-dev/scheduler.tf
+++ terraform/aws/analytical-platform-data-engineering-production/dms-dev/scheduler.tf
@@ -33,17 +33,17 @@ resource "aws_iam_role_policy" "scheduler_dms_policy" {
 }
 
 # Stop DMS task — Monday 14:00 UK time (later need to change to weekend for preprod)
-resource "aws_scheduler_schedule" "dms_stop_mon_1630" {
-  name                         = "dms-stop-mon-4-30pm-uk-dev"
-  description                  = "Stop DMS replication task every mon 16:30 UK"
-  schedule_expression          = "cron(30 16 ? * 2 *)"
+resource "aws_scheduler_schedule" "dms_stop_tue_1130" {
+  name                         = "dms-stop-tue-11-30am-uk-dev"
+  description                  = "Stop DMS replication task every tue 11:30 UK"
+  schedule_expression          = "cron(30 11 ? * 3 *)"
   schedule_expression_timezone = "Europe/London"
   state                        = "ENABLED"
 
   flexible_time_window { mode = "OFF" }
 
   target {
-    arn      = "arn:aws:scheduler:::aws-sdk:dms:StopReplicationTask"
+    arn      = "arn:aws:scheduler:::aws-sdk:databasemigration:stopReplicationTask"
     role_arn = aws_iam_role.scheduler_dms_role.arn
     input    = jsonencode({ ReplicationTaskArn = module.dev_dms_oasys.dms_cdc_task_arn })
   }
@@ -52,16 +52,16 @@ resource "aws_scheduler_schedule" "dms_stop_mon_1630" {
 
 # Start DMS task — Thursdays 13:30 UK time (later need to change to weekend for preprod)
 resource "aws_scheduler_schedule" "dms_start_mon_1700" {
-  name                         = "dms-start-mon-5pm-uk-dev"
-  description                  = "Start DMS replication task every mon 17:00 UK"
-  schedule_expression          = "cron(0 17 ? * 2 *)"
+  name                         = "dms-start-tue-12pm-uk-dev"
+  description                  = "Start DMS replication task every tue 12:00 UK"
+  schedule_expression          = "cron(0 12 ? * 3 *)"
   schedule_expression_timezone = "Europe/London"
   state                        = "ENABLED"
 
   flexible_time_window { mode = "OFF" }
 
   target {
-    arn      = "arn:aws:scheduler:::aws-sdk:dms:StartReplicationTask"
+    arn      = "arn:aws:scheduler:::aws-sdk:databasemigration:startReplicationTask"
     role_arn = aws_iam_role.scheduler_dms_role.arn
     input = jsonencode({
       ReplicationTaskArn       = module.dev_dms_oasys.dms_cdc_task_arn,
