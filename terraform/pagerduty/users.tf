3fbf42d03d698189a6cf2e55ee8d8cae2fbab09b
dms1981 <david.sibley@digital.justice.gov.uk>
2025-09-10 13:26:24 +0100

    Remove david.sibley from pagerduty users

--- terraform/pagerduty/users.tf
+++ terraform/pagerduty/users.tf
@@ -11,11 +11,6 @@ locals {
       role  = "responder"
     },
     {
-      name  = "David Sibley"
-      email = "david.sibley@digital.justice.gov.uk"
-      role  = "responder"
-    },
-    {
       name  = "Gary Henderson"
       email = "gary.henderson@digital.justice.gov.uk"
       role  = "responder"
