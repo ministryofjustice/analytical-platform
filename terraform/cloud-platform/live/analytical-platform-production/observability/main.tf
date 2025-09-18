92e12da5b44c48f7cea2fbeeecb14d799c324dc0
BrianEllwood <brian.ellwood@digital.justice.gov.uk>
2025-09-18 16:28:14 +0100

    fix linting issues

--- terraform/cloud-platform/live/analytical-platform-production/observability/main.tf
+++ terraform/cloud-platform/live/analytical-platform-production/observability/main.tf
@@ -1,4 +1,6 @@
-resource "aws_secretsmanager_secret" "ap-github-token" {
+resource "aws_secretsmanager_secret" "ap_github_token" {
+  #checkov:skip=CKV2_AWS_57:Automatic rotation is not required for this secret
+  #checkov:skip=CKV_AWS_149:CMK encryption is not required for this secret
   name        = "/analytical-platform/ap-github-token"
   description = "Fine graind PAT for use in AP"
 }
