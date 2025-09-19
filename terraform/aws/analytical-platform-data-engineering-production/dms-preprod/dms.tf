80a8414d3761b7cb9963fd461c99713f0402dcac
lalithanagarur <123457715+lalithanagarur@users.noreply.github.com>
2025-09-19 13:44:42 +0100

    revert in preprod

--- terraform/aws/analytical-platform-data-engineering-production/dms-preprod/dms.tf
+++ terraform/aws/analytical-platform-data-engineering-production/dms-preprod/dms.tf
@@ -70,7 +70,7 @@ module "preprod_dms_delius" {
     engine_version             = "3.5.4"
     kms_key_arn                = module.dms_preprod_kms.key_arn
     multi_az                   = false
-    replication_instance_class = "dms.r6i.4xlarge"
+    replication_instance_class = "dms.t3.medium"
     inbound_cidr               = "192.0.2.0/32" # test unassigned
     apply_immediately          = true
   }
