8e1046d8abf5d0bbced314f9c3a144afb157f12d
p-sin <philip.sinfield@justice.gov.uk>
2025-09-22 17:59:28 +0100

    Add kms

---
+++ terraform/aws/analytical-platform-data-engineering-sandbox-a/opg-fabric-connector/kms.tf
@@ -0,0 +1,34 @@
+module "opg_kms_dev" {
+
+  # Commit hash for v3.1.0
+  source = "git::https://github.com/terraform-aws-modules/terraform-aws-kms?ref=fe1beca2118c0cb528526e022a53381535bb93cd"
+
+  aliases               = ["opg/opg-fabric-sandbox"]
+  description           = "Used in the OPG fabric domain to encode secrets"
+  enable_default_policy = true
+
+  key_statements = [
+    {
+      sid    = "AllowLambdaServiceAccess"
+      effect = "Allow"
+      actions = [
+        "kms:Encrypt*",
+        "kms:Decrypt*",
+        "kms:GenerateDataKey*",
+        "kms:Describe*"
+      ]
+      resources = ["*"]
+
+      principals = [
+        {
+          type        = "Service"
+          identifiers = ["lambda.amazonaws.com"]
+        }
+      ]
+    }
+  ]
+
+  deletion_window_in_days = 7
+
+  tags = var.tags
+}
--- terraform/aws/analytical-platform-data-engineering-sandbox-a/opg-fabric-connector/secrets.tf
+++ terraform/aws/analytical-platform-data-engineering-sandbox-a/opg-fabric-connector/secrets.tf
@@ -1,6 +1,8 @@
 resource "aws_kms_key" "secrets" {
   description         = "CMK for Secrets Manager (OPG)"
   enable_key_rotation = true
+  policy              = data.aws_iam_policy_document.kms_for_secrets.json
+  tags                = { Purpose = "SecretsManager" }
 }
 
 resource "aws_kms_alias" "secrets_alias" {
@@ -11,22 +13,22 @@ resource "aws_kms_alias" "secrets_alias" {
 
 module "tenant_id_secret" {
   source  = "terraform-aws-modules/secrets-manager/aws"
-  version = "~> 1.2"
+  version = "1.3.1"
 
-  name        = "opg-fabric-connector/tenant-id"
-  description = "Tenant ID for OPG Fabric application"
-  kms_key_id  = aws_kms_key.secrets.arn
+  name                  = "opg-fabric-connector/tenant-id"
+  description           = "Tenant ID for OPG Fabric application"
+  kms_key_id            = aws_kms_key.secrets.arn
   ignore_secret_changes = true
-  secret_string = var.default_tenant_value
+  secret_string         = var.default_tenant_value
 }
 
 module "object_id_secret" {
   source  = "terraform-aws-modules/secrets-manager/aws"
-  version = "~> 1.2"
+  version = "1.3.1"
 
-  name        = "opg-fabric-connector/object-id"
-  description = "Object ID for OPG Fabric application"
-  kms_key_id  = aws_kms_key.secrets.arn
+  name                  = "opg-fabric-connector/object-id"
+  description           = "Object ID for OPG Fabric application"
+  kms_key_id            = aws_kms_key.secrets.arn
   ignore_secret_changes = true
-  secret_string = var.default_object_value
+  secret_string         = var.default_object_value
 }
