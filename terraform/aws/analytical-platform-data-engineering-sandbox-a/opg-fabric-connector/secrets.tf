b9c97d242327b42d4f9569b4a98fdc03f70689b0
p-sin <philip.sinfield@justice.gov.uk>
2025-09-22 17:35:00 +0100

    Add CMK encryption to secret

--- terraform/aws/analytical-platform-data-engineering-sandbox-a/opg-fabric-connector/secrets.tf
+++ terraform/aws/analytical-platform-data-engineering-sandbox-a/opg-fabric-connector/secrets.tf
@@ -1,25 +1,32 @@
-resource "aws_secretsmanager_secret" "fabric_connector_tenant" {
+resource "aws_kms_key" "secrets" {
+  description         = "CMK for Secrets Manager (OPG)"
+  enable_key_rotation = true
+}
+
+resource "aws_kms_alias" "secrets_alias" {
+  name          = "alias/opg/secrets"
+  target_key_id = aws_kms_key.secrets.key_id
+}
+
+
+module "tenant_id_secret" {
+  source  = "terraform-aws-modules/secrets-manager/aws"
+  version = "~> 1.2"
+
   name        = "opg-fabric-connector/tenant-id"
-  description = "Tenant ID for OPG fabric application"
+  description = "Tenant ID for OPG Fabric application"
+  kms_key_id  = aws_kms_key.secrets.arn
+  ignore_secret_changes = true
+ secret_string = var.default_tenant_value
 }
 
-resource "aws_secretsmanager_secret_version" "fabric_connector_tenant_value" {
-  secret_id     = aws_secretsmanager_secret.fabric_connector_tenant.id
-  secret_string = var.default_tenant_value
-  lifecycle {
-    ignore_changes = [secret_string, secret_binary]
-  }
-}
+module "object_id_secret" {
+  source  = "terraform-aws-modules/secrets-manager/aws"
+  version = "~> 1.2"
 
-resource "aws_secretsmanager_secret" "fabric_connector_object" {
   name        = "opg-fabric-connector/object-id"
-  description = "Object ID for OPG fabric application"
-}
-
-resource "aws_secretsmanager_secret_version" "fabric_connector_object_value" {
-  secret_id     = aws_secretsmanager_secret.fabric_connector_object.id
+  description = "Object ID for OPG Fabric application"
+  kms_key_id  = aws_kms_key.secrets.arn
+  ignore_secret_changes = true
   secret_string = var.default_object_value
-  lifecycle {
-    ignore_changes = [secret_string, secret_binary]
-  }
 }
