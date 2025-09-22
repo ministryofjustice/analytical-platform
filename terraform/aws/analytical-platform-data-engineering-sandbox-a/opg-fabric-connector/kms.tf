d45b618183d6c1a1a68733b920d26170a1d21ca1
p-sin <philip.sinfield@justice.gov.uk>
2025-09-22 18:11:29 +0100

    Tidy up kms use

--- terraform/aws/analytical-platform-data-engineering-sandbox-a/opg-fabric-connector/kms.tf
+++ terraform/aws/analytical-platform-data-engineering-sandbox-a/opg-fabric-connector/kms.tf
@@ -3,30 +3,22 @@ module "opg_kms_dev" {
   # Commit hash for v3.1.0
   source = "git::https://github.com/terraform-aws-modules/terraform-aws-kms?ref=fe1beca2118c0cb528526e022a53381535bb93cd"
 
-  aliases               = ["opg/opg-fabric-sandbox"]
-  description           = "Used in the OPG fabric domain to encode secrets"
-  enable_default_policy = true
+  aliases                 = ["opg/opg-fabric-sandbox"]
+  description             = "Used in the OPG fabric domain to encode secrets"
+  enable_default_policy   = true
+  deletion_window_in_days = 7
 
   key_statements = [
     {
-      sid    = "AllowLambdaServiceAccess"
-      effect = "Allow"
-      actions = [
-        "kms:Encrypt*",
-        "kms:Decrypt*",
-        "kms:GenerateDataKey*",
-        "kms:Describe*"
-      ]
-      resources = ["*"]
-
-      principals = [
-        {
-          type        = "Service"
-          identifiers = ["lambda.amazonaws.com"]
-        }
+      sid        = "AllowSecretsManagerUseOfKey"
+      effect     = "Allow"
+      actions    = ["kms:Encrypt", "kms:Decrypt", "kms:ReEncrypt*", "kms:GenerateDataKey*", "kms:DescribeKey"]
+      resources  = ["*"]
+      principals = [{ type = "Service", identifiers = ["secretsmanager.amazonaws.com"] }]
+      conditions = [
+        { test = "StringEquals", variable = "kms:ViaService", values = ["secretsmanager.${data.aws_region.current.name}.amazonaws.com"] },
+        { test = "StringEquals", variable = "kms:CallerAccount", values = [data.aws_caller_identity.current.account_id] }
       ]
     }
   ]
-
-  deletion_window_in_days = 7
 }
--- terraform/aws/analytical-platform-data-engineering-sandbox-a/opg-fabric-connector/secrets.tf
+++ terraform/aws/analytical-platform-data-engineering-sandbox-a/opg-fabric-connector/secrets.tf
@@ -1,23 +1,10 @@
-resource "aws_kms_key" "secrets" {
-  description         = "CMK for Secrets Manager (OPG)"
-  enable_key_rotation = true
-  policy              = data.aws_iam_policy_document.kms_for_secrets.json
-  tags                = { Purpose = "SecretsManager" }
-}
-
-resource "aws_kms_alias" "secrets_alias" {
-  name          = "alias/opg/secrets"
-  target_key_id = aws_kms_key.secrets.key_id
-}
-
-
 module "tenant_id_secret" {
   source  = "terraform-aws-modules/secrets-manager/aws"
   version = "1.3.1"
 
   name                  = "opg-fabric-connector/tenant-id"
   description           = "Tenant ID for OPG Fabric application"
-  kms_key_id            = aws_kms_key.secrets.arn
+  kms_key_id            = module.opg_kms_dev.key_arn
   ignore_secret_changes = true
   secret_string         = var.default_tenant_value
 }
@@ -28,7 +15,7 @@ module "object_id_secret" {
 
   name                  = "opg-fabric-connector/object-id"
   description           = "Object ID for OPG Fabric application"
-  kms_key_id            = aws_kms_key.secrets.arn
+  kms_key_id            = module.opg_kms_dev.key_arn
   ignore_secret_changes = true
   secret_string         = var.default_object_value
 }
