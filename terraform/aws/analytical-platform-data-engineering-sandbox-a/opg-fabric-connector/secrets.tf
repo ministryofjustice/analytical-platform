d77d22041d6796242c15e13fb09467dad82d5192
p-sin <philip.sinfield@justice.gov.uk>
2025-09-23 09:38:15 +0100

    Remove module version

--- terraform/aws/analytical-platform-data-engineering-sandbox-a/opg-fabric-connector/secrets.tf
+++ terraform/aws/analytical-platform-data-engineering-sandbox-a/opg-fabric-connector/secrets.tf
@@ -1,6 +1,5 @@
 module "tenant_id_secret" {
   source  = "git::https://github.com/terraform-aws-modules/terraform-aws-secrets-manager?ref=85977d132b8491281266ca412cee3e9ce7f2b457"
-  version = "1.3.1"
 
   name                  = "opg-fabric-connector/tenant-id"
   description           = "Tenant ID for OPG Fabric application"
