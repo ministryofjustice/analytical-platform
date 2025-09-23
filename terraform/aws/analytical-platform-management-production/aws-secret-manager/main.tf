b0a628c781d6a36c1c0947234dcbcbc46a2d1d01
BrianEllwood <brian.ellwood@digital.justice.gov.uk>
2025-09-23 16:02:20 +0100

    amend secret name

--- terraform/aws/analytical-platform-management-production/aws-secret-manager/main.tf
+++ terraform/aws/analytical-platform-management-production/aws-secret-manager/main.tf
@@ -2,7 +2,7 @@
 resource "aws_secretsmanager_secret" "ap_github_token" {
   #checkov:skip=CKV2_AWS_57:Automatic rotation is not required for this secret
   #checkov:skip=CKV_AWS_149:CMK encryption is not required for this secret
-  name        = "/analytical-platform/apmp/ap-github-token"
+  name        = "/aws-secrets-manager"
   description = "Fine graind PAT for use in AP"
   kms_key_id  = "alias/aws/secretsmanager"
 }
