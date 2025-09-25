be058a4976cb317271ea966e39a3805418304c4b
Antony Scott <antony.scott@justice.gov.uk>
2025-09-25 13:46:18 +0100

    fixing linting and making backend

--- terraform/aws/analytical-platform-data-engineering-sandbox-a/antony-vpc-sandbox/terraform.tf
+++ terraform/aws/analytical-platform-data-engineering-sandbox-a/antony-vpc-sandbox/terraform.tf
@@ -1,4 +1,14 @@
 terraform{
+
+    backend "s3" {
+        acl = "private"
+        bucket = "moj-analytical-platform-terraform-state-dwwaswef2r"
+        dynamodb_table = "moj-analytical-platform-terraform-locks"
+        encrypt = true
+        key = "aws/analytical-platform-data-engineering-sandbox-a/antony-vpc-sandbox/terraform.tfstate"
+        region = "eu-west-2"
+    }
+
     required_version = ">= 1.3.0"
     required_providers {
         aws = {
