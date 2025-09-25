56fd1af1b27239c6294a8a31be0644249f033c2e
Antony Scott <antony.scott@justice.gov.uk>
2025-09-25 14:54:59 +0100

    fixing workflow error

--- terraform/aws/analytical-platform-data-engineering-sandbox-a/antony-vpc-sandbox/secrets.tf
+++ terraform/aws/analytical-platform-data-engineering-sandbox-a/antony-vpc-sandbox/secrets.tf
@@ -1,9 +1,9 @@
 resource "random_password" "vpc_master_password" {
-  length   = 24
-  special  = true
-  upper    = true
-  lower    = true
-  numeric  = true
+  length  = 24
+  special = true
+  upper   = true
+  lower   = true
+  numeric = true
 }
 
 resource "aws_secretsmanager_secret" "vpc_master_user" {
@@ -11,6 +11,14 @@ resource "aws_secretsmanager_secret" "vpc_master_user" {
   description = "RDS export master user for antony-vpc-sandbox"
   tags        = var.tags
   kms_key_id  = var.kms_key_arn
+}
+
+resource "aws_secretsmanager_secret_rotation" "secret_rotator" {
+  secret_id = aws_secretsmanager_secret.vpc_master_user.id
+  rotation_lambda_arn = "arn:aws:lambda:eu-west-2:123456789012:function:secretsmanager-rotation-lambda-PostgreSQLSingleUser"
+  rotation_rules {
+    automatically_after_days = 30
+  }
 }
 
 resource "aws_secretsmanager_secret_version" "vpc_master_user" {
