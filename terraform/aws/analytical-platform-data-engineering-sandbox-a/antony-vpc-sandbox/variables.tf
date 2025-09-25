c566dc90e5669e0dea932ecb5da718614496b017
Antony Scott <antony.scott@justice.gov.uk>
2025-09-25 13:59:06 +0100

    fixing linting

--- terraform/aws/analytical-platform-data-engineering-sandbox-a/antony-vpc-sandbox/variables.tf
+++ terraform/aws/analytical-platform-data-engineering-sandbox-a/antony-vpc-sandbox/variables.tf
@@ -2,10 +2,6 @@ variable "tags" {
   description = "A map of tags to assign to resources"
   type        = map(string)
 }
-variable "vpc_default_sg_id" {
-  description = "The default security group ID of the VPC"
-  type        = string
-}
 variable "database_subnet_ids" {
   description = "List of subnet IDs for the RDS instance"
   type        = list(string)
