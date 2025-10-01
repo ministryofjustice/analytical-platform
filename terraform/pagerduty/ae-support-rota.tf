fdc78fe117c6ee6935aeab1aff122a41df78d827
owen.buckley@justice.gov.uk <owen.buckley@justice.gov.uk>
2025-10-01 14:09:16 +0000

    remove user

--- terraform/pagerduty/ae-support-rota.tf
+++ terraform/pagerduty/ae-support-rota.tf
@@ -63,7 +63,6 @@ locals {
           users = [
             module.users_ae_g7["alex.pavlopoulos@justice.gov.uk"].id,
             module.users_ae_g7["holly.furniss@justice.gov.uk"].id,
-            module.users_ae_g7["neil.wilkins@justice.gov.uk"].id,
             module.users_ae_g7["ivy.lau1@justice.gov.uk"].id,
             module.users_ae_g7["ian.rickard@justice.gov.uk"].id,
             module.users_ae_g7["ben.waterfield1@justice.gov.uk"].id,
@@ -169,11 +168,6 @@ locals {
       role  = "responder"
     },
     {
-      name  = "Neil Wilkins"
-      email = "neil.wilkins@justice.gov.uk"
-      role  = "responder"
-    },
-    {
       name  = "Ivy Lau"
       email = "ivy.lau1@justice.gov.uk"
       role  = "responder"
@@ -258,11 +252,6 @@ import {
 }
 
 import {
-  to = module.users_ae_g7["neil.wilkins@justice.gov.uk"].pagerduty_user.this
-  id = "PFUIUUH"
-}
-
-import {
   to = module.users_ae_g7["alex.pavlopoulos@justice.gov.uk"].pagerduty_user.this
   id = "PEMXSEN"
 }
@@ -290,9 +279,4 @@ import {
 import {
   to = module.users_ae_g7["quinta.davies@justice.gov.uk"].pagerduty_user.this
   id = "P5D5U7A"
-}
-
-moved {
-  from = module.users_ae_seo["neil.wilkins@justice.gov.uk"].pagerduty_user.this
-  to   = module.users_ae_g7["neil.wilkins@justice.gov.uk"].pagerduty_user.this
 }
