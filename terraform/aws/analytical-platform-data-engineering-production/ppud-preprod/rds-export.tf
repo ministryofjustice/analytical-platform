99614321287368b24879fb890a9a61745db9bed9
lalithanagarur <123457715+lalithanagarur@users.noreply.github.com>
2025-10-02 14:29:09 +0100

    removing rds export module resources (#9010)
    * removing rds export module resources
    
    * trivy update
    
    * trivy and checkov updates
    
    * updating for ppud-preprod
    
    * updating trivy comment

--- terraform/aws/analytical-platform-data-engineering-production/ppud-preprod/rds-export.tf
+++ terraform/aws/analytical-platform-data-engineering-production/ppud-preprod/rds-export.tf
@@ -1,6 +1,6 @@
 # Security group for the RDS instance
-# checkov:skip=CKV2_AWS_5: Attached to VPC
 resource "aws_security_group" "db" {
+  # checkov:skip=CKV2_AWS_5: Attached to VPC
   name        = "${local.name}-${local.env}"
   description = "Security group for RDS instance ${local.name}-${local.env}"
   vpc_id      = module.vpc.vpc_id
@@ -20,21 +20,21 @@ resource "aws_security_group_rule" "db_ingress" {
 }
 # checkov:skip=CKV_TF_1: Pointing to branch name whilst in development, will change to commit hash once in main
 # checkov:skip=CKV_TF_2: Pointing to branch name whilst in development, will change to commit hash once in main
-module "rds_export" {
-  source = "github.com/ministryofjustice/terraform-rds-export?ref=ppud-rds-check"
+# module "rds_export" {
+#   source = "github.com/ministryofjustice/terraform-rds-export?ref=ppud-rds-check"
 
-  providers = {
-    aws = aws
-  }
+#   providers = {
+#     aws = aws
+#   }
 
-  name                     = local.name
-  database_refresh_mode    = "incremental"
-  vpc_id                   = module.vpc.vpc_id
-  database_subnet_ids      = module.vpc.private_subnets
-  kms_key_arn              = module.rds_export_kms.key_arn
-  master_user_secret_id    = module.rds_export_secret.secret_arn
-  environment              = var.tags["environment"]
-  output_parquet_file_size = 200
+#   name                     = local.name
+#   database_refresh_mode    = "incremental"
+#   vpc_id                   = module.vpc.vpc_id
+#   database_subnet_ids      = module.vpc.private_subnets
+#   kms_key_arn              = module.rds_export_kms.key_arn
+#   master_user_secret_id    = module.rds_export_secret.secret_arn
+#   environment              = var.tags["environment"]
+#   output_parquet_file_size = 200
 
-  tags = var.tags
-}
+#   tags = var.tags
+# }
