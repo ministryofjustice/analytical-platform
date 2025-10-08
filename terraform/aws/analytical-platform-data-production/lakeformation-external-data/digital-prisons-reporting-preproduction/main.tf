a564365746ba222badbc59fccb0c66ff7bfdc392
Andrew Craik <72750362+andrewc-moj@users.noreply.github.com>
2025-10-08 11:11:07 +0100

    dmet-prisons - turn off old LF shares

--- terraform/aws/analytical-platform-data-production/lakeformation-external-data/digital-prisons-reporting-preproduction/main.tf
+++ terraform/aws/analytical-platform-data-production/lakeformation-external-data/digital-prisons-reporting-preproduction/main.tf
@@ -1,13 +1,13 @@
-# Module: lake_formation_analytical_platform_data_prod
-module "lake_formation_analytical_platform_data_prod" {
-  source = "github.com/ministryofjustice/terraform-aws-analytical-platform-lakeformation?ref=6fab8677e457c2e276fa1feec8ee83bbccc1220a"
+# # Module: lake_formation_analytical_platform_data_prod
+# module "lake_formation_analytical_platform_data_prod" {
+#   source = "github.com/ministryofjustice/terraform-aws-analytical-platform-lakeformation?ref=6fab8677e457c2e276fa1feec8ee83bbccc1220a"
 
 
-  providers = {
-    aws.source      = aws.digital_prisons_reporting_preprod_eu_west_2
-    aws.destination = aws
-  }
+#   providers = {
+#     aws.source      = aws.digital_prisons_reporting_preprod_eu_west_2
+#     aws.destination = aws
+#   }
 
-  data_locations     = local.data_locations
-  databases_to_share = local.databases
-}
+#   data_locations     = local.data_locations
+#   databases_to_share = local.databases
+# }
