68ce9bea57d37d7e1aab3fedbf2ee847f1c98dc0
BrianEllwood <brian.ellwood@digital.justice.gov.uk>
2025-10-08 14:16:00 +0100

    amend version and replace references to ap

--- terraform/aws/analytical-platform-management-production/aws-secrets-manager/main.tf
+++ terraform/aws/analytical-platform-management-production/aws-secrets-manager/main.tf
@@ -1,8 +1,8 @@
 #tfsec:ignore:AVD-AWS-0098 CMK encryption is not required for this secret
-resource "aws_secretsmanager_secret" "ap_github_token" {
+resource "aws_secretsmanager_secret" "analytical_platform_github_token" {
   #checkov:skip=CKV2_AWS_57:Automatic rotation is not required for this secret
   #checkov:skip=CKV_AWS_149:CMK encryption is not required for this secret
-  name        = "ap-github-token"
+  name        = "analytical-platform-github-token"
   description = "Fine grained PAT for use in AP"
   kms_key_id  = "alias/aws/secretsmanager"
 }
