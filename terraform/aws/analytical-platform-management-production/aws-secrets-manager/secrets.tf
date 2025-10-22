440ac50c972a45a21e6dc705be19f8f7f83ab150
BrianEllwood <brian.ellwood@digital.justice.gov.uk>
2025-10-22 10:33:31 +0100

    format fix

--- terraform/aws/analytical-platform-management-production/aws-secrets-manager/secrets.tf
+++ terraform/aws/analytical-platform-management-production/aws-secrets-manager/secrets.tf
@@ -1,6 +1,6 @@
 #tfsec:ignore:AVD-AWS-0098 CMK encryption is not required for this secret
 resource "aws_secretsmanager_secret" "analytical_platform_github_token" {
-  provider    = aws.analytical-platform-management-production-eu-west-1
+  provider = aws.analytical-platform-management-production-eu-west-1
   #checkov:skip=CKV2_AWS_57:Automatic rotation is not required for this secret
   #checkov:skip=CKV_AWS_149:CMK encryption is not required for this secret
   name        = "analytical-platform-github-token"
