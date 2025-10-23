60804b0f57f42870e17f4fe08f83a92f4bd3bff9
lalithanagarur <lalitha.nagarur3@justice.gov.uk>
2025-10-23 14:23:30 +0100

    linting update

--- terraform/aws/analytical-platform-data-engineering-production/dms-dev/vpc.tf
+++ terraform/aws/analytical-platform-data-engineering-production/dms-dev/vpc.tf
@@ -177,32 +177,6 @@ module "endpoints" {
   }
   endpoints = {
     # interface endpoints need subnet_ids and sg_id
-    
-    # Interface endpoint for ec2 bastion
-    # ec2messages = {
-    #   service             = "ec2messages"
-    #   service_type        = "Interface"
-    #   subnet_ids          = module.vpc.private_subnets
-    #   private_dns_enabled = true
-    #   tags                = { Name = "ec2messages-eu-west-1c-dev" }
-    # }
-
-    # ssmmessages = {
-    #   service             = "ssmmessages"
-    #   service_type        = "Interface"
-    #   subnet_ids          = module.vpc.private_subnets
-    #   private_dns_enabled = true
-    #   tags                = { Name = "ssmmessages-eu-west-1c-dev" }
-    # }
-
-    # ssm = {
-    #   service             = "ssm"
-    #   service_type        = "Interface"
-    #   subnet_ids          = module.vpc.private_subnets
-    #   private_dns_enabled = true
-    #   tags                = { Name = "ssm-eu-west-1c-dev" }
-    # }
-
 
     s3 = {
 
@@ -233,6 +207,31 @@ module "endpoints" {
       private_dns_enabled = true
       tags                = { Name = "sts-eu-west-1-dev" }
     }
+
+    # Interface endpoint for ec2 bastion
+    # ec2messages = {
+    #   service             = "ec2messages"
+    #   service_type        = "Interface"
+    #   subnet_ids          = module.vpc.private_subnets
+    #   private_dns_enabled = true
+    #   tags                = { Name = "ec2messages-eu-west-1c-dev" }
+    # }
+
+    # ssmmessages = {
+    #   service             = "ssmmessages"
+    #   service_type        = "Interface"
+    #   subnet_ids          = module.vpc.private_subnets
+    #   private_dns_enabled = true
+    #   tags                = { Name = "ssmmessages-eu-west-1c-dev" }
+    # }
+
+    # ssm = {
+    #   service             = "ssm"
+    #   service_type        = "Interface"
+    #   subnet_ids          = module.vpc.private_subnets
+    #   private_dns_enabled = true
+    #   tags                = { Name = "ssm-eu-west-1c-dev" }
+    # }
 
   }
 
