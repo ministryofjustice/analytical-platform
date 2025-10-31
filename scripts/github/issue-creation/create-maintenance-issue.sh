2a82c3a91bbdabea5f12117c58a88b344eaa860f
tamsinforbes <32667518+tamsinforbes@users.noreply.github.com>
2025-10-31 16:55:23 +0000

    Separate into script dnr

---
+++ scripts/github/issue-creation/create-maintenance-issue.sh
@@ -0,0 +1,19 @@
+if [[ "${CLOSE_PREVIOUS}" == true ]]; then
+  previous_issue_number=$(gh issue list \
+    --label "$LABELS" \
+    --json number \
+    --jq '.[0].number')
+  if [[ -n $previous_issue_number ]]; then
+    gh issue close "$previous_issue_number"
+    gh issue unpin "$previous_issue_number"
+  fi
+fi
+new_issue_url=$(gh issue create \
+  --title "$TITLE" \
+  --assignee "$ASSIGNEES" \
+  --label "$LABELS" \
+  --body "$BODY")
+if [[ $PINNED == true ]]; then
+  gh issue pin "$new_issue_url"
+fi
+gh project item-add 27 --owner ministryofjustice --url "$new_issue_url"
