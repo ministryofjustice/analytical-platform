d847c58002333bc2b1b4911f277f1101fef8344a
tamsinforbes <32667518+tamsinforbes@users.noreply.github.com>
2025-11-05 16:27:54 +0000

    Add success message and re-enabled sleep 30

--- scripts/github/issue-creation/create-maintenance-issue.sh
+++ scripts/github/issue-creation/create-maintenance-issue.sh
@@ -17,7 +17,7 @@ if [[ $PINNED == true ]]; then
   gh issue pin "$new_issue_url"
 fi
 echo "Created new issue: ${new_issue_url}"
-# sleep 30
+sleep 30
 project_view=$(gh project view 27 --owner ministryofjustice --format=json)
 count_of_project_items=$(echo "$project_view" | jq '.items[]')
 project_id=$(echo "$project_view" | jq -r '.id')
@@ -29,14 +29,14 @@ project_item_id=$(gh project item-list 27 \
   | jq -r --arg url "$new_issue_url" '.items[] | select(.content.url==$url) | .id')
 
 if [[ -z "$project_item_id" ]]; then
-  echo "Error: Could not find Project V2 Item ID for issue: ${new_issue_url}"
+  echo "❌Error: Could not find Project V2 Item ID for issue: ${new_issue_url}"
   exit 1
 fi
 
 echo "Getting field list for Project Analytical Platform (Project 27)"
 field_list=$(gh project field-list 27 --owner "ministryofjustice" --format=json)
 if [[ -z "$field_list" ]]; then
-  echo "Error: Could not find field list for project: Analytical Platform (Project 27)"
+  echo "❌ Error: Could not find field list for project: Analytical Platform (Project 27)"
   exit 1
 fi
 kanban_status_field_id=$(echo "$field_list" | jq -r '.fields[] | select(.name=="Kanban Status") | .id')
@@ -52,3 +52,5 @@ gh project item-edit --project-id "$project_id" --id "$project_item_id" --field-
 gh project item-edit --project-id "$project_id" --id "$project_item_id" --field-id "$refined_field_id"  --single-select-option-id "$refined_yes_id"
 gh project item-edit --project-id "$project_id" --id "$project_item_id" --field-id "$estimation_field_id"  --single-select-option-id "$estimation_2_id"
 gh project item-edit --project-id "$project_id" --id "$project_item_id" --field-id "$priority_field_id"  --single-select-option-id "$priority_medium_id"
+
+echo "🎉 Updated Analytical Platform Project Item fields: Kanban Status, Refined, Estimation, Priority"
