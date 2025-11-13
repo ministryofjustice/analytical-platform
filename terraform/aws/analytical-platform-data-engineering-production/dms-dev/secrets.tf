8aa9cbd1f3e762c617226d29a45a4efcf1aa7417
Antony Scott <antony.scott@justice.gov.uk>
2025-11-13 10:55:03 +0000

    corrections made

--- terraform/aws/analytical-platform-data-engineering-production/dms-dev/secrets.tf
+++ terraform/aws/analytical-platform-data-engineering-production/dms-dev/secrets.tf
@@ -1,12 +1,12 @@
 resource "aws_secretsmanager_secret" "slack_webhook" {
   # checkov:skip=CKV2_AWS_57: Skipping because automatic rotation not needed.
-  name       = "delius-dev-slack-webhook"
+  name       = "oasys-dev-slack-webhook"
   kms_key_id = module.dms_dev_kms.key_arn
 }
 
-resource "aws_secretsmanager_secret" "oasys_preprod_secret" {
+resource "aws_secretsmanager_secret" "oasys_dev_secret" {
   # checkov:skip=CKV2_AWS_57: Skipping because automatic rotation not needed.
-  name       = "oasys-preprod-secret"
+  name       = "oasys-dev-secret"
   kms_key_id = module.dms_dev_kms.key_arn
 }
 
