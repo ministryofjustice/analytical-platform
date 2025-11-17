1555300856962e179de7402559b01da88d89a9a1
Philip <112626966+p-sin@users.noreply.github.com>
2025-11-17 13:03:29 +0000

    OPG: Set fabric kms deny provider to 'AWS' (#9323)

--- terraform/aws/analytical-platform-data-engineering-sandbox-a/opg-fabric-connector/kms.tf
+++ terraform/aws/analytical-platform-data-engineering-sandbox-a/opg-fabric-connector/kms.tf
@@ -12,7 +12,7 @@ module "opg_kms_dev" {
     {
       sid        = "DenyNonSecretsManagerUse"
       effect     = "Deny"
-      principals = [{ type = "*", identifiers = ["*"] }]
+      principals = [{ type = "AWS", identifiers = ["*"] }]
       actions    = ["kms:Encrypt", "kms:Decrypt", "kms:ReEncrypt*", "kms:GenerateDataKey*", "kms:DescribeKey"]
       resources  = ["*"]
       conditions = [
