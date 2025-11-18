499feb9afeea87305ffb54b14b28d698d2869167
lalithanagarur <lalitha.nagarur3@justice.gov.uk>
2025-11-18 16:53:58 +0000

    trying a different policy

--- terraform/aws/analytical-platform-data-engineering-production/ppud-dev/main.tf
+++ terraform/aws/analytical-platform-data-engineering-production/ppud-dev/main.tf
@@ -1,3 +1,32 @@
+data "aws_iam_policy_document" "ppud_dev"{
+  statement {
+    sid    = "Set-permissions-for-objects"
+    effect = "Allow"
+    principals {
+      type = "AWS"
+      identifiers = "arn:aws:iam::${local.account_ids["ppud-development"]}:role/service-role/iam_role_s3_bucket_moj_database_source_dev_test"
+    }
+    actions = [
+      "s3:ReplicateObject",
+      "s3:ReplicateDelete"
+    ]
+    resources = "arn:aws:s3:::mojap-data-engineering-production-ppud-dev/*"
+  }
+  statement {
+    sid    = "Set-permissions-on-bucket"
+    effect = "Allow"
+    principals {
+      type = "AWS"
+      identifiers = "arn:aws:iam::${local.account_ids["ppud-development"]}:role/service-role/iam_role_s3_bucket_moj_database_source_dev_test"
+    }
+    actions = [
+      "s3:GetBucketVersioning",
+      "s3:PutBucketVersioning"
+    ]
+    resources = "arn:aws:s3:::mojap-data-engineering-production-ppud-dev"
+  }
+}
+
 # trivy:ignore:avd-aws-0132: Skipping because replicating existing bucket that does not encrypt data with a customer managed key
 # trivy:ignore:avd-aws-0088: Skipping because has server side encryption
 # trivy:ignore:avd-aws-0089: Skipping because access logging is managed externally.
@@ -35,41 +64,7 @@ module "ppud_dev" {
     restrict_public_buckets = true
   }
   attach_policy = true
-  policy = jsonencode(
-    {
-      Statement = [
-        {
-          Sid    = "Set-permissions-for-objects"
-          Effect = "Allow"
-          Principal = {
-            AWS = [
-              "arn:aws:iam::${local.account_ids["ppud-development"]}:role/service-role/iam_role_s3_bucket_moj_database_source_dev_test"
-            ]
-          }
-          Action = [
-            "s3:ReplicateObject",
-            "s3:ReplicateDelete"
-          ]
-          Resource = "arn:aws:s3:::mojap-data-engineering-production-ppud-dev/*"
-        },
-        {
-          Sid    = "Set-permissions-on-bucket"
-          Effect = "Allow"
-          Principal = {
-            AWS = [
-              "arn:aws:iam::${local.account_ids["ppud-development"]}:role/service-role/iam_role_s3_bucket_moj_database_source_dev_test"
-            ]
-          }
-          Action = [
-            "s3:GetBucketVersioning",
-            "s3:PutBucketVersioning"
-          ]
-          Resource = "arn:aws:s3:::mojap-data-engineering-production-ppud-dev"
-        }
-      ]
-      Version = "2012-10-17"
-    }
-  )
+  policy = sensitive(data.aws_iam_policy_document.ppud_dev.json)
 
   tags = var.tags
 }
