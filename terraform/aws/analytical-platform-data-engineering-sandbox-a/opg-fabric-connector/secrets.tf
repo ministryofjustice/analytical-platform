6cbfdf02cc139746f18aa0905238a5392b69f31c
p-sin <philip.sinfield@justice.gov.uk>
2025-11-25 17:40:38 +0000

    Remove kms

--- terraform/aws/analytical-platform-data-engineering-sandbox-a/opg-fabric-connector/secrets.tf
+++ terraform/aws/analytical-platform-data-engineering-sandbox-a/opg-fabric-connector/secrets.tf
@@ -1,19 +1,20 @@
+#checkov:skip=CKV_TF_2:Module registry does not support tags for versions
 module "tenant_id_secret" {
-  source = "git::https://github.com/terraform-aws-modules/terraform-aws-secrets-manager?ref=85977d132b8491281266ca412cee3e9ce7f2b457"
+  source  = "terraform-aws-modules/secrets-manager/aws"
+  version = "2.0.1"
 
   name                  = "opg-fabric-connector/tenant-id"
   description           = "Tenant ID for OPG Fabric application"
-  kms_key_id            = module.opg_kms_dev.key_arn
   ignore_secret_changes = true
   secret_string         = var.default_tenant_value
 }
 
 module "object_id_secret" {
-  source = "git::https://github.com/terraform-aws-modules/terraform-aws-secrets-manager?ref=85977d132b8491281266ca412cee3e9ce7f2b457"
+  source  = "terraform-aws-modules/secrets-manager/aws"
+  version = "2.0.1"
 
   name                  = "opg-fabric-connector/object-id"
   description           = "Object ID for OPG Fabric application"
-  kms_key_id            = module.opg_kms_dev.key_arn
   ignore_secret_changes = true
   secret_string         = var.default_object_value
 }
