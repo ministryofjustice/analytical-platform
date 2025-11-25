4010d384242a4f7cf81620ff5a0d397a8e8bc395
lalithanagarur <123457715+lalithanagarur@users.noreply.github.com>
2025-11-25 15:29:36 +0000

    Update ppud prod (#9388)
    * added resources to get ppud prod ready
    
    * updated tag
    
    * added data.tf and terraform fmt applied
    
    * linting update

---
+++ terraform/aws/analytical-platform-data-engineering-production/ppud-prod/data.tf
@@ -0,0 +1,11 @@
+### Account Information
+
+data "aws_secretsmanager_secret" "account_ids" {
+  provider = aws.session
+  name     = "analytical-platform/platform-account-ids"
+}
+
+data "aws_secretsmanager_secret_version" "account_ids_version" {
+  provider  = aws.session
+  secret_id = data.aws_secretsmanager_secret.account_ids.id
+}
---
+++ terraform/aws/analytical-platform-data-engineering-production/ppud-prod/locals.tf
@@ -0,0 +1,7 @@
+locals {
+  account_ids = jsondecode(data.aws_secretsmanager_secret_version.account_ids_version.secret_string)
+
+  name = "ppud"
+
+  env = var.tags["environment"]
+}
