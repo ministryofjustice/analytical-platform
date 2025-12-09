e161fd55bafb8043b9d5ca6f60e758b6f9bd89e8
Andrew <72750362+andrewc-moj@users.noreply.github.com>
2025-12-09 11:03:46 +0000

    dmet-prisons: add get lakeformation access

--- terraform/aws/analytical-platform-data-production/github-actions-roles/mojas-create-a-derived-table.tf
+++ terraform/aws/analytical-platform-data-production/github-actions-roles/mojas-create-a-derived-table.tf
@@ -93,6 +93,16 @@ data "aws_iam_policy_document" "create_a_derived_table" {
       "arn:aws:glue:*:${var.account_ids["analytical-platform-data-production"]}:catalog"
     ]
   }
+  
+  statement {
+    sid    = "LakeFormationGetDataAccess"
+    effect = "Allow"
+    actions = [
+      "lakeformation:GetDataAccess"
+    ]
+    resources = ["*"]
+  }
+
   statement {
     sid    = "AirflowAccess"
     effect = "Allow"
