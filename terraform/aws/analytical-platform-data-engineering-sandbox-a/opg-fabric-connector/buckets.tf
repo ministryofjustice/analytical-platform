1db5e373651e4bce1ba645a5e6cb761edeea7bf7
p-sin <philip.sinfield@justice.gov.uk>
2025-12-09 09:11:06 +0000

    Remove policy block from bucket

--- terraform/aws/analytical-platform-data-engineering-sandbox-a/opg-fabric-connector/buckets.tf
+++ terraform/aws/analytical-platform-data-engineering-sandbox-a/opg-fabric-connector/buckets.tf
@@ -22,41 +22,4 @@ module "opg_fabric_store" {
   ignore_public_acls      = true
   restrict_public_buckets = true
 
-
-  attach_policy = true
-  policy = jsonencode(
-    {
-      Statement = [
-        {
-          Sid    = "Set-permissions-for-objects"
-          Effect = "Allow"
-          Principal = {
-            AWS = [
-              "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/service-role/iam_role_s3_bucket_moj_database_source_dev"
-            ]
-          }
-          Action = [
-            "s3:ReplicateObject",
-            "s3:ReplicateDelete"
-          ]
-          Resource = "arn:aws:s3:::mojap-data-engineering-production-ppud-dev/*"
-        },
-        {
-          Sid    = "Set-permissions-on-bucket"
-          Effect = "Allow"
-          Principal = {
-            AWS = [
-              "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/service-role/iam_role_s3_bucket_moj_database_source_dev"
-            ]
-          }
-          Action = [
-            "s3:GetBucketVersioning",
-            "s3:PutBucketVersioning"
-          ]
-          Resource = "arn:aws:s3:::mojap-data-engineering-production-ppud-dev"
-        }
-      ]
-      Version = "2012-10-17"
-    }
-  )
 }
