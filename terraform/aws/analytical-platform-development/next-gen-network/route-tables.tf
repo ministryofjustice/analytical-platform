7568082b4003de902778f1a4fc52dc73213b8237
Jacob Woffenden <jacob.woffenden@justice.gov.uk>
2025-12-10 14:29:46 +0000

    feat: refactor route tables for next-gen network, adding public to firewall and private subnets routing
    Signed-off-by: Jacob Woffenden <jacob.woffenden@justice.gov.uk>

--- terraform/aws/analytical-platform-development/next-gen-network/route-tables.tf
+++ terraform/aws/analytical-platform-development/next-gen-network/route-tables.tf
@@ -14,16 +14,3 @@ resource "aws_route_table_association" "main" {
   subnet_id      = aws_subnet.main[each.key].id
   route_table_id = aws_route_table.main[each.key].id
 }
-
-resource "aws_route_table" "internet_gateway" {
-  vpc_id = aws_vpc.main.id
-
-  tags = {
-    Name = "${local.application_name}-${local.environment}-igw"
-  }
-}
-
-resource "aws_route_table_association" "internet_gateway" {
-  gateway_id     = aws_internet_gateway.main.id
-  route_table_id = aws_route_table.internet_gateway.id
-}
--- terraform/aws/analytical-platform-development/next-gen-network/routes.tf
+++ terraform/aws/analytical-platform-development/next-gen-network/routes.tf
@@ -24,11 +24,21 @@ resource "aws_route" "private_network_firewall" {
   depends_on = [aws_networkfirewall_firewall.main]
 }
 
-resource "aws_route" "internet_gateway_to_firewall" {
+resource "aws_route" "public_to_firewall_subnets" {
   for_each = local.environment_configuration.vpc_subnets.public
 
-  route_table_id         = aws_route_table.internet_gateway.id
-  destination_cidr_block = aws_subnet.main["public-${each.key}"].cidr_block
+  route_table_id         = aws_route_table.main["public-${each.key}"].id
+  destination_cidr_block = aws_subnet.main["firewall-${each.key}"].cidr_block
+  vpc_endpoint_id        = data.aws_vpc_endpoint.network_firewall[each.key].id
+
+  depends_on = [aws_networkfirewall_firewall.main]
+}
+
+resource "aws_route" "public_to_private_subnets" {
+  for_each = local.environment_configuration.vpc_subnets.public
+
+  route_table_id         = aws_route_table.main["public-${each.key}"].id
+  destination_cidr_block = aws_subnet.main["private-${each.key}"].cidr_block
   vpc_endpoint_id        = data.aws_vpc_endpoint.network_firewall[each.key].id
 
   depends_on = [aws_networkfirewall_firewall.main]
