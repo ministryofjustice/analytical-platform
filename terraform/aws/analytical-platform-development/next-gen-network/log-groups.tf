6924f12437e7f6e78fb66d58fa5534e43dd7fea9
Jacob Woffenden <jacob.woffenden@justice.gov.uk>
2025-12-10 17:26:36 +0000

    feat: implement network firewall rule groups and logging configuration for enhanced traffic management
    Signed-off-by: Jacob Woffenden <jacob.woffenden@justice.gov.uk>

--- terraform/aws/analytical-platform-development/next-gen-network/log-groups.tf
+++ terraform/aws/analytical-platform-development/next-gen-network/log-groups.tf
@@ -11,3 +11,10 @@ module "network_firewall_flow_logs_log_group" {
 
   name = "/aws/network-firewall/${local.application_name}-${local.environment}-flow-logs"
 }
+
+module "network_firewall_alert_logs_log_group" {
+  source  = "terraform-aws-modules/cloudwatch/aws//modules/log-group"
+  version = "5.7.2"
+
+  name = "/aws/network-firewall/${local.application_name}-${local.environment}-alert-logs"
+}
--- terraform/aws/analytical-platform-development/next-gen-network/network-firewall.tf
+++ terraform/aws/analytical-platform-development/next-gen-network/network-firewall.tf
@@ -1,15 +1,133 @@
-resource "aws_networkfirewall_firewall_policy" "default" {
-  name = "default"
+resource "aws_networkfirewall_rule_group" "allow_domains" {
+  name     = "allow-approved-domains"
+  type     = "STATEFUL"
+  capacity = 100
+
+  rule_group {
+    rules_source {
+      rules_source_list {
+        generated_rules_type = "ALLOWLIST"
+        target_types         = ["HTTP_HOST", "TLS_SNI"]
+        targets = [
+          # Government domains
+          ".gov.uk",
+          ".justice.gov.uk",
+
+          # Development tools
+          ".github.com",
+          ".githubusercontent.com",
+          ".gitlab.com",
+
+          # Package managers
+          ".pypi.org",
+          ".pythonhosted.org",
+          ".npmjs.org",
+          ".npmjs.com",
+          ".cran.r-project.org",
+
+          # AWS services
+          ".amazonaws.com",
+          ".aws.amazon.com",
+        ]
+      }
+    }
+  }
+}
+
+resource "aws_networkfirewall_rule_group" "allow_non_http_services" {
+  name     = "allow-non-http-services"
+  type     = "STATEFUL"
+  capacity = 100
+
+  rule_group {
+    rules_source {
+      # Example: Allow SSH to specific internal server
+      stateful_rule {
+        action = "PASS"
+        header {
+          destination      = "10.0.1.100/32"
+          destination_port = "22"
+          direction        = "FORWARD"
+          protocol         = "TCP"
+          source           = "ANY"
+          source_port      = "ANY"
+        }
+        rule_option {
+          keyword  = "sid"
+          settings = ["1"]
+        }
+      }
+
+      # Example: Allow PostgreSQL to RDS endpoint
+      stateful_rule {
+        action = "PASS"
+        header {
+          destination      = "10.0.2.0/24"
+          destination_port = "5432"
+          direction        = "FORWARD"
+          protocol         = "TCP"
+          source           = "ANY"
+          source_port      = "ANY"
+        }
+        rule_option {
+          keyword  = "sid"
+          settings = ["2"]
+        }
+      }
+
+      # Example: Allow NTP (non-HTTP protocol)
+      stateful_rule {
+        action = "PASS"
+        header {
+          destination      = "ANY"
+          destination_port = "123"
+          direction        = "FORWARD"
+          protocol         = "UDP"
+          source           = "ANY"
+          source_port      = "ANY"
+        }
+        rule_option {
+          keyword  = "sid"
+          settings = ["3"]
+        }
+      }
+    }
+
+    stateful_rule_options {
+      rule_order = "DEFAULT_ACTION_ORDER"
+    }
+  }
+}
+
+resource "aws_networkfirewall_firewall_policy" "main" {
+  name = "main"
 
   firewall_policy {
     stateless_default_actions          = ["aws:forward_to_sfe"]
     stateless_fragment_default_actions = ["aws:forward_to_sfe"]
+
+    # FQDN-based filtering for HTTP/HTTPS traffic
+    stateful_rule_group_reference {
+      resource_arn = aws_networkfirewall_rule_group.allow_domains.arn
+    }
+
+    # IP/Port-based filtering for non-HTTP protocols (SSH, databases, etc.)
+    stateful_rule_group_reference {
+      resource_arn = aws_networkfirewall_rule_group.allow_non_http_services.arn
+    }
+
+    # Start permissive - drop_strict can be added later once stable
+    # stateful_default_actions = ["aws:drop_strict"]
+
+    stateful_engine_options {
+      rule_order = "DEFAULT_ACTION_ORDER"
+    }
   }
 }
 
 resource "aws_networkfirewall_firewall" "main" {
   name                = "${local.application_name}-${local.environment}"
-  firewall_policy_arn = aws_networkfirewall_firewall_policy.default.arn
+  firewall_policy_arn = aws_networkfirewall_firewall_policy.main.arn
   vpc_id              = aws_vpc.main.id
 
   dynamic "subnet_mapping" {
@@ -34,6 +152,13 @@ resource "aws_networkfirewall_logging_configuration" "cloudwatch" {
       }
       log_destination_type = "CloudWatchLogs"
       log_type             = "FLOW"
+    }
+    log_destination_config {
+      log_destination = {
+        logGroup = module.network_firewall_alert_logs_log_group.cloudwatch_log_group_name
+      }
+      log_destination_type = "CloudWatchLogs"
+      log_type             = "ALERT"
     }
   }
 }
