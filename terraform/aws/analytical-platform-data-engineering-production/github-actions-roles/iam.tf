e64b0043614128856786f34d326db33149995cd7
lalithanagarur <lalitha.nagarur3@justice.gov.uk>
2025-12-17 15:59:47 +0000

    formatting update

--- terraform/aws/analytical-platform-data-engineering-production/github-actions-roles/iam.tf
+++ terraform/aws/analytical-platform-data-engineering-production/github-actions-roles/iam.tf
@@ -108,9 +108,7 @@ module "create_a_derived_table_dev_iam_role" {
         "arn:aws:iam::${var.account_ids["analytical-platform-data-engineering-production"]}:oidc-provider/%s",
         trimprefix(jsondecode(data.aws_secretsmanager_secret_version.analytical_platform_compute_cluster_data.secret_string)["analytical-platform-compute-production-oidc-endpoint"], "https://")
       )
-      namespace_service_accounts = [actions-runner-mojas-cadt-probation-dev
-        ""
-      ]
+      namespace_service_accounts = ["actions-runner-mojas-cadt-probation-dev"]
     }
   }
 }
