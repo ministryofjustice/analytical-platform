13f8df46b0cdb8c12053a1f4666bf1fc0bfce580
lalithanagarur <123457715+lalithanagarur@users.noreply.github.com>
2025-12-22 13:45:56 +0000

    added namespace to trust relationship (#9521)

--- terraform/aws/analytical-platform-data-engineering-production/github-actions-roles/iam.tf
+++ terraform/aws/analytical-platform-data-engineering-production/github-actions-roles/iam.tf
@@ -112,7 +112,7 @@ module "create_a_derived_table_dev_iam_role" {
         "arn:aws:iam::${var.account_ids["analytical-platform-data-engineering-production"]}:oidc-provider/%s",
         trimprefix(jsondecode(data.aws_secretsmanager_secret_version.analytical_platform_compute_cluster_data.secret_string)["analytical-platform-compute-production-oidc-endpoint"], "https://")
       )
-      namespace_service_accounts = ["actions-runner-mojas-cadt-probation-dev"]
+      namespace_service_accounts = ["actions-runners:actions-runner-mojas-cadt-probation-dev"]
     }
   }
 
@@ -248,7 +248,7 @@ module "create_a_derived_table_preprod_iam_role" {
         "arn:aws:iam::${var.account_ids["analytical-platform-data-engineering-production"]}:oidc-provider/%s",
         trimprefix(jsondecode(data.aws_secretsmanager_secret_version.analytical_platform_compute_cluster_data.secret_string)["analytical-platform-compute-production-oidc-endpoint"], "https://")
       )
-      namespace_service_accounts = ["actions-runner-mojas-cadt-probation-preprod"]
+      namespace_service_accounts = ["actions-runners:actions-runner-mojas-cadt-probation-preprod"]
     }
   }
 
