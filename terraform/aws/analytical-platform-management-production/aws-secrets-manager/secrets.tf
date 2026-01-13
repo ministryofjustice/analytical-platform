fbabafe7853c04a9278ab206b13cb5216d0e059f
BrianEllwood <45009603+BrianEllwood@users.noreply.github.com>
2026-01-13 15:12:18 +0000

    Import and Create AWS secret github-token  in code (#9551)
    import and create aws secret in code

--- terraform/aws/analytical-platform-management-production/aws-secrets-manager/secrets.tf
+++ terraform/aws/analytical-platform-management-production/aws-secrets-manager/secrets.tf
@@ -27,3 +27,23 @@ resource "aws_secretsmanager_secret" "release_failure_webhook_url" {
   description = "Slack webhook URL for release failure notifications"
   kms_key_id  = "alias/aws/secretsmanager"
 }
+
+
+import {
+  to = aws_secretsmanager_secret.github_token
+  identity = {
+    "arn" = "arn:aws:secretsmanager:eu-west-1:042130406152:secret:github-token-SAag9J"
+  }
+}
+#tfsec:ignore:AVD-AWS-0098 CMK encryption is not required for this secret
+resource "aws_secretsmanager_secret" "github_token" {
+  provider = aws.analytical-platform-management-production-eu-west-1
+  #checkov:skip=CKV2_AWS_57:Automatic rotation is not required for this secret
+  #checkov:skip=CKV_AWS_149:CMK encryption is not required for this secret
+  name        = "github-token"
+  description = "Token for use in Analytical Platform/MOJ Analytical Services"
+  kms_key_id  = "alias/aws/secretsmanager"
+  tags = {
+    credential-expiration = "2027-01-06"
+  }
+}
