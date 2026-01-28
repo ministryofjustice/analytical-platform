2cbcba98dd95401daabe35bc804d39e49f4bf632
Gary <26419401+Gary-H9@users.noreply.github.com>
2026-01-28 17:04:19 +0000

    feat: add ec2:DescribeAvailabilityZones to EBS CSI driver IAM policy (#9621)
    The EBS CSI Driver v1.54.0 now performs a health check using
    ec2:DescribeAvailabilityZones. Without this permission, the controller
    pods fail health checks and enter CrashLoopBackOff.
    
    This adds the missing permission to both development and production
    cluster IAM policies.

--- terraform/aws/analytical-platform-production/cluster/iam-policies.tf
+++ terraform/aws/analytical-platform-production/cluster/iam-policies.tf
@@ -16,6 +16,7 @@ data "aws_iam_policy_document" "ebs_csi_driver" {
       "ec2:DeleteSnapshot",
       "ec2:DeleteTags",
       "ec2:DeleteVolume",
+      "ec2:DescribeAvailabilityZones",
       "ec2:DescribeInstances",
       "ec2:DescribeSnapshots",
       "ec2:DescribeTags",
