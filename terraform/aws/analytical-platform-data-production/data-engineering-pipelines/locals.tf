a9638a23d638165a768d201c71be1553ebcaa126
Andy-Cook <129843482+Andy-Cook@users.noreply.github.com>
2026-02-03 15:46:13 +0000

    add current and expected AP-airflow DAG roles to Mags (magistrates) database permissions

--- terraform/aws/analytical-platform-data-production/data-engineering-pipelines/locals.tf
+++ terraform/aws/analytical-platform-data-production/data-engineering-pipelines/locals.tf
@@ -20,6 +20,8 @@ locals {
         "airflow_prod_mags_data_processor",
         "restricted-admin",
         "airflow_dev_mags_data_processor",
+        "airflow-production-hmcts-dev-mags-combined-dev",
+        "airflow-production-hmcts-mags-combined"
       ]
     },
     {
