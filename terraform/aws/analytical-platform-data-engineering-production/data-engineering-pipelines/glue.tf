209894ad92283f973e54207aa830fb4a815d50e4
lalithanagarur <lalitha.nagarur3@justice.gov.uk>
2026-02-17 16:10:38 +0000

    formatting updates

--- terraform/aws/analytical-platform-data-engineering-production/data-engineering-pipelines/glue.tf
+++ terraform/aws/analytical-platform-data-engineering-production/data-engineering-pipelines/glue.tf
@@ -3,6 +3,6 @@
 # for the connection so if it doesn't exist schema changes
 # error
 resource "aws_glue_catalog_database" "cadet_probation_schema" {
-  name = "probation"
+  name        = "probation"
   description = "Used in profiles.yaml for CaDeT hmpps_probation_data_tables project. Do not delete."
 }
