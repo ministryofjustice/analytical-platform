166c96a287625819886737285d6fe618747ce7e8
lalithanagarur <lalitha.nagarur3@justice.gov.uk>
2026-02-23 10:59:28 +0000

    updated service account name

--- terraform/aws/analytical-platform-data-engineering-production/github-actions-roles/iam.tf
+++ terraform/aws/analytical-platform-data-engineering-production/github-actions-roles/iam.tf
@@ -405,7 +405,7 @@ module "create_a_derived_table_prod_iam_role" {
         "arn:aws:iam::${var.account_ids["analytical-platform-data-engineering-production"]}:oidc-provider/%s",
         trimprefix(jsondecode(data.aws_secretsmanager_secret_version.analytical_platform_compute_cluster_data.secret_string)["analytical-platform-compute-production-oidc-endpoint"], "https://")
       )
-      namespace_service_accounts = ["actions-runners:actions-runner-mojas-cadt-probation-prod", "mwaa:probation-cadet-derived"]
+      namespace_service_accounts = ["actions-runners:actions-runner-mojas-cadt-probation-prod", "mwaa:probation-cadet"]
     }
   }
 
