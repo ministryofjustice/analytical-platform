03e0b6e6644e640669ee4063a70c066b527a31f5
Gary H <26419401+Gary-H9@users.noreply.github.com>
2026-02-24 16:52:18 +0000

    Refactor for username/password/dockerconfigjson

--- terraform/aws/analytical-platform-development/cluster/kubernetes-secrets.tf
+++ terraform/aws/analytical-platform-development/cluster/kubernetes-secrets.tf
@@ -1,12 +1,24 @@
+locals {
+  chainguard-credentials = jsondecode(data.aws_secretsmanager_secret_version.chainguard_image_pull_token.secret_string)
+}
+
 resource "kubernetes_secret_v1" "chainguard" {
   metadata {
     name      = "chainguard"
     namespace = "ingress-nginx"
   }
 
-  type = "Opaque"
+  type = "kubernetes.io/dockerconfigjson"
 
   data = {
-    "token" = data.aws_secretsmanager_secret_version.chainguard_image_pull_token.secret_string
+    ".dockerconfigjson" = jsonencode({
+      auths = {
+        "cgr.dev" = {
+          username = local.chainguard-credentials["username"]
+          password = local.chainguard-credentials["password"]
+          auth     = base64encode("${local.chainguard-credentials["username"]}:${local.chainguard-credentials["password"]}")
+        }
+      }
+    })
   }
 }
