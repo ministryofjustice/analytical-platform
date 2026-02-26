d1422dab0a851d05caa2f375d94c09723550ec4d
Antony Scott <antony.scott@justice.gov.uk>
2026-02-26 16:26:50 +0000

    dms module updated with minimal full load changes

--- terraform/aws/analytical-platform-data-engineering-production/dms-dev/dms.tf
+++ terraform/aws/analytical-platform-data-engineering-production/dms-dev/dms.tf
@@ -1,5 +1,5 @@
 module "dev_dms_oasys" {
-  source      = "github.com/ministryofjustice/terraform-dms-module?ref=b190c92217786c0454b756996cdb2fcb190256db"
+  source      = "github.com/ministryofjustice/terraform-dms-module?ref=857d1065fc98bb826900bd7171cc895759182f50"
   vpc_id      = module.vpc.vpc_id
   environment = var.tags.environment-name
 
@@ -39,6 +39,16 @@ module "dev_dms_oasys" {
     bucket = "mojap-data-engineering-production-table-mappings-metadata-dev"
     key    = "dev/oasys/table_mappings.json"
   }
+
+  # independent_full_loads = {
+  #   oasys_dev_offender_rsr_scores = {
+  #     full_load_name = "oasys-dev-offender-rsr-scores"
+  #     path = {
+  #       bucket = "mojap-data-engineering-production-table-mappings-metadata-dev"
+  #       key    = "dev/oasys/offender_rsr_scores_full_load.json"
+  #     }
+  #   }
+  # }
 
   tags = merge(
     { "managed-by" = "Terraform" },
