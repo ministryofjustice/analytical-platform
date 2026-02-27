89e386aa29a603aa8311f6a8efe48b7b1165379a
Andrew Craik <andrew.craik@justice.gov.uk>
2026-02-27 15:59:55 +0000

    add trigger to s3 key

--- terraform/aws/analytical-platform-data-engineering-sandbox-a/hmpps-prison-curious2-secret-updater/main.tf
+++ terraform/aws/analytical-platform-data-engineering-sandbox-a/hmpps-prison-curious2-secret-updater/main.tf
@@ -37,6 +37,9 @@ resource "aws_secretsmanager_secret_version" "initial" {
   secret_string = "initial-placeholder"
 }
 
+
+# Create a lambda which takes the content of the bucket/object key and
+# updates the secret with name secret_name
 module "prison_curious_secret_updater" {
   source = "../../analytical-platform/baseline/modules/lambda-secret-updater"
 
@@ -46,4 +49,28 @@ module "prison_curious_secret_updater" {
   object_key  = aws_s3_object.sas_token_file.key
 
   secret_name = aws_secretsmanager_secret.curious_sandbox_secret.name
+}
+
+# Allow s3 to invoke the lambda when the file is updated
+resource "aws_lambda_permission" "allow_s3" {
+  statement_id  = "AllowExecutionFromS3"
+  action        = "lambda:InvokeFunction"
+  function_name = module.prison_curious_secret_updater.lambda_name
+  principal     = "s3.amazonaws.com"
+  source_arn    = aws_s3_bucket.curious_sandbox_sas_bucket.arn
+}
+
+# Configure S3 to invoke the Lambda when sas_token_info.txt
+# is created or updated in hmpps/prison-curious/
+resource "aws_s3_bucket_notification" "lambda_trigger" {
+  bucket = aws_s3_bucket.curious_sandbox_sas_bucket.id
+
+  lambda_function {
+    lambda_function_arn = module.prison_curious_secret_updater.lambda_arn
+    events              = ["s3:ObjectCreated:Put"]
+
+    filter_suffix = aws_s3_object.sas_token_file.key
+  }
+
+  depends_on = [aws_lambda_permission.allow_s3]
 }
