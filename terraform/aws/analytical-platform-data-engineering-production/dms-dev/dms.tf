7998e3530dd6057956e51e2dc1a8fe205b5f302b
Antony Scott <antony.scott@justice.gov.uk>
2026-03-10 13:59:33 +0000

    comment to apply - DO NOT MERGE

--- terraform/aws/analytical-platform-data-engineering-production/dms-dev/dms.tf
+++ terraform/aws/analytical-platform-data-engineering-production/dms-dev/dms.tf
@@ -1,4 +1,4 @@
-# Apply trigger comment 
+# Apply trigger  comment
 
 module "dev_dms_oasys" {
   source      = "github.com/ministryofjustice/terraform-dms-module?ref=dms-module-full-load-min-change"
