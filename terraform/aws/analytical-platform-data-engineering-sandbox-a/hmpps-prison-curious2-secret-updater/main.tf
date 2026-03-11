87f62dd5e411683f72fc93019e0b92a83870a0be
Andrew Craik <andrew.craik@justice.gov.uk>
2026-03-11 15:42:05 +0000

    remove commented out provider block

--- terraform/aws/analytical-platform-data-engineering-sandbox-a/hmpps-prison-curious2-secret-updater/main.tf
+++ terraform/aws/analytical-platform-data-engineering-sandbox-a/hmpps-prison-curious2-secret-updater/main.tf
@@ -1,12 +1,12 @@
-# terraform {
-#   required_providers {
-#     aws = {
-#       source  = "hashicorp/aws"
-#       version = "6.9.0"
-#     }
-#   }
-#   required_version = "~> 1.5"
-# }
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "6.9.0"
+    }
+  }
+  required_version = "~> 1.5"
+}
 
 # data "aws_caller_identity" "current" {}
 
