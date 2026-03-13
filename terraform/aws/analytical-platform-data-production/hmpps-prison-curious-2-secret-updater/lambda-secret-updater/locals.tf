1319a92baf13e0a8ebff9ad8e94a2fe6d4a513ae
Andrew Craik <andrew.craik@justice.gov.uk>
2026-03-13 10:57:19 +0000

    use substr and replace

--- terraform/aws/analytical-platform-data-production/hmpps-prison-curious-2-secret-updater/lambda-secret-updater/locals.tf
+++ terraform/aws/analytical-platform-data-production/hmpps-prison-curious-2-secret-updater/lambda-secret-updater/locals.tf
@@ -9,7 +9,7 @@ locals {
 
   lambda_role_name           = "${var.lambda_name}-role"
   lambda_policy_name         = "${var.lambda_name}-policy"
-  signer_profile_name_prefix = substr(regexreplace(var.lambda_name, "[^0-9A-Za-z]", ""), 0, 38)
+  signer_profile_name_prefix = substr(replace(var.lambda_name, "/[^0-9A-Za-z]/", ""), 0, 38)
 
   log_group_arn = "arn:aws:logs:${data.aws_region.current.id}:${data.aws_caller_identity.current.account_id}:log-group:/aws/lambda/${var.lambda_name}"
 }
