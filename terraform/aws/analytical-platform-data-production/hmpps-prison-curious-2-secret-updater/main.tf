d37bc479825c2a641c2fc49ab9c6460f52bbe6d6
Andrew Craik <andrew.craik@justice.gov.uk>
2026-03-13 10:41:23 +0000

    add VPC and subnet data sources for airflow production environment

--- terraform/aws/analytical-platform-data-production/hmpps-prison-curious-2-secret-updater/main.tf
+++ terraform/aws/analytical-platform-data-production/hmpps-prison-curious-2-secret-updater/main.tf
@@ -8,6 +8,24 @@ terraform {
   required_version = "~> 1.5"
 }
 
+data "aws_vpc" "airflow_prod" {
+  tags = {
+    Name = "airflow-prod"
+  }
+}
+
+data "aws_subnets" "airflow_prod_private" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.airflow_prod.id]
+  }
+
+  filter {
+    name   = "tag:Name"
+    values = ["airflow-prod-private-*"]
+  }
+}
+
 # Create a lambda which takes the content of the bucket/object key and
 # updates the secret with name secret_name
 module "hmpps_prison_curious2_secret_updater" {
@@ -17,6 +35,9 @@ module "hmpps_prison_curious2_secret_updater" {
 
   bucket_name = "mojap-land"
   object_key  = "hmpps/prison-curious//sas_token_info.txt"
+
+  vpc_id         = data.aws_vpc.airflow_prod.id
+  vpc_subnet_ids = data.aws_subnets.airflow_prod_private.ids
 
   secret_name = "/airflow/production/hmpps/prison-curious2/curious-azure-credential"
 }
