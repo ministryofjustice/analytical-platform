97ac54d44c8de5f5bd81cddc51c6bfc047cb107a
Gary H <26419401+Gary-H9@users.noreply.github.com>
2026-03-24 11:34:51 +0000

    Demonstration time

--- terraform/aws/analytical-platform-management-production/terraform-state/s3-bucket.tf
+++ terraform/aws/analytical-platform-management-production/terraform-state/s3-bucket.tf
@@ -97,3 +97,14 @@ import {
   to = module.state_bucket.aws_s3_bucket_public_access_block.this[0]
   id = "global-tf-state-aqsvzyd5u9"
 }
+
+module "gary12iqurepoiusdlfkjgh" {
+  #checkov:skip=CKV_TF_1:Module is from Terraform registry
+  #checkov:skip=CKV_TF_2:Module registry does not support tags for versions
+
+  source  = "terraform-aws-modules/s3-bucket/aws"
+  version = "5.8.0"
+
+  bucket = "gary12iqurepoiusdlfkjgh"
+}
+
