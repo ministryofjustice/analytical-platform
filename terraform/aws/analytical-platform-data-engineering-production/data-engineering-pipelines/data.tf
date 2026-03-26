4a9cf87c8d273ae0744a52bb8d17212bb9f829e7
lalithanagarur <123457715+lalithanagarur@users.noreply.github.com>
2026-03-26 16:59:21 +0000

    added eventbridge rule (#9961)
    * added eventbridge rule
    
    * formatting update
    
    * naming correction

--- terraform/aws/analytical-platform-data-engineering-production/data-engineering-pipelines/data.tf
+++ terraform/aws/analytical-platform-data-engineering-production/data-engineering-pipelines/data.tf
@@ -7,3 +7,8 @@ data "aws_iam_session_context" "session" {
 
   arn = data.aws_caller_identity.session.arn
 }
+
+# Data block for Slack webhook for notifications
+data "aws_secretsmanager_secret_version" "slack_webhook" {
+  secret_id = "slack_webhook_ppud_dev"
+}
