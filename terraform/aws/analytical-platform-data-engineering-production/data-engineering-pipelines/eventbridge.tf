8f93fdda0ae12be5be404d323d637389d5263ec1
lalithanagarur <lalitha.nagarur3@justice.gov.uk>
2026-03-27 09:52:19 +0000

    kms key, sns topic subscription, and secret

--- terraform/aws/analytical-platform-data-engineering-production/data-engineering-pipelines/eventbridge.tf
+++ terraform/aws/analytical-platform-data-engineering-production/data-engineering-pipelines/eventbridge.tf
@@ -41,6 +41,7 @@ resource "aws_cloudwatch_event_rule" "ae_download_athena_csv" {
 resource "aws_sns_topic" "ae_download_athena_csv" {
   name = "ae-download-athena-csv-events"
   tags = var.tags
+  kms_master_key_id = "alias/aws/sns"
 }
 
 data "aws_iam_policy_document" "ae_download_athena_csv" {
@@ -88,8 +89,8 @@ resource "aws_cloudwatch_event_target" "ae_download_athena_csv" {
 
 # Create a resource to subscribe to SNS topic
 # Slack notifications
-# resource "aws_sns_topic_subscription" "ae_download_athena_csv_slack" {
-#   topic_arn = aws_sns_topic.ae_download_athena_csv.arn
-#   protocol  = "https"
-#   endpoint  = data.aws_secretsmanager_secret_version.ae_download_athena_csv_secret_slack_webhook.secret_string
-# }
+resource "aws_sns_topic_subscription" "ae_download_athena_csv_slack" {
+  topic_arn = aws_sns_topic.ae_download_athena_csv.arn
+  protocol  = "https"
+  endpoint  = data.aws_secretsmanager_secret_version.ae_download_athena_csv_secret_slack_webhook.secret_string
+}
