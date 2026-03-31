93eadd48f557810601213829e67a64d3adfb526b
lalithanagarur <lalitha.nagarur3@justice.gov.uk>
2026-03-31 16:38:25 +0100

    added resource permissions too

--- terraform/aws/analytical-platform-data-engineering-production/data-engineering-pipelines/lakeformation.tf
+++ terraform/aws/analytical-platform-data-engineering-production/data-engineering-pipelines/lakeformation.tf
@@ -76,7 +76,24 @@ module "lakeformation_registration_iam_role" {
   }
 }
 
+# ------------------------------------------------------------------------
+# Lake Formation - register S3 resource
+# ------------------------------------------------------------------------
+
 resource "aws_lakeformation_resource" "probation_dev" {
   arn      = module.datalake_dev.bucket.arn
   role_arn = module.lakeformation_registration_iam_role.arn
+}
+
+# ------------------------------------------------------------------------
+# Lake Formation - grant permissions to principal
+# ------------------------------------------------------------------------
+
+resource "aws_lakeformation_permissions" "probation_dev" {
+    principal = data.aws_iam_role.aws_sso_mp_analytics_eng.arn
+    permissions = ["DATA_LOCATION_ACCESS"]
+
+    data_location {
+        arn = aws_lakeformation_resource.probation_dev.arn
+    }
 }
