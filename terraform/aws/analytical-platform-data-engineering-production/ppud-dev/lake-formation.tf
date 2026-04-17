177b637997cd6ff08d9f1a33dbaa8e02bd4e33ac
AntonyJScott <112949130+AntonyJScott@users.noreply.github.com>
2026-04-17 16:17:30 +0100

    Lake formation for ppud v2 (#10053)
    * delius update tables added
    
    * change undone
    
    * second lf attempt - with some better error catching
    
    * working on data in data-eng-prod account
    
    * working on data in data-eng-prod account
    
    * working on data in data-eng-prod account
    
    * assume roles put in

--- terraform/aws/analytical-platform-data-engineering-production/ppud-dev/lake-formation.tf
+++ terraform/aws/analytical-platform-data-engineering-production/ppud-dev/lake-formation.tf
@@ -1,9 +1,31 @@
-data "aws_caller_identity" "current" {}
+provider "aws" {
+  alias  = "lakeformation_eu_west_1"
+  region = "eu-west-1"
+
+  assume_role {
+    role_arn = "arn:aws:iam::${var.account_ids["analytical-platform-data-engineering-production"]}:role/GlobalGitHubActionAdmin"
+  }
+}
+
+data "aws_caller_identity" "lakeformation_current" {
+  provider = aws.lakeformation_eu_west_1
+}
+
+data "aws_region" "lakeformation_current" {
+  provider = aws.lakeformation_eu_west_1
+}
 
 locals {
-  catalog_id = data.aws_caller_identity.current.account_id
+  expected_account_id = var.account_ids["analytical-platform-data-engineering-production"]
+  expected_region     = "eu-west-1"
 
-  athena_principal_arn = "arn:aws:iam::593291632749:role/AWSReservedSSO_modernisation-platform-data-eng_499410b42334a7d7"
+  catalog_id = data.aws_caller_identity.lakeformation_current.account_id
+  region     = data.aws_region.lakeformation_current.id
+
+  database_name = "mock_ppud_dev_dbt"
+  table_name    = "offenders_main"
+
+  athena_principal_arn = "arn:aws:iam::189157455002:role/AWSReservedSSO_modernisation-platform-data-eng_89c7a4cbe024b69a"
 
   visible_columns = [
     "last",
@@ -14,32 +36,79 @@ locals {
   ]
 }
 
-# Register the S3 location that backs the table.
-# Registering the S3 path is required for LF-governed Athena access.
-resource "aws_lakeformation_resource" "table_1_data_location" {
-  arn = "arn:aws:s3:::mojap-derived-tables/mock_ppud_dev_dbt/table_1"
+resource "terraform_data" "assert_expected_context" {
+  lifecycle {
+    precondition {
+      condition     = local.catalog_id == local.expected_account_id
+      error_message = "Lake Formation apply is running in AWS account ${local.catalog_id}, but ${local.database_name}.${local.table_name} exists in account ${local.expected_account_id}."
+    }
+
+    precondition {
+      condition     = local.region == local.expected_region
+      error_message = "Lake Formation apply is running in region ${local.region}, but ${local.database_name}.${local.table_name} exists in region ${local.expected_region}."
+    }
+  }
 }
 
-# Let the role browse the database in Athena.
-resource "aws_lakeformation_permissions" "table_1_database_describe" {
+data "aws_glue_catalog_table" "offenders_main" {
+  provider = aws.lakeformation_eu_west_1
+
+  database_name = local.database_name
+  name          = local.table_name
+
+  depends_on = [terraform_data.assert_expected_context]
+}
+
+resource "aws_lakeformation_resource" "offenders_main_data_location" {
+  provider = aws.lakeformation_eu_west_1
+
+  arn = "arn:aws:s3:::probation-datalake-dev-20251218164046759500000001/data/dev/models/domain_name=antony_test/database_name=mock_ppud_dev_dbt/table_name=offenders_main"
+
+  depends_on = [data.aws_glue_catalog_table.offenders_main]
+}
+
+resource "aws_lakeformation_permissions" "offenders_main_database_describe" {
+  provider = aws.lakeformation_eu_west_1
+
   principal   = local.athena_principal_arn
   permissions = ["DESCRIBE"]
 
   database {
     catalog_id = local.catalog_id
-    name       = "mock_ppud_dev_dbt"
+    name       = local.database_name
   }
+
+  depends_on = [
+    aws_lakeformation_resource.offenders_main_data_location,
+    data.aws_glue_catalog_table.offenders_main,
+  ]
 }
 
-# Let the role see only the first five columns.
-resource "aws_lakeformation_permissions" "table_1_first_five_columns" {
+resource "aws_lakeformation_permissions" "offenders_main_first_five_columns" {
+  provider = aws.lakeformation_eu_west_1
+
   principal   = local.athena_principal_arn
   permissions = ["SELECT"]
 
   table_with_columns {
     catalog_id    = local.catalog_id
-    database_name = "mock_ppud_dev_dbt"
-    name          = "table_1"
+    database_name = local.database_name
+    name          = local.table_name
     column_names  = local.visible_columns
+  }
+
+  depends_on = [
+    aws_lakeformation_resource.offenders_main_data_location,
+    data.aws_glue_catalog_table.offenders_main,
+  ]
+}
+
+output "lakeformation_apply_context" {
+  value = {
+    account_id = local.catalog_id
+    region     = local.region
+    database   = local.database_name
+    table      = data.aws_glue_catalog_table.offenders_main.name
+    principal  = local.athena_principal_arn
   }
 }
