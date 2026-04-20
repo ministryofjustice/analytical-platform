2455e8d4cf8c44ac3854e4e3d444b82dca045b96
Antony Scott <antony.scott@justice.gov.uk>
2026-04-20 14:46:34 +0100

    linted correctly

--- terraform/aws/analytical-platform-data-engineering-sandbox-a/antony-lf-sandbox/lake-formation.tf
+++ terraform/aws/analytical-platform-data-engineering-sandbox-a/antony-lf-sandbox/lake-formation.tf
@@ -1,3 +1,14 @@
+terraform {
+  required_version = ">= 1.5.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+}
+
 provider "aws" {
   alias  = "sandbox_a_eu_west_1"
   region = "eu-west-1"
