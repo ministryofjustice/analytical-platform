e5dd6db62e330b1161165c2bd425ab3831f1abfa
lauren_tb <lauren.taylor-brown@justice.gov.uk>
2026-04-27 13:04:39 +0000

    Imported bucket and added replication configuration

---
+++ terraform/aws/analytical-platform-data-production/s3-replication/iam.tf
@@ -0,0 +1,65 @@
+data "aws_iam_policy_document" "alpha_mojap_ho_data_transfer_replication" {
+  statement {
+    sid    = "DestinationBucketPermissions"
+    effect = "Allow"
+    actions = [
+      "s3:ReplicateObject",
+      "s3:ObjectOwnerOverrideToBucketOwner",
+      "s3:GetObjectVersionTagging",
+      "s3:ReplicateTags",
+      "s3:ReplicateDelete"
+    ]
+    resources = ["arn:aws:s3:::dsa-cdl-police-s3-deposit-cjs-npa/*"]
+  }
+
+  statement {
+    sid    = "SourceBucketPermissions"
+    effect = "Allow"
+    actions = [
+      "s3:GetReplicationConfiguration",
+      "s3:ListBucket"
+    ]
+    resources = [module.alpha-mojap-ho-data-transfer-test.s3_bucket_arn]
+  }
+
+  statement {
+    sid    = "SourceBucketObjectPermissions"
+    effect = "Allow"
+    actions = [
+      "s3:InitiateReplication",
+      "s3:GetObjectVersionForReplication",
+      "s3:GetObjectVersionAcl",
+      "s3:GetObjectVersionTagging",
+      "s3:ObjectOwnerOverrideToBucketOwner"
+    ]
+    resources = ["${module.alpha-mojap-ho-data-transfer-test.s3_bucket_arn}/*"]
+  }
+}
+
+resource "aws_iam_policy" "alpha_mojap_ho_data_transfer_replication" {
+  name   = "alpha-mojap-ho-data-transfer-test-replication"
+  policy = data.aws_iam_policy_document.alpha_mojap_ho_data_transfer_replication.json
+}
+
+data "aws_iam_policy_document" "alpha_mojap_ho_data_transfer_replication_trust" {
+  statement {
+    sid     = "TrustS3"
+    effect  = "Allow"
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["s3.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_role" "alpha_mojap_ho_data_transfer_replication" {
+  name               = "alpha-mojap-ho-data-transfer-test-replication"
+  assume_role_policy = data.aws_iam_policy_document.alpha_mojap_ho_data_transfer_replication_trust.json
+}
+
+resource "aws_iam_role_policy_attachment" "alpha_mojap_ho_data_transfer_replication" {
+  role       = aws_iam_role.alpha_mojap_ho_data_transfer_replication.name
+  policy_arn = aws_iam_policy.alpha_mojap_ho_data_transfer_replication.arn
+}
---
+++ terraform/aws/analytical-platform-data-production/s3-replication/s3-bucket.tf
@@ -0,0 +1,88 @@
+#tfsec:ignore:AVD-AWS-0089:Bucket logging not enabled currently
+module "alpha-mojap-ho-data-transfer-test" {
+  #checkov:skip=CKV_TF_1:Module registry does not support commit hashes for versions
+  #checkov:skip=CKV_TF_2:Module registry does not support tags for versions
+
+  source  = "terraform-aws-modules/s3-bucket/aws"
+  version = "5.12.0"
+
+  bucket = "alpha-mojap-ho-data-transfer-test"
+
+  versioning = {
+    enabled = true
+  }
+
+  server_side_encryption_configuration = {
+    rule = {
+      apply_server_side_encryption_by_default = {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+
+  replication_configuration = {
+    role = aws_iam_role.alpha_mojap_ho_data_transfer_replication.arn
+    rules = [
+      {
+        id                        = "alpha-mojap-ho-data-transfer-test-replication"
+        status                    = "Enabled"
+        delete_marker_replication = true
+
+        destination = {
+          account_id    = "591168578261"
+          bucket        = "arn:aws:s3:::dsa-cdl-police-s3-deposit-cjs-npa"
+          storage_class = "STANDARD"
+
+          access_control_translation = {
+            owner = "Destination"
+          }
+        }
+      }
+    ]
+  }
+
+  logging = {
+    target_bucket = "moj-analytics-s3-logs"
+    target_prefix = "alpha-mojap-ho-data-transfer-test/"
+    target_object_key_format = {
+      simple_prefix = {}
+    }
+  }
+
+  lifecycle_rule = [
+    {
+      enabled = true
+      id      = "alpha-mojap-ho-data-transfer-test_lifecycle_configuration"
+
+      transition = {
+        days          = 0
+        storage_class = "INTELLIGENT_TIERING"
+      }
+    }
+  ]
+}
+
+import {
+  to = module.alpha-mojap-ho-data-transfer-test.aws_s3_bucket_lifecycle_configuration.this[0]
+  id = "alpha-mojap-ho-data-transfer-test"
+}
+
+import {
+  to = module.alpha-mojap-ho-data-transfer-test.aws_s3_bucket_server_side_encryption_configuration.this[0]
+  id = "alpha-mojap-ho-data-transfer-test"
+}
+
+import {
+  to = module.alpha-mojap-ho-data-transfer-test.aws_s3_bucket_versioning.this[0]
+  id = "alpha-mojap-ho-data-transfer-test"
+}
+
+import {
+  to = module.alpha-mojap-ho-data-transfer-test.aws_s3_bucket_logging.this[0]
+  id = "alpha-mojap-ho-data-transfer-test"
+}
+
+import {
+  to = module.alpha-mojap-ho-data-transfer-test.aws_s3_bucket_public_access_block.this[0]
+  id = "alpha-mojap-ho-data-transfer-test"
+}
