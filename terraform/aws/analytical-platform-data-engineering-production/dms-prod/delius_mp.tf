79c76ce1e4d004c219c0e2d0fac58b795e84411e
lalithanagarur <123457715+lalithanagarur@users.noreply.github.com>
2026-05-14 11:25:22 +0100

    Update delius_mp.tf (#10222)
    updated address

--- terraform/aws/analytical-platform-data-engineering-production/dms-prod/delius_mp.tf
+++ terraform/aws/analytical-platform-data-engineering-production/dms-prod/delius_mp.tf
@@ -7,7 +7,7 @@ locals {
   secrets_manager_arn  = aws_secretsmanager_secret.delius_prod_secret.arn
   sid                  = "prdndas2"
 
-  extra_connection_attributes = "addSupplementalLogging=N;additionalArchivedLogDestId=3;allowSelectNestedTables=True;archivedLogDestId=1;asm_server=delius-core-prod-db-1.delius-core.hmpps-production.modernisation-platform.service.justice.gov.uk/+ASM;asm_user=delius_analytics_platform;parallelASMReadThreads=8;readAheadBlocks=200000;useBfile=Y;useLogminerReader=N;"
+  extra_connection_attributes = "addSupplementalLogging=N;additionalArchivedLogDestId=3;allowSelectNestedTables=True;archivedLogDestId=1;asm_server=delius-core-prod-db-3.delius-core.hmpps-production.modernisation-platform.service.justice.gov.uk/+ASM;asm_user=delius_analytics_platform;parallelASMReadThreads=8;readAheadBlocks=200000;useBfile=Y;useLogminerReader=N;"
 }
 
 data "aws_secretsmanager_secret_version" "database_credentials" {
@@ -22,7 +22,7 @@ resource "aws_dms_endpoint" "source_delius_mp" {
   engine_name   = local.engine_name
 
   database_name = local.sid
-  server_name   = "delius-core-prod-db-1.delius-core.hmpps-production.modernisation-platform.service.justice.gov.uk"
+  server_name   = "delius-core-prod-db-3.delius-core.hmpps-production.modernisation-platform.service.justice.gov.uk"
   username      = local.database_credentials["username"]
   password      = "${local.database_credentials["oracle_password"]},${local.database_credentials["asm_password"]}"
   port          = local.database_credentials["port"]
