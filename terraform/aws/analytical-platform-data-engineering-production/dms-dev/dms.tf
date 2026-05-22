28b7705928f669e8d956565189cbe25065748b1b
Joseph Phillips <joseph.phillips@justice.gov.uk>
2026-05-22 14:56:55 +0100

    removed dms for oasys dev

--- terraform/aws/analytical-platform-data-engineering-production/dms-dev/dms.tf
+++ terraform/aws/analytical-platform-data-engineering-production/dms-dev/dms.tf
@@ -47,17 +47,6 @@ module "dev_dms_oasys" {
 
   glue_catalog_arn      = "arn:aws:glue:eu-west-1:${var.account_ids["analytical-platform-data-production"]}:catalog"
   glue_catalog_role_arn = "arn:aws:iam::${var.account_ids["analytical-platform-data-production"]}:role/data-engineering-probation-glue"
-
-  independent_full_loads = {
-    oasys_dev_set_table = {
-      full_load_name = "oasys-set-table"
-
-      path = {
-        bucket = "mojap-data-engineering-production-table-mappings-metadata-dev"
-        key    = "dev/oasys/oasys_dev_set_table_mapping.json"
-      }
-    }
-  }
 }
 
 module "dev_dms_delius" {
