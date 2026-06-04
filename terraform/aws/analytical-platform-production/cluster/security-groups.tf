29e55e64636e60d5e80f8507d69a1c7013796a68
Muhammad Ahmad <muhammad.ahmad@justice.gov.uk>
2026-06-04 14:05:37 +0100

    feat: restrict production EKS public access and APS egress to MoJ/VPC CIDRs

--- terraform/aws/analytical-platform-production/cluster/security-groups.tf
+++ terraform/aws/analytical-platform-production/cluster/security-groups.tf
@@ -27,12 +27,14 @@ resource "aws_security_group" "aps" {
     to_port         = 443
     protocol        = "tcp"
     security_groups = [module.eks.worker_security_group_id]
+    description     = "allow EKS cluster to access VPC endpoint for managed prometheus"
   }
   egress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = [var.vpc_cidr]
+    description = "allow EKS cluster to access VPC endpoint for managed prometheus"
   }
 }
 
