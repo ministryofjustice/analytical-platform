73109c73566bb27c0b9c2ee3a43041b3b5383444
lalithanagarur <123457715+lalithanagarur@users.noreply.github.com>
2026-06-18 09:59:47 +0100

    Update dms.tf

--- terraform/aws/analytical-platform-data-engineering-production/dms-dev/dms.tf
+++ terraform/aws/analytical-platform-data-engineering-production/dms-dev/dms.tf
@@ -1,3 +1,4 @@
+# retrigger terraform
 module "dev_dms_oasys" {
   source      = "github.com/ministryofjustice/terraform-dms-module?ref=bea6fa8a0f431b616895423e515f5929c022b013"
   vpc_id      = module.vpc.vpc_id
