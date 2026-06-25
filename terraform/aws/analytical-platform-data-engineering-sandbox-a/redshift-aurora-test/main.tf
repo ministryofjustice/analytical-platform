e7b438f83e392a91c95121f1e3c26e4aa40f8466
Guy Wheeler <guy.wheeler@justice.gov.uk>
2026-06-25 16:28:59 +0100

    fix linting

--- terraform/aws/analytical-platform-data-engineering-sandbox-a/redshift-aurora-test/main.tf
+++ terraform/aws/analytical-platform-data-engineering-sandbox-a/redshift-aurora-test/main.tf
@@ -87,7 +87,7 @@ module "kms" {
 module "aurora" {
   source = "./modules/aurora"
 
-  tags         = local.tags
+  tags = local.tags
 
   vpc_id              = module.vpc.vpc_id
   database_subnet_ids = module.vpc.database_subnet_ids
