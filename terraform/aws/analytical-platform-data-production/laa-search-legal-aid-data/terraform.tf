0634513f138953bb7cd5abbdd21eeb9bc72f3cd7
Jacob Woffenden <jacob.woffenden@justice.gov.uk>
2026-06-26 11:25:22 +0000

    feat: add LAA Search component
    Signed-off-by: Jacob Woffenden <jacob.woffenden@justice.gov.uk>

---
+++ terraform/aws/analytical-platform-data-production/laa-search-legal-aid-data/terraform.tf
@@ -0,0 +1,31 @@
+terraform {
+  backend "s3" {
+    acl          = "private"
+    bucket       = "global-tf-state-aqsvzyd5u9"
+    encrypt      = true
+    key          = "aws/analytical-platform-data-production/laa-search-legal-aid-data/terraform.tfstate"
+    region       = "eu-west-2"
+    use_lockfile = true
+  }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "6.52.0"
+    }
+  }
+  required_version = "~> 1.10"
+}
+
+provider "aws" {
+  alias = "session"
+}
+
+provider "aws" {
+  region = "eu-west-2"
+  assume_role {
+    role_arn = "arn:aws:iam::${var.account_ids["analytical-platform-data-production"]}:role/GlobalGitHubActionAdmin"
+  }
+  default_tags {
+    tags = var.tags
+  }
+}
