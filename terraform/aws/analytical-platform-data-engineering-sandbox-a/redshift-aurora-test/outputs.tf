3d01cf7b21b3c72e5f56732d36122d86715669fe
Guy Wheeler <89020559+guyvw1@users.noreply.github.com>
2026-07-01 09:35:59 +0100

    Gw/add redshift instance tf (#10376)
    * add redshift
    
    * add variables
    
    * re-add bastion instance var
    
    * update outputs
    
    * update redshift
    
    * remove bastion_instance_type

--- terraform/aws/analytical-platform-data-engineering-sandbox-a/redshift-aurora-test/outputs.tf
+++ terraform/aws/analytical-platform-data-engineering-sandbox-a/redshift-aurora-test/outputs.tf
@@ -37,3 +37,21 @@ output "bastion_ssm_start_session_command" {
   description = "Command to start an SSM session to the bastion instance"
   value       = "aws ssm start-session --target ${aws_instance.bastion.id} --region ${data.aws_region.current.name}"
 }
+
+# -----------------------------------------------------------------------------
+# Redshift Outputs
+# -----------------------------------------------------------------------------
+output "redshift_endpoint_address" {
+  description = "The DNS address for the Redshift Serverless instance"
+  value       = module.redshift.endpoint_address
+}
+
+output "redshift_endpoint_port" {
+  description = "The port that Redshift Serverless listens on"
+  value       = module.redshift.endpoint_port
+}
+
+output "redshift_admin_secret_arn" {
+  description = "The ARN of the Redshift admin user secret (retrieve from Secrets Manager)"
+  value       = module.redshift.admin_secret_arn
+}
--- terraform/aws/analytical-platform-data-engineering-sandbox-a/redshift-aurora-test/variables.tf
+++ terraform/aws/analytical-platform-data-engineering-sandbox-a/redshift-aurora-test/variables.tf
@@ -44,3 +44,9 @@ variable "bastion_instance_type" {
   description = "Instance type for the bastion host"
   default     = "t4g.micro"
 }
+
+variable "redshift_price_performance_level" {
+  type        = number
+  description = "Price-performance target level for Redshift Serverless (1=LOW_COST, 25=ECONOMICAL, 50=BALANCED, 75=RESOURCEFUL, 100=HIGH_PERFORMANCE)"
+  default     = 1
+}
