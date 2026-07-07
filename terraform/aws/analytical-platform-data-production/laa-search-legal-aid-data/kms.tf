020da1b53c7dffcd5862bc59655b217e7493d5af
Kyle O'Brien <65071578+TawneeOwl@users.noreply.github.com>
2026-07-07 18:45:07 +0100

    Update key policy and data

--- terraform/aws/analytical-platform-data-production/laa-search-legal-aid-data/kms.tf
+++ terraform/aws/analytical-platform-data-production/laa-search-legal-aid-data/kms.tf
@@ -1,5 +1,4 @@
 data "aws_iam_policy_document" "s3_kms_policy" {
-
   statement {
     sid = "AllowAccountRootAdmin"
 
@@ -11,62 +10,45 @@ data "aws_iam_policy_document" "s3_kms_policy" {
     }
 
     actions = [
-      "kms:DescribeKey",
-      "kms:GetKeyPolicy",
-      "kms:GetKeyRotationStatus",
-      "kms:ListAliases",
-      "kms:ListResourceTags",
-      "kms:PutKeyPolicy",
-      "kms:EnableKeyRotation",
-      "kms:DisableKeyRotation",
-      "kms:ScheduleKeyDeletion",
-      "kms:CancelKeyDeletion"
+      "kms:*"
     ]
 
-    resources = [
-      aws_kms_key.s3_kms_key.arn
-    ]
+    resources = ["*"]
   }
-
 
   statement {
     sid = "AllowS3UseOfKey"
 
     principals {
       type = "Service"
-      identifiers = [
-        "s3.amazonaws.com"
-      ]
+      identifiers = ["s3.amazonaws.com"]
     }
 
     actions = [
       "kms:Encrypt",
       "kms:Decrypt",
-      "kms:GenerateDataKey",
-      "kms:GenerateDataKeyWithoutPlaintext",
+      "kms:GenerateDataKey*",
       "kms:DescribeKey"
     ]
 
-    resources = [
-      aws_kms_key.s3_kms_key.arn
-    ]
+    resources = ["*"]
 
     condition {
       test     = "StringEquals"
       variable = "kms:CallerAccount"
-
-      values = [
-        data.aws_caller_identity.current.account_id
-      ]
+      values   = [data.aws_caller_identity.current.account_id]
     }
 
     condition {
       test     = "StringEquals"
       variable = "kms:ViaService"
-
-      values = [
-        "s3.${data.aws_region.current.name}.amazonaws.com"
-      ]
+      values   = ["s3.${data.aws_region.current.name}.amazonaws.com"]
     }
   }
+}
+
+resource "aws_kms_key" "s3_kms_key" {
+  description         = "S3 bucket encryption key"
+  enable_key_rotation = true
+  policy              = data.aws_iam_policy_document.s3_kms_policy.json
 }
