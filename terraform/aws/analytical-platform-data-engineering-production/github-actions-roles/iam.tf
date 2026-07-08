885bed811f0559c8c415f53a69781ff7c6a529ef
lalithanagarur <123457715+lalithanagarur@users.noreply.github.com>
2026-07-08 16:25:57 +0100

    added service account (#10408)

--- terraform/aws/analytical-platform-data-engineering-production/github-actions-roles/iam.tf
+++ terraform/aws/analytical-platform-data-engineering-production/github-actions-roles/iam.tf
@@ -147,7 +147,7 @@ module "create_a_derived_table_dev_iam_role" {
         "arn:aws:iam::${var.account_ids["analytical-platform-data-engineering-production"]}:oidc-provider/%s",
         trimprefix(jsondecode(data.aws_secretsmanager_secret_version.analytical_platform_compute_cluster_data.secret_string)["analytical-platform-compute-development-oidc-endpoint"], "https://")
       )
-      namespace_service_accounts = ["mwaa:probation-cadet"]
+      namespace_service_accounts = ["mwaa:probation-cadet", "mwaa:probation-pb-hearings"]
     }
   }
 
