29f82ff550d948c88cd4d211b40c913dcfece415
gwynwilkinsonmoj <gwyn.wilkinson@justice.gov.uk>
2026-07-10 09:11:40 +0100

    Remove extra _ from sns.tf

--- terraform/aws/analytical-platform-data-production/laa-search-legal-aid-data/sns.tf
+++ terraform/aws/analytical-platform-data-production/laa-search-legal-aid-data/sns.tf
@@ -3,7 +3,7 @@ resource "aws_sns_topic" "s3_sns_alerting_topic" {
   kms_master_key_id = aws_kms_key.cloudwatch_sns_alerts_key.id
 }
 
-resource "aws_sns_topic_policy" "s3_sns_alerting__topic_policy" {
+resource "aws_sns_topic_policy" "s3_sns_alerting_topic_policy" {
   arn = aws_sns_topic.s3_sns_alerting_topic.arn
 
   policy = data.aws_iam_policy_document.s3_sns_alerting_topic_policy.json
