c1b701f8433f71ad21572b2c451926f69d71c510
Kyle O'Brien <65071578+TawneeOwl@users.noreply.github.com>
2026-08-03 14:18:49 +0100

    Add specific ap s3 log

--- terraform/aws/analytical-platform-data-production/laa-search-legal-aid-data/locals.tf
+++ terraform/aws/analytical-platform-data-production/laa-search-legal-aid-data/locals.tf
@@ -2,7 +2,7 @@ locals {
   app = jsondecode(file("${path.module}/application_variables.json"))
 
   application_name    = local.app.application_name
-  logging_bucket_name = local.app.logging_bucket_name
+  logging_bucket_name = "moj-analytics-s3-logs-eu-west-2"
   splink_bucket_name  = local.app.splink_bucket_name
   tags                = local.app.tags
 }
