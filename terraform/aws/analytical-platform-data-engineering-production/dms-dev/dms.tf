75e0506a3afa260bcefc4a0d8582f38bb664a884
joephillipsjustice <joseph.phillips@justice.gov.uk>
2026-08-05 15:52:37 +0100

    Retrigger Tf for dms
    Updated the DMS module source reference in dms.tf.

--- terraform/aws/analytical-platform-data-engineering-production/dms-dev/dms.tf
+++ terraform/aws/analytical-platform-data-engineering-production/dms-dev/dms.tf
@@ -1,4 +1,4 @@
-# retrigger terraform apply
+# retrigger terraform
 module "dev_dms_oasys" {
   source      = "github.com/ministryofjustice/terraform-dms-module?ref=update-dms-module"
   vpc_id      = module.vpc.vpc_id
