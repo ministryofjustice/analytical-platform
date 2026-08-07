8c384e4b6402d08ff4503aee60f4365a7b0eb368
AntonyJScott <112949130+AntonyJScott@users.noreply.github.com>
2026-08-07 15:54:55 +0100

    Antony terraform training task1 (#10538)
    * delius update tables added
    
    * change undone
    
    * antony task1 attempt 1
    
    * new line added
    
    * data deleted
    
    * cases corrected

---
+++ terraform/aws/analytical-platform-data-engineering-sandbox-a/antony-terraform-training/task1/terraform.tf
@@ -0,0 +1,27 @@
+terraform {
+  backend "s3" {
+    acl          = "private"
+    bucket       = "global-tf-state-aqsvzyd5u9"
+    encrypt      = true
+    key          = "aws/analytical-platform-data-engineering-sandbox-a/antony-terraform-training/task1/terraform.tfstate"
+    region       = "eu-west-2"
+    use_lockfile = true
+  }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "6.27.0"
+    }
+  }
+  required_version = "~> 1.5"
+}
+
+provider "aws" {
+  region = "eu-west-1"
+  assume_role {
+    role_arn = "arn:aws:iam::${var.account_ids["analytical-platform-data-engineering-sandbox-a"]}:role/GlobalGitHubActionAdmin"
+  }
+  default_tags {
+    tags = var.tags
+  }
+}
