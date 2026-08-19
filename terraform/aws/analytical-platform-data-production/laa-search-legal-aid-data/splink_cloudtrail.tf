702d7c55d48e22658221b36de16d96bef13c0fb4
arun-moj-laa <arunnirmal.chinnadurai@justice.gov.uk>
2026-08-19 16:30:56 +0100

    fixed the checkov errors

--- terraform/aws/analytical-platform-data-production/laa-search-legal-aid-data/splink_cloudtrail.tf
+++ terraform/aws/analytical-platform-data-production/laa-search-legal-aid-data/splink_cloudtrail.tf
@@ -59,6 +59,10 @@ resource "aws_s3_bucket_lifecycle_configuration" "cloudtrail_logs" {
 
     filter {}
 
+    abort_incomplete_multipart_upload {
+      days_after_initiation = 7
+    }
+
     noncurrent_version_expiration {
       noncurrent_days = 90
     }
