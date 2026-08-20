ceea67a744e3d8a2dd1c8340e871af3ee0194867
arun-moj-laa <arunnirmal.chinnadurai@justice.gov.uk>
2026-08-20 18:00:00 +0100

    modify the logic to fix the error

--- terraform/aws/analytical-platform-data-production/laa-search-legal-aid-data/locals.tf
+++ terraform/aws/analytical-platform-data-production/laa-search-legal-aid-data/locals.tf
@@ -18,12 +18,71 @@ locals {
   splink_source_input_bucket_name  = local.app.splink_source_input_bucket_name
   splink_source_output_bucket_name = local.app.splink_source_output_bucket_name
   splink_audit_bucket_name         = local.app.splink_audit_bucket_name
-  logging_bucket_name              = local.app.logging_bucket_name
-  splink_bucket_name               = local.app.splink_bucket_name
-  splink_test_bucket_name          = local.app.splink_test_bucket_name
-  splink_test_sns_topic_name       = "splink-s3-event-notification-topic-test"
-  splink_test_event_rule_name      = "splink-bucket-event-rule-test"
-  tags                             = local.app.tags
+  logging_bucket_name         = local.app.logging_bucket_name
+  splink_bucket_name          = local.app.splink_bucket_name
+  splink_test_bucket_name     = local.app.splink_test_bucket_name
+  splink_test_sns_topic_name  = "splink-s3-event-notification-topic-test"
+  splink_test_event_rule_name = "splink-bucket-event-rule-test"
+  bucket_access_policy_statements = {
+    (local.splink_search_input_bucket_name) = [
+      {
+        Sid       = "ReadOnlyBucket"
+        Effect    = "Deny"
+        Principal = "*"
+        Action    = ["s3:PutObject", "s3:DeleteObject", "s3:DeleteObjectVersion"]
+        Resource  = "arn:aws:s3:::${local.splink_search_input_bucket_name}/*"
+      }
+    ]
+    (local.splink_search_output_bucket_name) = [
+      {
+        Sid       = "WriteOnlyBucketObjects"
+        Effect    = "Deny"
+        Principal = "*"
+        Action    = ["s3:GetObject", "s3:GetObjectVersion"]
+        Resource  = "arn:aws:s3:::${local.splink_search_output_bucket_name}/*"
+      },
+      {
+        Sid       = "WriteOnlyBucketListing"
+        Effect    = "Deny"
+        Principal = "*"
+        Action    = ["s3:ListBucket"]
+        Resource  = "arn:aws:s3:::${local.splink_search_output_bucket_name}"
+      }
+    ]
+    (local.splink_source_output_bucket_name) = [
+      {
+        Sid       = "WriteOnlyBucketObjects"
+        Effect    = "Deny"
+        Principal = "*"
+        Action    = ["s3:GetObject", "s3:GetObjectVersion"]
+        Resource  = "arn:aws:s3:::${local.splink_source_output_bucket_name}/*"
+      },
+      {
+        Sid       = "WriteOnlyBucketListing"
+        Effect    = "Deny"
+        Principal = "*"
+        Action    = ["s3:ListBucket"]
+        Resource  = "arn:aws:s3:::${local.splink_source_output_bucket_name}"
+      }
+    ]
+    (local.splink_audit_bucket_name) = [
+      {
+        Sid       = "WriteOnlyBucketObjects"
+        Effect    = "Deny"
+        Principal = "*"
+        Action    = ["s3:GetObject", "s3:GetObjectVersion"]
+        Resource  = "arn:aws:s3:::${local.splink_audit_bucket_name}/*"
+      },
+      {
+        Sid       = "WriteOnlyBucketListing"
+        Effect    = "Deny"
+        Principal = "*"
+        Action    = ["s3:ListBucket"]
+        Resource  = "arn:aws:s3:::${local.splink_audit_bucket_name}"
+      }
+    ]
+  }
+  tags                        = local.app.tags
   test_tags = merge(
     local.tags,
     {
