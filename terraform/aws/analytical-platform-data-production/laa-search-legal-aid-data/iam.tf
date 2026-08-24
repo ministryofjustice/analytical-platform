f43cb542ec2b41b3197d153b659dd449093be10c
arun-moj-laa <arunnirmal.chinnadurai@justice.gov.uk>
2026-08-24 18:57:14 +0100

    new bucket files are introduced individually

--- terraform/aws/analytical-platform-data-production/laa-search-legal-aid-data/iam.tf
+++ terraform/aws/analytical-platform-data-production/laa-search-legal-aid-data/iam.tf
@@ -18,10 +18,7 @@ data "aws_iam_policy_document" "splink_bucket_alerting_topic_policy" {
     condition {
       test     = "ArnEquals"
       variable = "aws:SourceArn"
-      values = concat(
-        [aws_cloudwatch_event_rule.s3_bucket_splink_event_rule.arn],
-        [for rule in values(aws_cloudwatch_event_rule.additional) : rule.arn]
-      )
+      values   = [aws_cloudwatch_event_rule.s3_bucket_splink_event_rule.arn]
     }
   }
 }
