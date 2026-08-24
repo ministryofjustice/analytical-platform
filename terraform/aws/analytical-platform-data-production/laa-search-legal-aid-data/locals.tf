4b5b80ba048afb041324c3f3a10f7375c2359c2f
arun-moj-laa <arunnirmal.chinnadurai@justice.gov.uk>
2026-08-24 13:49:55 +0100

    updated the deign more grandular for buckets

--- terraform/aws/analytical-platform-data-production/laa-search-legal-aid-data/locals.tf
+++ terraform/aws/analytical-platform-data-production/laa-search-legal-aid-data/locals.tf
@@ -12,11 +12,34 @@ locals {
     "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/${role}"
   ]
 
-  splink_s3_input_bucket_key_user_arns = [
-    for role in local.kms_key_users.splink_s3_input_bucket.key_users :
+  splink_s3_input_write_bucket_key_user_arns = [
+    for role in local.kms_key_users.splink_s3_input_write_bucket.key_users :
     "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/${role}"
   ]
 
+  splink_s3_input_read_bucket_key_user_arns = [
+    for role in local.kms_key_users.splink_s3_input_read_bucket.key_users :
+    "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/${role}"
+  ]
+  splink_s3_output_write_bucket_key_user_arns = [
+    for role in local.kms_key_users.splink_s3_output_write_bucket.key_users :
+    "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/${role}"
+  ]
+
+  splink_s3_output_read_bucket_key_user_arns = [
+    for role in local.kms_key_users.splink_s3_output_read_bucket.key_users :
+    "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/${role}"
+  ]
+
+  splink_s3_audit_write_bucket_key_user_arns = [
+    for role in local.kms_key_users.splink_s3_audit_write_bucket.key_users :
+    "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/${role}"
+  ]
+
+  splink_s3_audit_read_bucket_key_user_arns = [
+    for role in local.kms_key_users.splink_s3_audit_read_bucket.key_users :
+    "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/${role}"
+  ]
 
   application_name                 = local.app.application_name
   splink_search_input_bucket_name  = local.app.splink_search_input_bucket_name
@@ -32,46 +55,116 @@ locals {
   bucket_access_policy_statements = {
     (local.splink_search_input_bucket_name) = [
       {
-        Sid       = "ReadOnlyBucket"
+        Sid       = "WriteAccessBucketObjects"
         Effect    = "Deny"
         Principal = "*"
         Action    = ["s3:PutObject", "s3:DeleteObject", "s3:DeleteObjectVersion"]
         Resource  = "arn:aws:s3:::${local.splink_search_input_bucket_name}/*"
         Condition = {
           ArnNotEquals = {
-            "aws:PrincipalArn" = local.splink_s3_input_bucket_key_user_arns
+            "aws:PrincipalArn" = local.splink_s3_input_write_bucket_key_user_arns
+          }
+        }
+      },
+      {
+        Sid       = "ReadOnlyBucketObjects"
+        Effect    = "Deny"
+        Principal = "*"
+        Action    = ["s3:GetObject", "s3:GetObjectVersion"]
+        Resource  = "arn:aws:s3:::${local.splink_search_input_bucket_name}/*"
+        Condition = {
+          ArnNotEquals = {
+            "aws:PrincipalArn" = local.splink_s3_input_read_bucket_key_user_arns
+          }
+        }
+      },
+      {
+        Sid       = "ReadOnlyBucketListing"
+        Effect    = "Deny"
+        Principal = "*"
+        Action    = ["s3:ListBucket"]
+        Resource  = "arn:aws:s3:::${local.splink_search_input_bucket_name}"
+        Condition = {
+          ArnNotEquals = {
+            "aws:PrincipalArn" = local.splink_s3_input_read_bucket_key_user_arns
           }
         }
       }
     ]
     (local.splink_source_input_bucket_name) = [
       {
-        Sid       = "ReadOnlyBucket"
+        Sid       = "WriteAccessBucketObjects"
         Effect    = "Deny"
         Principal = "*"
         Action    = ["s3:PutObject", "s3:DeleteObject", "s3:DeleteObjectVersion"]
         Resource  = "arn:aws:s3:::${local.splink_source_input_bucket_name}/*"
         Condition = {
           ArnNotEquals = {
-            "aws:PrincipalArn" = local.splink_s3_input_bucket_key_user_arns
+            "aws:PrincipalArn" = local.splink_s3_input_write_bucket_key_user_arns
+          }
+        }
+      },
+      {
+        Sid       = "ReadonlyBucketObjects"
+        Effect    = "Deny"
+        Principal = "*"
+        Action    = ["s3:GetObject", "s3:GetObjectVersion"]
+        Resource  = "arn:aws:s3:::${local.splink_source_input_bucket_name}/*"
+        Condition = {
+          ArnNotEquals = {
+            "aws:PrincipalArn" = local.splink_s3_input_read_bucket_key_user_arns
+          }
+        }
+      },
+      {
+        Sid       = "ReadonlyBucketListing"
+        Effect    = "Deny"
+        Principal = "*"
+        Action    = ["s3:ListBucket"]
+        Resource  = "arn:aws:s3:::${local.splink_source_input_bucket_name}"
+        Condition = {
+          ArnNotEquals = {
+            "aws:PrincipalArn" = local.splink_s3_input_read_bucket_key_user_arns
           }
         }
       }
     ]
     (local.splink_search_output_bucket_name) = [
       {
-        Sid       = "WriteOnlyBucketObjects"
+        Sid       = "WriteAccessBucketObjects"
+        Effect    = "Deny"
+        Principal = "*"
+        Action    = ["s3:PutObject", "s3:DeleteObject", "s3:DeleteObjectVersion"]
+        Resource  = "arn:aws:s3:::${local.splink_search_output_bucket_name}/*"
+        Condition = {
+          ArnNotEquals = {
+            "aws:PrincipalArn" = local.splink_s3_output_write_bucket_key_user_arns
+          }
+        }
+      },
+      {
+        Sid       = "ReadAccessBucketObjects"
         Effect    = "Deny"
         Principal = "*"
         Action    = ["s3:GetObject", "s3:GetObjectVersion"]
         Resource  = "arn:aws:s3:::${local.splink_search_output_bucket_name}/*"
+        Condition = {
+          ArnNotEquals = {
+            "aws:PrincipalArn" = local.splink_s3_output_read_bucket_key_user_arns
+          }
+        }
       },
       {
-        Sid       = "WriteOnlyBucketListing"
+        Sid       = "ReadAccessBucketListing"
         Effect    = "Deny"
         Principal = "*"
         Action    = ["s3:ListBucket"]
         Resource  = "arn:aws:s3:::${local.splink_search_output_bucket_name}"
+        Condition = {
+          ArnNotEquals = {
+            "aws:PrincipalArn" = local.splink_s3_output_read_bucket_key_user_arns
+          }
+        }
       }
     ]
     (local.splink_source_output_bucket_name) = [
@@ -79,15 +172,37 @@ locals {
         Sid       = "WriteOnlyBucketObjects"
         Effect    = "Deny"
         Principal = "*"
-        Action    = ["s3:GetObject", "s3:GetObjectVersion"]
+        Action    = ["s3:PutObject", "s3:DeleteObject", "s3:DeleteObjectVersion"]
         Resource  = "arn:aws:s3:::${local.splink_source_output_bucket_name}/*"
+        Condition = {
+          ArnNotEquals = {
+            "aws:PrincipalArn" = local.splink_s3_output_write_bucket_key_user_arns
+          }
+        }
       },
       {
-        Sid       = "WriteOnlyBucketListing"
+        Sid       = "ReadAccessBucketObjects"
+        Effect    = "Deny"
+        Principal = "*"
+        Action    = ["s3:GetObject", "s3:GetObjectVersion"]
+        Resource  = "arn:aws:s3:::${local.splink_source_output_bucket_name}/*"
+        Condition = {
+          ArnNotEquals = {
+            "aws:PrincipalArn" = local.splink_s3_output_read_bucket_key_user_arns
+          }
+        }
+      },
+      {
+        Sid       = "ReadAccessBucketListing"
         Effect    = "Deny"
         Principal = "*"
         Action    = ["s3:ListBucket"]
         Resource  = "arn:aws:s3:::${local.splink_source_output_bucket_name}"
+        Condition = {
+          ArnNotEquals = {
+            "aws:PrincipalArn" = local.splink_s3_output_read_bucket_key_user_arns
+          }
+        }
       }
     ]
     (local.splink_audit_bucket_name) = [
@@ -95,15 +210,37 @@ locals {
         Sid       = "WriteOnlyBucketObjects"
         Effect    = "Deny"
         Principal = "*"
-        Action    = ["s3:GetObject", "s3:GetObjectVersion"]
+        Action    = ["s3:PutObject", "s3:DeleteObject", "s3:DeleteObjectVersion"]
         Resource  = "arn:aws:s3:::${local.splink_audit_bucket_name}/*"
+        Condition = {
+          ArnNotEquals = {
+            "aws:PrincipalArn" = local.splink_s3_audit_write_bucket_key_user_arns
+          }
+        }
       },
       {
-        Sid       = "WriteOnlyBucketListing"
+        Sid       = "ReadAccessBucketObjects"
+        Effect    = "Deny"
+        Principal = "*"
+        Action    = ["s3:GetObject", "s3:GetObjectVersion"]
+        Resource  = "arn:aws:s3:::${local.splink_audit_bucket_name}/*"
+        Condition = {
+          ArnNotEquals = {
+            "aws:PrincipalArn" = local.splink_s3_audit_read_bucket_key_user_arns
+          }
+        }
+      },
+      {
+        Sid       = "ReadAccessBucketListing"
         Effect    = "Deny"
         Principal = "*"
         Action    = ["s3:ListBucket"]
         Resource  = "arn:aws:s3:::${local.splink_audit_bucket_name}"
+        Condition = {
+          ArnNotEquals = {
+            "aws:PrincipalArn" = local.splink_s3_audit_read_bucket_key_user_arns
+          }
+        }
       }
     ]
   }
