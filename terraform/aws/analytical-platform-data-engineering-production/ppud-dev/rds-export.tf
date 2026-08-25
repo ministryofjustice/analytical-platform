54ea78726fc8e91e2ab1dad6f055d61c2eb2c0d5
Joseph Phillips <joseph.phillips@justice.gov.uk>
2026-08-25 12:47:21 +0100

    chnaged rds export ref

--- terraform/aws/analytical-platform-data-engineering-production/ppud-dev/rds-export.tf
+++ terraform/aws/analytical-platform-data-engineering-production/ppud-dev/rds-export.tf
@@ -21,7 +21,7 @@ resource "aws_security_group_rule" "db_ingress" {
 
 module "rds_export" {
   # using source ref whilst testing
-  source = "github.com/ministryofjustice/terraform-rds-export?ref=26c16ad6944e91a147280d4bff088929d17f6b21"
+  source = "github.com/ministryofjustice/terraform-rds-export"
 
   providers = {
     aws = aws
