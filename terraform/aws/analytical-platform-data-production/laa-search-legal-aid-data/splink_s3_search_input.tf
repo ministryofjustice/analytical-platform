1ee6b6587829d482a38701456824e0c6f4c30f09
arun-moj-laa <arunnirmal.chinnadurai@justice.gov.uk>
2026-08-25 11:49:32 +0100

    added deny delete bucket

--- terraform/aws/analytical-platform-data-production/laa-search-legal-aid-data/splink_s3_search_input.tf
+++ terraform/aws/analytical-platform-data-production/laa-search-legal-aid-data/splink_s3_search_input.tf
@@ -1,32 +1,3 @@
-locals {
-  search_input_access_policy_statements = [
-    {
-      Sid       = "DenyReadsForUnauthorisedPrincipals"
-      Effect    = "Deny"
-      Principal = "*"
-      Action    = ["s3:GetObject", "s3:GetObjectVersion"]
-      Resource  = "arn:aws:s3:::${local.splink_search_input_bucket_name}/*"
-      Condition = { ArnNotEquals = { "aws:PrincipalArn" = local.splink_s3_input_read_bucket_key_user_arns } }
-    },
-    {
-      Sid       = "DenyListingForUnauthorisedPrincipals"
-      Effect    = "Deny"
-      Principal = "*"
-      Action    = ["s3:ListBucket"]
-      Resource  = "arn:aws:s3:::${local.splink_search_input_bucket_name}"
-      Condition = { ArnNotEquals = { "aws:PrincipalArn" = local.splink_s3_input_read_bucket_key_user_arns } }
-    },
-    {
-      Sid       = "DenyWritesToReadOnlyBucket"
-      Effect    = "Deny"
-      Principal = "*"
-      Action    = ["s3:PutObject", "s3:DeleteObject", "s3:DeleteObjectVersion"]
-      Resource  = "arn:aws:s3:::${local.splink_search_input_bucket_name}/*"
-      Condition = { ArnNotEquals = { "aws:PrincipalArn" = local.splink_s3_input_read_bucket_key_user_arns } }
-    }
-  ]
-}
-
 module "s3_bucket_search_input" {
   source = "git::https://github.com/terraform-aws-modules/terraform-aws-s3-bucket.git?ref=97bb13eff35489bd38993487c3d04c5b6d024cb6"
 
@@ -75,7 +46,32 @@ module "s3_bucket_search_input" {
         Resource  = "arn:aws:s3:::${local.splink_search_input_bucket_name}/*"
         Condition = { StringNotEquals = { "s3:x-amz-server-side-encryption-aws-kms-key-id" = aws_kms_key.s3_kms_key.arn } }
       }
-    ], local.search_input_access_policy_statements)
+      ], [
+      {
+        Sid       = "DenyReadsForUnauthorisedPrincipals"
+        Effect    = "Deny"
+        Principal = "*"
+        Action    = ["s3:GetObject", "s3:GetObjectVersion"]
+        Resource  = "arn:aws:s3:::${local.splink_search_input_bucket_name}/*"
+        Condition = { ArnNotEquals = { "aws:PrincipalArn" = local.splink_s3_input_read_bucket_key_user_arns } }
+      },
+      {
+        Sid       = "DenyListingForUnauthorisedPrincipals"
+        Effect    = "Deny"
+        Principal = "*"
+        Action    = ["s3:ListBucket"]
+        Resource  = "arn:aws:s3:::${local.splink_search_input_bucket_name}"
+        Condition = { ArnNotEquals = { "aws:PrincipalArn" = local.splink_s3_input_read_bucket_key_user_arns } }
+      },
+      {
+        Sid       = "DenyWritesToReadOnlyBucket"
+        Effect    = "Deny"
+        Principal = "*"
+        Action    = ["s3:DenyBucketDeletion", "s3:PutObject", "s3:DeleteObject", "s3:DeleteObjectVersion"]
+        Resource  = "arn:aws:s3:::${local.splink_search_input_bucket_name}/*"
+        Condition = { ArnNotEquals = { "aws:PrincipalArn" = local.splink_s3_input_read_bucket_key_user_arns } }
+      }
+    ])
   })
 
   server_side_encryption_configuration = {
