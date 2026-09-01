1108fcd4b81021fc883e97d3f66ad17723c4f63e
lalithanagarur <lalitha.nagarur3@justice.gov.uk>
2026-09-01 10:35:03 +0100

    renamed resources and files

--- terraform/aws/analytical-platform-data-engineering-production/ppud-dev/locals.tf
+++ terraform/aws/analytical-platform-data-engineering-production/ppud-dev/locals.tf
@@ -7,5 +7,5 @@ locals {
 
   batch_destination_bucket_arn = "arn:aws:s3:::ppud-parquet-exports-development-${local.account_ids["digital-prison-reporting-development"]}-eu-west-2-an"
 
-  migration_replication_cutoff_date = "2026-08-20T00:00:00Z"
+  migration_batch_copy_cutoff_date = "2026-08-20T00:00:00Z"
 }
