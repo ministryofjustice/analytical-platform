9e1d6feec4b0a887ef5dae7bcbe84db74205da79
BrianEllwood <brian.ellwood@digital.justice.gov.uk>
2026-09-03 09:42:50 +0100

    linting fix

--- terraform/aws/analytical-platform-management-production/aws-secrets-manager/iam-roles.tf
+++ terraform/aws/analytical-platform-management-production/aws-secrets-manager/iam-roles.tf
@@ -1,7 +1,7 @@
 module "github_actions_secret_check_iam_role" {
   #checkov:skip=CKV_TF_1:Module registry does not support commit hashes for versions
   #checkov:skip=CKV_TF_2:Module registry does not support tags for versions
-  
+
   source  = "terraform-aws-modules/iam/aws//modules/iam-role"
   version = "6.6.1"
 
