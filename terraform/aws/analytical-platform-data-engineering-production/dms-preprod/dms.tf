3a180fb6194957adb0d675a5da2c817655e8b544
Joseph Phillips <joseph.phillips@justice.gov.uk>
2026-09-07 15:22:43 +0100

    chnage machine size for preprdo

--- terraform/aws/analytical-platform-data-engineering-production/dms-preprod/dms.tf
+++ terraform/aws/analytical-platform-data-engineering-production/dms-preprod/dms.tf
@@ -1,4 +1,4 @@
-#re-run tf
+#re-run delius for releaes
 module "preprod_dms_oasys" {
   source      = "github.com/ministryofjustice/terraform-dms-module?ref=bea6fa8a0f431b616895423e515f5929c022b013"
   vpc_id      = module.vpc.vpc_id
@@ -70,7 +70,7 @@ module "preprod_dms_delius" {
     engine_version             = "3.5.4"
     kms_key_arn                = module.dms_preprod_kms.key_arn
     multi_az                   = false
-    replication_instance_class = "dms.t3.medium"
+    replication_instance_class = "dms.r6i.2xlarge"
     inbound_cidr               = "192.0.2.0/32" # test unassigned
     apply_immediately          = true
   }
