78296ac0a858bdc50f47f9aeb48a55de2f1764ad
Joseph Phillips <joseph.phillips@justice.gov.uk>
2026-09-14 14:00:08 +0100

    added batch copy triggers for preprod ppud

--- terraform/aws/analytical-platform-data-engineering-production/ppud-preprod/locals.tf
+++ terraform/aws/analytical-platform-data-engineering-production/ppud-preprod/locals.tf
@@ -4,4 +4,8 @@ locals {
   name = "ppud"
 
   env = var.tags["environment"]
+
+  batch_destination_bucket_arn = "arn:aws:s3:::ppud-parquet-exports-preprod-${local.account_ids["digital-prison-reporting-development"]}-eu-west-2-an"
+
+  migration_replication_cutoff_date = "2026-09-14T00:00:00Z"
 }
