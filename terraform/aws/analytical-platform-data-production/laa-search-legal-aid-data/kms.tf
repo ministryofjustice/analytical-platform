c6df3b5de795da6ebe31fd2b3ea38205bde0298b
arun-moj-laa <arunnirmal.chinnadurai@justice.gov.uk>
2026-09-16 15:51:05 +0100

    fixing the terraform reference error

--- terraform/aws/analytical-platform-data-production/laa-search-legal-aid-data/kms.tf
+++ terraform/aws/analytical-platform-data-production/laa-search-legal-aid-data/kms.tf
@@ -771,7 +771,7 @@ data "aws_iam_policy_document" "s3_kms_policy_test" {
 
     principals {
       type        = "AWS"
-      identifiers = local.splink_s3_key_user_arns
+      identifiers = local.splink_s3_source_write_test_bucket_key_user_arns
     }
 
     actions = [
