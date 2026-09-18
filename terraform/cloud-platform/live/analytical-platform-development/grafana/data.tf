84bfe043d8ab09f3ccc12cc8cc8cbc3653ec5020
tamsinforbes <32667518+tamsinforbes@users.noreply.github.com>
2026-09-18 15:24:41 +0000

    Testing replacing analytical-platform-github-token with octo-sts

--- terraform/cloud-platform/live/analytical-platform-development/grafana/data.tf
+++ terraform/cloud-platform/live/analytical-platform-development/grafana/data.tf
@@ -26,12 +26,6 @@ data "aws_secretsmanager_secret_version" "cloud_platform_live_analytical_platfor
   secret_id = "cloud-platform/live/analytical-platform-development/token"
 }
 
-data "aws_secretsmanager_secret_version" "github_token" {
-  provider = aws.analytical-platform-management-production-eu-west-1
-
-  secret_id = "analytical-platform-github-token"
-}
-
 data "github_team" "analytical_platform_engineers" {
   slug = "analytical-platform-engineers"
 }
