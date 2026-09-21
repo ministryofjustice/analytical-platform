8c2359344fb00efc2c084a092e551fa25d2276d4
kraihanmoj <113022805+kraihanmoj@users.noreply.github.com>
2026-09-21 14:35:31 +0100

    EM pageduty rota fix (#10714)
    * EM pageduty rota fix
    
    * lint fix
    
    * lint fix
    
    * added moved block

---
+++ terraform/pagerduty/modules/team_em/variables.tf
@@ -0,0 +1,22 @@
+variable "name" {
+  type        = string
+  description = "Name of the team"
+}
+
+variable "managers" {
+  type = map(object({
+    name = string
+    id   = string
+  }))
+  description = "List of user IDs to be added as managers to the team"
+  default     = {}
+}
+
+variable "responders" {
+  type = map(object({
+    name = string
+    id   = string
+  }))
+  description = "List of user IDs to be added as responders to the team"
+  default     = {}
+}
